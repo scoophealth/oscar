8817e7362c21abf3062f6eb18a3559c8db1bf0da
wcampajr <wcampajr>
2003-11-14 13:53:27 +0000

    Added method updateAttendanceType (tipo de atendimento).

--- web/WEB-INF/classes/src/oscar/billing/dao/ProcedimentoRealizadoDAO.java
+++ web/WEB-INF/classes/src/oscar/billing/dao/ProcedimentoRealizadoDAO.java
@@ -20,10 +20,17 @@ public class ProcedimentoRealizadoDAO extends DAO {
         super(pvar);
     }
 
+
+	/**
+	 * Lists all done procedures within a given appointment.
+	 * @param id - the appointment id.
+	 * @returns a List of the procedures found.
+	 */
     public List list(String id) throws SQLException {
         ArrayList list = new ArrayList();
         String sql =
-            "select pr.co_procedimento, p.ds_procedimento, pr.dt_realizacao " +
+            "select pr.co_procedimento, p.ds_procedimento, " +
+            "pr.dt_realizacao, pr.co_tipo_atendimento " +
             "from cad_procedimento_realizado pr, cad_procedimentos p " +
             "where pr.co_procedimento = p.co_procedimento and " +
             "pr.appointment_no = " + id;
@@ -34,13 +41,16 @@ public class ProcedimentoRealizadoDAO extends DAO {
 
         try {
             ResultSet rs = db.GetSQL(sql);
-
+            
+            // for each procedure found, assemble the 
+            // object and put it in the list
             while (rs.next()) {
                 ProcedimentoRealizado pr = new ProcedimentoRealizado();
                 pr.getCadProcedimentos().setCoProcedimento(rs.getLong(1));
                 pr.getCadProcedimentos().setDsProcedimento(rs.getString(2));
                 pr.setDtRealizacao(rs.getDate(3,
                         DateUtils.getDateFormatter().getCalendar()));
+                pr.getTpAtendimento().setCoTipoatendimento(rs.getLong(4));
                 pr.getAppointment().setAppointmentNo(Long.parseLong(id));
                 pr.setSave(true);
                 list.add(pr);
@@ -51,4 +61,31 @@ public class ProcedimentoRealizadoDAO extends DAO {
 
         return list;
     }
+
+
+	/**
+	 * Update the attendance type of all done procedures within a given 
+	 * appointment. tomita - 14 nov 2003
+	 * @param id - the appointment id.
+	 * @param atType - the attendance type
+	 */
+    public void updateAttendanceType(String id, String atType) throws SQLException {
+
+        String sql = "UPDATE cad_procedimento_realizado " +
+        	"SET co_tipo_atendimento = '" + atType + "' " +
+        	"WHERE appointment_no = " + id;
+        	
+        System.out.println(sql);
+
+        DBHandler db = getDb();
+        try {
+            db.RunSQL(sql);
+        } finally {
+            db.CloseConn();
+        }
+
+        return;
+    }
+
+
 }
