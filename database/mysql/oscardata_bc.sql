0ccb72994144b666a9555aeca93a4b482e4864ae
jhbwood <jhbwood>
2004-02-11 19:50:21 +0000

    Moved BC forms data in encounterForms table to a separate file

---
+++ database/mysql/oscardata_bc.sql
@@ -0,0 +1,13 @@
+-- MySQL dump 9.07
+--
+-- Host: localhost    Database: oscar_mcmaster
+---------------------------------------------------------
+-- Server version	4.0.12-standard
+
+--
+-- Dumping data for table 'encounterForm'
+--
+
+INSERT INTO encounterForm VALUES ('BC-AR','../form/formbcar.jsp?demographic_no=','formBCAR', '1');
+INSERT INTO encounterForm VALUES ('BC-LabourBirth','../form/formbcbirthsummo.jsp?demographic_no=','formBCBirthSumMo', '1');
+INSERT INTO encounterForm VALUES ('BC-NewBorn','../form/formbcnewborn.jsp?demographic_no=','formBCNewBorn', '1');
