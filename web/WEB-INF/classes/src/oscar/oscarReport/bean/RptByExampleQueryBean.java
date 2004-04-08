76bc0940ce028d37c5935c9041ac52ac6bcdf4ed
ichan <ichan>
2004-04-08 19:50:56 +0000

    comments:	initial check in

---
+++ web/WEB-INF/classes/src/oscar/oscarReport/bean/RptByExampleQueryBean.java
@@ -0,0 +1,129 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarReport.bean;
+
+import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import oscar.oscarDB.DBHandler;
+
+public class RptByExampleQueryBean{
+
+       int id;
+       String providerNo;  
+       String providerLastName;
+       String providerFirstName;
+       String query;
+       String queryWithEscapeChar;
+       String queryName;
+       String date;       
+       
+       public RptByExampleQueryBean(){
+       }
+
+       public RptByExampleQueryBean(int id, String query, String queryName){   
+            oscar.oscarReport.data.RptByExampleData exampleData  = new oscar.oscarReport.data.RptByExampleData();                
+            this.id = id;
+            this.query = query;
+            this.queryName = queryName;
+            this.queryWithEscapeChar = exampleData.replaceSQLString ("'","\\'",query);
+            System.out.println("query with escape char: " + queryWithEscapeChar);
+       }
+      
+       public RptByExampleQueryBean(String providerLastName, String providerFirstName, String query, String date){            
+            oscar.oscarReport.data.RptByExampleData exampleData  = new oscar.oscarReport.data.RptByExampleData();             
+            this.providerLastName = providerLastName;
+            this.providerFirstName = providerFirstName;
+            this.query = query;
+            this.date = date;
+            this.queryWithEscapeChar = exampleData.replaceSQLString ("'","\\'",query);
+       }
+       
+       public int getId(){
+           return id;
+       }
+       
+       public void setId(int id){
+           this.id = id;
+       }
+       
+       public String getProviderNo(){
+           return providerNo;
+       }
+       
+       public void setProviderNo(String providerNo){
+           this.providerNo = providerNo;
+       }
+       
+       public String getProviderLastName(){
+           return providerLastName;
+       }
+       
+       public void setProviderLastName(String providerLastName){
+           this.providerLastName = providerLastName;
+       }
+       
+       public String getProviderFirstName(){
+           return providerFirstName;
+       }
+       
+       public void setProviderFirstName(String providerFirstName){
+           this.providerFirstName = providerFirstName;
+       }
+       
+       public String getQuery(){
+           return query;
+       }
+       
+       public void setQuery(String query){
+           this.query = query;
+       }
+       
+       public String getQueryName(){
+           return queryName;
+       }
+       
+       public void setQueryName(String queryName){
+           this.queryName = queryName;
+       }
+       
+       public String getQueryWithEscapeChar(){
+           return queryWithEscapeChar;
+       }
+       
+       public void setQueryWithEscapeChar(String queryWithEscapeChar){
+           this.queryWithEscapeChar = queryWithEscapeChar;
+       }
+       
+       public String getDate(){
+           return date;
+       }
+       
+       public void setDate(String date){
+           this.date = date;
+       }
+       
+}
---
+++ web/WEB-INF/classes/src/oscar/oscarReport/bean/RptByExampleQueryBeanHandler.java
@@ -0,0 +1,129 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarReport.bean;
+
+import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import java.util.Collection;
+import oscar.oscarDB.DBHandler;
+
+public class RptByExampleQueryBeanHandler {
+    
+    Vector favoriteVector = new Vector();
+    Vector allQueryVector = new Vector();
+    Vector queryVector = new Vector();
+    String startDate;
+    String endDate;
+    
+    public RptByExampleQueryBeanHandler() {       
+    }
+    
+    public RptByExampleQueryBeanHandler(String startDate, String endDate) {   
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+    
+    public RptByExampleQueryBeanHandler(String providerNo) {       
+        getFavoriteCollection(providerNo);
+    }
+        
+    public Collection getFavoriteCollection(String providerNo){
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql = "SELECT * from reportByExamplesFavorite WHERE providerNo='" + providerNo + "'ORDER BY name";
+            System.out.println("Sql Statement: " + sql);
+            ResultSet rs;
+            int i = 0;
+            for(rs = db.GetSQL(sql); rs.next(); )
+            {
+                if (i<10){
+                    RptByExampleQueryBean query = new RptByExampleQueryBean(rs.getInt("id"), rs.getString("query"), rs.getString("name"));
+                    favoriteVector.add(query);                 
+                }
+                else
+                    break;
+                i++;                
+            }
+
+            rs.close();
+            db.CloseConn();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());            
+        }
+        return favoriteVector;
+    }
+    
+    public Vector getFavoriteVector(){
+        return favoriteVector;
+    }
+    
+    public Collection getAllQueryVector(){
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql = "SELECT r.query, r.date, p.last_name, p.first_name from reportByExamples r, provider p WHERE r.providerNo=p.provider_No ORDER BY date DESC";
+            System.out.println("Sql Statement: " + sql);
+            ResultSet rs;
+            for(rs = db.GetSQL(sql); rs.next(); )
+            {
+                RptByExampleQueryBean query = new RptByExampleQueryBean(rs.getString("last_name"), rs.getString("first_name"), rs.getString("query"), rs.getString("date"));
+                allQueryVector.add(query);
+            }
+
+            rs.close();
+            db.CloseConn();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());            
+        }
+        return allQueryVector;
+    }
+    
+    public Vector getQueryVector(){
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql = "SELECT r.query, r.date, p.last_name, p.first_name from reportByExamples r, provider p "
+                         + "WHERE r.providerNo=p.provider_No AND date>= '" + startDate + "' AND date <='" + endDate
+                         + "' ORDER BY date DESC";
+            System.out.println("Sql Statement: " + sql);
+            ResultSet rs;
+            for(rs = db.GetSQL(sql); rs.next(); )
+            {
+                RptByExampleQueryBean query = new RptByExampleQueryBean(rs.getString("last_name"), rs.getString("first_name"), rs.getString("query"), rs.getString("date"));
+                queryVector.add(query);
+            }
+
+            rs.close();
+            db.CloseConn();
+        }
+        catch(SQLException e) {
+            System.out.println(e.getMessage());            
+        }
+        return queryVector;
+    }
+}
+
