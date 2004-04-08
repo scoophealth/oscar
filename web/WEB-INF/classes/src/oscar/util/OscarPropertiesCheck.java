ace276fe46beee8442482caa1405bda13087b983
jaygallagher <jaygallagher>
2004-04-08 18:57:26 +0000

    init

---
+++ web/WEB-INF/classes/src/oscar/util/OscarPropertiesCheck.java
@@ -0,0 +1,99 @@
+/*
+ * 
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License. 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+ * 
+ * <OSCAR TEAM>
+ * 
+ * This software was written for the 
+ * Department of Family Medicine 
+ * McMaster University 
+ * Hamilton 
+ * Ontario, Canada 
+ */
+
+package oscar.util;
+
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.TagSupport;
+import oscar.OscarProperties;
+
+
+
+/** Tag class for evaulating a property from oscar properties.
+ *
+ * If the value is equal, the jsp code will be included in the page.
+ *
+ */
+public class OscarPropertiesCheck extends TagSupport {
+
+    protected String value = null;
+    protected String property = null;
+    
+    public String getValue() {
+        return (this.value);
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getProperty() {
+        return (this.property);
+    }
+
+    public void setProperty(String property) {
+        this.property = property;
+    }
+
+
+
+    public int doStartTag() throws JspException {
+
+        boolean conditionMet = false ;
+        
+        String prop = getProperty();
+        String val  = getValue();
+        
+        try{            
+            String oscarVal = OscarProperties.getInstance().getProperty(prop);
+            if (oscarVal.equals(val)){
+                conditionMet = true;
+            }
+        }catch(Exception invalidProp){ }
+               
+        if (conditionMet)
+            return (EVAL_BODY_INCLUDE);
+        else
+            return (SKIP_BODY);
+
+    }
+
+    public int doEndTag() throws JspException {
+
+        return (EVAL_PAGE);
+
+    }
+   
+    public void release() {
+
+        super.release();                
+        value = null;        
+        property = null;        
+    }
+
+
+
+
+}
