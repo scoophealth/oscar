bb8e7579fb13d8d65a3615ed2a74dbc41d05f881
ichan <ichan>
2004-04-16 15:27:50 +0000

    fixed a bug: the dataField typeDescription and typeDisplayName was mixed up

--- web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/pageUtil/EctAddMeasurementTypeAction.java
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/pageUtil/EctAddMeasurementTypeAction.java
@@ -71,7 +71,7 @@ public class EctAddMeasurementTypeAction extends Action {
 
             //Write to database
             String sql = "INSERT INTO measurementType"
-                +"(type, typeDisplayName, typeDescription, measuringInstruction, validation)"
+                +"(type, typeDescription, typeDisplayName, measuringInstruction, validation)"
                 +" VALUES ('"+str.q(typeUp)+"','"+str.q(typeDesc)+"','"+str.q(typeDisplayName)+"','"+str.q(measuringInstrc)+"','"
                 + str.q(validation)+"')";
             System.out.println(" sql statement "+sql);
