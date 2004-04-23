4aa908c7ef9b55731c9824b4dc4985f79c8b6b23
ichan <ichan>
2004-04-23 14:51:30 +0000

    inital commit

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctInsertTemplateAction.java
@@ -0,0 +1,72 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarEncounter.pageUtil;
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.*;
+import oscar.oscarDB.DBHandler;
+import org.apache.struts.action.*;
+
+
+public final class EctInsertTemplateAction extends Action {
+
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        String templateName = (String) request.getParameter("templateName");
+            
+        try{
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql = "SELECT encountertemplate_value FROM encountertemplate WHERE encountertemplate_name='" + templateName + "'";
+            ResultSet rs = db.GetSQL(sql);
+            if (rs.next()){
+                String encounterTmpValue = rs.getString("encountertemplate_value");                   
+                encounterTmpValue = encounterTmpValue.replaceAll("\\\\", "\\\\u005C"); // replace \ with unicode equiv.
+                encounterTmpValue = encounterTmpValue.replaceAll("\"", "\\\\u0022"); // replace " with unicode equiv.
+                encounterTmpValue = encounterTmpValue.replaceAll("'", "\\\\u0027"); // replace ' with unicode equiv.
+                encounterTmpValue = encounterTmpValue.replaceAll(">", "\\\\u003E");
+                encounterTmpValue = encounterTmpValue.replaceAll("<", "\\\\u003C");
+                encounterTmpValue = encounterTmpValue.replaceAll("\n", "\\\\u000A");
+                encounterTmpValue = encounterTmpValue.replaceAll("\r", "\\\\u000D");
+                request.setAttribute("templateValue", encounterTmpValue);
+            }                     
+            db.CloseConn();
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }            
+        
+        return (mapping.findForward("success"));
+    }
+}
