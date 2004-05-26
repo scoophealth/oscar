56280e7bc05eed08a9ab0f59d8955d1a1f52363e
ichan <ichan>
2004-05-26 14:54:31 +0000

    stored the quickListName, created by provider No and indicate if the item is used last time

---
+++ web/WEB-INF/classes/src/oscar/oscarResearch/oscarDxResearch/bean/dxQuickListBean.java
@@ -0,0 +1,69 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarResearch.oscarDxResearch.bean;
+
+import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import oscar.oscarDB.DBHandler;
+
+public class dxQuickListBean{
+       
+       String quickListName;              
+       String createdBy;
+       String lastUsed = "";
+       
+       public dxQuickListBean(){
+       }
+
+       public dxQuickListBean( String quickListName, 
+                               String createdBy){
+            this.quickListName = quickListName;
+            this.createdBy = createdBy;
+       }              
+       
+       public String getQuickListName(){
+           return quickListName;
+       }       
+       public void setQuickListName(String quickListName){
+           this.quickListName = quickListName;
+       }
+       
+       public String getCreatedBy(){
+           return createdBy;
+       }       
+       public void setCreatedBy(String createdBy){
+           this.createdBy = createdBy;
+       }
+       
+       public String getLastUsed(){
+           return lastUsed;
+       }       
+       public void setLastUsed(String lastUsed){
+           this.lastUsed = lastUsed;
+       }
+       
+}
