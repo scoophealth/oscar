caa810741a7dc96e5b4818a84cf7f63ff69392f1
ichan <ichan>
2004-06-03 18:51:22 +0000

    initial commit

---
+++ web/WEB-INF/classes/src/oscar/oscarResearch/oscarDxResearch/pageUtil/dxResearchUpdateQuickListAction.java
@@ -0,0 +1,121 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarResearch.oscarDxResearch.pageUtil;
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+import org.apache.struts.validator.*;
+import org.apache.commons.validator.*;
+import oscar.oscarDB.DBHandler;
+import oscar.oscarEncounter.pageUtil.EctSessionBean;
+import oscar.OscarProperties;
+import oscar.util.*;
+import oscar.oscarResearch.oscarDxResearch.bean.*;
+
+public class dxResearchUpdateQuickListAction extends Action {
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException
+    {
+        dxResearchUpdateQuickListForm frm = (dxResearchUpdateQuickListForm) form;
+        String quickListName = frm.getQuickListName();
+        String forward = frm.getForward(); 
+        String curUser = (String) request.getSession().getAttribute("user");
+        dxResearchLoadQuickListItemsForm qLItemsFrm = (dxResearchLoadQuickListItemsForm) request.getSession().getAttribute("dxResearchLoadQuickListItemsFrm");
+        qLItemsFrm.setQuickListName(quickListName);
+        boolean valid = true;
+        
+        try{
+            
+            
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql;
+            
+            if(forward.equals("add")){
+                String [] xml_research = new String[5];
+                xml_research[0] = frm.getXml_research1();
+                xml_research[1] = frm.getXml_research2();
+                xml_research[2] = frm.getXml_research3();
+                xml_research[3] = frm.getXml_research4();
+                xml_research[4] = frm.getXml_research5();
+                
+                ActionErrors errors = new ActionErrors();  
+
+                for(int i=0; i<xml_research.length; i++){
+                    int Count = 0;
+                    if (xml_research[i].compareTo("")!=0){
+
+                        //need to validate the dxresearch code before write to the database
+                        sql = "select * from ichppccode where ichppccode like '" + xml_research[i] +"'";
+                        //System.out.println("Validate: " + sql);
+                        ResultSet rsCode = db.GetSQL(sql);
+
+                        if(!rsCode.next() || rsCode==null){
+                            valid = false;
+                            errors.add(errors.GLOBAL_ERROR,
+                            new ActionError("errors.codeNotFound", xml_research[i], "ICHPPC"));
+                            saveErrors(request, errors);   
+                        }
+                        else{
+                            sql = "select * from quickList where quickListName = '" + quickListName + "' AND dxResearchCode='"+xml_research[i]+"'";
+                            ResultSet rs = db.GetSQL(sql);                            
+                            if(!rs.next()){                                                            
+                                sql = "insert into quickList (quickListName, dxResearchCode, createdByProvider, codingSystem) values('"
+                                        + quickListName +"','" + xml_research[i] + "','" + curUser +"', 'ichppc')";
+                                db.RunSQL(sql);
+                            }
+                        }
+                    }
+
+                    db.CloseConn();
+                }
+            }
+            else if(forward.equals("remove")){
+                String[] removedItems = frm.getQuickListItems();
+                for(int i=0; i<removedItems.length; i++){
+                    sql = "Delete from quickList where quickListName = '"+ quickListName + "' AND dxResearchCode = '"+removedItems[i]+"'";
+                    db.RunSQL(sql);
+                }
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }    
+
+        if(!valid){
+            return (new ActionForward(mapping.getInput()));        
+        }        
+        
+        return mapping.findForward("success");
+    }     
+     
+}
---
+++ web/WEB-INF/classes/src/oscar/oscarResearch/oscarDxResearch/pageUtil/dxResearchUpdateQuickListForm.java
@@ -0,0 +1,113 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarResearch.oscarDxResearch.pageUtil;
+import java.io.PrintStream;
+import java.util.*;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.struts.action.*;
+
+public final class dxResearchUpdateQuickListForm extends ActionForm {
+
+    //Using map-backed method to get the value of each field
+    //key: the field property
+    //value: the value of the associated key
+/*    private final Map values = new HashMap();
+
+    public void setValue(String key, Object value) {
+        values.put(key, value);
+    }
+
+    public Object getValue(String key) {
+        return values.get(key);
+    }
+*/
+    
+    private String[] quickListItems;        
+    private String quickListName;
+    private String forward;
+    private String xml_research1;
+    private String xml_research2;
+    private String xml_research3;
+    private String xml_research4;
+    private String xml_research5;
+     
+    public String[] getQuickListItems() {
+        return quickListItems;
+    }    
+    public void setQuickListItems(String[] quickListItems) {
+        this.quickListItems = quickListItems;
+    }
+    
+    public String getQuickListName() {
+        return quickListName;
+    }    
+    public void setQuickListName(String quickListName) {
+        this.quickListName = quickListName;
+    }
+    
+    public String getForward() {
+        return forward;
+    }    
+    public void setForward(String forward) {
+        this.forward = forward;
+    }
+    
+    public String getXml_research1() {
+        return xml_research1;
+    }    
+    public void setXml_research1(String xml_research1) {
+        this.xml_research1 = xml_research1;
+    }
+    
+    public String getXml_research2() {
+        return xml_research2;
+    }    
+    public void setXml_research2(String xml_research2) {
+        this.xml_research2 = xml_research2;
+    }
+    
+    public String getXml_research3() {
+        return xml_research3;
+    }    
+    public void setXml_research3(String xml_research3) {
+        this.xml_research3 = xml_research3;
+    }
+    
+    public String getXml_research4() {
+        return xml_research4;
+    }    
+    public void setXml_research4(String xml_research4) {
+        this.xml_research4 = xml_research4;
+    }
+    
+    public String getXml_research5() {
+        return xml_research5;
+    }    
+    public void setXml_research5(String xml_research5) {
+        this.xml_research5 = xml_research5;
+    }
+
+}
