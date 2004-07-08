87d379ad9d35c42d983b62aa9a51a2138c648466
langdeau <langdeau>
2004-07-08 22:07:38 +0000

    A filter that checks requests to make sure user is logged in.

---
+++ web/WEB-INF/classes/src/oscar/oscarSecurity/LoginFilter.java
@@ -0,0 +1,89 @@
+//-----------------------------------------------------------------------------------------------------------------------
+//*
+//*
+//* Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+//* This software is published under the GPL GNU General Public License. 
+//* This program is free software; you can redistribute it and/or 
+//* modify it under the terms of the GNU General Public License 
+//* as published by the Free Software Foundation; either version 2 
+//* of the License, or (at your option) any later version. * 
+//* This program is distributed in the hope that it will be useful, 
+//* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+//* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+//* GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+//* along with this program; if not, write to the Free Software 
+//* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+//* 
+//* <OSCAR TEAM>
+//* This software was written for the 
+//* Department of Family Medicine 
+//* McMaster Unviersity 
+//* Hamilton 
+//* Ontario, Canada 
+//*
+//-----------------------------------------------------------------------------------------------------------------------
+package oscar.oscarSecurity;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Dennis Langdeau
+ */
+public class LoginFilter implements Filter
+{
+    /*
+     * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
+     */
+    public void init(FilterConfig config) throws ServletException
+    {
+    }
+
+    /*
+     * @see javax.servlet.Filter#doFilter(javax.servlet.ServletRequest,
+     *      javax.servlet.ServletResponse, javax.servlet.FilterChain)
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        if (httpRequest.getSession().getAttribute("user") == null)
+        {
+            String requestURI = httpRequest.getRequestURI();
+            String contextPath = httpRequest.getContextPath();
+            int contextPathLength = contextPath.length();
+
+            /*
+             * If the requested resource is in any subdirectory other than
+             * /images/ then redirect to the logout page.
+             */
+            if (!requestURI.startsWith(contextPath + "/images/")
+                    && requestURI.indexOf("/", contextPathLength + 1) > 0)
+            {
+                System.out.println("Not logged in while accessing URL:");
+                System.out.println(requestURI);
+                httpResponse.sendRedirect(contextPath + "/logout.jsp");
+                return;
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    /*
+     * @see javax.servlet.Filter#destroy()
+     */
+    public void destroy()
+    {
+    }
+
+}
