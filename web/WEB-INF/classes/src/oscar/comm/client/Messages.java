d13fb658163c6c8a907aa3ca6fdd29dffd45efa8
jaygallagher <jaygallagher>
2004-07-09 17:30:27 +0000

    Added Escaping of Strings in sql statement

--- web/WEB-INF/classes/src/oscar/comm/client/Messages.java
+++ web/WEB-INF/classes/src/oscar/comm/client/Messages.java
@@ -9,6 +9,7 @@ import org.w3c.dom.NodeList;
 
 import oscar.oscarDB.DBHandler;
 import oscar.util.UtilXML;
+import oscar.util.UtilMisc;
 
 class Messages {
     private DBHandler db;
@@ -27,15 +28,20 @@ class Messages {
             String sql = "INSERT INTO messagetbl (thedate, theime, sentby, "
                 + "sentto, sentbyNo, sentbyLocation, thesubject, themessage, "
                 + "attachment, actionstatus) VALUES ('"
-                + msg.get("msgDate") + "', '" + msg.get("msgTime") + "', '"
-                + msg.get("sentby") + "', '" + msg.get("sentto") + "', '"
-                + msg.get("sentbyNo") + "', '" + msg.get("sentbyLocation") + "', '"
-                + msg.get("subject") + "', '" + msg.get("msgBody") + "', '"
-                + msg.get("attachment") + "', '" + msg.get("actionstatus")
-                + "')";
+                + UtilMisc.mysqlEscape(msg.get("msgDate"))        + "', '" 
+                + UtilMisc.mysqlEscape(msg.get("msgTime"))        + "', '"
+                + UtilMisc.mysqlEscape(msg.get("sentby"))         + "', '" 
+                + UtilMisc.mysqlEscape(msg.get("sentto"))         + "', '"
+                + UtilMisc.mysqlEscape(msg.get("sentbyNo"))       + "', '" 
+                + UtilMisc.mysqlEscape(msg.get("sentbyLocation")) + "', '"
+                + UtilMisc.mysqlEscape(msg.get("subject"))        + "', '" 
+                + UtilMisc.mysqlEscape(msg.get("msgBody"))        + "', '"
+                + UtilMisc.mysqlEscape(msg.get("attachment"))     + "', '" 
+                + UtilMisc.mysqlEscape(msg.get("actionstatus"))   + "')";
+                        
             db.RunSQL(sql);
 
-            int msgId = -1;
+            int msgId = -1;   
 
             sql = "SELECT LAST_INSERT_ID()";
             ResultSet rs = db.GetSQL(sql);
@@ -64,11 +70,11 @@ class Messages {
             NodeList lst = message.getChildNodes();
 
             for(int i=0; i<lst.getLength(); i++) {
-                Node nod = lst.item(i);
-
+                Node nod = lst.item(i);            
+                
                 if(nod.getNodeType() == Node.ELEMENT_NODE) {
-                    if(nod.getNodeName().equals("recipient")==false) {
-                        fld.setProperty(nod.getNodeName(), UtilXML.getText(nod));
+                    if(nod.getNodeName().equals("recipient")==false) {                       
+                          fld.setProperty(nod.getNodeName(), UtilXML.getText(nod));                                              
                     }
                 }
             }
