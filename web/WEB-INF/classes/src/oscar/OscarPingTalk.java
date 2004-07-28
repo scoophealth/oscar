04e6baff4963c265be2b04a7dd315009409bd7e5
yili119 <yili119>
2004-07-28 17:52:47 +0000

    make it use ssl

--- web/WEB-INF/classes/src/oscar/OscarPingTalk.java
+++ web/WEB-INF/classes/src/oscar/OscarPingTalk.java
@@ -23,18 +23,28 @@
  */
 package oscar;
 
-import java.util.*;
-import org.chip.ping.client.*;
-import org.chip.ping.xml.*;
-import org.chip.ping.xml.talk.*;
-import org.chip.ping.xml.record.*;
-import org.chip.ping.xml.record.impl.*;
-import org.chip.ping.xml.cddm.impl.*;
-import org.chip.ping.xml.cddm.*;
-import javax.xml.bind.*;
-import javax.xml.parsers.*;
-import org.w3c.dom.*;
-import oscar.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.chip.ping.client.PingTalkClient;
+import org.chip.ping.client.PingTalkClientImpl;
+import org.chip.ping.xml.DefaultCddmGenerator;
+import org.chip.ping.xml.cddm.BodyContentType;
+import org.chip.ping.xml.cddm.BodyType;
+import org.chip.ping.xml.cddm.CddmType;
+import org.chip.ping.xml.cddm.DataType;
+import org.chip.ping.xml.talk.AddCddmResultType;
+import org.chip.ping.xml.talk.AuthenticateResultType;
+import org.w3c.dom.Document;
 
 /*
  * PingOscar.java
@@ -43,96 +53,104 @@ import oscar.*;
  */
 
 /**
- *
- * @author  root
+ * @author root
  */
 public class OscarPingTalk {
-    
-    PingTalkClientImpl client ;
+
+    PingTalkClientImpl client;
+
     /** Creates a new instance of OscarPingTalk */
     public OscarPingTalk() {
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String urlHostName, SSLSession session) {
+                System.out.println("Warning: URL: " + urlHostName + ", session host "
+                        + session.getPeerHost());
+                return true;
+            }
+        });
     }
-    
-    
-    
-    public String connect(String username,String password) throws Exception {        
-        String serverURL = OscarProperties.getInstance().getProperty("PING-SERVER",""); //"http://127.0.0.1:8080/ping-server/PingServlet";
+
+    public String connect(String username, String password) throws Exception {
+        String serverURL = OscarProperties.getInstance().getProperty("PING-SERVER", ""); //"http://127.0.0.1:8080/ping-server/PingServlet";
         String actorTicket = null;
         Map properties = new Properties();
+        properties.put(PingTalkClient.CERT_TRUST_KEY, PingTalkClient.ALL_CERTS_ACCEPTED);
         properties.put(PingTalkClient.VALIDATE_REQUESTS, Boolean.TRUE);
         properties.put(PingTalkClient.SERVER_LOCATION, serverURL);
         client = new PingTalkClientImpl(properties);
 
-        AuthenticateResultType art=   client.authenticate(username,password);      
-        if (art == null){                
+        AuthenticateResultType art = client.authenticate(username, password);
+        if (art == null) {
             throw new Exception("Problem Authenticating User");
         }
         actorTicket = art.getActorTicket();
         return actorTicket;
     }
-    
-    public boolean sendCddm(String actorTicket,String pingId,String owner,String originAgent,String author,String level1,String level2,DataType dataType) throws Exception{                                          
-        DefaultCddmGenerator cddmGenerator  = new DefaultCddmGenerator() ;
-        CddmType cddmType =  cddmGenerator.generateDefaultCddm(owner,originAgent,author,level1,level2);                                                                        
-        BodyType bodyType = cddmType.getCddmBody();            
-        List dataList = bodyType.getData();                                                                                    
-        dataList.add(dataType);                      
-        System.out.println(actorTicket+" "+pingId+" "+cddmType);        
-        AddCddmResultType adrt = client.addCddm(actorTicket,pingId,cddmType);        
+
+    public boolean sendCddm(String actorTicket, String pingId, String owner, String originAgent,
+            String author, String level1, String level2, DataType dataType) throws Exception {
+        DefaultCddmGenerator cddmGenerator = new DefaultCddmGenerator();
+        CddmType cddmType = cddmGenerator.generateDefaultCddm(owner, originAgent, author, level1,
+                level2);
+        BodyType bodyType = cddmType.getCddmBody();
+        List dataList = bodyType.getData();
+        dataList.add(dataType);
+        System.out.println(actorTicket + " " + pingId + " " + cddmType);
+        AddCddmResultType adrt = client.addCddm(actorTicket, pingId, cddmType);
         return true;
     }
-    
-    public boolean sendCddm(String actorTicket,String pingId,CddmType cddmType) throws Exception{         
-        AddCddmResultType adrt = client.addCddm(actorTicket,pingId,cddmType);        
+
+    public boolean sendCddm(String actorTicket, String pingId, CddmType cddmType) throws Exception {
+        AddCddmResultType adrt = client.addCddm(actorTicket, pingId, cddmType);
         return true;
     }
-    
-    public  CddmType getCddm(String owner,String originAgent,String author,String level1,String level2,DataType dataType){       
-        DefaultCddmGenerator cddmGenerator  = new DefaultCddmGenerator() ;
-        CddmType cddmType =  cddmGenerator.generateDefaultCddm(owner,originAgent,author,level1,level2);                                                                        
-        BodyType bodyType = cddmType.getCddmBody();            
-        List dataList = bodyType.getData();                                                                                    
+
+    public CddmType getCddm(String owner, String originAgent, String author, String level1,
+            String level2, DataType dataType) {
+        DefaultCddmGenerator cddmGenerator = new DefaultCddmGenerator();
+        CddmType cddmType = cddmGenerator.generateDefaultCddm(owner, originAgent, author, level1,
+                level2);
+        BodyType bodyType = cddmType.getCddmBody();
+        List dataList = bodyType.getData();
         dataList.add(dataType);
         return cddmType;
     }
-    
-    public DataType getDataType(org.w3c.dom.Element element) throws Exception{                
-        org.chip.ping.xml.cddm.ObjectFactory objectFactory = new org.chip.ping.xml.cddm.ObjectFactory();                                        
-        DataType dt = objectFactory.createDataType();            
-        dt.setBodyContentFormat("text/xml");            
-        BodyContentType content = objectFactory.createBodyContentType();                                                
+
+    public DataType getDataType(org.w3c.dom.Element element) throws Exception {
+        org.chip.ping.xml.cddm.ObjectFactory objectFactory = new org.chip.ping.xml.cddm.ObjectFactory();
+        DataType dt = objectFactory.createDataType();
+        dt.setBodyContentFormat("text/xml");
+        BodyContentType content = objectFactory.createBodyContentType();
         content.setAny(element);
         dt.setBodyContent(content);
         return dt;
     }
-    
-    public DataType getDataType(Object body) throws Exception{   
-        
-        JAXBContext context = JAXBContext.newInstance("org.chip.ping.xml.talk:org.chip.ping.xml.record:org.chip.ping.xml.pid:org.chip.ping.xml.cddm");
+
+    public DataType getDataType(Object body) throws Exception {
+
+        JAXBContext context = JAXBContext
+                .newInstance("org.chip.ping.xml.talk:org.chip.ping.xml.record:org.chip.ping.xml.pid:org.chip.ping.xml.cddm");
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        
+
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         dbf.setNamespaceAware(true);
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document doc = db.newDocument();
-        
+
         org.chip.ping.xml.cddm.ObjectFactory objectFactory = new org.chip.ping.xml.cddm.ObjectFactory();
 
-        DataType dt = objectFactory.createDataType();            
-        dt.setBodyContentFormat("text/xml");     
-        
+        DataType dt = objectFactory.createDataType();
+        dt.setBodyContentFormat("text/xml");
+
         BodyContentType content = objectFactory.createBodyContentType();
-        
+
         marshaller.marshal(body, doc);
         content.setAny(doc.getDocumentElement());
 
         dt.setBodyContent(content);
-                                                
+
         return dt;
     }
-    
-    
-  
-    
+
 }
