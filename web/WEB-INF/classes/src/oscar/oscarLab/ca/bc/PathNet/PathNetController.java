f9e2583001b92387f3bf8d1e928c7a33222418fc
jaygallagher <jaygallagher>
2004-07-29 17:18:52 +0000

    classes used for pathnet interface

---
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/bc/PathNet/PathNetController.java
@@ -0,0 +1,110 @@
+/*
+ * PathNetController.java
+ *
+ * Created on July 23, 2004, 11:51 AM
+ */
+
+package oscar.oscarLab.ca.bc.PathNet;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import oscar.oscarDB.*;
+import oscar.*;
+import oscar.oscarLab.ca.bc.PathNet.HL7.*;
+
+/**
+ *
+ * @author  root
+ */
+public class PathNetController {
+   
+   /** Creates a new instance of PathNetController */
+   public PathNetController() {
+   }
+   
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {                  
+      
+      System.out.println("Running oscarCommClient...");
+      if(args.length != 1) {
+         System.out.println("Usage: oscarCommClient pathOfPropertiesFile");	 
+         System.exit(1);
+      }
+      
+      System.out.println("    propertiesFile:  " + args[0]);         	
+               
+      try{
+         init(args[0]);
+      }catch(Exception e){
+         System.err.println("ERROR: Initializing DB : "+e.getMessage() );
+         e.printStackTrace();
+         System.exit(1);
+      }                           
+         
+      //
+      //String dataCenterId = OscarProperties.getInstance().getProperty("dataCenterId");
+      ////
+      Connection connection = new Connection();
+      String username = OscarProperties.getInstance().getProperty("PathNetUsername");
+      String password = OscarProperties.getInstance().getProperty("PathNetPassword");
+      if (connection.Open(username, password)) {
+         ArrayList messages = connection.Retrieve();
+            if (messages != null) {
+               boolean success = true;
+               try {
+                  int size = messages.size();
+                  DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+                  String now =
+                  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+                  for (int i = 0; i < size; i++) {
+                     Message message = new Message(now);
+                     message.Parse((String) messages.get(i));
+                     message.ToDatabase(db);
+                  }
+                  db.CloseConn();
+               }
+               catch (Exception ex) {
+                  //success = false; //<- for future when transactional
+                  System.err.println("Error - oscar.PathNet.Contorller - Message: "+ ex.getMessage()+ " = "+ ex.toString());
+               }
+               connection.Acknowledge(success);
+            }
+            connection.Close();
+         }else{
+            System.out.println("Error connecting to pathnet");
+            System.exit(1);
+         }
+         //long sleep = Long.parseLong(OscarProperties.getInstance().getProperty("PathNetSleep")) * 60000;
+         //try {
+         //   Thread.sleep(sleep);
+         //}
+         //catch (Exception ex) {
+         //   System.err.println("Thread has failed to sleep. Thread will end until error corrected and restarted - oscar.PathNet.Controller - Message: "+ ex.getMessage());
+         //   //run = false;
+         //}
+      
+      ////      
+      
+   }
+   
+   ///
+   private static synchronized void init (String file) throws java.sql.SQLException, java.io.IOException  {
+      Properties param = new Properties();
+      DBHandler db = null; 
+      param.load(new FileInputStream(file)); 
+      DBHandler.init(param.getProperty("db_name"),param.getProperty("db_driver"),param.getProperty("db_uri") ,param.getProperty("db_username"),param.getProperty("db_password") );
+      db = new DBHandler(DBHandler.OSCAR_DATA);
+   }
+
+	
+
+   
+   
+   ///
+   
+   
+   
+}
---
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/bc/PathNet/PathNetInfo.java
@@ -0,0 +1,68 @@
+/*
+ * PathNetInfo.java
+ *
+ * Created on July 28, 2004, 11:36 AM
+ */
+
+package oscar.oscarLab.ca.bc.PathNet;
+
+import java.sql.*;
+
+import oscar.oscarDB.*;
+
+/**
+ *
+ * @author  root
+ */
+public class PathNetInfo {
+   
+   /** Creates a new instance of PathNetInfo */
+   public PathNetInfo() {
+   }
+                
+   
+   public int getPendingLabCount() throws SQLException{
+      int pendingLabs = 0;
+      String select_pending = "SELECT count(hl7_pid.pid_id) as `count` " +
+                              "FROM hl7_pid left join hl7_link on hl7_pid.pid_id=hl7_link.pid_id " +
+                              "WHERE hl7_link.status='P' OR hl7_link.status is null";      
+      DBHandler dbLab = new DBHandler(DBHandler.OSCAR_DATA);
+      ResultSet rsLab = dbLab.GetSQL(select_pending);
+      if(rsLab.next()){
+         pendingLabs = rsLab.getInt("count");
+      }
+      rsLab.close();
+      dbLab.CloseConn();
+      return pendingLabs;
+   }
+    
+   public int getNotSignedLabCount(String providerNo) throws SQLException{
+      String select_not_signed = "SELECT count(hl7_link.pid_id) as `count` " +
+                              "FROM hl7_link, demographic  " +
+                              "WHERE demographic.provider_no= '"+providerNo+"' " +
+                              "AND demographic.demographic_no=hl7_link.demographic_no " +
+                              "AND (hl7_link.status='N' OR hl7_link.status='A')";
+      int notSigned = 0;
+      DBHandler dbLab = new DBHandler(DBHandler.OSCAR_DATA);
+      ResultSet rsLab = dbLab.GetSQL(select_not_signed);
+      if(rsLab.next()){
+         notSigned = rsLab.getInt("count");
+      }
+      rsLab.close();
+      dbLab.CloseConn();
+      return notSigned;
+   }
+   
+   public String getLabTab(String providerNo){
+      String retval = "";
+      try{  
+         String labText = "Lab" + ((getPendingLabCount()>0)? " *" : "");
+         String labTab = "<font size=\"2\" " + ((getNotSignedLabCount(providerNo)>0)? "color='red'" : "") + ">" + labText + "</font>";
+         retval = labTab;
+      }catch(Exception e){
+         e.printStackTrace();
+         retval = "Lab";
+      }
+      return retval;
+   }
+}
