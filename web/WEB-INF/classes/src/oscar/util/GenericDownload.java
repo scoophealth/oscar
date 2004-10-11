a9a92e1ef9f827cb7c9c79a68d18b85daa010466
jaygallagher <jaygallagher>
2004-10-11 20:59:54 +0000

    GenericDownload Commit

---
+++ web/WEB-INF/classes/src/oscar/util/GenericDownload.java
@@ -0,0 +1,102 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of GenericDownload
+ *
+ *
+ * GenericDownload.java
+ *
+ * Created on October 8, 2004, 11:26 AM
+ */
+
+package oscar.util;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import oscar.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class GenericDownload extends HttpServlet {
+       
+   public GenericDownload() {
+   }
+   
+   	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+                HttpSession session = req.getSession(true);
+                
+                OscarProperties oscarProps = OscarProperties.getInstance();
+
+		
+		String filename      = req.getParameter("filename");
+		String dir_property  = req.getParameter("dir_property");
+                String contentType   =req.getParameter("contentType"); 
+                String dir           = oscarProps.getProperty(dir_property);		
+                String user          = (String) session.getValue("user");
+    
+		
+                if (filename != null && dir_property != null && dir != null && user != null){
+			ServletOutputStream stream = res.getOutputStream();
+			transferFile(res, stream,dir , filename, contentType);			
+			stream.close();
+		} else {
+			res.setContentType("text/html");
+			PrintWriter out = res.getWriter();
+			out.println("<html>");
+			out.println("<head><body>You have no right to download the file(s).");
+			out.println("</body>");
+			out.println("</html>");
+		}
+	}
+        
+	private void transferFile(HttpServletResponse res,ServletOutputStream stream, String dir, String filename,String contentType) throws IOException {
+		String setContentType = "application/octet-stream";
+                if (contentType != null){
+                   setContentType = contentType;
+                }
+		res.setContentType(setContentType);
+		res.setHeader("Content-Disposition", "attachment;filename=" + filename);
+		BufferedInputStream bfis = new BufferedInputStream(new FileInputStream(dir + filename));
+		int data;
+		while ((data = bfis.read()) != -1) {
+			stream.write(data);
+			stream.flush();
+		}
+		bfis.close();
+	}
+}
+
+
+
+///
+
+
+
+
+///
