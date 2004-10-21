7d1260c9de22fbf8162bef16de71ab38e96acc98
jaygallagher <jaygallagher>
2004-10-21 04:23:24 +0000

    change upload process to use apache commons.    Supports uploads with mozilla better
    also fixes a memory leak. from an unclosed buffer.

--- web/WEB-INF/classes/src/oscar/DocumentMgtUploadServlet.java
+++ web/WEB-INF/classes/src/oscar/DocumentMgtUploadServlet.java
@@ -38,6 +38,11 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import java.util.zip.*;
 
+import org.apache.commons.fileupload.DiskFileUpload;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUpload;
+import org.apache.commons.fileupload.FileUploadException;
+
 public class DocumentMgtUploadServlet extends HttpServlet{
   final static int BUFFER = 2048;
   public java.util.Date today;
@@ -54,16 +59,16 @@ public class DocumentMgtUploadServlet extends HttpServlet{
     byte enddata[] = new byte[2];
 
 
- HttpSession session = request.getSession(true);
-     //String backupfilepath = ((String) session.getAttribute("homepath"))!=null?((String) session.getAttribute("homepath")):"null" ;
-  formatter = new SimpleDateFormat("yyyyMMddHmmss");
-  today = new java.util.Date();
-  output = formatter.format(today);
+    HttpSession session = request.getSession(true);
+    String backupfilepath = ((String) session.getAttribute("homepath"))!=null?((String) session.getAttribute("homepath")):"null" ;
+    formatter = new SimpleDateFormat("yyyyMMddHmmss");
+    today = new java.util.Date();
+    output = formatter.format(today);
 
     count=request.getContentType().indexOf('=');
     String temp = request.getContentType().substring(count+1);
     String filename = "test.txt", fileoldname="", foldername="", fileheader="", forwardTo="", function="", function_id="", filedesc="", creator="", doctype="", docxml="";
-  String home_dir="", doc_forward="";
+    String home_dir="", doc_forward="";
 
     // Get properties from oscar_mcmaster.properties
     Properties ap = OscarProperties.getInstance();
@@ -71,11 +76,47 @@ public class DocumentMgtUploadServlet extends HttpServlet{
     forwardTo  = ap.getProperty("DOC_FORWARD");
     foldername = ap.getProperty("DOCUMENT_DIR");
 
-   // function = request.getParameter("function");
-   // function_id = request.getParameter("functionid");
-   // filedesc = request.getParameter("filedesc");
-   // creator = request.getParameter("creator");
+    if (forwardTo == null || forwardTo.length() < 1) return;
 
+        boolean isMultipart = FileUpload.isMultipartContent(request);
+        //		 Create a new file upload handler
+        DiskFileUpload upload = new DiskFileUpload();
+
+        try {
+            //		 Parse the request
+            List  items = upload.parseRequest(request);
+//          Process the uploaded items
+            Iterator iter = items.iterator();
+            while (iter.hasNext()) {
+                FileItem item = (FileItem) iter.next();
+
+                if (item.isFormField()) {
+                    //String name = item.getFieldName();
+                    //String value = item.getString(); 
+                    //System.out.println("Fieldname: " + item.getFieldName());
+                } else {
+                    String pathName = item.getName();  
+                    String [] fullFile = pathName.split("[/|\\\\]");
+            		File savedFile = new File(foldername, output + fullFile[fullFile.length-1]);
+                    //System.out.println(item.getName() + "fullFile: " + fullFile[fullFile.length-1]);
+                    fileheader = output + fullFile[fullFile.length-1];
+            		
+            		item.write(savedFile);
+                }
+            }
+        } catch (FileUploadException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    
+     function = request.getParameter("function");
+     function_id = request.getParameter("functionid");
+     filedesc = request.getParameter("filedesc");
+     creator = request.getParameter("creator");
+/*
     ServletInputStream sis = request.getInputStream();
     BufferedOutputStream dest = null;
     FileOutputStream fos = null;
@@ -86,76 +127,67 @@ public class DocumentMgtUploadServlet extends HttpServlet{
     byte boundary[] = temp.getBytes();
 
     while (bf?true:((count = sis.readLine(data, 0, BUFFER)) != -1)) {
-      bf = false;
-     	benddata = false;
-
-	/* if the line is a blank line */
-     	if(count==2 && data[0]==13 && data[1]==10) {
-     		enddata[0] = 13;
-     		enddata[1] = 10;
-     		for(int i=0;i<BUFFER;i++) data[i]=0;
-
-     		count = sis.readLine(data, 0, BUFFER);
-		if(count==2 && data[0]==13 && data[1]==10) {
-		    dest.write(enddata, 0, 2);
-		    bf = true;
-		    continue;
-		} else {
-		    benddata = true;
-		}
-	} /* end blank line */
-
-
-     	String s = new String(data,2,temp.length());
-     	if(temp.equals(s)) {
-    		if(benddata) break;
-     		if((c =sis.readLine(data1, 0, BUFFER)) != -1) {
-     			filename = new String(data1);
+       bf = false;
+       benddata = false;
+       
+       if(count==2 && data[0]==13 && data[1]==10) {
+          enddata[0] = 13;
+     	  enddata[1] = 10;
+     	  for(int i=0;i<BUFFER;i++) data[i]=0;
+     	     count = sis.readLine(data, 0, BUFFER);
+	     if(count==2 && data[0]==13 && data[1]==10) {
+	        dest.write(enddata, 0, 2);
+		bf = true;
+		continue;
+	     } else {
+		benddata = true;
+	     }
+	  }
+     	  String s = new String(data,2,temp.length());
+     	  if(temp.equals(s)) {
+    	     if(benddata) break;
+     	     if((c =sis.readLine(data1, 0, BUFFER)) != -1) {
+     	        filename = new String(data1);
       		if(filename.length()>2 && filename.indexOf("filename")!=-1) {
-		    /* extract the filename of this string.
-		       On windows, filename should be filename="C:\file.name"
-		       But on linux filename is filename="file.name"
-		    */
-		    filename = filename.substring(filename.lastIndexOf("filename=\"") + "filename\"".length() +1,
+	
+		   filename = filename.substring(filename.lastIndexOf("filename=\"") + "filename\"".length() +1,
 						  filename.lastIndexOf('"'));
-		    /* on windows, this is necessary. If the file has the string '\', this will fail. */
-		    filename = filename.substring(filename.lastIndexOf('\\')+1, filename.length());
-			  
-        fileheader = output +  filename;
-            fos = new FileOutputStream(foldername+ output + filename);
-            dest = new BufferedOutputStream(fos, BUFFER);
-          }
+	
+		   filename = filename.substring(filename.lastIndexOf('\\')+1, filename.length());
+		   fileheader = output +  filename;
+                   fos = new FileOutputStream(foldername+ output + filename);
+                   dest = new BufferedOutputStream(fos, BUFFER);
+                }
        		c =sis.readLine(data2, 0, BUFFER);
-     		  if((c =sis.readLine(data2, 0, BUFFER)) != -1) {
-       		  bwri = bfbo?true:false;
-          }
-     		}
-     		bfbo = bfbo?false:true;
-     		for(int i=0;i<BUFFER;i++) data[i]=0;
-     		continue;
-     	} //end period
+     		if((c =sis.readLine(data2, 0, BUFFER)) != -1) {
+       		   bwri = bfbo?true:false;
+                }
+     	     }
+             bfbo = bfbo?false:true;
+             for(int i=0;i<BUFFER;i++) data[i]=0;
+     	        continue;
+     	  } //end period
 
-   		if(benddata) {
-   			benddata = false;
-   			dest.write(enddata, 0, 2);
-   		  for(int i=0;i<2;i++) enddata[i]=0;
-   		}
-      if(bwri) {
-       	dest.write(data, 0, count);
-     		for(int i=0;i<BUFFER;i++) data[i]=0;
-      }
+   	  if(benddata) {
+   	     benddata = false;
+             dest.write(enddata, 0, 2);
+   	     for(int i=0;i<2;i++) enddata[i]=0;
+   	  }
+          if(bwri) {
+       	     dest.write(data, 0, count);
+     	     for(int i=0;i<BUFFER;i++) data[i]=0;
+          }
     } //end while
     //dest.flush();
+    fos.close();
     dest.close();
     sis.close();
+*/
+    DocumentBean documentBean = new DocumentBean();
 
+    request.setAttribute("documentBean", documentBean);
 
-       DocumentBean documentBean = new DocumentBean();
-
-         request.setAttribute("documentBean", documentBean);
-
-
-        documentBean.setFilename(fileheader);
+    documentBean.setFilename(fileheader);
 
        //  documentBean.setFileDesc(filedesc);
 
@@ -169,14 +201,10 @@ public class DocumentMgtUploadServlet extends HttpServlet{
 
        //  documentBean.setDocCreator(creator);
 
-
-
-
-
     // Call the output page.
 
-		 RequestDispatcher dispatch = getServletContext().getRequestDispatcher(forwardTo);
-		 dispatch.forward(request, response);
+    RequestDispatcher dispatch = getServletContext().getRequestDispatcher(forwardTo);
+    dispatch.forward(request, response);
   }
 
 }
