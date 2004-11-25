0ecc38c990959e5e41f9c58ff53cec93436b6bba
jaygallagher <jaygallagher>
2004-11-25 05:56:09 +0000

    init commit

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/immunization/pageUtil/EctImmDeleteImmScheduleAction.java
@@ -0,0 +1,95 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ *
+ * EctImmDeleteImmScheduleAction.java
+ *
+ * Created on November 23, 2004, 4:19 PM
+ */
+
+package oscar.oscarEncounter.immunization.pageUtil;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+import org.w3c.dom.*;
+import oscar.oscarEncounter.immunization.data.*;
+import oscar.util.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class EctImmDeleteImmScheduleAction extends Action {
+   
+   /**
+   
+    Creates a new instance of EctImmDeleteImmScheduleAction
+    */
+   public EctImmDeleteImmScheduleAction() {
+   }
+   
+   public ActionForward perform(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException{
+      
+      EctImmImmunizationData immData = new EctImmImmunizationData();
+
+      String demographicNo = request.getParameter("demoNo");
+      String providerNo = (String) request.getSession().getAttribute("user");
+      String method = request.getParameter("method");
+      String id = request.getParameter("tblSet");
+      int setnum = Integer.parseInt(id);
+      
+           
+      try {
+                  
+         String imm = immData.getImmunizations(demographicNo);           
+         ///String sDoc = UtilMisc.decode64(imm);
+         Document doc = UtilXML.parseXML(imm);
+         NodeList sets = doc.getElementsByTagName("immunizationSet");
+         Element set = (Element) sets.item(setnum);
+         
+         //System.out.println(set.getAttribute("name"));
+         
+         if (method.equals("delete")){
+            set.setAttribute("status", "deleted");
+         }else{
+            set.removeAttribute("status");
+         }
+         
+         
+         
+         String sXML = UtilXML.toXML(doc);
+         //System.out.println(sXML);
+         //EctImmImmunizationData imm = new EctImmImmunizationData();
+         immData.saveImmunizations(demographicNo, providerNo, sXML);
+      }
+      catch(Exception ex) {
+         throw new ServletException("Exception occurred in EctImmDeleteImmScheduleAction", ex);
+      }           
+           
+      return mapping.findForward("success");
+   }
+   
+   
+}
