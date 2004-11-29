a5462d9dd525393933f3ee453e75bef0cdf537ec
jaygallagher <jaygallagher>
2004-11-29 21:43:43 +0000

    Changed database driver url username and password to be pulled from properties file instead of compiled into the code

--- mdsFileManagement/oscar/oscarMDSLab/dbUtil/DBHandler.java
+++ mdsFileManagement/oscar/oscarMDSLab/dbUtil/DBHandler.java
@@ -7,92 +7,90 @@
 package oscar.oscarMDSLab.dbUtil;
 import java.io.*;
 import java.sql.*;
+import oscar.oscarMDSLab.*;
 
-public class DBHandler
-{
-
-    public static String MDS_DATA = "oscar_mcmaster";
-    private String  connDriver = "com.mysql.jdbc.Driver";
-    private String  connURL = "jdbc:mysql:///";
-    private String  connUser = "root";
-    private String  connPwd = "liyi";
-    private int     connInitialConnections = 1;
-    private int     connMaxConnections = 10;
-    private boolean connWaitIfBusy = true;
-
-    private Connection conn;
-    private ConnectionPool pool;
-
-    public DBHandler(String dbName)
-    {
-        try
-        {               
-            pool = MDSLabPool.getInstance(connDriver, connURL + dbName, connUser,
-                    connPwd, connInitialConnections, connMaxConnections, connWaitIfBusy);
-
-            conn = pool.getConnection();
-
-        } catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-            e.printStackTrace(System.out);
-        }
-    }
-
-    synchronized public java.sql.ResultSet GetSQL(String SQLStatement) throws SQLException
-    {
-        Statement stmt;
-        ResultSet rs = null;
-
-        System.out.println(SQLStatement);
-
-        try
-        {
-            stmt = conn.createStatement();
-            rs = stmt.executeQuery(SQLStatement);
-        } catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-            e.printStackTrace(System.out);
-            throw e;
-        }
-
-        return rs;
-    }
-
-    synchronized public boolean RunSQL(String SQLStatement) throws SQLException
-    {
-        boolean b = false;
-
-        System.out.println(SQLStatement);
-
-        try
-        {
-            Statement stmt;
-            stmt = conn.createStatement();
-            b = stmt.execute(SQLStatement);
-        } catch (SQLException e)
-        {
-            System.out.println(e.getMessage());
-            e.printStackTrace(System.out);
-            throw e;
-        }
-
-        return b;
-    }
-    
-    public void CloseConn() throws SQLException
-    {
-        if(!conn.isClosed())
-        {
-                pool.free(conn);
-
-//                System.out.println("Connection freed.");
-        }
-    }
+public class DBHandler {
+   
+   public static String MDS_DATA = "oscar_mcmaster";
+   private String  connDriver = "com.mysql.jdbc.Driver";
+   private String  connURL = "jdbc:mysql://127.0.0.1:3307/";
+   private String  connUser = "root";
+   private String  connPwd = "";
+   private int     connInitialConnections = 1;
+   private int     connMaxConnections = 10;
+   private boolean connWaitIfBusy = true;
+   
+   private Connection conn;
+   private ConnectionPool pool;
+   
+   public void init(){
+      LabProperties labProperties = LabProperties.getInstance();
+      this.MDS_DATA   = labProperties.getProperty("MDS_DATA"); 
+      this.connDriver = labProperties.getProperty("connDriver");
+      this.connURL    = labProperties.getProperty("connURL");
+      this.connUser   = labProperties.getProperty("connUser");
+      this.connPwd    = labProperties.getProperty("connPwd");
+   }
+   
+   public DBHandler(String dbName) {
+      init();
+      try {
+         pool = MDSLabPool.getInstance(connDriver, connURL + dbName, connUser,
+         connPwd, connInitialConnections, connMaxConnections, connWaitIfBusy);
+         
+         conn = pool.getConnection();
+         
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+         e.printStackTrace(System.out);
+      }
+   }
+   
+   synchronized public java.sql.ResultSet GetSQL(String SQLStatement) throws SQLException {
+      Statement stmt;
+      ResultSet rs = null;
+      
+      System.out.println(SQLStatement);
+      
+      try {
+         stmt = conn.createStatement();
+         rs = stmt.executeQuery(SQLStatement);
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+         e.printStackTrace(System.out);
+         throw e;
+      }
+      
+      return rs;
+   }
+   
+   synchronized public boolean RunSQL(String SQLStatement) throws SQLException {
+      boolean b = false;
+      
+      System.out.println(SQLStatement);
+      
+      try {
+         Statement stmt;
+         stmt = conn.createStatement();
+         b = stmt.execute(SQLStatement);
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+         e.printStackTrace(System.out);
+         throw e;
+      }
+      
+      return b;
+   }
+   
+   public void CloseConn() throws SQLException {
+      if(!conn.isClosed()) {
+         pool.free(conn);
+         
+         //                System.out.println("Connection freed.");
+      }
+   }
 }
 
 
-    
-    
-    
+
+
