86e1384f5422037f9f7de2516cce9bec96b459c2
jaygallagher <jaygallagher>
2005-01-03 21:25:51 +0000

    Removed import org.apache.struts.action.ActionError

--- web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingReProcessBillAction.java
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingReProcessBillAction.java
@@ -44,7 +44,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import oscar.oscarBilling.ca.bc.pageUtil.*;
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
