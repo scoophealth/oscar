2e9ac826eeccea52b6ca9081d108fb9367e3e631
yili119 <yili119>
2005-03-06 02:16:13 +0000

    correct the updateDbRecord returned boolean value

--- web/WEB-INF/classes/src/oscar/oscarBilling/ca/on/data/BillingONDataHelp.java
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/on/data/BillingONDataHelp.java
@@ -1,25 +1,25 @@
 /*
- * 
+ *
  * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
- * This software is published under the GPL GNU General Public License. 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation; either version 2 
- * of the License, or (at your option) any later version. * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
- * 
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
  * <OSCAR TEAM>
- * 
- * This software was written for the 
- * Department of Family Medicine 
- * McMaster Unviersity 
- * Hamilton 
- * Ontario, Canada 
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster Unviersity
+ * Hamilton
+ * Ontario, Canada
  */
 /*
  * Created on Mar 14, 2004
@@ -35,7 +35,7 @@ import oscar.OscarProperties;
 import oscar.oscarDB.DBHandler;
 /**
  * @author yilee18
- * 
+ *
  * To change the template for this generated type comment go to Window -
  * Preferences - Java - Code Generation - Code and Comments
  */
@@ -72,7 +72,7 @@ public class BillingONDataHelp {
 		}
 		return ret;
 	}
-	
+
 	public synchronized boolean updateDBRecord(String sql) {
 		boolean ret = false;
 		try {
@@ -80,12 +80,14 @@ public class BillingONDataHelp {
 			ret = db.RunSQL(sql);
 
 			db.CloseConn();
+			ret = true;
 		} catch (SQLException e) {
+		    ret = false;
 			System.out.println(e.getMessage());
 		}
 		return ret;
 	}
-	
+
 	public synchronized ResultSet searchDBRecord(String sql) {
 		ResultSet ret = null;
 		try {
