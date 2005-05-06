f69b2dd75215b4bbde4f494c60ca828e88e744d3
jaygallagher <jaygallagher>
2005-05-06 05:32:35 +0000

    Thread for searching drug to drug interactions. This is launched from RxInteractionData.  it will return search results to a cache of drug to drug interactions

---
+++ web/WEB-INF/classes/src/oscar/oscarRx/data/RxInteractionWorker.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of RxInteractionWorker
+ * 
+ *
+ * RxInteractionWorker.java
+ *
+ * Created on May 5, 2005, 6:54 PM
+ */
+
+package oscar.oscarRx.data;
+
+import java.util.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class RxInteractionWorker extends Thread {
+   RxInteractionData interactionData = null;
+   Vector atcCodes = null;
+   
+   public RxInteractionWorker() {
+   }
+   
+   public RxInteractionWorker(RxInteractionData rxInt,Vector v){
+      atcCodes = v;
+      interactionData = rxInt;
+   }
+   
+   public void run() {
+      System.out.println("STARTING THREAD");
+      long start = System.currentTimeMillis(); 
+      
+          RxDrugData.Interaction[] interactions = null;                 
+          try{   
+             if (atcCodes != null && interactionData != null){
+                RxDrugData drugData = new RxDrugData();
+                interactions = drugData.getInteractions(atcCodes);
+                if (interactions != null){
+                   interactionData.addToHash(atcCodes,interactions);
+                   interactionData.removeFromWorking(atcCodes);
+                }else{
+                   System.out.println("What to do");
+                   System.out.println("atc codes "+atcCodes);
+                }
+             }
+          }catch(Exception e){
+             e.printStackTrace();
+          }
+      long end = System.currentTimeMillis() - start;
+      System.out.println("THREAD ENDING "+end);
+   }
+   
+}
