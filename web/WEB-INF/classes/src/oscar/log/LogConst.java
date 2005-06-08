0fb27723128dbd1c89b9aa4351ee46c53aa1f2c3
yili119 <yili119>
2005-06-08 03:45:53 +0000

    conts for programming

---
+++ web/WEB-INF/classes/src/oscar/log/LogConst.java
@@ -0,0 +1,21 @@
+/*
+ * Created on 2005-6-1
+ */
+package oscar.log;
+
+/**
+ * @author yilee18
+ */
+public class LogConst {
+    public static final String LOGIN = "log in";
+    public static final String LOGOUT = "log out";
+    public static final String READ = "read";
+    public static final String ADD = "add";
+    public static final String UPDATE = "update";
+    public static final String DELETE = "delete";
+
+    public static final String CON_LOGIN = "login";
+    public static final String CON_APPT = "appointment";
+    public static final String CON_ECHART = "eChart";
+    public static final String CON_DEMOGRAPHIC = "demographic";
+}
