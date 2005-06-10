c5a096176de8db16a1c33c82cdd9751a6df4971e
jaygallagher <jaygallagher>
2005-06-10 03:17:53 +0000

    changed addLog method to launch a thread

--- web/WEB-INF/classes/src/oscar/log/LogAction.java
+++ web/WEB-INF/classes/src/oscar/log/LogAction.java
@@ -1,4 +1,18 @@
-/*
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *
  * Created on 2005-6-1
  *
  */
@@ -15,17 +29,9 @@ import oscar.login.DBHelp;
  * @author yilee18
  */
 public class LogAction {
-    private static final Logger _logger = Logger.getLogger(LogAction.class);
-
-    public boolean addLog(String provider_no, String action, String content, String contentId) {
-        boolean ret = false;
-        DBHelp db = new DBHelp();
-        String sql = "insert into log (provider_no,action,content,contentId) values('" + provider_no;
-        sql += "', '" + action + "','" + StringEscapeUtils.escapeSql(content) + "','" + contentId + "')";
-        try {
-            ret = db.updateDBRecord(sql, provider_no);
-        } catch (SQLException e) {
-        }
-        return ret;
-    }
+    private static final Logger _logger = Logger.getLogger(LogAction.class);    
+    public static void addLog(String provider_no, String action, String content, String contentId) {
+       LogWorker logWorker = new LogWorker(provider_no,action,content,contentId);
+       logWorker.start();
+    }    
 }
