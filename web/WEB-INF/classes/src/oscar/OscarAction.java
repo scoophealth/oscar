e9a78d32129b2056fbe7ff5c610978fe8686e290
jlegris <jlegris>
2005-06-11 21:56:40 +0000

    Added Comments, Convenience Method for returning data over Response

--- web/WEB-INF/classes/src/oscar/OscarAction.java
+++ web/WEB-INF/classes/src/oscar/OscarAction.java
@@ -1,23 +1,43 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * <OSCAR TEAM>
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ * AUTHOR: Joel Legris
+ * DATE: May 13, 2005
+ * DESCRIPTION:
+ * <p>Description:SuperClass for actions in OSCAR, performs login verification and contains commonly used
+ *  objects for database access </p>
+ */
+
 package oscar;
 
 import java.io.*;
 import java.sql.*;
 import java.util.*;
+import javax.servlet.*;
 import javax.servlet.http.*;
 
 import org.apache.struts.action.*;
 import oscar.oscarDB.*;
 
-/**
- *
- * <p>Title: OscarAction</p>
- * <p>Description:SuperClass for actions in OSCAR, performs login verification and contains commonly used
- *  objects for database access </p>
- * <p>Copyright: Copyright (c) 2004</p>
- * <p>Company: </p>
- * @author not attributable
- * @version 1.0
- */
 public class OscarAction
     extends Action {
   protected String target = "success";
@@ -25,10 +45,16 @@ public class OscarAction
   protected String propName = "";
   protected DBPreparedHandler handler = null;
   protected Properties oscarVariables = null;
+  public final String MIME_PDF = "application/pdf";
   String propFile = "";
 
-  public OscarAction() {
 
+  public OscarAction() {
+    if (!System.getProperties().containsKey("jasper.reports.compile.class.path")) {
+     // String classpath = (String) getServlet().getServletContext().
+      //    getAttribute("org.apache.catalina.jsp_classpath");
+     // System.setProperty("jasper.reports.compile.class.path", classpath);
+    }
   }
 
   /**
@@ -69,6 +95,11 @@ public class OscarAction
     return dbParams;
   }
 
+  /**
+   * A convenience method that provides direct access to an OSCAR database connection
+   * @param request HttpServletRequest
+   * @return Connection
+   */
   protected Connection getDBConnection(HttpServletRequest request) {
     String[] dbParams = this.getDBParams(request);
     try {
@@ -80,5 +111,46 @@ public class OscarAction
     }
     return handler.getConn();
   }
+
+  /**
+   * Configures the response header for upload of specified mime-type
+   * @param response HttpServletResponse
+   * @param docName String
+   * @param docType String
+   * @return StringBuffer
+   */
+  public void cfgHeader(HttpServletResponse response, String docName,
+                        String docType) {
+    String mimeType = "application/octet-stream";
+    if (docType.equals("pdf")) {
+      mimeType = "application/pdf";
+    }
+    else if (docType.equals("csv")) {
+      mimeType = "application/csv";
+    }
+    response.setContentType(mimeType);
+    response.setHeader("Content-Disposition",
+                       "attachment;filename=" + docName + "." + docType);
+  }
+
+
+  /**
+   * A convenience method for retrieving the servlet outputstream without
+   * cluttering the calling code with verbose exception handling
+   * @param response HttpServletResponse
+   * @return ServletOutputStream
+   */
+  protected ServletOutputStream getServletOstream(HttpServletResponse response) {
+    ServletOutputStream outputStream = null;
+    try {
+      outputStream = response.getOutputStream();
+    }
+    catch (IOException ex) {
+      ex.printStackTrace();
+    }
+    return outputStream;
+  }
+
+
 
 }
