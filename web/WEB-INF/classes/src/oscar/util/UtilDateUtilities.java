95163b0d5466364ff6983d7678fcfb079b05708b
jaygallagher <jaygallagher>
2005-06-24 15:01:21 +0000

    Added Methods
    public static int getNumMonths(Calendar dStart,Calendar dEnd)
    public static int getNumYears(Date dStart,Date dEnd)
    public static int getNumMonths(Date dStart, Date dEnd)
    public static Date getDateFromString(String dateStr, String datePattern)
    public static int getNumMonthsBetweenDates(Date dStart, Date dEnd)
    public static String calcAgeAtDate(Date DOB,Date pointInTime)

--- web/WEB-INF/classes/src/oscar/util/UtilDateUtilities.java
+++ web/WEB-INF/classes/src/oscar/util/UtilDateUtilities.java
@@ -81,9 +81,21 @@ public class UtilDateUtilities {
         GregorianCalendar gregoriancalendar = new GregorianCalendar(i, j, k);
         return gregoriancalendar.getTime();
     }
-
-    public static String calcAge(Date DOB)    {
+        
+    public static String calcAge(Date DOB){
+       return calcAgeAtDate(DOB,new GregorianCalendar().getTime());
+    }
+    
+    
+    /**
+     * This returns the Patients Age string at a point in time.  IE. How old the patient will be right now or how old will they be on march.31 of this year.
+     * @param DOB Demographics Date of birth
+     * @param pointInTime The date you would like to calculate there age at.
+     * @return age string ( ie 2 months, 4 years .etc )
+     */    
+    public static String calcAgeAtDate(Date DOB,Date pointInTime)    {
         GregorianCalendar now = new GregorianCalendar();
+        now.setTime(pointInTime);
         int curYear = now.get(Calendar.YEAR);
         int curMonth = now.get(Calendar.MONTH) + 1;
         int curDay = now.get(Calendar.DAY_OF_MONTH);
@@ -125,7 +137,8 @@ public class UtilDateUtilities {
         }
         return result;
     }
-
+            
+    
     public static int calcAge(String year_of_birth, String month_of_birth, String date_of_birth)    {
       GregorianCalendar now = new GregorianCalendar();
       int curYear = now.get(Calendar.YEAR);
@@ -153,4 +166,89 @@ public class UtilDateUtilities {
        Format formatter = new SimpleDateFormat(datePattern);
        return formatter.format(Today());       
     }
+    
+    /**
+     * For Parsing Dates.
+     * @param dateStr The date string to be parsed
+     * @param datePattern The date pattern to use to parse the date string
+     * @return Date object. If date was unable to be parsed the object will be null
+     */    
+    public static Date getDateFromString(String dateStr, String datePattern){
+      Date date = null;
+      try {
+         // Some examples
+         DateFormat formatter = new SimpleDateFormat(datePattern);
+         date = (Date)formatter.parse(dateStr);                        
+      } catch (ParseException e) {
+      }
+      return date   ;     
+    }
+    
+    
+    //This if probably not the most effiecent way to calcu
+    /**
+     * Gets the number of months between two date objects
+     * @param dStart Start Date
+     * @param dEnd End Date
+     * @return the number of months
+     */    
+    public static int getNumMonths(Date dStart, Date dEnd) {
+       int i = 0;
+       //System.out.println("Getting the number of months between "+dStart.toString()+ " and "+dEnd.toString() );        
+       Calendar calendar = Calendar.getInstance();
+       calendar.setTime(dStart);
+       while (calendar.getTime().before(dEnd) || calendar.getTime().equals(dEnd)) {
+          calendar.add(Calendar.MONTH, 1);
+          i++;
+       }
+       i--;
+       if (i < 0) { i = 0; }
+       return i;
+   }
+    
+    
+    /**
+     * Gets the number of years between two date objects
+     * @param dStart Start Date
+     * @param dEnd End Date
+     * @return Number of year between
+     */    
+   public static int getNumYears(Date dStart,Date dEnd){
+        GregorianCalendar now = new GregorianCalendar();
+        now.setTime(dEnd);
+        int curYear = now.get(Calendar.YEAR);
+        int curMonth = now.get(Calendar.MONTH) + 1;
+        int curDay = now.get(Calendar.DAY_OF_MONTH);
+        
+        GregorianCalendar birthDate = new GregorianCalendar();
+        birthDate.setTime(dStart);
+        int birthYear = birthDate.get(Calendar.YEAR);
+        int birthMonth = birthDate.get(Calendar.MONTH) + 1;
+        int birthDay = birthDate.get(5);
+                
+        int ageInYears = curYear - birthYear;
+                        
+        if (curMonth > birthMonth || curMonth == birthMonth && curDay >= birthDay)        {
+            ageInYears = curYear - birthYear;
+        } else        {
+            ageInYears = curYear - birthYear - 1;
+        }
+        return ageInYears;
+    }
+    
+    
+   
+   /**
+    * Gets the number of months between two Calendar objects
+    * @param dStart start date
+    * @param dEnd end date
+    * @return number of months between
+    */   
+   public static int getNumMonths(Calendar dStart,Calendar dEnd) {        
+      return getNumMonths(dStart.getTime(),dEnd.getTime());
+   }
+
+    
+   
 }
+
