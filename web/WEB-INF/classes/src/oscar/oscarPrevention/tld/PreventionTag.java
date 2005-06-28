ca4360de6b6d6dcf0a2d41f0a08a872180bfc6d1
jaygallagher <jaygallagher>
2005-06-28 00:16:42 +0000

    tag class for turning prevention label red if there are warnings for that patient

---
+++ web/WEB-INF/classes/src/oscar/oscarPrevention/tld/PreventionTag.java
@@ -0,0 +1,105 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of LabTag
+ *
+ *
+ * LabTag.java
+ *
+ * Created on May 4, 2005, 11:15 AM
+ */
+
+package oscar.oscarPrevention.tld;
+
+import java.io.PrintStream;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+import javax.servlet.jsp.tagext.TagSupport;
+import org.apache.struts.util.*;
+import oscar.oscarPrevention.*;
+
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class PreventionTag extends TagSupport {
+   
+  
+   public PreventionTag() {
+	numWarnings = 0;
+   }
+   
+   public int doStartTag() throws JspException    {
+      	PreventionData pd = new PreventionData();    
+	Prevention p = pd.getPrevention(demographicNo);  
+        try{
+	PreventionDS pf = PreventionDS.getInstance();
+	pf.getMessages(p);
+        }catch(Exception e){
+           e.printStackTrace();
+        }
+	ArrayList warnings = p.getWarnings();      
+	ArrayList recomendations = p.getReminder();
+        if (warnings != null){
+           numWarnings = warnings.size();
+           //System.out.println("the number of warnings "+numWarnings);           
+        }
+        
+        try{
+            JspWriter out = super.pageContext.getOut();            
+            if(numWarnings > 0)
+                out.print("<span style=\"color:red;\">  ");
+            else
+                out.print("<span>  ");
+        } catch(Exception eWriter) {
+            eWriter.printStackTrace(System.out);
+        }        
+        return(EVAL_BODY_INCLUDE);
+    }
+   
+
+    public void setDemographicNo(String demographicNo)    {
+       this.demographicNo = demographicNo;
+    }
+
+    public String getDemographicNo()    {
+        return demographicNo;
+    }
+
+    
+
+    public int doEndTag()        throws JspException    {
+       try{
+          JspWriter out = super.pageContext.getOut();         
+          out.print("</span>");
+       }catch(Exception p) {
+            p.printStackTrace(System.out);
+       }
+       return EVAL_PAGE;
+    }
+
+    private String demographicNo;
+    private int numWarnings;
+    private int numReminders;
+}
