da48b82fe168e04e9784beed7d762bccc8b0fa65
yili119 <yili119>
2005-06-29 18:14:48 +0000

    add log

--- web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctSaveEncounterAction.java
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctSaveEncounterAction.java
@@ -1,39 +1,46 @@
 /*
- * 
+ *
  * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
- * This software is published under the GPL GNU General Public License. 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation; either version 2 
- * of the License, or (at your option) any later version. * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
- * 
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
  * <OSCAR TEAM>
- * 
- * This software was written for the 
- * Department of Family Medicine 
- * McMaster Unviersity 
- * Hamilton 
- * Ontario, Canada 
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster Unviersity
+ * Hamilton
+ * Ontario, Canada
  */
 package oscar.oscarEncounter.pageUtil;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.sql.SQLException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import org.apache.struts.action.*;
-//import oscar.Misc;
-import oscar.oscarDB.DBHandler;
-import oscar.oscarEncounter.pageUtil.EctSessionBean;
-import oscar.util.*;
 import java.util.ResourceBundle;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import oscar.log.LogAction;
+import oscar.log.LogConst;
+import oscar.oscarDB.DBHandler;
+import oscar.util.UtilDateUtilities;
+import oscar.util.UtilMisc;
 public class EctSaveEncounterAction extends Action {
 
     public ActionForward execute(ActionMapping actionmapping, ActionForm actionform, HttpServletRequest httpservletrequest, HttpServletResponse httpservletresponse)
@@ -48,10 +55,10 @@ public class EctSaveEncounterAction extends Action {
         sessionbean.reminders = httpservletrequest.getParameter("reTextarea");
         sessionbean.encounter = httpservletrequest.getParameter("enTextarea");
         sessionbean.subject = httpservletrequest.getParameter("subject");
-        java.util.Date date = UtilDateUtilities.Today();               
+        java.util.Date date = UtilDateUtilities.Today();
         sessionbean.eChartTimeStamp = date;
-        
-        if(!httpservletrequest.getParameter("btnPressed").equals("Exit")) {            
+
+        if(!httpservletrequest.getParameter("btnPressed").equals("Exit")) {
             try {
                 ResourceBundle prop = ResourceBundle.getBundle("oscarResources", httpservletrequest.getLocale());
                 if(httpservletrequest.getParameter("btnPressed").equals("Sign,Save and Exit"))
@@ -63,12 +70,15 @@ public class EctSaveEncounterAction extends Action {
                 sessionbean.template = "";
             } catch (Exception e) {
                 e.printStackTrace();
-            }            
+            }
             try {
                 DBHandler dbhandler = new DBHandler(DBHandler.OSCAR_DATA);
                 String s = "insert into eChart (timeStamp, demographicNo,providerNo,subject,socialHistory,familyHistory,medicalHistory,ongoingConcerns,reminders,encounter) values ('" + UtilDateUtilities.DateToString(date, "yyyy-MM-dd HH:mm:ss") + "'," + sessionbean.demographicNo + ",'" + sessionbean.providerNo + "','" +  UtilMisc.charEscape(sessionbean.subject, '\'') + "','" + UtilMisc.charEscape(sessionbean.socialHistory, '\'') + "','" + UtilMisc.charEscape(sessionbean.familyHistory, '\'') + "','" + UtilMisc.charEscape(sessionbean.medicalHistory, '\'') + "','" + UtilMisc.charEscape(sessionbean.ongoingConcerns, '\'') + "','" + UtilMisc.charEscape(sessionbean.reminders, '\'') + "','" + UtilMisc.charEscape(sessionbean.encounter, '\'') + "')" ;
                 dbhandler.RunSQL(s);
-                
+                // add log here
+                String ip = httpservletrequest.getRemoteAddr();
+                LogAction.addLog((String) httpservletrequest.getSession().getAttribute("user"), LogConst.ADD, LogConst.CON_ECHART, sessionbean.demographicNo, ip);
+
                 //change the appt status
                 if (sessionbean.status != null && !sessionbean.status.equals("")) {
                     oscar.appt.ApptStatusData as = new oscar.appt.ApptStatusData();
@@ -87,7 +97,7 @@ public class EctSaveEncounterAction extends Action {
                 System.out.println(sqlexception.getMessage());
             }
         }
-        
+
         try {  // save enc. window sizes
             DBHandler dbhandler = new DBHandler(DBHandler.OSCAR_DATA);
             String s = "delete from encounterWindow where provider_no='"+sessionbean.providerNo+"'";
@@ -97,13 +107,13 @@ public class EctSaveEncounterAction extends Action {
             httpservletrequest.getParameter("rowOneSize")+"', '"+
             httpservletrequest.getParameter("rowTwoSize")+"', '"+
             httpservletrequest.getParameter("presBoxSize")+"', '"+
-            httpservletrequest.getParameter("rowThreeSize")+"')";            
+            httpservletrequest.getParameter("rowThreeSize")+"')";
             dbhandler.RunSQL(s);
             dbhandler.CloseConn();
         } catch(Exception e) {
             e.printStackTrace(System.out);
         }
-        
+
         if(httpservletrequest.getParameter("btnPressed").equals("Sign,Save and Exit")
         || httpservletrequest.getParameter("btnPressed").equals("Verify and Sign"))
             return actionmapping.findForward("success");
