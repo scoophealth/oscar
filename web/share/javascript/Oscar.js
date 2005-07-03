24c799eec295dca2f20c1c6f0fd5348e0508adeb
jaygallagher <jaygallagher>
2005-07-03 21:44:40 +0000

    Added javascript methods
    function showHideItem(id)
    function showItem(id)
    function hideItem(id)

--- web/share/javascript/Oscar.js
+++ web/share/javascript/Oscar.js
@@ -22,18 +22,34 @@
  * Ontario, Canada 
  */
 
-function popup( height, width, url, windowName)   
-{   
+function popup( height, width, url, windowName){   
   var page = url;  
   windowprops = "height="+height+",width="+width+",location=no,scrollbars=yes,menubars=no,toolbars=no,resizable=yes,screenX=0,screenY=0,top=0,left=0";  
   var popup=window.open(url, windowName, windowprops);  
-  if (popup != null)   
-  {  
-    if (popup.opener == null)   
-    {  
+  if (popup != null){  
+    if (popup.opener == null){  
       popup.opener = self;  
     }  
   }  
   popup.focus();  
   return false;  
 }
+
+
+function showHideItem(id){ 
+    if(document.getElementById(id).style.display == 'none')
+        showItem(id);        
+    else
+        hideItem(id);        
+}
+
+function showItem(id){
+        document.getElementById(id).style.display = ''; 
+}
+
+function hideItem(id){
+        document.getElementById(id).style.display = 'none'; 
+}
+
+
+
