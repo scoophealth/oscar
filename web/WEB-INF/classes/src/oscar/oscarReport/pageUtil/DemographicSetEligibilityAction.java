a3053aa72162af068852d1e749a1aaafa6ef2343
jaygallagher <jaygallagher>
2005-07-13 18:57:15 +0000

    Used to edit demographic sets

---
+++ web/WEB-INF/classes/src/oscar/oscarReport/pageUtil/DemographicSetEligibilityAction.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of DemographicExportAction
+ *
+ * DemographicSetEligibilityAction.java
+ *
+ * Created on July 13, 2005, 10:30 AM
+ */
+
+package oscar.oscarReport.pageUtil;
+
+import java.util.*;
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+import oscar.oscarReport.data.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class DemographicSetEligibilityAction extends Action {
+   
+   public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)  {
+   
+        String[] s = request.getParameterValues("demoNo") ;
+        String setName = request.getParameter("setName");
+        DemographicSets dsets = new DemographicSets();
+        
+        if(s != null){
+           for (int i = 0; i < s.length;i++){
+              dsets.setDemographicIneligible(setName, s[i]);
+           }
+        }
+                
+        return mapping.findForward("success");
+   }
+  
+   public DemographicSetEligibilityAction() {
+   }
+   
+}
+
+
+
+ 
+
+
