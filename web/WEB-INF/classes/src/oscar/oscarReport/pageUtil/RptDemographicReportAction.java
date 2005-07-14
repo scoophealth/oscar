66c00b06e961e4ba755f276b7fdd4b41815d5e06
jaygallagher <jaygallagher>
2005-07-14 13:51:06 +0000

    removed unneeded import statement

--- web/WEB-INF/classes/src/oscar/oscarReport/pageUtil/RptDemographicReportAction.java
+++ web/WEB-INF/classes/src/oscar/oscarReport/pageUtil/RptDemographicReportAction.java
@@ -38,7 +38,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
