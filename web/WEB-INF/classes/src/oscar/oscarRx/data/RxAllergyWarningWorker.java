b753eb70d4457858d7d87531a9b6724a353c47e8
jaygallagher <jaygallagher>
2005-07-17 22:05:17 +0000

    Thread class to search for an allergy warning

---
+++ web/WEB-INF/classes/src/oscar/oscarRx/data/RxAllergyWarningWorker.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of RxInteractionWorker
+ * 
+ *
+ * RxAllergyWarningWorker.java
+ *
+ * Created on June 16, 2005, 2:54 PM
+ */
+
+
+package oscar.oscarRx.data;
+
+import java.util.*;
+import oscar.oscarRx.pageUtil.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class RxAllergyWarningWorker extends Thread {
+   RxSessionBean sessionBean = null;
+   String atcCode = null;
+   oscar.oscarRx.data.RxPatientData.Patient.Allergy[] allergies = null;
+   
+   
+   public RxAllergyWarningWorker() {
+   }
+   
+   public RxAllergyWarningWorker(RxSessionBean sessionBean,String actCode,oscar.oscarRx.data.RxPatientData.Patient.Allergy[] allergies){
+      //drugData.getAllergyWarnings(rx.getAtcCode(),allergies)
+      
+      this.atcCode = actCode;
+      this.sessionBean = sessionBean;
+      this.allergies = allergies;
+   }
+   
+   public void run() {
+      System.out.println("STARTING THREAD - RxAllergyWarningWorker ");
+      long start = System.currentTimeMillis(); 
+      
+          oscar.oscarRx.data.RxPatientData.Patient.Allergy[] allergyWarnings = null;                 
+          try{   
+             if (atcCode != null && sessionBean != null && allergies != null){
+                RxDrugData drugData = new RxDrugData();
+                allergyWarnings = drugData.getAllergyWarnings(atcCode,allergies);                 
+                if (allergyWarnings != null){
+                   
+                   sessionBean.addAllergyWarnings(atcCode,allergyWarnings);
+                   sessionBean.removeFromWorkingAllergyWarnings(atcCode);
+                }else{
+                   System.out.println("What to do will allergies atc codes "+atcCode);
+                }
+             }
+          }catch(Exception e){
+             e.printStackTrace();
+          }
+      long end = System.currentTimeMillis() - start;
+      System.out.println("THREAD ENDING -RxAllergyWarningWorker "+end);
+   }
+   
+}
