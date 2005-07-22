f016722e9b884f798cd2a1ee3fcfbbd4580c07da
jaygallagher <jaygallagher>
2005-07-22 01:27:48 +0000

    init -commit Thread class that can be launched to add ticklers

---
+++ web/WEB-INF/classes/src/oscar/oscarTickler/TicklerWorker.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of RxInteractionWorker
+ * 
+ *
+ * TicklerWorker.java
+ *
+ * Created on July 20, 2005, 2:37 PM
+ */
+
+package oscar.oscarTickler;
+
+import java.sql.*;
+import oscar.oscarDB.*;
+import oscar.oscarDemographic.data.*;
+import oscar.util.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class TicklerWorker extends Thread {
+      
+   public String provider = null;
+   public String ticklerMessage = null;
+   public String status = TicklerData.ACTIVE;
+   public String priority = TicklerData.NORMAL;
+         
+   public TicklerWorker() {
+      //System.out.println("CONSTRUCTOR FOR TICKLERWORKER CALLED");
+   }
+      
+   
+   public void run() {            
+      //System.out.println("TICKLERWORKER THREAD STARTED");
+      TicklerData td = new TicklerData();
+      try {         
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+         String sql = "select * from consultationRequests where to_days(now()) - to_days(referalDate) > 14 and status = '1' and providerNo = '"+provider+"' ";                                          
+         //System.out.println(sql);
+         ResultSet rs = db.GetSQL(sql);
+         while (rs.next()){
+            String demo = rs.getString("demographicNo");
+            String date = rs.getString("referalDate");
+            //System.out.println("Check to see if "+demo+" does not have this tickler already");
+            ticklerMessage = DemographicNameAgeString.getInstance().getNameAgeString(demo) + " has an Consultation Request with a status of 'Nothing Done'. Referral Date was "+date;
+            if (!td.hasTickler(demo,provider,ticklerMessage)){                                                         
+               td.addTickler(demo, ticklerMessage, status, UtilDateUtilities.getToday("yyyy-MM-dd"), "0", priority, provider);
+            }
+         }
+         rs.close();
+         db.CloseConn();         
+      } catch (SQLException e) {
+         //System.out.println(e.getMessage());
+      }      
+      /// check to see if tickler is needed
+      //if so, check to see if one is already added
+      //if so add it
+      //System.out.println("TICKLERWORKER THREAD ENDED");
+   }
+}
+
+
+
+ 
