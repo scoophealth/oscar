543b15c031f9cfba5b185b515a6fb6efbed97143
jlegris <jlegris>
2005-08-31 06:35:53 +0000

    Added stacktrace for debugging

--- web/WEB-INF/classes/src/oscar/login/LoginCheckLoginBean.java
+++ web/WEB-INF/classes/src/oscar/login/LoginCheckLoginBean.java
@@ -176,6 +176,7 @@ public class LoginCheckLoginBean {
             }
             return secBean;
         } catch (SQLException e) {
+          e.printStackTrace();
             return null;
         }
     }
