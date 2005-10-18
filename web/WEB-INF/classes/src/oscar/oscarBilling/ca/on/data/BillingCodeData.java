9a86e98f48de4b2c60a0b6f809cb7e755aa1b545
jaygallagher <jaygallagher>
2005-10-18 20:45:10 +0000

    Data classes used to access billingservice table

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/on/data/BillingCodeData.java
@@ -0,0 +1,196 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ * 
+ * Jason Gallagher
+ * 
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of BillingCodeData
+ *
+ *
+ * BillingCodeData.java
+ *
+ * Created on October 6, 2005, 10:33 AM
+ */
+
+package oscar.oscarBilling.ca.on.data;
+
+import java.sql.*;
+import java.util.*;
+import org.apache.commons.lang.*;
+import oscar.oscarDB.*;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class BillingCodeData {
+   
+   public List search(String str){
+      ArrayList list = new ArrayList();
+      String sql = "select * from billingservice where service_code like '"+str+"' or description like '%"+str+"%' ";
+      try {
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+         ResultSet rs = db.GetSQL(sql);
+         while (rs.next()){            
+             list.add(fillCodeDataHashtable(rs));
+         }
+         rs.close();
+	 db.CloseConn();
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+      }      
+      return list;
+   }
+   
+   public Hashtable fillCodeDataHashtable(ResultSet rs) throws SQLException{
+      Hashtable h = new Hashtable();
+       h.put("service_compositecode", c(rs.getString("service_compositecode")));
+       h.put("service_code", c(rs.getString("service_code")));
+       h.put("description", c(rs.getString("description")));
+       h.put("value", c(rs.getString("value")));
+       h.put("percentage", c(rs.getString("percentage")));
+       h.put("billingservice_date", c(rs.getString("billingservice_date")));
+       h.put("specialty", c(rs.getString("specialty")));
+       h.put("region", c(rs.getString("region")));
+       h.put("anaesthesia", c(rs.getString("anaesthesia")));             
+      return h;
+   }
+
+   String c(String str){
+      return ( str == null ) ? "" : str;    
+   }
+   
+   public Hashtable searchBillingCode(String str){
+      Hashtable h = null;
+      int count = 0;
+      String sql = "select * from billingservice where service_code like '"+str+"%' ";
+      try {
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+         ResultSet rs = db.GetSQL(sql);
+         while (rs.next()){
+             count++;
+             h = fillCodeDataHashtable(rs);
+         }
+         System.out.println(count+" for "+str);
+         if (h != null){               
+            h.put("count",""+count);
+         }
+         rs.close();
+	 db.CloseConn();
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+      }        
+      return h;
+   }
+   
+   
+   ///
+   public int searchNumBillingCode(String str){      
+      int count = 0;
+      String sql = "select count(*) as coun from billingservice where service_code like '"+str+"%' ";
+      try {
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+         ResultSet rs = db.GetSQL(sql);
+         while (rs.next()){
+             count = rs.getInt("coun");
+         }                  
+         rs.close();
+	 db.CloseConn();
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+      }        
+      return count;
+   }
+   
+   
+   public void lastLetterofEachBillingCode(){
+      
+      String sql = "select service_code from billingservice ";
+      try {
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+         ResultSet rs = db.GetSQL(sql);
+         while (rs.next()){
+            String service_code = rs.getString("service_code");
+            System.out.println(service_code.charAt(service_code.length()-1));
+         }
+         rs.close();
+	 db.CloseConn();
+      } catch (SQLException e) {
+         System.out.println(e.getMessage());
+      }            
+   }
+   
+   
+   public BillingCodeData() {
+   }
+   
+   public boolean editBillingCodeDesc(String desc, String val, String codeId){
+      boolean retval = true;
+      try{
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+
+         String str = "update billingservice set "+
+                      "description = '"+StringEscapeUtils.escapeSql(desc) +"', "+
+                      "value       = '"+StringEscapeUtils.escapeSql(val)  +"' "+
+                      "where billingservice_no = '"+StringEscapeUtils.escapeSql(codeId)+"'";
+         db.RunSQL(str);
+         db.CloseConn();
+      }catch(Exception e1){
+         e1.printStackTrace();
+      }
+      return retval;
+   }
+   
+  
+   public boolean editBillingCode(String val, String codeId){
+      boolean retval = true;
+      try{
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+
+         String str = "update billingservice set "+                      
+                      "value =                   '"+StringEscapeUtils.escapeSql(val)  +"' "+
+                      "where billingservice_no = '"+StringEscapeUtils.escapeSql(codeId)+"'";
+         System.out.println(str);
+         db.RunSQL(str);
+         System.out.println("NOW updated");
+         db.CloseConn();
+      }catch(Exception e1){
+         e1.printStackTrace();
+      }
+      return retval;
+   }
+   
+   
+   
+   public boolean editBillingCodeByServiceCode(String val, String codeId){
+      boolean retval = true;
+      try{
+         DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+
+         String str = "update billingservice set "+                      
+                      "value =                   '"+StringEscapeUtils.escapeSql(val)  +"' "+
+                      "where service_code = '"+StringEscapeUtils.escapeSql(codeId)+"'";
+         System.out.println(str);
+         db.RunSQL(str);
+         System.out.println("NOW updated");
+         db.CloseConn();
+      }catch(Exception e1){
+         e1.printStackTrace();
+      }
+      return retval;
+   }
+}
