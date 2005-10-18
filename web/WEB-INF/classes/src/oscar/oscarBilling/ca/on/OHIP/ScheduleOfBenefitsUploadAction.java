448bbc56856ae4169abf888ba56ad23998975ff2
jaygallagher <jaygallagher>
2005-10-18 20:42:48 +0000

    Action class to handle Upload of SoB

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/on/OHIP/ScheduleOfBenefitsUploadAction.java
@@ -0,0 +1,153 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada   Creates a new instance of LabUploadAction
+ *
+ *
+ * LabUploadAction.java
+ *
+ * Created on August 22, 2005, 10:20 PM
+ */
+
+package oscar.oscarBilling.ca.on.OHIP;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.http.*;
+import org.apache.log4j.*;
+import org.apache.struts.action.*;
+import org.apache.struts.upload.*;
+import oscar.*;
+import oscar.oscarDB.*;
+import oscar.oscarLab.*;
+import oscar.oscarLab.ca.bc.PathNet.*;
+import oscar.oscarLab.ca.bc.PathNet.HL7.*;
+import oscar.oscarLab.ca.bc.PathNet.pageUtil.*;
+import oscar.util.*;
+
+
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class ScheduleOfBenefitsUploadAction extends Action {
+   Logger _logger = Logger.getLogger(this.getClass());
+   
+   boolean checkBox (String str){
+      boolean check = false;
+      if ( str != null && str.equals("on")){
+         check = true;
+      }
+      return check;
+   }
+   
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)  {
+       LabUploadForm frm = (LabUploadForm) form; 
+       FormFile importFile = frm.getImportFile();
+       List warnings = new ArrayList();
+       String filename = "";
+       String proNo = (String) request.getSession().getAttribute("user");
+       String outcome = "";
+        
+       try{  
+          //System.out.println("Schedule Of Benefits content type = "+importFile.getContentType());
+          InputStream is = importFile.getInputStream();
+          filename = importFile.getFileName();
+          
+          ScheduleOfBenefits sob = new ScheduleOfBenefits();
+          String codeChanges  = request.getParameter("showChangedCodes");
+          String newCodes = request.getParameter("showNewCodes");            
+          
+          
+          boolean showNewCodes = checkBox(newCodes);
+          boolean showChangedCodes = checkBox(codeChanges);
+          
+          warnings = sob.processNewFeeSchedule(is,showNewCodes,showChangedCodes);                    
+          
+       }catch(Exception e){ 
+          e.printStackTrace(); 
+          outcome = "exception";
+       } 
+       System.out.println("warnings "+warnings.size());
+       request.setAttribute("warnings",warnings);
+       request.setAttribute("outcome", outcome);
+       return mapping.findForward("success");
+    }
+      
+    public ScheduleOfBenefitsUploadAction() {
+   }
+   
+   
+   /**
+     * 
+     * Save a Jakarta FormFile to a preconfigured place.
+     * 
+     * @param file
+     * @return
+     */
+    public static boolean saveFile(InputStream stream,String filename ){
+        String retVal = null;        
+        boolean isAdded = true;
+        
+        try {
+            //retrieve the file data
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            //InputStream stream = file.getInputStream();
+            OscarProperties props = OscarProperties.getInstance();
+
+            //properties must exist            
+            String place= props.getProperty("DOCUMENT_DIR");
+            
+            if(!place.endsWith("/"))
+                    place = new StringBuffer(place).insert(place.length(),"/").toString();
+            retVal = place+"LabUpload."+filename+"."+(new Date()).getTime();
+            System.out.println(retVal);
+            //write the file to the file specified
+            OutputStream bos = new FileOutputStream(retVal);
+            int bytesRead = 0;
+            //byte[] buffer = file.getFileData();
+            //while ((bytesRead = stream.read(buffer)) != -1){
+            //   bos.write(buffer, 0, bytesRead);            
+            while ((bytesRead = stream.read()) != -1){
+                    bos.write(bytesRead);
+            }
+            bos.close();
+
+            //close the stream
+            stream.close();
+        }
+        catch (FileNotFoundException fnfe) {
+            
+            System.out.println("File not found");
+            fnfe.printStackTrace();            
+            return isAdded=false;
+            
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+            return isAdded=false;
+        }
+
+        return isAdded;
+    }
+   
+}
