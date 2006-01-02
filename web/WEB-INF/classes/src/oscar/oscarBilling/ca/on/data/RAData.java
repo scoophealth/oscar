c4436ff947894320f92bf80c111032bb0f8fa400
jaygallagher <jaygallagher>
2006-01-02 01:46:08 +0000

    Class to deal with ontario remittance advice data

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/on/data/RAData.java
@@ -0,0 +1,105 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada   Creates a new instance of Prevention
+ *
+ * RAData.java
+ *
+ * Created on December 31, 2005, 12:12 PM
+ *
+ */
+
+package oscar.oscarBilling.ca.on.data;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import org.apache.commons.lang.StringEscapeUtils;
+import oscar.oscarDB.DBHandler;
+
+/**
+ *
+ * @author jay
+ */
+public class RAData {
+    
+    /** Creates a new instance of RAData */
+    public RAData() {
+    }
+   //select * from radetail limit 100,10;
+   //radetail_no | raheader_no | providerohip_no | billing_no | service_code | service_count | hin        | amountclaim | amountpay | service_date | error_code | billtype |
+     
+    
+    public ArrayList getRAData(String billingNo){
+        ArrayList list = new ArrayList();
+        String sql = "Select * from radetail where billing_no = '"+StringEscapeUtils.escapeSql(billingNo)+"'";
+        try {
+           DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+           ResultSet rs = db.GetSQL(sql);
+           while (rs.next()) {
+              Hashtable h = new Hashtable();
+              h.put("radetail_no", rs.getString("radetail_no"));
+              h.put("raheader_no", rs.getString("raheader_no"));
+              h.put("providerohip_no",rs.getString("providerohip_no"));
+              h.put("billing_no", rs.getString("billing_no"));
+              h.put("service_code", rs.getString("service_code"));
+              h.put("service_count", rs.getString("service_count"));
+              h.put("hin", rs.getString("hin"));
+              h.put("amountclaim", rs.getString("amountclaim"));
+              h.put("amountpay", rs.getString("amountpay"));
+              h.put("service_date", rs.getString("service_date"));
+              h.put("error_code", rs.getString("error_code"));
+              list.add(h);
+           }
+           rs.close();
+           db.CloseConn();
+        }catch (Exception e) {
+           e.printStackTrace();
+        }
+        return list;
+    }
+    
+    public String getErrorCodes(ArrayList a){
+        StringBuffer sb = new StringBuffer();
+        for (int i =0; i < a.size(); i++){
+            Hashtable h = (Hashtable) a.get(i);
+            sb.append(h.get("error_code"));
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+    
+    public String getAmountPaid(ArrayList a){
+        BigDecimal total = new BigDecimal(0).setScale(2, BigDecimal.ROUND_HALF_UP);
+        for (int i =0; i < a.size(); i++){
+           Hashtable h = (Hashtable) a.get(i);
+           BigDecimal valueToAdd = new BigDecimal("0.00");
+           try{
+              valueToAdd = new BigDecimal(""+h.get("amountpay")).setScale(2, BigDecimal.ROUND_HALF_UP);  
+           }catch(Exception badValueException){ 
+              System.out.println(" Error calculating value for "+h.get("billing_no")); 
+           }
+           total = total.add(valueToAdd);
+        }
+        return total.toString();
+    }
+    
+}
