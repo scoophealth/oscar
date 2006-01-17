2b9734baff8323abbdb9bf6cc710da3d3b96b705
jaygallagher <jaygallagher>
2006-01-17 22:37:00 +0000

    Fixed Imports Added logic to initialize Session Bean if it hasn't been activated yet. Now includeds deleted appt in search for next appt

--- web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgViewMessageByPositionAction.java
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgViewMessageByPositionAction.java
@@ -1,4 +1,25 @@
 /*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada  
+ *
  * MsgViewMessageByPositionAction.java
  *
  * Created on May 14, 2005, 4:38 PM
@@ -11,25 +32,17 @@ package oscar.oscarMessenger.pageUtil;
  * @author ichan
  */
 import oscar.oscarDB.DBHandler;
-import oscar.oscarMessenger.util.*;
 import oscar.util.*;
 
 import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Locale;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionError;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.ActionServlet;
-import org.apache.struts.util.MessageResources;
+import oscar.oscarProvider.data.ProviderData;
 
 public class MsgViewMessageByPositionAction extends Action {
 
@@ -40,6 +53,16 @@ public class MsgViewMessageByPositionAction extends Action {
 	throws IOException, ServletException {
         // System.out.println("in view message action jackson");
         // Extract attributes we will need
+        String provNo = (String) request.getSession().getAttribute("user");
+        
+        if ( request.getSession().getAttribute("msgSessionBean") == null){
+           MsgSessionBean bean = new MsgSessionBean();
+           bean.setProviderNo(provNo);
+           ProviderData pd = new ProviderData();
+           bean.setUserName(pd.getProviderName(provNo));
+           request.getSession().setAttribute("msgSessionBean", bean); 
+       } 
+        
         
         String orderBy = request.getParameter("orderBy")==null?"date":request.getParameter("orderBy");        
         String messagePosition = request.getParameter("messagePosition");           
@@ -53,7 +76,7 @@ public class MsgViewMessageByPositionAction extends Action {
            java.sql.ResultSet rs;
            
                 String sql = new String("select m.messageid from messagelisttbl ml, messagetbl m, msgDemoMap map"
-                +" where map.demographic_no = '"+ demographic_no+"' and status not like \'del\' and remoteLocation = '"+displayMsgBean.getCurrentLocationId()+"' "
+                +" where map.demographic_no = '"+ demographic_no+"' and remoteLocation = '"+displayMsgBean.getCurrentLocationId()+"' "
                 +" and m.messageid = map.messageID and ml.message=m.messageid order by "+ displayMsgBean.getOrderBy(orderBy) + " limit " + messagePosition +", 1");
                 System.out.println(sql);
                 rs = db.GetSQL(sql);
