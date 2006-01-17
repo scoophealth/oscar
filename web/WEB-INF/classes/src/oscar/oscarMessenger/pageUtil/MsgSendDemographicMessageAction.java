e645bacedf2a5777fb1a296fd9fe15f68c84519b
jaygallagher <jaygallagher>
2006-01-17 22:41:39 +0000

    Action class to directly send a message with the demographic attached

---
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgSendDemographicMessageAction.java
@@ -0,0 +1,67 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada  
+ *
+ * MsgSendDemographicMessageAction.java
+ *
+ * Created on January 15, 2006, 5:29 PM
+ *
+ */
+
+package oscar.oscarMessenger.pageUtil;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import oscar.oscarProvider.data.ProviderData;
+
+/**
+ *
+ * @author jay
+ */
+public class MsgSendDemographicMessageAction extends Action {
+
+    /**
+     * Creates a new instance of MsgSendDemographicMessageAction 
+     */
+    public MsgSendDemographicMessageAction() {
+    }
+
+    public ActionForward execute(ActionMapping mapping,ActionForm form,HttpServletRequest request,HttpServletResponse response) throws IOException, ServletException {
+       String provNo = (String) request.getSession().getAttribute("user");
+       
+       if ( request.getSession().getAttribute("msgSessionBean") == null){
+           MsgSessionBean bean = new MsgSessionBean();
+           bean.setProviderNo(provNo);
+           ProviderData pd = new ProviderData();
+           bean.setUserName(pd.getProviderName(provNo));
+           request.getSession().setAttribute("msgSessionBean", bean); 
+       } 
+        
+       request.setAttribute("demographic_no", request.getParameter("demographic_no"));
+       return mapping.findForward("success");   
+    }
+}
