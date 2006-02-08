f8b33604768a66b5fd46218023daef8e7f3968f1
jlegris <jlegris>
2006-02-08 02:34:33 +0000

    Added getRawTotal method

--- web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/data/PayRefSummary.java
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/data/PayRefSummary.java
@@ -1,3 +1,27 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * <OSCAR TEAM>
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster Unviersity
+ * Hamilton
+ * Ontario, Canada
+ */
+
 package oscar.oscarBilling.ca.bc.data;
 
 import java.text.NumberFormat;
@@ -100,7 +124,12 @@ public class PayRefSummary {
   }
 
   public String getTotal() {
-    return NumberFormat.getCurrencyInstance().format(cash+cheque+visa+mc+amex+electronic+debit+other);
+    return NumberFormat.getCurrencyInstance().format(getRawTotal());
   }
+
+  public double getRawTotal() {
+    return cash+cheque+visa+mc+amex+electronic+debit+other;
+  }
+
 
 }
