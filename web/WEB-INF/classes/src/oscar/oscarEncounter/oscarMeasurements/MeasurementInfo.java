942c0a27cfcd1bb3cddc412678291a3278b44d19
jaygallagher <jaygallagher>
2006-02-14 07:27:44 +0000

    Class used to encapsulate patient data for a measurement flowsheet.

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/MeasurementInfo.java
@@ -0,0 +1,167 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada   
+ *
+ * MeasurementInfo.java
+ *
+ * Created on January 29, 2006, 10:12 PM
+ *
+ */
+
+package oscar.oscarEncounter.oscarMeasurements;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.Vector;
+import oscar.oscarEncounter.oscarMeasurements.bean.EctMeasurementsDataBeanHandler;
+
+/**
+ *
+ * @author jay
+ */
+public class MeasurementInfo {
+    ArrayList warning = null;
+    Hashtable warningHash = new Hashtable();
+    ArrayList recommendations = null;
+    Hashtable recommendationHash = new Hashtable();
+    
+    ArrayList measurementList = new ArrayList();
+    Hashtable measurementHash = new Hashtable();
+    String demographicNo = "";
+    /** Creates a new instance of MeasurementInfo */
+    public MeasurementInfo(String demographic) {
+        demographicNo = demographic;
+    }
+    
+    public ArrayList getWarnings(){
+        if (warning == null){
+           warning = new ArrayList();    
+        }
+        return warning;
+    }
+    
+    public ArrayList getRecommendations(){
+        if (recommendations == null){
+           recommendations = new ArrayList();    
+        }
+        return recommendations;
+    }
+    
+    public void setDemographic(String demographic){
+        demographicNo = demographic;
+    }
+    
+    /** Creates a new instance of MeasurementInfo */
+    public MeasurementInfo() {
+    }
+    
+    
+    public void getMeasurements(ArrayList list){
+        for (int i =0; i < list.size(); i++){
+           String measurement = (String) list.get(i);
+           EctMeasurementsDataBeanHandler ect = new EctMeasurementsDataBeanHandler(demographicNo, measurement);
+           Collection v = ect.getMeasurementsDataVector();
+           measurementList.add(new ArrayList(v));
+           measurementHash.put(measurement,new ArrayList(v));
+        }
+        
+    }
+    
+    public ArrayList getMeasurementData(String measurement){
+        return (ArrayList) measurementHash.get(measurement);
+    }
+    
+    public void addRecommendation(String measurement,String recommendationMessage){
+        if (recommendations == null){
+           recommendations = new ArrayList();    
+        }
+        recommendationHash.put(measurement,recommendationMessage);
+        recommendations.add(recommendationMessage);
+    }
+    
+    public void addWarning(String measurement,String warningMessage){
+        if (warning == null){
+           warning = new ArrayList();    
+        }
+        warningHash.put(measurement,warningMessage);
+        warning.add(warningMessage);
+    }
+    
+    
+    public boolean hasWarning(String measurement){
+        boolean warn = false;
+        if (warningHash.get(measurement) != null){
+            warn = true;
+        }
+        return warn;
+    }
+    
+    public boolean hasRecommendation(String measurement){
+        boolean warn = false;
+        if (recommendationHash.get(measurement) != null){
+            warn = true;
+        }
+        return warn;
+    }
+    
+    public String getRecommendation(String measurement){
+        return (String) recommendationHash.get(measurement);
+    }
+    
+    public String getWarning(String measurement){
+        return (String) warningHash.get(measurement);
+    }
+    
+    public int getLastDateRecordedInMonths(String measurement){
+        
+        int numMonths = -1;
+        ArrayList list = getMeasurementData(measurement);       
+        Hashtable h =  null;
+        if ( list != null && list.size() > 0){
+            h = (Hashtable) list.get(0);
+        }
+        
+        EctMeasurementsDataBeanHandler.getLast(demographicNo, measurement);
+        if (h != null ){
+           Date date = (Date) h.get("dateObserved_date");
+           numMonths = getNumMonths(date, Calendar.getInstance().getTime());
+        }
+        System.out.println("Returning the number of months "+numMonths);
+        return numMonths;
+    }
+    
+    private int getNumMonths(Date dStart, Date dEnd) {
+        int i = 0;
+        System.out.println("Getting the number of months between "+dStart.toString()+ " and "+dEnd.toString() );        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(dStart);
+        while (calendar.getTime().before(dEnd) || calendar.getTime().equals(dEnd)) {
+            calendar.add(Calendar.MONTH, 1);
+            i++;
+        }
+        i--;
+        if (i < 0) { i = 0; }
+        return i;
+   }
+}
