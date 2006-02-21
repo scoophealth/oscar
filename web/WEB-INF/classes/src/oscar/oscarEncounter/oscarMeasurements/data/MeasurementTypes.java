e0df4130c17499340ecb395b73fc410363de780a
jaygallagher <jaygallagher>
2006-02-21 08:04:50 +0000

    Class to Cache Measurement Type Data

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/data/MeasurementTypes.java
@@ -0,0 +1,148 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada   Creates a new instance of Prevention
+ *
+ * MeasurementTypes.java
+ *
+ * Created on February 15, 2006, 9:43 PM
+ *
+ */
+
+package oscar.oscarEncounter.oscarMeasurements.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Hashtable;
+import oscar.oscarDB.DBHandler;
+import oscar.oscarEncounter.oscarMeasurements.bean.EctMeasurementTypesBean;
+
+/**
+ *
+ * @author Jay Gallagher
+ */
+public class MeasurementTypes {
+    
+    static MeasurementTypes measurementTypes = new MeasurementTypes();
+    boolean loaded = false;
+    Hashtable byId = null;
+    Hashtable byType = null;
+    
+    /** Creates a new instance of MeasurementTypes */
+    private MeasurementTypes() {
+    }
+   
+    public EctMeasurementTypesBean getByType(String type){
+       while(loaded == false){         
+          try {
+            wait();
+          } catch (InterruptedException e) { }
+       }
+       return (EctMeasurementTypesBean) byType.get(type);     
+    }
+    
+    public EctMeasurementTypesBean getById(String type){
+       while(loaded == false){         
+          try {
+            wait();
+          } catch (InterruptedException e) { }
+       }
+       return (EctMeasurementTypesBean) byId.get(type);     
+    }
+    
+    static public MeasurementTypes getInstance(){
+       if (!measurementTypes.loaded) {
+         measurementTypes.loadMeasurementTypes();
+       }
+       return measurementTypes;
+    }
+
+    public void reInit(){
+        System.out.println("Reloading measurement types");
+        loaded = false;
+        measurementTypes.loadMeasurementTypes();
+    }
+    
+    private synchronized void loadMeasurementTypes() {
+        System.out.println("LOADING GETS CALLED");
+        if (!loaded){
+            System.out.println("LOADING RUNS");
+            byId = new Hashtable();
+            byType = new Hashtable();
+
+            try {
+               DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+               String sql = "SELECT * FROM measurementType";   
+
+               ResultSet rs = db.GetSQL(sql);        
+               while(rs.next()){                
+                  //System.out.println("validation "+rs.getString("validation"));  
+                  EctMeasurementTypesBean ret = null;
+                  ret = new EctMeasurementTypesBean(rs.getInt("id"), rs.getString("type"), 
+                                                     rs.getString("typeDisplayName"), 
+                                                     rs.getString("typeDescription"), 
+                                                     rs.getString("measuringInstruction"),  
+                                                     rs.getString("validation")); 
+                  ret.setValidationName(getValidation(rs.getString("validation")));
+                  byId.put(""+rs.getInt("id"),ret);
+                  byType.put(rs.getString("type"),ret);
+
+
+                }
+                rs.close();            
+                db.CloseConn();
+                loaded = true;
+                notifyAll();
+            }
+            catch(SQLException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+            //return ret;
+    }
+    
+    private String getValidation(String val){
+        String validation = null;
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sqlValidation = "SELECT name FROM validations WHERE id='"+val+"'";
+            ResultSet rs = db.GetSQL(sqlValidation);
+            if (rs.next()){ 
+                validation = rs.getString("name");
+                //System.out.println("setting validation to "+validation);
+            }
+            rs.close();            
+            db.CloseConn();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
+        return validation;
+    }
+        
+        
+        
+        
+  
+    
+    
+    
+  
+    
+}
