47b4ad2a6ac00b5234e9ef7bc25eb6b58ff51ced
jlegris <jlegris>
2006-03-22 06:48:20 +0000

    Added code parameter to editCode method

--- web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingEditCodeAction.java
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingEditCodeAction.java
@@ -1,25 +1,25 @@
 /*
- * 
+ *
  * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
- * This software is published under the GPL GNU General Public License. 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation; either version 2 
- * of the License, or (at your option) any later version. * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
- * 
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
  * <OSCAR TEAM>
- * 
- * This software was written for the 
- * Department of Family Medicine 
- * McMaster Unviersity 
- * Hamilton 
- * Ontario, Canada 
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster Unviersity
+ * Hamilton
+ * Ontario, Canada
  */
 package oscar.oscarBilling.ca.bc.pageUtil;
 
@@ -42,43 +42,43 @@ import oscar.oscarBilling.ca.bc.data.*;
 import oscar.oscarDB.*;
 
 public final class BillingEditCodeAction extends Action {
-    
+
     public ActionForward execute(ActionMapping mapping,
     ActionForm form,
     HttpServletRequest request,
     HttpServletResponse response)
     throws IOException, ServletException {
-        
+
         Locale locale = getLocale(request);
-        
+
         if(request.getSession().getAttribute("user") == null  ){
             return (mapping.findForward("Logout"));
         }
-        
+
         BillingEditCodeForm frm = (BillingEditCodeForm) form;
-                
-        String codeId  =frm.getCodeId(); 
+
+        String codeId  =frm.getCodeId();
         String code    =frm.getCode();
-        String desc    =frm.getDesc(); 
+        String desc    =frm.getDesc();
         String value   =frm.getValue();
         String whereTo =frm.getWhereTo();
         String submitButton = frm.getSubmitButton();
-                        
+
         System.out.println(submitButton);
         if (submitButton.equals("Edit")){
            System.out.println("here with codeid "+codeId);
           BillingCodeData bcd = new BillingCodeData();
-          bcd.editBillingCode(desc, value,codeId);
+          bcd.editBillingCode(code,desc, value,codeId);
         }
-                 
+
         ActionForward retval;
         if(whereTo == null || whereTo.equals("")){
            retval = mapping.findForward("success");
         }else{
            retval = mapping.findForward("private");
         }
-                   
+
         return retval;
     }
-    
+
 }
