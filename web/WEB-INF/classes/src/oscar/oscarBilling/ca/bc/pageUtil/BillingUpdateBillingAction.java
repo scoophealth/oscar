f6878d10d5073e7cb5426e815fc6cc809a01a4b0
jlegris <jlegris>
2006-03-22 06:51:00 +0000

    Added functionality for receiving payments and updating the method of payment for a bill

--- web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingUpdateBillingAction.java
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/BillingUpdateBillingAction.java
@@ -2,24 +2,24 @@
 // *
 // *
 // * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
-// * This software is published under the GPL GNU General Public License. 
-// * This program is free software; you can redistribute it and/or 
-// * modify it under the terms of the GNU General Public License 
-// * as published by the Free Software Foundation; either version 2 
-// * of the License, or (at your option) any later version. * 
-// * This program is distributed in the hope that it will be useful, 
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
-// * along with this program; if not, write to the Free Software 
-// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
-// * 
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
 // * <OSCAR TEAM>
-// * This software was written for the 
-// * Department of Family Medicine 
-// * McMaster University 
-// * Hamilton 
-// * Ontario, Canada 
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster University
+// * Hamilton
+// * Ontario, Canada
 // *
 // -----------------------------------------------------------------------------------------------------------------------
 
@@ -37,41 +37,55 @@ import javax.servlet.http.*;
 import org.apache.struts.action.*;
 import oscar.oscarBilling.ca.bc.MSP.*;
 import oscar.oscarBilling.ca.bc.data.*;
+import oscar.util.SqlUtils;
 
 /**
  *
  * @author  root
  */
-public final class BillingUpdateBillingAction extends Action {
-    
-    public ActionForward execute(ActionMapping mapping,
-    ActionForm form,
-    HttpServletRequest request,
-    HttpServletResponse response)
-    throws IOException, ServletException {
-       
-       String creator = (String) request.getSession().getAttribute("user");
-       
-       BillingUpdateBillingForm frm = (BillingUpdateBillingForm) form;              
-       
-       String messageNotes = frm.getMessageNotes();
-       String status       = frm.getStatus();
-       String billingId    = frm.getBillingNo();
-       
-       MSPReconcile msprec = new MSPReconcile();
-       msprec.updateBillingStatus(billingId,status);
-              
-       BillingNote n = new BillingNote();
-       try{
-          n.addNoteFromBillingNo(billingId,creator,messageNotes);
-       }catch(Exception e){          
-          e.printStackTrace();
-       }          
-       return mapping.findForward("success");       
+public final class BillingUpdateBillingAction
+    extends Action {
+  HttpServletRequest request;
+  public ActionForward execute(ActionMapping mapping,
+                               ActionForm form,
+                               HttpServletRequest request,
+                               HttpServletResponse response) throws IOException,
+      ServletException {
+    BillingViewForm frm = (BillingViewForm)form;
+    String creator = (String) request.getSession().getAttribute("user");
+
+    BillRecipient recip = new BillRecipient();
+    recip.setName(frm.getRecipientName());
+    recip.setAddress(frm.getRecipientAddress());
+    recip.setCity(frm.getRecipientCity());
+    recip.setProvince(frm.getRecipientProvince());
+    recip.setPostal(frm.getRecipientPostal());
+    MSPReconcile msprec = new MSPReconcile();
+    msprec.updateBillingStatus(frm.getBillingNo(), frm.getBillStatus());
+    msprec.saveOrUpdateBillRecipient(recip);
+    msprec.updatePaymentMethod(frm.getBillingNo(),frm.getPaymentMethod());
+    BillingNote n = new BillingNote();
+    try {
+      n.addNoteFromBillingNo(frm.getBillingNo(), creator, frm.getMessageNotes());
     }
-   
-   /** Creates a new instance of BillingUpdateBillingAction */
-   public BillingUpdateBillingAction() {
-   }
-   
+    catch (Exception e) {
+      e.printStackTrace();
+    }
+    String amountReceived = frm.getAmountReceived();
+    if(amountReceived!=null&&!"".equals(amountReceived)){
+      Double dblAmt = new Double(amountReceived);
+      this.receivePayment(frm.getBillingNo(), dblAmt);
+    }
+    return mapping.findForward("success");
+  }
+
+  /** Creates a new instance of BillingUpdateBillingAction */
+  public BillingUpdateBillingAction() {
+  }
+
+  public void receivePayment(String billingMasterNo, Double amount) {
+    PrivateBillTransactionsDAO dao = new PrivateBillTransactionsDAO();
+    dao.savePrivateBillTransaction(billingMasterNo, amount);
+  }
+
 }
