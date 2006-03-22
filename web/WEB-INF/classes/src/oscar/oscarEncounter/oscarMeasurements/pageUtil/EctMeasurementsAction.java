507a1e951363d062f8d22075376c2fac27eb5cfa
jlegris <jlegris>
2006-03-22 08:20:26 +0000

    changed parameter from char to string in reference to StringUtils.rightPad method(wouldn't build)

--- web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/pageUtil/EctMeasurementsAction.java
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/pageUtil/EctMeasurementsAction.java
@@ -2,24 +2,24 @@
 // *
 // *
 // * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
-// * This software is published under the GPL GNU General Public License. 
-// * This program is free software; you can redistribute it and/or 
-// * modify it under the terms of the GNU General Public License 
-// * as published by the Free Software Foundation; either version 2 
-// * of the License, or (at your option) any later version. * 
-// * This program is distributed in the hope that it will be useful, 
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
-// * along with this program; if not, write to the Free Software 
-// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
-// * 
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
 // * <OSCAR TEAM>
-// * This software was written for the 
-// * Department of Family Medicine 
-// * McMaster Unviersity 
-// * Hamilton 
-// * Ontario, Canada 
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster Unviersity
+// * Hamilton
+// * Ontario, Canada
 // *
 // -----------------------------------------------------------------------------------------------------------------------
 package oscar.oscarEncounter.oscarMeasurements.pageUtil;
@@ -50,13 +50,13 @@ public class EctMeasurementsAction extends Action {
         throws ServletException, IOException
     {
         EctMeasurementsForm frm = (EctMeasurementsForm) form;
-        
+
         HttpSession session = request.getSession();
         request.getSession().setAttribute("EctMeasurementsForm", frm);
-        
+
         EctSessionBean bean = (EctSessionBean)request.getSession().getAttribute("EctSessionBean");
         request.getSession().setAttribute("EctSessionBean", bean);
-        
+
         java.util.Calendar calender = java.util.Calendar.getInstance();
         String day =  Integer.toString(calender.get(java.util.Calendar.DAY_OF_MONTH));
         String month =  Integer.toString(calender.get(java.util.Calendar.MONTH)+1);
@@ -65,34 +65,34 @@ public class EctMeasurementsAction extends Action {
         String min = Integer.toString(calender.get(java.util.Calendar.MINUTE));
         String second = Integer.toString(calender.get(java.util.Calendar.SECOND));
         String dateEntered = year+"-"+month+"-"+day+" " + hour + ":" + min + ":" + second + ":";
-        
-        String numType = (String) frm.getValue("numType");        
+
+        String numType = (String) frm.getValue("numType");
         int iType = Integer.parseInt(numType);
-         
+
         String demographicNo = null;
         String providerNo = (String) session.getAttribute("user");
         if ( bean != null)
-            demographicNo = bean.getDemographicNo();        
-        
-        MsgStringQuote str = new MsgStringQuote();       
-        
+            demographicNo = bean.getDemographicNo();
+
+        MsgStringQuote str = new MsgStringQuote();
+
         List messages = new LinkedList();
-        
+
         Properties p = (Properties) session.getAttribute("providerBean");
         String by = "";
         if (p != null ){
            by = p.getProperty(providerNo,"");
         }
-        
-        String textOnEncounter = "**"+StringUtils.rightPad(by,80,'*')+"\\n";
-         
+
+        String textOnEncounter = "**"+StringUtils.rightPad(by,80,"*")+"\\n";
+
         boolean valid = true;
         try
             {
                 DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
-                EctValidation ectValidation = new EctValidation();                    
-                ActionErrors errors = new ActionErrors();   
-                
+                EctValidation ectValidation = new EctValidation();
+                ActionErrors errors = new ActionErrors();
+
                 String inputValueName, inputTypeName, inputTypeDisplayName, mInstrcName, commentsName;
                 String dateName,validationName, inputValue, inputType, inputTypeDisplay, mInstrc;
                 String comments, dateObserved, validation;
@@ -102,7 +102,7 @@ public class EctMeasurementsAction extends Action {
                 double dMin = 0;
                 int iMax = 0;
                 int iMin = 0;
-                
+
                 ResultSet rs;
                 String regCharExp;
                 //goes through each type to check if the input value is valid
@@ -119,54 +119,54 @@ public class EctMeasurementsAction extends Action {
                     mInstrc = (String) frm.getValue(mInstrcName);
                     comments = (String) frm.getValue(commentsName);
                     dateObserved = (String) frm.getValue(dateName);
-                    
+
                     msg = null;
                     regExp = null;
                     dMax = 0;
                     dMin = 0;
                     iMax = 0;
                     iMin = 0;
-                                                       
+
                     rs = ectValidation.getValidationType(inputType, mInstrc);
                     regCharExp = ectValidation.getRegCharacterExp();
-                    
+
                     if (rs.next()){
                         dMax = rs.getDouble("maxValue");
                         dMin = rs.getDouble("minValue");
                         iMax = rs.getInt("maxLength");
                         iMin = rs.getInt("minLength");
                         regExp = rs.getString("regularExp");
-                    }     
+                    }
                     rs.close();
-	
-                    if(!ectValidation.isInRange(dMax, dMin, inputValue)){                       
+
+                    if(!ectValidation.isInRange(dMax, dMin, inputValue)){
                         errors.add(inputValueName, new ActionError("errors.range", inputTypeDisplay, Double.toString(dMin), Double.toString(dMax)));
                         saveErrors(request, errors);
                         valid = false;
                     }
-                    if(!ectValidation.maxLength(iMax, inputValue)){                       
+                    if(!ectValidation.maxLength(iMax, inputValue)){
                         errors.add(inputValueName, new ActionError("errors.maxlength", inputTypeDisplay, Integer.toString(iMax)));
                         saveErrors(request, errors);
                         valid = false;
                     }
-                    if(!ectValidation.minLength(iMin, inputValue)){                       
+                    if(!ectValidation.minLength(iMin, inputValue)){
                         errors.add(inputValueName, new ActionError("errors.minlength", inputTypeDisplay, Integer.toString(iMin)));
                         saveErrors(request, errors);
                         valid = false;
                     }
-                    if(!ectValidation.matchRegExp(regExp, inputValue)){                        
+                    if(!ectValidation.matchRegExp(regExp, inputValue)){
                         errors.add(inputValueName,
                         new ActionError("errors.invalid", inputTypeDisplay));
                         saveErrors(request, errors);
                         valid = false;
                     }
-                    if(!ectValidation.isValidBloodPressure(regExp, inputValue)){                        
+                    if(!ectValidation.isValidBloodPressure(regExp, inputValue)){
                         errors.add(inputValueName,
                         new ActionError("error.bloodPressure"));
                         saveErrors(request, errors);
                         valid = false;
                     }
-                    if(!ectValidation.isDate(dateObserved)&&inputValue.compareTo("")!=0){                        
+                    if(!ectValidation.isDate(dateObserved)&&inputValue.compareTo("")!=0){
                         errors.add(dateName,
                         new ActionError("errors.invalidDate", inputTypeDisplay));
                         saveErrors(request, errors);
@@ -183,7 +183,7 @@ public class EctMeasurementsAction extends Action {
                         mInstrcName = "inputMInstrc-" + i;
                         commentsName = "comments-" + i;
                         validationName = "validation-" + i;
-                        dateName = "date-" + i;                       
+                        dateName = "date-" + i;
 
                         inputValue = (String) frm.getValue(inputValueName);
                         inputType = (String) frm.getValue(inputTypeName);
@@ -191,8 +191,8 @@ public class EctMeasurementsAction extends Action {
                         comments = (String) frm.getValue(commentsName);
                         comments = org.apache.commons.lang.StringEscapeUtils.escapeSql(comments);
                         validation = (String) frm.getValue(validationName);
-                        dateObserved = (String) frm.getValue(dateName);                        
-                        
+                        dateObserved = (String) frm.getValue(dateName);
+
                         org.apache.commons.validator.GenericValidator gValidator = new org.apache.commons.validator.GenericValidator();
                         if(!gValidator.isBlankOrNull(inputValue)){
                             //Find if the same data has already been entered into the system
@@ -202,55 +202,55 @@ public class EctMeasurementsAction extends Action {
                             //System.out.println(sql);
                             rs = db.GetSQL(sql);
                             if(!rs.next()){
-                                //Write to the Dababase if all input values are valid                        
+                                //Write to the Dababase if all input values are valid
                                 sql = "INSERT INTO measurements"
                                         +"(type, demographicNo, providerNo, dataField, measuringInstruction, comments, dateObserved, dateEntered)"
                                         +" VALUES ('"+str.q(inputType)+"','"+str.q(demographicNo)+"','"+str.q(providerNo)+"','"+str.q(inputValue)+"','"
-                                        + str.q(mInstrc)+"','"+str.q(comments)+"','"+str.q(dateObserved)+"','"+str.q(dateEntered)+"')";                           
+                                        + str.q(mInstrc)+"','"+str.q(comments)+"','"+str.q(dateObserved)+"','"+str.q(dateEntered)+"')";
                                 //System.out.println(sql);
                                 db.RunSQL(sql);
                                 //prepare input values for writing to the encounter form
                                 textOnEncounter =  textOnEncounter + inputType + "    " + inputValue + " " + mInstrc + " " + comments + "\\n";
                             }
-                            rs.close();                            
+                            rs.close();
                         }
-                                            
+
                     }
                     textOnEncounter = textOnEncounter + "**********************************************************************************\\n";
                     System.out.println(textOnEncounter);
                 }
-                else{                                        
+                else{
                     String groupName = (String) frm.getValue("groupName");
                     String css = (String) frm.getValue("css");
                     request.setAttribute("groupName", groupName);
                     request.setAttribute("css", css);
-                    return (new ActionForward(mapping.getInput()));                    
+                    return (new ActionForward(mapping.getInput()));
                 }
                 /* select the correct db specific command */
                 String db_type = OscarProperties.getInstance().getProperty("db_type").trim();
                 String dbSpecificCommand;
                 if (db_type.equalsIgnoreCase("mysql")) {
                     dbSpecificCommand = "SELECT LAST_INSERT_ID()";
-                } 
+                }
                 else if (db_type.equalsIgnoreCase("postgresql")){
                     dbSpecificCommand = "SELECT CURRVAL('consultationrequests_numeric')";
                 }
                 else
                     throw new SQLException("ERROR: Database " + db_type + " unrecognized.");
-                                    
-                db.CloseConn();  
+
+                db.CloseConn();
             }
             catch(SQLException e)
             {
                 System.out.println(e.getMessage());
             }
-            
-        
+
+
         //put the inputvalue to the encounter form
-        session.setAttribute( "textOnEncounter", textOnEncounter );  
-        
+        session.setAttribute( "textOnEncounter", textOnEncounter );
+
         return mapping.findForward("success");
     }
 
-     
+
 }
