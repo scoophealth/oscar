14b1a09a1f5fee8479e84069e20f56b0b9753e31
jlegris <jlegris>
2006-03-28 12:32:01 +0000

    changed url for encounterForm WCB entry

--- database/mysql/oscardata_bc.sql
+++ database/mysql/oscardata_bc.sql
@@ -11,8 +11,8 @@
 INSERT INTO encounterForm VALUES ('BC-AR','../form/formbcar.jsp?demographic_no=','formBCAR', '1');
 INSERT INTO encounterForm VALUES ('BC-LabourBirth','../form/formbcbirthsummo.jsp?demographic_no=','formBCBirthSumMo', '1');
 INSERT INTO encounterForm VALUES ('BC-NewBorn','../form/formbcnewborn.jsp?demographic_no=','formBCNewBorn', '1');
-INSERT INTO encounterForm values ('BC-WCB','../form/formbcwcb.jsp?demographic_no=','wcb','0');
-
+INSERT INTO encounterForm values ('BC-WCB','../billing/CA/BC/viewformwcb.do?demographic_no=','wcb','0');
+                                        
 
 INSERT INTO billinglocation VALUES ('00','ABBOTSFORD','BC');
 INSERT INTO billinglocation VALUES ('00','AGASSIZ/HARRISON','BC');
