bea7cec066911678fa9457bbc84387438698fafb
jlegris <jlegris>
2006-07-21 18:46:51 +0000

    fixed alter table statement

--- database/mysql/oscarinit_bc.sql
+++ database/mysql/oscarinit_bc.sql
@@ -2324,7 +2324,7 @@ CREATE TABLE billing_msp_servicecode_times (
   PRIMARY KEY  (id)
 ) TYPE=MyISAM;
 
-ALTER `billing_msp_servicecode_times` MODIFY COLUMN `billingservice_no` VARCHAR(10) NOT NULL DEFAULT '0';
+ALTER table `billing_msp_servicecode_times` MODIFY COLUMN `billingservice_no` VARCHAR(10) NOT NULL DEFAULT '0';
 
 #Stores data about the types of bills that are used in OSCAR BC billing module
 CREATE TABLE billingtypes (
