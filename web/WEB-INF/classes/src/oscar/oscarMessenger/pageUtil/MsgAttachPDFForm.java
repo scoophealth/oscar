bcaf18da53f9067cc0615578ab6e0c44452e257f
rjonasz <rjonasz>
2006-09-25 18:24:04 +0000

    Removed import to ActionError

--- web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgAttachPDFForm.java
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgAttachPDFForm.java
@@ -2,30 +2,29 @@
 // *
 // *
 // * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
-// * This software is published under the GPL GNU General Public License. 
-// * This program is free software; you can redistribute it and/or 
-// * modify it under the terms of the GNU General Public License 
-// * as published by the Free Software Foundation; either version 2 
-// * of the License, or (at your option) any later version. * 
-// * This program is distributed in the hope that it will be useful, 
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
-// * along with this program; if not, write to the Free Software 
-// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
-// * 
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
 // * <OSCAR TEAM>
-// * This software was written for the 
-// * Department of Family Medicine 
-// * McMaster Unviersity 
-// * Hamilton 
-// * Ontario, Canada 
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster Unviersity
+// * Hamilton
+// * Ontario, Canada
 // *
 // -----------------------------------------------------------------------------------------------------------------------
 package oscar.oscarMessenger.pageUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
@@ -44,7 +43,7 @@ public final class MsgAttachPDFForm extends ActionForm {
     String[] titleArray;
     String[] indexArray;
     String demographic_no;
-   
+    
     public String getSrcText(){
         return srcText;
     }
@@ -53,8 +52,8 @@ public final class MsgAttachPDFForm extends ActionForm {
         this.srcText = srcText;
     }
     
-
-   public boolean getIsPreview(){
+    
+    public boolean getIsPreview(){
         return isPreview;
     }
     
@@ -68,7 +67,7 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public void setUriArray(String[] uriArray ){
         this.uriArray = uriArray;
-    }    
+    }
     
     public String[] getTitleArray(){
         return titleArray;
@@ -76,7 +75,7 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public void setTitleArray(String[] titleArray ){
         this.titleArray = titleArray;
-    }    
+    }
     
     public String[] getIndexArray(){
         return indexArray;
@@ -84,12 +83,12 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public void setIndexArray(String[] indexArray ){
         this.indexArray = indexArray;
-    }    
-        
+    }
+    
     
     public void setIndexArray(String value, int i ){
         this.indexArray[i] = value;
-    }    
+    }
     
     public void setIsAttaching(boolean isAttaching ){
         this.isAttaching =  isAttaching;
@@ -97,15 +96,15 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public boolean getIsAttaching(){
         return isAttaching;
-    } 
-           
+    }
+    
     public void setIsNew(boolean isNew ){
         this.isNew =  isNew;
     }
     
     public boolean getIsNew(){
         return isNew;
-    } 
+    }
     
     public void setAttachmentCount(String attachmentCount ){
         this.attachmentCount =  attachmentCount;
@@ -113,7 +112,7 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public String getAttachmentCount(){
         return attachmentCount;
-    } 
+    }
     
     public void setAttachmentTitle(String attachmentTitle ){
         this.attachmentTitle =  attachmentTitle;
@@ -121,45 +120,45 @@ public final class MsgAttachPDFForm extends ActionForm {
     
     public String getAttachmentTitle(){
         return attachmentTitle;
-    } 
-        
+    }
+    
     public void setDemographic_no(String demographic_no ){
         this.demographic_no =  demographic_no;
     }
     
     public String getDemographic_no(){
         return demographic_no;
-    } 
-
+    }
+    
     
     /*
     public void setAttachment(String attachment ){
         this.attachment =  attachment;
     }
-    
+     
     public String getAttachment(){
         return attachment;
     }
-
+     
     public void setFile_id(String file_id ){
         this.file_id =  file_id;
     }
-    
+     
     public String getFile_id (){
         return file_id;
-    }       
-    
-
-    
+    }
+     
+     
+     
         public String getJsessionid(){
         return jsessionid;
     }
-    
+     
     public void setJsessionid(String jsessionid ){
         this.jsessionid = jsessionid;
     }
      */
-
+    
 }
 
 
--- web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgCreateMessageForm.java
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgCreateMessageForm.java
@@ -25,7 +25,6 @@
 package oscar.oscarMessenger.pageUtil;
 
 import javax.servlet.http.HttpServletRequest;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionMapping;
