85b6e72d808bcf4a84939358bab40697c6b60d46
rjonasz <rjonasz>
2006-09-26 19:41:36 +0000

    Removed ActionError import and ActionErrors errors  as it is not being used

--- web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgDisplayMessagesAction.java
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/pageUtil/MsgDisplayMessagesAction.java
@@ -36,7 +36,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -56,8 +55,7 @@ public class MsgDisplayMessagesAction extends Action {
             Locale locale = getLocale(request);
             MessageResources messages = getResources(request);
 
-            // Setup variables
-            ActionErrors errors = new ActionErrors();
+            // Setup variables            
             oscar.oscarMessenger.pageUtil.MsgSessionBean bean = null;
             String[] messageNo = ((MsgDisplayMessagesForm)form).getMessageNo();
             String providerNo;
