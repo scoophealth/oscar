9271512f401c9f1fd174558bc1ad3cd5fdc7718a
rjonasz <rjonasz>
2006-09-28 17:11:26 +0000

    Provides interface to ProviderFaxUpdater class

---
+++ web/WEB-INF/classes/src/oscar/oscarProvider/pageUtil/ProEditFaxNumAction.java
@@ -0,0 +1,54 @@
+/*
+ * 
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License. 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+ * 
+ * <OSCAR TEAM>
+ * 
+ * This software was written for the 
+ * Department of Family Medicine 
+ * McMaster University 
+ * Hamilton 
+ * Ontario, Canada 
+ */
+package oscar.oscarProvider.pageUtil;
+
+import javax.servlet.http.*;
+import org.apache.struts.action.*;
+import oscar.oscarProvider.data.ProviderFaxUpdater;
+
+
+public class ProEditFaxNumAction extends Action {
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
+        throws Exception
+    {
+        String forward;
+        String providerNo = (String) request.getSession().getAttribute("user");
+        if ( providerNo == null)
+              return mapping.findForward("eject");
+
+        DynaActionForm frm = (DynaActionForm)form;
+                
+        ProviderFaxUpdater faxUpdater = new ProviderFaxUpdater(providerNo);
+        if( faxUpdater.setFax((String)frm.get("faxNumber"))) {
+            request.setAttribute("status",new String("complete"));
+            forward = new String("success");
+        }
+        else
+            forward = new String("error");
+        
+        return mapping.findForward(forward);
+        
+    }
+}
