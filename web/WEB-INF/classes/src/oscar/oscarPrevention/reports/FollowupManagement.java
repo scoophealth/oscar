658d65083f4b1bfaaf85356cfbd6c6a0fe5150f4
jaygallagher <jaygallagher>
2006-09-28 03:15:52 +0000

    Init Commit: utility class for tracking follow up letters and phone call to patients

---
+++ web/WEB-INF/classes/src/oscar/oscarPrevention/reports/FollowupManagement.java
@@ -0,0 +1,103 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * FollowupManagement.java
+ *
+ * Created on September 25, 2006, 4:05 PM
+ *
+ */
+
+package oscar.oscarPrevention.reports;
+
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Vector;
+import oscar.oscarEncounter.oscarMeasurements.util.WriteNewMeasurements;
+import oscar.util.UtilDateUtilities;
+
+/**
+ *
+ * @author jay
+ */
+public class FollowupManagement {
+    final public String LETTER1 = "L1";
+    final public String LETTER2 = "L2";
+    final public String PHONE1 = "P1";
+    
+    /** Creates a new instance of FollowupManagement */
+    public FollowupManagement() {
+    }
+    
+    
+    
+    /**
+     * Class used to mark prevention follow up procedures
+     *@param String followUpType   --  procedure type eg PAPF,FLUF,MAMF,FOBF,CIMF 
+     *@param String followUpValue  --  proedure value eg L1,L2,P1 letter 1, letter 2, phone call 1
+     *@param List demographicList  --  list of demographic numbers as strings 
+     *@param providerNo            --  provider # generating the list
+     *@param dateObserved          --  date list is generated 
+     */
+    public void markFollowupProcedure(String followUpType,String followUpValue, List demographicList, String providerNo,Date dateObserved,String comment){
+        for (int i = 0; i < demographicList.size();i++){
+            String demographicNo = (String) demographicList.get(i);
+            writeProcedure(followUpType, followUpValue, demographicNo, providerNo ,dateObserved,comment);           
+        }
+        
+    }
+    
+    /**
+     * Class used to mark prevention follow up procedures
+     *@param String followUpType   --  procedure type eg PAPF,FLUF,MAMF,FOBF,CIMF 
+     *@param String followUpValue  --  proedure value eg L1,L2,P1 letter 1, letter 2, phone call 1
+     *@param List demographicList  --  list of demographic numbers as strings 
+     *@param providerNo            --  provider # generating the list
+     *@param dateObserved          --  date list is generated 
+     */
+    public void markFollowupProcedure(String followUpType,String followUpValue, String[] demographicList, String providerNo,Date dateObserved,String comment){
+        for (int i = 0; i < demographicList.length;i++){   
+            writeProcedure(followUpType, followUpValue, demographicList[i], providerNo ,dateObserved,comment);
+            
+        }
+        
+    }
+
+    private void writeProcedure(final String followUpType, final String followUpValue, final String demographicNo, final String providerNo,final Date dateObserved,final String comment ) {        
+        System.out.println("Calling WriteProcedure for "+demographicNo);
+        Hashtable measure = new Hashtable();
+        measure.put("value",followUpValue);
+        measure.put("type",followUpType);
+        measure.put("measuringInstruction","");
+        measure.put("comments",  comment == null ? "":comment  );
+        measure.put("dateObserved",UtilDateUtilities.DateToString(dateObserved, "yyyy-MM-dd HH:mm:ss"));
+        measure.put("dateEntered",UtilDateUtilities.DateToString(UtilDateUtilities.now(), "yyyy-MM-dd HH:mm:ss"));
+        WriteNewMeasurements.write(measure,demographicNo,providerNo);
+    }
+    
+    
+    
+    
+    
+    
+}
