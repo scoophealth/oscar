c39f43df28ee539a236f0ddba26c25cd8474e9e7
jaygallagher <jaygallagher>
2006-09-28 03:09:12 +0000

    Init commit:  Action used to generate spread sheet with patient list

---
+++ web/WEB-INF/classes/src/oscar/oscarReport/pageUtil/GeneratePatientSpreadSheetListAction.java
@@ -0,0 +1,101 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * GenerateEnvelopesAction.java
+ *
+ * Created on September 14, 2006, 4:10 PM
+ *
+ */
+
+package oscar.oscarReport.pageUtil;
+
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Paragraph;
+import java.io.FileOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import oscar.oscarDemographic.data.DemographicData.Demographic;
+import oscar.oscarDemographic.data.DemographicData;
+import oscar.util.UtilDateUtilities;
+
+/**
+ *
+ * @author jay
+ */
+public class GeneratePatientSpreadSheetListAction  extends Action {
+   
+   public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)  {
+       
+    String[] demos = request.getParameterValues("demo");
+       
+    System.out.println("Generating Spread Sheet file ..");
+    response.setContentType("application/octet-stream");
+    response.setHeader("Content-Disposition", "attachment; filename=\"patientlist_spreadsheet-"+UtilDateUtilities.getToday("yyyy-mm-dd.hh.mm.ss")+".xls\"");
+                
+    
+    HSSFWorkbook wb = new HSSFWorkbook();
+    HSSFSheet sheet = wb.createSheet("patient list");
+   
+   for (int i = 0; i <demos.length;i++){
+         DemographicData demoData = new DemographicData();
+         Demographic d = demoData.getDemographic(demos[i]);
+       
+         
+           // Create a row and put some cells in it. Rows are 0 based.
+         HSSFRow row = sheet.createRow((short)i);
+         
+         row.createCell((short)0).setCellValue(d.getFirstName());    
+         row.createCell((short)1).setCellValue(d.getLastName());
+         row.createCell((short)2).setCellValue(d.getAddress());
+         row.createCell((short)3).setCellValue(d.getCity());
+         row.createCell((short)3).setCellValue(d.getProvince());
+         row.createCell((short)3).setCellValue(d.getPostal());
+         
+    }
+    try{    
+       wb.write(response.getOutputStream());
+    }catch(Exception e){
+       e.printStackTrace();   
+    }   
+       return null;
+   }
+    
+    /** Creates a new instance of GenerateEnvelopesAction */
+    public GeneratePatientSpreadSheetListAction() {
+    }
+    
+    
+    Paragraph getEnvelopeLabel(String text){
+      Paragraph p = new Paragraph(text,FontFactory.getFont(FontFactory.HELVETICA, 18));
+      p.setLeading(22);
+      return p;
+    }
+}
