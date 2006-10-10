23546ef6913a511ef30a3b71f998f723bd830d74
rjonasz <rjonasz>
2006-10-10 20:56:31 +0000

    Action class handles printing of attached docs for a consultation request

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarConsultationRequest/pageUtil/ConsultationPrintDocsAction.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada   Creates a new instance of Prevention
+ *
+ * ConsultationPrintDocsAction.java
+ *
+ * Created on July 29, 2006, 8:35 PM
+ *
+ */
+
+package oscar.oscarEncounter.oscarConsultationRequest.pageUtil;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import oscar.OscarProperties;
+import oscar.dms.EDocUtil;
+import oscar.dms.EDoc;
+import oscar.util.ConcatPDF;
+import oscar.util.UtilDateUtilities;
+
+/**
+ *
+ * @author jay
+ */
+public class ConsultationPrintDocsAction extends Action {
+    
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+        
+        String reqId = request.getParameter("reqId");
+        ArrayList docs = EDocUtil.listDocs( reqId, EDocUtil.ATTACHED);        
+        ArrayList alist = new ArrayList();
+        String path = OscarProperties.getInstance().getProperty("DOCUMENT_DIR");        
+        EDoc doc;
+        for (int i =0; i < docs.size(); i++) {
+            doc = (EDoc)docs.get(i);
+            if( doc.isPDF())
+                alist.add(path+doc.getFileName());
+        }    
+
+        if (alist.size() > 0 ){
+            response.setContentType("application/pdf");  //octet-stream
+            response.setHeader("Content-Disposition", "attachment; filename=\"combinedPDF-"+UtilDateUtilities.getToday("yyyy-mm-dd.hh.mm.ss")+".pdf\"");
+            try {
+                ConcatPDF.concat(alist,response.getOutputStream());            
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }            
+            return null;
+        }
+                  
+        return mapping.findForward("noprint");
+    }
+    
+    /**
+     * Creates a new instance of ConsultationPrintDocsAction
+     */
+    public ConsultationPrintDocsAction() {
+    }
+    
+}
