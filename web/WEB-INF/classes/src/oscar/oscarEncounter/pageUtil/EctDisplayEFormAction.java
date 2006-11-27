4ce88d5ac9b99810725a94357f6a245453b5ad46
rjonasz <rjonasz>
2006-11-27 22:19:10 +0000

    Lists all eforms in autocomplete menu and provides date created for existing eforms Provides + menu for adding eform

--- web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctDisplayEFormAction.java
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctDisplayEFormAction.java
@@ -26,10 +26,12 @@
 package oscar.oscarEncounter.pageUtil;
 
 import oscar.eform.*;
+import oscar.util.DateUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.net.URLEncoder;
+import java.sql.Date;
 import java.io.UnsupportedEncodingException;
 import javax.servlet.http.HttpServletRequest;
 import org.apache.struts.util.MessageResources;
@@ -46,10 +48,34 @@ public class EctDisplayEFormAction extends EctDisplayAction {
         String url = request.getContextPath() + "/eform/efmpatientformlist.jsp?demographic_no=" + bean.demographicNo;
         String heading = "<h3><a href=\"#\" onClick=\"popupPage(500,950,'" + winName + "', '" + url + "');return false;\">" + messages.getMessage("global.eForms") + "</a></h3>";
         Dao.setLeftHeading(heading);
+        
+        winName = "AddeForm" + bean.demographicNo;
+        url = request.getContextPath() + "/eform/efmformslistadd.jsp?demographic_no=" + bean.demographicNo;
+        heading = "<h3><a href=\"#\" style=\"clear: both; display: inline; float: right;\" onClick=\"popupPage(500,950,'" + winName + "', '" + url + "');return false;\">+</a></h3>";
+        Dao.setRightHeading(heading);
 
         StringBuffer javascript = new StringBuffer("<script type=\"text/javascript\">");        
-        String js = "";        
-        ArrayList eForms = EFormUtil.listPatientEForms(EFormUtil.DATE, EFormUtil.CURRENT, bean.demographicNo);
+        String js = ""; 
+        ArrayList eForms = EFormUtil.listEForms(EFormUtil.NAME, EFormUtil.CURRENT);
+        for( int i = 0; i < eForms.size(); ++i ) {
+            Hashtable curform = (Hashtable) eForms.get(i);
+            winName = (String)curform.get("formName") + bean.demographicNo;
+            
+            try {
+                winName = URLEncoder.encode(winName, "UTF-8");
+            }
+            catch( UnsupportedEncodingException e ) {
+                System.out.println("URLEncoding error " + e.getMessage());
+                winName = "eforms" + bean.demographicNo;
+            }
+            
+            url = "popupPage( 700, 800, '" + winName + "', '" + request.getContextPath() + "/eform/efmformadd_data.jsp?fid=" + curform.get("fid") + "&demographic_no=" + bean.demographicNo + "', 'FormA" + i + "');";
+            js = "autoCompleted['" + (String)curform.get("formName") + " (new)" + "'] = \"" + url + "\"; autoCompList.push('" + (String)curform.get("formName") + " (new)" + "');";
+            javascript.append(js);
+        }
+        
+        eForms.clear();
+        eForms = EFormUtil.listPatientEForms(EFormUtil.DATE, EFormUtil.CURRENT, bean.demographicNo);
         for( int i=0; i<eForms.size(); i++) {
             Hashtable curform = (Hashtable) eForms.get(i);
             NavBarDisplayDAO.Item item = Dao.Item();
@@ -64,14 +90,16 @@ public class EctDisplayEFormAction extends EctDisplayAction {
             }
             
             url = "popupPage( 700, 800, '" + winName + "', '" + request.getContextPath() + "/eform/efmshowform_data.jsp?fdid=" + curform.get("fdid") + "');";            
-            js = "autoCompleted['" + (String)curform.get("formName") + "'] = \"" + url + "\"; autoCompList.push('" + (String)curform.get("formName") + "');";
+            Date date = (Date)curform.get("formDateAsDate");
+            String formattedDate = DateUtils.getDate(date,dateFormat);
+            js = "autoCompleted['" + (String)curform.get("formName") + " " + formattedDate + "'] = \"" + url + "\"; autoCompList.push('" + (String)curform.get("formName") + " " + formattedDate + "');";
             javascript.append(js);
             url += " return false;";
             item.setURL(url);
-            item.setTitle((String)curform.get("formName"));
+            item.setTitle((String)curform.get("formName") + " " + formattedDate);
             Dao.addItem(item);
         }
-        
+                        
         javascript.append("</script>");
         Dao.setJavaScript(javascript.toString());
 
