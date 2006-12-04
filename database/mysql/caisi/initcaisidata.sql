3007271855f8ee9e035ea519b8b23a42294660a0
hexbinary <hexbinary>
2006-12-04 17:20:34 +0000

    up to date data init for caisi plugins

--- database/mysql/caisi/initcaisidata.sql
+++ database/mysql/caisi/initcaisidata.sql
@@ -26,3 +26,59 @@ insert into default_role_access (role_id,access_id) values ((select role_id from
 insert into default_role_access (role_id,access_id) values ((select role_id from caisi_role where name='doctor'),(select access_id from access_type where name='master file'));
 
 
+#issues
+INSERT INTO issue (code, role,update_date,description) SELECT icd9.icd9,'doctor',now(),icd9.description from icd9;
+
+INSERT INTO issue (code,description,role,update_date) values('CTCMM1000','Safety','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM2000','Health','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM3000','Personal ID','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM4000','Financial','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM5000','Legal','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM6000','Housing','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM7000','Education','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM8000','Employment','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM9000','Family','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM10000','Community Support','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM11000','Psycho-social concerns','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM12000','Client feedback / suggestions','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM13000','Physical Health','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM14000','Mental Health','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM15000','Addictions','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM16000','Social Support','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM17000','Respite','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM18000','Counselling Support','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM19000','Recovery/Healing','counsellor',Now());
+INSERT INTO issue (code,description,role,update_date) values('CTCMM20000','Wellness Program','counsellor',Now());
+insert into issue (code,description,role,update_date) values("CTCMM21000","Medication","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM22000","Hygiene/Clothing","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM23000","Increase Community Supports","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM24000","Discharge Planning","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM25000","Re- Admission Update","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM26000","Drug Card","counsellor",Now());
+insert into issue (code,description,role,update_date) values("CTCMM27000","Suicidal Tendencies","counsellor",Now());
+
+INSERT INTO issue (code,description,role,update_date) values('ICSW100','program-client conflict','CSW',Now());
+INSERT INTO issue (code,description,role,update_date) values('ICSW200','difficulties with hygeine','CSW',Now());
+INSERT INTO issue (code,description,role,update_date) values('ICSW300','difficulties with eating','CSW',Now());
+INSERT INTO issue (code,description,role,update_date) values('ICSW301','anorexia, loss of apetite','CSW',Now());
+
+#community programs
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10001,0,'Subsidized Housing','Subsidized Housing','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10002,0,'Private Market Housing','Private Market Housing','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10003,0,'Returned to Previous Address','Returned to Previous Address','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10004,0,'Returned To Partner','Returned To Partner','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10005,0,'Returned to Parents','Returned to Parents','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10006,0,'Moved in with Friends or Relatives','Moved in with Friends or Relatives','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10007,0,'Continued at Another Hospital or Treatment','Continued at Another Hospital or Treatment','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10008,0,'Left the City','Left the City','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10009,0,'Whereabouts Unknown (AWOL)','Whereabouts Unknown (AWOL)','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10010,0,'Community, unknown destination','Community, unknown destination','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10011,0,'Incarcerated','Incarcerated','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10012,0,'Deceased','Deceased','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic)  VALUES (10013,0,'Community Shelter','Community Shelter','','','','','','','community','',99999,0,0,'\0','\0','\0');
+INSERT INTO `program` (program_id,agency_id,name,descr,address,phone,fax,url,email,emergency_number,type,location,max_allowed,num_of_members,holding_tank,allow_batch_admission,allow_batch_discharge,hic) VALUES (10014,0,'Other','Other','','','','','','','community','',99999,0,0,'\0','\0','\0');
+
+#ER oscar role
+INSERT INTO `secRole` (role_name) values ('ER Clerk');
+
+
