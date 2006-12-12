df8bd4911c7e14ca014c519f10fc271873f60a6c
jaygallagher <jaygallagher>
2006-12-12 15:46:20 +0000

    Data class for workflow state

---
+++ web/WEB-INF/classes/src/oscar/oscarWorkflow/WFState.java
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * WFState.java
+ *
+ * Created on November 14, 2006, 9:24 AM
+ *
+ */
+
+package oscar.oscarWorkflow;
+
+/**
+ *
+ * @author jay
+ */
+public class WFState {
+    
+    private String key = null;
+    private String name = null;
+    private String desc = null;
+    
+    /** Creates a new instance of WFState */
+    public WFState() {
+    }
+    
+    public WFState(String key, String name, String desc){
+        this.key = key;
+        this.name = name;
+        this.desc = desc;
+        
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    
+    
+    
+}
