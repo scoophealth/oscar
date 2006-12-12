863fadb4dae09383f221bde7b284583b31bf66d9
jaygallagher <jaygallagher>
2006-12-12 15:38:02 +0000

    Class to execute DS rules for Workflow

---
+++ web/WEB-INF/classes/src/oscar/oscarWorkflow/WorkFlowDS.java
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * WorkFlowDS.java
+ *
+ * Created on November 21, 2006, 5:34 PM
+ *
+ */
+
+package oscar.oscarWorkflow;
+
+import org.drools.RuleBase;
+import org.drools.WorkingMemory;
+
+/**
+ *
+ * @author jay
+ */
+public class WorkFlowDS {
+    
+    RuleBase ruleBase = null;
+    
+    /** Creates a new instance of WorkFlowDS */
+    public WorkFlowDS() {
+    }
+   
+    public WorkFlowDS(RuleBase r){
+        ruleBase = r;
+    }
+   
+   public WorkFlowInfo getMessages(WorkFlowInfo w) throws Exception{
+      try{
+         WorkingMemory workingMemory = ruleBase.newWorkingMemory();
+         workingMemory.assertObject(w);
+         workingMemory.fireAllRules();
+      }catch(Exception e){e.printStackTrace(); throw new Exception("ERROR: Drools ",e);}
+         return w;
+   }
+
+   
+   
+ 
+}
