45c63588ee7e2b2cc4d49451a1d5968a9eaa6bd2
yili119 <yili119>
2006-12-28 19:31:22 +0000

    make the billing part work

---
+++ web/WEB-INF/classes/src/oscar/appt/JdbcApptImpl.java
@@ -0,0 +1,28 @@
+package oscar.appt;
+
+import org.apache.log4j.Logger;
+
+public class JdbcApptImpl {
+	private static final Logger _logger = Logger.getLogger(JdbcApptImpl.class);
+
+	public boolean deleteAppt(String apptNo) {
+		boolean retval = false;
+		return retval;
+	}
+
+	public String getLocationFromSchedule(String apptDate, String provider_no) {
+		String retval = "";
+		return retval;
+	}
+
+	private String getLocationFromSpec(String apptDate, String provider_no, String priority) {
+		String retval = "";
+		return retval;
+	}
+
+	public String getPrevApptDate(String thisServiceDate) {
+		String retval = "";
+		return retval;
+	}
+
+}
