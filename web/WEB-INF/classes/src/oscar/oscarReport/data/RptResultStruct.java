7a8054ecbb085ce3a9b2a9ce052c96fe039fdb45
apavel <apavel>
2007-01-07 19:00:15 +0000

    The Report By Template module - added another method with more styling

--- web/WEB-INF/classes/src/oscar/oscarReport/data/RptResultStruct.java
+++ web/WEB-INF/classes/src/oscar/oscarReport/data/RptResultStruct.java
@@ -62,6 +62,58 @@ public static String getStructure(ResultSet rs) throws SQLException {
 	sb.append("</table>");
 	return sb.toString();
 	}
+
+//improvement over getStructure() - changed CSS naming conventions, added enterspaces for cleaner html,
+//added more CSS classes for additional customization
+//used by 'report by template'
+/*
+CSS:
+ *table.reportTable {}
+ *th.reportHeader{}
+ *tr.reportRow1{}
+ *tr.reportRow2{}
+ *td.reportCell{}
+ */
+//~apavel (Paul)
+public static String getStructure2(ResultSet rs) throws SQLException {
+
+// assuming  multiple rows in rs
+	StringBuffer sb = new StringBuffer();
+        boolean results = true;
+	ResultSetMetaData rsmd = rs.getMetaData();
+
+	int columns = rsmd.getColumnCount();
+	String rowCSS ="reportRow1";
+	String[] columnNames = new String[columns];
+	sb.append("<table class=\"reportTable\">\n");
+	for (int i=0; i<columns; i++) {  // for each column in result set
+		columnNames[i] = rsmd.getColumnName(i+1);
+		// put names in array
+		// use i+1 or else you're going to get an exception
+	//  insert headings for table
+		sb.append("<th class=\"reportHeader\">");
+		sb.append(columnNames[i]);
+		sb.append("</th>\n");
+	}
+        if (!rs.next()) {
+            sb.append("</table><center><font color=\"red\">No Results</font></center>");
+            results = false;
+        } else {
+            do {
+                    sb.append("<tr class=\""+rowCSS+"\">");
+                    for(int j=0; j<columns; j++) {
+                            sb.append("<td>");
+                            sb.append(rs.getString(columnNames[j]));
+                            sb.append("</td>\n");
+
+                    }
+                    rowCSS = rowCSS.compareTo("reportRow1")==0?"reportRow2":"reportRow1";
+                    sb.append("</tr>\n");
+            } while (rs.next());
+        }
+        if (results) sb.append("</table>\n");
+	return sb.toString();
+	}
 }
 
 
