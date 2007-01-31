389de0dfb638398ecaf86900ed343295fc4a8ced
rjonasz <rjonasz>
2007-01-31 17:08:38 +0000

    Activate all existing schedules by setting status to A

--- database/mysql/updates/update-2007-01-29.sql
+++ database/mysql/updates/update-2007-01-29.sql
@@ -7,3 +7,8 @@ alter table rschedule add column status char(1) NOT NULL;
 alter table scheduledate drop primary key;
 alter table scheduledate add column id int(6) NOT NULL auto_increment primary key;
 alter table scheduledate add column status char(1) NOT NULL;
+
+#Activate all current schedules
+
+update rschedule set status = 'A';
+update scheduledate set status = 'A';
