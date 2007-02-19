5a18ad10c940494624af8c06f9ad0ab98cedc877
jaygallagher <jaygallagher>
2007-02-19 04:43:20 +0000

    Struts action to download billing files

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/DownloadFileAction.java
@@ -0,0 +1,77 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * DownloadFileAction.java
+ *
+ * Created on February 17, 2007, 2:43 PM
+ *
+ */
+package oscar.oscarBilling.ca.bc.pageUtil;
+
+import java.io.File;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DownloadAction;
+import org.apache.struts.actions.DownloadAction.FileStreamInfo;
+import oscar.OscarProperties;
+
+
+/**
+ * Processes downloading files
+ * @author jay
+ */
+public class DownloadFileAction extends DownloadAction{
+    
+    /** Creates a new instance of DownloadFileAction */
+    public DownloadFileAction() {
+    }
+    
+    protected StreamInfo getStreamInfo(ActionMapping mapping, 
+                                       ActionForm form,
+                                       HttpServletRequest request, 
+                                       HttpServletResponse response)
+            throws Exception {
+        //TODO:NEED TO CHECK TO SEE IF THEY HAVE admin or admin.billing privies
+        
+        String contentType = "application/octet-stream";
+        String home_dir = OscarProperties.getInstance().getProperty("HOME_DIR");
+        String fileName = request.getParameter("filename");
+        
+        response.setHeader("Content-disposition", 
+                           "attachment; filename=" + fileName);
+        File file = null;
+        try{
+           File directory = new File(home_dir);
+           if(!directory.exists()){
+              throw new Exception("Directory:  "+home_dir+ " does not exist");
+           }
+           file = new File(directory,fileName);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new Exception("Could not open file "+home_dir+fileName +" does "+home_dir+ " exist ?",e);
+        }
+        return new FileStreamInfo(contentType, file);   
+    }   
+}
