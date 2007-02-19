91c9959ccfe0b32b00fb1693382abec5452a1e9e
jaygallagher <jaygallagher>
2007-02-19 04:57:11 +0000

    DAO for teleplan_submission_link table (not yet committed)

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/MSP/TeleplanSubmissionLinkDAO.java
@@ -0,0 +1,76 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * TeleplanSubmissionLinkDAO.java
+ *
+ * Created on January 27, 2007, 1:52 PM
+ *
+ */
+
+package oscar.oscarBilling.ca.bc.MSP;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import oscar.oscarDB.DBHandler;
+
+/**
+  CREATE TABLE `teleplan_submission_link` (
+     `id` int(10) NOT NULL auto_increment,
+     `bill_activity_id` int(10),
+     `billingmaster_no` int(10) default NULL,
+     PRIMARY KEY  (`id`),
+     KEY (`bill_activity_id`),
+     KEY (`billingmaster_no`)
+    ) 
+ * @author jay
+ */
+public class TeleplanSubmissionLinkDAO {
+    
+    /** Creates a new instance of TeleplanSubmissionLinkDAO */
+    public TeleplanSubmissionLinkDAO() {
+    }
+    
+    String nsql ="insert into teleplan_submission_link (bill_activity_id,billingmaster_no) values (?,?)";
+    public void save(int billActId,List billingMasterList ){
+         try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            PreparedStatement pstmt = db.GetConnection().prepareStatement(nsql);
+            for (int i =0; i < billingMasterList.size(); i++){
+               String bi = (String) billingMasterList.get(i);
+               int b = Integer.parseInt(bi);
+               executeUpdate(pstmt,billActId,b);
+            }
+            pstmt.close();  
+            db.CloseConn();
+         }catch (SQLException e) {
+            e.printStackTrace();
+         }
+    }
+    
+    private void executeUpdate(PreparedStatement pstmt, int billActId, int billingmasterNo) throws SQLException{
+        pstmt.setInt(1,billActId);
+        pstmt.setInt(2,billingmasterNo);    
+        pstmt.executeUpdate();      
+    }
+}
