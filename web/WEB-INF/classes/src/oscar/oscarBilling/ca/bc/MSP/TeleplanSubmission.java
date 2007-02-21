44e835e4b94f55865ce13e69eb08ff7b929819ef
jaygallagher <jaygallagher>
2007-02-21 03:17:33 +0000

    init-commit

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/MSP/TeleplanSubmission.java
@@ -0,0 +1,230 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * TeleplanSubmission.java
+ *
+ * Created on January 26, 2007, 11:05 PM
+ *
+ */
+
+package oscar.oscarBilling.ca.bc.MSP;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanSequenceDAO;
+import oscar.oscarBilling.ca.bc.data.BillActivityDAO;
+import oscar.oscarDB.DBHandler;
+import oscar.util.StringUtils;
+
+/**
+ * Holds Data about a teleplan submission 
+ * @author jay
+ */
+public class TeleplanSubmission {
+    
+    private String mspFileStr = null;
+    private String mspHtmlStr = null;
+    private int sequenceNum = 0;
+    private ArrayList billingToBeMarkedAsBilled = null;
+    private ArrayList billingmasterToBeMarkedAsBilled = null;
+    private BigDecimal bigTotal = null;
+    private ArrayList logList = null;
+    private int totalClaims = 0;
+    
+    
+    /** Creates a new instance of TeleplanSubmission */
+    public TeleplanSubmission() {
+    }
+    
+    public TeleplanSubmission( String mspFileStr,String mspHtmlStr,int sequenceNum,
+            ArrayList billingToBeMarkedAsBilled,ArrayList billingmasterToBeMarkedAsBilled,BigDecimal bigTotal,ArrayList logList,int totalClaims){
+         this.mspFileStr = mspFileStr;
+         this.mspHtmlStr = mspHtmlStr;
+         this.sequenceNum = sequenceNum;
+         this.billingToBeMarkedAsBilled = billingToBeMarkedAsBilled;
+         this.billingmasterToBeMarkedAsBilled = billingmasterToBeMarkedAsBilled;
+         this.bigTotal = bigTotal ;
+         this.logList = logList;
+         this.totalClaims = totalClaims;   
+    }
+
+    
+    public String toString(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("msp file content\n");
+        sb.append(this.getMspFile());
+        sb.append("\n");
+        sb.append("html file content\n");
+        sb.append(this.getHtmlFile());
+        sb.append("\n");
+        sb.append("billing to be marked\n");
+        sb.append(StringUtils.getCSV(this.billingToBeMarkedAsBilled));
+        sb.append("\n");
+        sb.append("billingmaster");
+        sb.append(StringUtils.getCSV(this.billingmasterToBeMarkedAsBilled));
+        sb.append("\n");
+        sb.append("Ending Seq: "+sequenceNum+" total Claims: "+this.getNumClaims()+ " BigTotal: "+this.getBigTotal()+" # in Log: "+this.logList.size());
+        return sb.toString();
+    }
+    
+    public String getHtmlFile(){
+        return mspHtmlStr;
+    }
+    
+    public String getMspFile(){
+        return mspFileStr;
+    }
+    
+    public int getNumClaims(){
+        return totalClaims;
+    }
+                           
+    public BigDecimal getBigTotal(){
+        return bigTotal;
+    }
+                        
+        
+    // -commit log
+    // +commit billing
+    // +commit billingmaster
+    // +Save the File
+    // +Save the HTML
+    // -It would be nice to know what claims went on what submission!
+    //
+    //Create the file objects, two for the file and the html file
+    //If would be nice 
+    public void commit(String fileName,String dirToSaveFiles,String monthCode,String batchCount,String providerNo)  throws Exception{
+        File htmlFile = null;
+        File mspFile  = null;
+        try{
+           File directory = new File(dirToSaveFiles);
+           if(!directory.exists()){
+              throw new Exception("Directory:  "+dirToSaveFiles+ " does not exist");
+           }
+           
+           htmlFile = new File(directory,fileName+".html");
+           mspFile  = new File(directory,fileName);
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new Exception("Could not open file "+dirToSaveFiles+fileName +" does "+dirToSaveFiles+ " exist ?",e);
+        }
+        
+        try{
+           writeFile(mspFile);
+        }catch(Exception e){
+           throw new Exception("Could not write to file "+mspFile.getAbsolutePath()+" are permissions set correctly to "+dirToSaveFiles +"  ?",e);
+        }
+        
+        try{
+           writeHtml(htmlFile);
+        }catch(Exception e){
+           throw new Exception("Could not write to file "+htmlFile.getAbsolutePath() +" are permissions set correctly to "+dirToSaveFiles +"  ?",e);
+        } 
+        
+          BillActivityDAO bActDao  = new BillActivityDAO();
+            
+          int bActId = bActDao.saveBillactivity(monthCode,batchCount,
+                  fileName+".html", 
+                  fileName, 
+                  providerNo, 
+                  this.getHtmlFile(),
+                  this.getMspFile(), 
+                  new Date(),
+                  this.getNumClaims(), 
+                  this.getBigTotal().toString());
+    
+        
+        /*
+        BillingmasterDAO billingmaster = new BillingmasterDAO();
+        billingmaster.markListAsBilled(billingmasterToBeMarkedAsBilled);
+        
+        markListAsBilled(billingToBeMarkedAsBilled);
+        */
+        TeleplanLogDAO logDAO = new TeleplanLogDAO();
+        logDAO.save(logList);
+        
+        TeleplanSubmissionLinkDAO  subLinkDAO = new TeleplanSubmissionLinkDAO(); 
+        subLinkDAO.save(bActId,billingmasterToBeMarkedAsBilled);
+        
+        TeleplanSequenceDAO sequenceDAO = new TeleplanSequenceDAO();
+        sequenceDAO.saveUpdateSequence(sequenceNum);
+    }
+    
+    public void commitLog(){
+        TeleplanLogDAO logDAO = new TeleplanLogDAO();
+        logDAO.save(logList);
+        
+    }
+
+   
+        
+    //NEEDS TO BE MOVED OUT OF HERE    
+    private void markListAsBilled(List list){
+        String query = "update billing set status = 'B' where billing_no in ("+ StringUtils.getCSV(list) +")"; 
+        try {             
+           DBHandler dbhandler = new DBHandler(DBHandler.OSCAR_DATA);
+           dbhandler.RunSQL(query);                                              //1 2 3 4 5 6 7 8 9 0 1 2 3
+           dbhandler.CloseConn();
+        }catch (SQLException sqlexception) {
+           System.out.println(sqlexception.getMessage());
+        }
+    }
+    
+  
+    public void writeFile(String fileName,File home_dir)  throws Exception{
+        File file = new File(home_dir,fileName);
+        write(file,mspFileStr);
+    }
+ 
+    public void writeHtml(String fileName,File home_dir)  throws Exception{
+        File file = new File(home_dir,fileName);
+        write(file,mspHtmlStr);
+    }
+    
+    public void writeFile(File file) throws Exception{
+        write(file,mspFileStr);
+    }
+
+    public void writeHtml(File file) throws Exception{
+        write(file,mspHtmlStr);
+    }
+    
+    private void write(File file,String fileValue) throws Exception{
+          FileOutputStream out = new FileOutputStream(file);
+          BufferedOutputStream bufout = new BufferedOutputStream(out);
+          PrintStream p = new PrintStream(bufout);
+          p.println(fileValue);
+          p.close();
+    }
+    
+    
+  
+    
+}
