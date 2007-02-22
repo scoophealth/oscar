eb9148f184010a0c75c47e990180689c122ec26b
rjonasz <rjonasz>
2007-02-22 21:48:30 +0000

    Add sentto property

--- web/WEB-INF/classes/src/oscar/oscarMessenger/data/MsgDisplayMessage.java
+++ web/WEB-INF/classes/src/oscar/oscarMessenger/data/MsgDisplayMessage.java
@@ -33,6 +33,7 @@ public class MsgDisplayMessage {
     public String thesubject = null;
     public String thedate    = null;
     public String sentby     = null;
+    public String sentto     = null;
     public String attach     = null;
     public String pdfAttach     = null;
 
