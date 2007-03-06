d51235603d857fc32b203726636b1aad19615104
jaygallagher <jaygallagher>
2007-03-06 21:34:18 +0000

    Fix Bug #121   Added Try/catch around date parsing method because MDS does not always send a date in there Hl7 file

--- web/WEB-INF/classes/src/oscar/oscarMDS/data/GroupedReports.java
+++ web/WEB-INF/classes/src/oscar/oscarMDS/data/GroupedReports.java
@@ -1,54 +1,52 @@
 /*
- * 
+ *
  * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
- * This software is published under the GPL GNU General Public License. 
- * This program is free software; you can redistribute it and/or 
- * modify it under the terms of the GNU General Public License 
- * as published by the Free Software Foundation; either version 2 
- * of the License, or (at your option) any later version. * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
- * along with this program; if not, write to the Free Software 
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
- * 
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
  * <OSCAR TEAM>
- * 
- * This software was written for the 
- * Department of Family Medicine 
- * McMaster Unviersity 
- * Hamilton 
- * Ontario, Canada 
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster Unviersity
+ * Hamilton
+ * Ontario, Canada
  */
-// Decompiled by DJ v3.5.5.77 Copyright 2003 Atanas Neshkov  Date: 05/12/2003 11:02:37 AM
-// Home Page : http://members.fortunecity.com/neshkov/dj.html  - Check often for new version!
-// Decompiler options: packimports(3) 
-// Source File Name:   GroupedReports.java
 
 package oscar.oscarMDS.data;
 
 import java.util.*;
 import java.text.SimpleDateFormat;
 
-public class GroupedReports
-{
-
-    GroupedReports(String oBR, String hL7TimeStamp, String qString)
-    {
+public class GroupedReports {
+    
+    GroupedReports(String oBR, String hL7TimeStamp, String qString) {
         resultsArray = new ArrayList();
         associatedOBR = oBR;
         queryString = qString;
-        GregorianCalendar cal = new GregorianCalendar(Locale.ENGLISH);
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yy HH:mm", Locale.ENGLISH);
-        
-        // boneheaded calendar numbers months from 0
-        cal.set(Integer.parseInt(hL7TimeStamp.substring(0,4)), Integer.parseInt(hL7TimeStamp.substring(4,6))-1, Integer.parseInt(hL7TimeStamp.substring(6,8)),
-                Integer.parseInt(hL7TimeStamp.substring(8,10)), Integer.parseInt(hL7TimeStamp.substring(10,12)), Integer.parseInt(hL7TimeStamp.substring(12,14)));
-       
-        timeStamp = dateFormat.format(cal.getTime());
+        try{
+            GregorianCalendar cal = new GregorianCalendar(Locale.ENGLISH);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMM-yy HH:mm", Locale.ENGLISH);
+            
+            // boneheaded calendar numbers months from 0
+            cal.set(Integer.parseInt(hL7TimeStamp.substring(0,4)), Integer.parseInt(hL7TimeStamp.substring(4,6))-1, Integer.parseInt(hL7TimeStamp.substring(6,8)),
+                    Integer.parseInt(hL7TimeStamp.substring(8,10)), Integer.parseInt(hL7TimeStamp.substring(10,12)), Integer.parseInt(hL7TimeStamp.substring(12,14)));
+            
+            timeStamp = dateFormat.format(cal.getTime());
+        }catch(Exception e){
+            timeStamp = "";
+        }
     }
-
+    
     public String associatedOBR;
     public ArrayList resultsArray;
     public String timeStamp;
