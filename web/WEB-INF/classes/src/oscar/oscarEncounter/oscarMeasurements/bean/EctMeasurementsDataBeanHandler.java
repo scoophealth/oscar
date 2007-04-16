16227a2f3d94ea05bbb284d7aca0348e703f6e49
jaygallagher <jaygallagher>
2007-04-16 18:12:53 +0000

    fixed bug in method getLast that would not get the Last measurement if it was on the same day

--- web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/bean/EctMeasurementsDataBeanHandler.java
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/bean/EctMeasurementsDataBeanHandler.java
@@ -221,7 +221,7 @@ public class EctMeasurementsDataBeanHandler {
                     "m.comments, m.dateObserved, m.dateEntered , p.first_name AS provider_first, p.last_name AS provider_last " + 
                     "FROM measurements m, provider p, measurementType mt " +
                     "WHERE m.demographicNo='" + demo + "' AND m.type = '" + type + "' AND m.providerNo= p.provider_no " +
-                    "AND m.type = mt.type GROUP BY m.id ORDER BY m.dateObserved DESC LIMIT 1";
+                    "AND m.type = mt.type GROUP BY m.id ORDER BY m.dateObserved,m.dateEntered DESC LIMIT 1";
         return getHashfromSQL(sql);
     }
     
