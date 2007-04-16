777209f994579619ad8a0a2f1fa08afbcbb0e6b9
jaygallagher <jaygallagher>
2007-04-16 19:06:30 +0000

    Represents a renal dosing ds object   -contains the dosing information, moreinfo and the logic decision if value is in range of a dose

---
+++ web/WEB-INF/classes/src/oscar/oscarRx/util/DosingRecomendation.java
@@ -0,0 +1,147 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * DosingRecomendation.java
+ *
+ * Created on December 21, 2006, 11:23 AM
+ *
+ */
+
+package oscar.oscarRx.util;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ *
+ * @author jay
+ */
+public class DosingRecomendation {
+    
+    private String name = null;
+    private String atccode = null;
+    private String moreinfo = null;
+    private ArrayList Dose = null;
+    
+    public String toString(){
+        return "name: "+name+" atccode: "+atccode;
+        
+    }
+    
+    /** Creates a new instance of DosingRecomendation */
+    public DosingRecomendation() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAtccode() {
+        return atccode;
+    }
+
+    public void setAtccode(String atccode) {
+        this.atccode = atccode;
+    }
+
+    public String getMoreinfo() {
+        return moreinfo;
+    }
+
+    public void setMoreinfo(String moreinfo) {
+        this.moreinfo = moreinfo;
+    }
+
+    public ArrayList getDose() {
+        return Dose;
+    }
+
+    public void setDose(ArrayList Dose) {
+        this.Dose = Dose;
+    }
+
+
+    
+    /*
+     *Evaluate string to form a comparison
+     *Values are stored in a hashtable with the key clcrrange
+     *Values will be in the form of:
+      
+     "30-50"    between 30 and 50
+     "&lt;15"   less than 15
+     "&gt;50"   greater than 50
+     */
+    public boolean valueInRangeOfDose(int val , Hashtable doseVal){
+        boolean valueInRange = false;
+        try{
+            String toParse  = (String) doseVal.get("clcrrange");
+
+            System.out.println("TO PARSE: "+toParse);
+            if (toParse == null){
+               return false;    
+            }
+
+            if (toParse.indexOf("-") != -1){ //between style
+                String[] betweenVals = toParse.split("-");
+                if (betweenVals.length == 2 ){
+                    int lower = Integer.parseInt(betweenVals[0]);
+                    int upper = Integer.parseInt(betweenVals[1]);
+
+                    if (val >= lower && val <= upper){
+                        valueInRange = true;
+                    }
+                } 
+
+            }else if (toParse.indexOf("&gt;") != -1){ // greater than style
+                toParse = toParse.replaceFirst("&gt;","");
+                int gt = Integer.parseInt(toParse);
+                if(val >= gt){
+                    valueInRange = true;
+                }          
+            }else if (toParse.indexOf("&lt;") != -1){ // less than style
+                toParse = toParse.replaceFirst("&lt;","");
+                int lt = Integer.parseInt(toParse);           
+                if(val <= lt){
+                    valueInRange = true;
+                }
+            }else if (!toParse.equals("")){ // less than style
+                int eq = Integer.parseInt(toParse); 
+                if(val == eq){
+                    valueInRange = true;
+                }
+            }
+            
+            
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return valueInRange;
+    }
+    
+    
+    
+}
