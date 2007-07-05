ed1aa431ab96e10303cf1b4df4a26867e68c30f9
apavel <apavel>
2007-07-05 19:16:04 +0000

    Update to the PHR module: - new model objects for sending docs and meds

---
+++ web/WEB-INF/classes/src/org/oscarehr/phr/model/PHRBinaryData.java
@@ -0,0 +1,120 @@
+/*
+ * PHRBinaryDocument.java
+ *
+ * Created on June 21, 2007, 10:40 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.oscarehr.phr.model;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import org.indivo.IndivoException;
+import org.indivo.xml.JAXBUtils;
+import org.indivo.xml.phr.DocumentGenerator;
+import org.indivo.xml.phr.DocumentUtils;
+import org.indivo.xml.phr.binarydata.BinaryData;
+import org.indivo.xml.phr.binarydata.BinaryDataType;
+import org.indivo.xml.phr.document.ContentDescriptionType;
+import org.indivo.xml.phr.document.DocumentHeaderType;
+import org.indivo.xml.phr.document.IndivoDocument;
+import org.indivo.xml.phr.document.IndivoDocumentType;
+import org.indivo.xml.phr.types.AuthorType;
+import org.indivo.xml.phr.urns.ContentTypeQNames;
+import org.indivo.xml.phr.urns.DocumentClassificationUrns;
+import org.oscarehr.phr.PHRConstants;
+import org.oscarehr.phr.indivo.IndivoConstantsImpl;
+import org.w3c.dom.Element;
+import oscar.dms.EDoc;
+import oscar.dms.EDocUtil;
+import oscar.oscarProvider.data.ProviderData;
+
+/**
+ *
+ * @author root
+ */
+public class PHRBinaryData extends PHRDocument {
+    
+    /** Creates a new instance of PHRBinaryDocument */
+    public PHRBinaryData() {
+    }
+    
+    public PHRBinaryData(ProviderData sender, String recipientOscarId, int recipientType, String recipientPhrId, EDoc document) throws JAXBException, IndivoException {
+        //EDocUtil eDocUtil = new EDocUtil();
+        //byte[] bfile = EDocUtil.getFile(filePath);
+        //if( bfile == null )
+        //    return false;
+        
+        String providerFullName = sender.getFirst_name() + " " + sender.getLast_name();
+        
+        //Temp data: We're not setting the document just yet
+        
+        GregorianCalendar calendar = new GregorianCalendar();
+        String fname = document.getType() + calendar.get(Calendar.DAY_OF_MONTH) + "-" + String.valueOf(calendar.get(Calendar.MONTH)+1) + "-" + calendar.get(Calendar.YEAR);
+        
+        BinaryDataType binaryDataType = getPhrBinaryData(document.getContentType(), document.getDescription(), fname, new byte[0]);
+        IndivoDocumentType indivoDoc = getPhrBinaryDataDocument(sender, binaryDataType);
+        
+        byte[] docContentBytes = JAXBUtils.marshalToByteArray((JAXBElement) new IndivoDocument(indivoDoc), JAXBContext.newInstance(IndivoDocumentType.class.getPackage().getName()));
+        String docContentStr = new String(docContentBytes);
+        PHRConstants phrConstants = new IndivoConstantsImpl();
+        this.setPhrClassification(phrConstants.DOCTYPE_BINARYDATA());
+        this.setSenderOscar(sender.getProvider_no());
+        this.setSenderType(PHRDocument.TYPE_PROVIDER);
+        this.setSenderPhr(sender.getMyOscarId());
+        this.setReceiverOscar(recipientOscarId);
+        this.setReceiverType(recipientType);
+        this.setReceiverPhr(recipientPhrId);
+        this.setDocContent(docContentStr);
+    }
+    
+    public static IndivoDocumentType mountDocument(String oscarId, IndivoDocumentType doc) throws JAXBException, IndivoException {
+        EDocUtil docUtil = new EDocUtil();
+        EDoc eDoc = docUtil.getDoc(oscarId);
+        byte[] data = docUtil.getFile(eDoc.getFilePath());
+        JAXBContext binaryDataContext = JAXBContext.newInstance(BinaryData.class.getPackage().getName());
+        BinaryDataType bdt = (BinaryDataType) DocumentUtils.getDocumentAnyObject(doc, binaryDataContext.createUnmarshaller());
+        bdt.setData(data);
+        
+        AuthorType author = doc.getDocumentHeader().getAuthor();
+        System.out.println("Name: " + author.getName());
+        System.out.println("indivoid: " + author.getIndivoId());
+        System.out.println("role: " + author.getRole());
+        IndivoDocumentType indivoDoc = getPhrBinaryDataDocument(author.getName(), author.getIndivoId(), author.getRole().getValue(), bdt);
+        return indivoDoc;
+    }
+    
+    private BinaryDataType getPhrBinaryData(String mimeType, String fileDesc, String filename, byte[] data) {
+        BinaryDataType binaryDataType = new BinaryDataType();
+        binaryDataType.setMimeType(mimeType);
+        binaryDataType.setFileDescription(fileDesc);
+        binaryDataType.setFilename(filename);
+        binaryDataType.setData(data);
+        return binaryDataType;
+    }
+    
+    private static IndivoDocumentType getPhrBinaryDataDocument(ProviderData sender, BinaryDataType binaryData) throws JAXBException, IndivoException {
+        String providerFullName = sender.getFirst_name() + " " + sender.getLast_name();
+        return getPhrBinaryDataDocument(providerFullName, sender.getMyOscarId(), PHRDocument.PHR_ROLE_PROVIDER, binaryData);
+    }
+    
+    private static IndivoDocumentType getPhrBinaryDataDocument(String providerFullName, String providerPhrId, String providerRole, BinaryDataType binaryData) throws JAXBException, IndivoException {
+        DocumentGenerator generator = new DocumentGenerator();
+        JAXBUtils jaxbUtils = new JAXBUtils();
+        org.indivo.xml.phr.binarydata.ObjectFactory binFactory = new org.indivo.xml.phr.binarydata.ObjectFactory();
+        BinaryData bd = binFactory.createBinaryData(binaryData);
+
+        Element element = jaxbUtils.marshalToElement(bd, JAXBContext.newInstance(BinaryData.class.getPackage().getName()));
+        IndivoDocumentType doc = generator.generateDefaultDocument(providerPhrId, providerFullName, providerRole,  DocumentClassificationUrns.BINARYDATA, ContentTypeQNames.BINARYDATA, element);
+        DocumentHeaderType header = doc.getDocumentHeader();
+        ContentDescriptionType contentDescription = header.getContentDescription();
+        contentDescription.setDescription(binaryData.getFileDescription());         
+        return doc;
+    }
+    
+}
---
+++ web/WEB-INF/classes/src/org/oscarehr/phr/model/PHRMedication.java
@@ -0,0 +1,158 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * PHRPrescription.java
+ *
+ * Created on June 1, 2007, 3:31 PM
+ *
+ */
+
+package org.oscarehr.phr.model;
+
+import java.util.ResourceBundle;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import org.indivo.IndivoException;
+import org.indivo.client.ActionNotPerformedException;
+import org.indivo.xml.JAXBUtils;
+import org.indivo.xml.phr.contact.ConciseContactInformationType;
+import org.indivo.xml.phr.contact.NameType;
+import org.indivo.xml.phr.document.IndivoDocument;
+import org.indivo.xml.phr.document.IndivoDocumentType;
+import org.indivo.xml.phr.medication.Medication;
+import org.indivo.xml.phr.medication.MedicationType;
+import org.indivo.xml.phr.types.CodedValueType;
+import org.indivo.xml.phr.types.CodingSystemReferenceType;
+import org.indivo.xml.phr.urns.ContentTypeQNames;
+import org.indivo.xml.phr.urns.DocumentClassificationUrns;
+import org.oscarehr.phr.PHRConstants;
+import org.oscarehr.phr.indivo.IndivoConstantsImpl;
+import org.w3c.dom.Element;
+import oscar.oscarEncounter.data.EctProviderData;
+import oscar.oscarRx.data.RxPrescriptionData;
+
+/**
+ *
+ * @author apavel
+ */
+public class PHRMedication extends PHRDocument{
+    
+    /** Creates a new instance of PHRMessage */
+    public PHRMedication() {
+        //super();
+    }
+    
+    //sending new meds to PHR
+    public PHRMedication(EctProviderData.Provider prov, String demographicNo, String demographicPhrId, RxPrescriptionData.Prescription drug) throws JAXBException, ActionNotPerformedException, IndivoException  {
+        //super();
+        IndivoDocumentType document = getPhrMedicationDocument(prov, drug);
+        JAXBContext docContext = JAXBContext.newInstance(IndivoDocumentType.class.getPackage().getName());
+        byte[] docContentBytes = JAXBUtils.marshalToByteArray((JAXBElement) new IndivoDocument(document), docContext);
+        String docContentStr = new String(docContentBytes);
+        
+        PHRConstants phrConstants = new IndivoConstantsImpl();
+        this.setPhrClassification(phrConstants.DOCTYPE_MEDICATION());
+        this.setReceiverOscar(demographicNo);
+        this.setReceiverType(this.TYPE_DEMOGRAPHIC);
+        this.setReceiverPhr(demographicPhrId);
+        this.setSenderOscar(prov.getProviderNo());
+        this.setSenderType(this.TYPE_PROVIDER);
+        this.setSenderPhr(prov.getIndivoId());
+        this.setSent(this.STATUS_SENT_PENDING);
+        this.setDocContent(docContentStr);
+    }
+    
+    //when adding a new medication
+    private IndivoDocumentType getPhrMedicationDocument(EctProviderData.Provider prov, RxPrescriptionData.Prescription drug) throws JAXBException, IndivoException {
+        String providerFullName = prov.getFirstName() + " " + prov.getSurname();
+        MedicationType medType = createPhrMedication(prov, drug);
+        org.indivo.xml.phr.DocumentGenerator generator = new org.indivo.xml.phr.DocumentGenerator();
+        org.indivo.xml.JAXBUtils jaxbUtils = new org.indivo.xml.JAXBUtils();
+        org.indivo.xml.phr.medication.ObjectFactory medFactory = new org.indivo.xml.phr.medication.ObjectFactory();
+        Medication med = medFactory.createMedication(medType);
+
+        Element element = jaxbUtils.marshalToElement(med, JAXBContext.newInstance("org.indivo.xml.phr.medication"));            
+        IndivoDocumentType document = generator.generateDefaultDocument(prov.getIndivoId(), providerFullName, PHRDocument.PHR_ROLE_PROVIDER, DocumentClassificationUrns.MEDICATION, ContentTypeQNames.MEDICATION, element);
+        return document;
+    }
+    
+    private MedicationType createPhrMedication(EctProviderData.Provider prov, RxPrescriptionData.Prescription drug) {
+        NameType name = new NameType();
+        name.setFirstName(prov.getFirstName());
+        name.setLastName(prov.getSurname());
+
+        ConciseContactInformationType contactInfo = new ConciseContactInformationType();
+        contactInfo.getPersonName().add(name);
+        
+        MedicationType medType = new MedicationType();
+        medType.setPrescription(true);
+        
+        if( drug.getCustomInstr() == false ) {
+            medType.setDose(drug.getDosageDisplay() + " " + drug.getUnit());            
+            medType.setDuration(drug.getDuration() + " " + drug.getDurationUnit());
+            //CodedValueType cvt = new CodedValueType();
+            //CodingSystemReferenceType csrt = new CodingSystemReferenceType();
+            //csrt.setServiceLocation("");
+            //cvt.setCodingSystem(csrt);
+            //cvt.setCode("");
+            //cvt.setHistoricalValue(drug.getDurationUnit());
+            //medType.setDurationUnit(cvt);
+            medType.setRefills(String.valueOf(drug.getRepeat()));
+            medType.setSubstitutionPermitted(drug.getNosubs());
+        }
+        else
+            medType.setDose(ResourceBundle.getBundle("oscarResources").getString("Send2Indivo.prescription.Instruction"));
+                
+        medType.setName(drug.getDrugName());
+        medType.setInstructions("<pre>" + drug.getSpecial() + "</pre>");        
+        medType.setProvider(contactInfo);
+        return medType;
+    }
+    
+    /*
+     *For updating a document on the IndivoServer:  
+     IndivoServer --> OSCAR  (sends back the latest doc)
+     OSCARmySQLdb --> OSCAR--> IndivoServer (OSCAR sends back the appended version of the doc)
+        try {
+            IndivoDocumentType currentDoc = new IndivoDocumentType();  //the new version of the doc
+            //DocumentHeaderType docHeaderType = currentDoc.getDocumentHeader();
+            //String docIndex = docHeaderType.getDocumentIndex();
+            Element documentElement = DocumentUtils.getDocumentAnyElement(currentDoc);
+            //Retrieve current file record from indivo
+            ReadDocumentResultType readResult = client.readDocument(auth.getToken(), demographicPhrId, oldDrugPhrId);
+            IndivoDocumentType phrDoc = readResult.getIndivoDocument();
+            DocumentVersionType version = phrDoc.getDocumentVersion().get(phrDoc.getDocumentVersion().size() - 1);
+            
+            VersionBodyType body = version.getVersionBody();
+            body.setAny(documentElement);
+            version.setVersionBody(body);
+            client.updateDocument(sessionTicket, recipientId, docIndex, version);            
+        } catch(ActionNotPerformedException anpe) {
+            anpe.printStackTrace();
+        } catch(IndivoException ie ) {
+            ie.printStackTrace();
+        } */
+}
+
+
