129d6b7eea4209afa9edef6691f24194458c79cf
jaygallagher <jaygallagher>
2007-07-09 04:05:55 +0000

    init-commit Teleplan Struts action

---
+++ web/WEB-INF/classes/src/oscar/oscarBilling/ca/bc/pageUtil/ManageTeleplanAction.java
@@ -0,0 +1,225 @@
+/*
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  Jason Gallagher
+ *
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada
+ *
+ * ManageTeleplanAction.java
+ *
+ * Created on June 22, 2007, 12:10 AM
+ *
+ */
+
+package oscar.oscarBilling.ca.bc.pageUtil;
+
+import java.io.File;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+import oscar.OscarProperties;
+import oscar.entities.Billactivity;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanAPI;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanResponse;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanSequenceDAO;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanService;
+import oscar.oscarBilling.ca.bc.Teleplan.TeleplanUserPassDAO;
+import oscar.oscarBilling.ca.bc.data.BillActivityDAO;
+
+/**
+ *
+ * @author jay
+ */
+public class ManageTeleplanAction extends DispatchAction {
+    
+    private static Log log = LogFactory.getLog(ManageTeleplanAction.class);
+    
+    /** Creates a new instance of ManageTeleplanAction */
+    public ManageTeleplanAction() {
+    }
+    
+    public ActionForward unspecified(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           log.debug("UNSPECIFIED ACTION!");
+           return mapping.findForward("success");
+    }
+    
+    public ActionForward setUserName(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           log.debug("SET USER NA<E ACTION JACKSON");
+           String username = (String) request.getParameter("user");
+           String password = (String) request.getParameter("pass");
+           
+           log.debug("username "+username+" password "+password);
+           
+           //TODO: validate username - make sure url is not null
+           
+           TeleplanUserPassDAO dao = new TeleplanUserPassDAO();
+           dao.saveUpdateUsername(username);
+           dao.saveUpdatePasssword(password);
+           return mapping.findForward("success");
+    }
+    
+    public ActionForward updateBillingCodes(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           TeleplanUserPassDAO dao = new TeleplanUserPassDAO();
+           String[] userpass = dao.getUsernamePassword();
+           TeleplanService tService = new TeleplanService();
+           TeleplanAPI tAPI = tService.getTeleplanAPI(userpass[0],userpass[1]);
+           
+           TeleplanResponse tr = tAPI.getAsciiFile("3");
+           log.debug(tr.toString());
+    
+           return mapping.findForward("success");
+    }
+    
+    public ActionForward getSequenceNumber(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           log.debug("getSequenceNumber");
+           OscarProperties prop = OscarProperties.getInstance();
+           String datacenter = prop.getProperty("dataCenterId","");
+           if(datacenter.length() != 5){
+               //this.addMessages() //TODO:ADD MESSAGE ABOUT DATA CENTER NOT BEING CORRECT 
+                log.debug("returning because of datacenter #"+datacenter);
+                return mapping.findForward("success");
+           }
+           TeleplanUserPassDAO dao = new TeleplanUserPassDAO();
+           String[] userpass = dao.getUsernamePassword();
+           TeleplanService tService = new TeleplanService();
+           
+           TeleplanAPI tAPI = null;
+           try{
+                tAPI = tService.getTeleplanAPI(userpass[0],userpass[1]);
+           }catch(Exception e){
+               log.debug(e.getMessage(),e);
+               request.setAttribute("error",e.getMessage());
+               return mapping.findForward("success");
+           }
+           
+           
+           int sequenceNumber = tService.getSequenceNumber(tAPI,datacenter);
+        
+           TeleplanSequenceDAO seq = new TeleplanSequenceDAO();
+           seq.saveUpdateSequence(sequenceNumber);
+           return mapping.findForward("success");
+    }
+    
+    public ActionForward setSequenceNumber(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           log.debug("setSequenceNumber");
+           String sequence = (String) request.getParameter("num");
+           int sequenceNumber = -1;
+           try{
+            sequenceNumber = Integer.parseInt(sequence);
+           }catch(Exception e){
+               //TODO: ADDED ERROR MESSAGE ABOUT THE NUMBER NOT BEING A NUMBER!
+               return mapping.findForward("success");
+           }
+           
+           if (sequenceNumber < 0 || sequenceNumber > 9999999){
+               //TODO: ADDED ERROR MESSAGE ABOUT NUMBER BEING OUT OF RANGE
+               return mapping.findForward("success");
+           }
+           
+           TeleplanSequenceDAO seq = new TeleplanSequenceDAO();
+           seq.saveUpdateSequence(sequenceNumber);
+           return mapping.findForward("success");
+    }
+    
+    
+    public ActionForward sendFile(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           log.debug("sendFile Start");
+           String id  = (String) request.getParameter("id");
+           
+           TeleplanUserPassDAO dao = new TeleplanUserPassDAO();
+           String[] userpass = dao.getUsernamePassword();
+           TeleplanService tService = new TeleplanService();
+           TeleplanAPI tAPI = null;
+           try{
+                tAPI = tService.getTeleplanAPI(userpass[0],userpass[1]);
+           }catch(Exception e){
+               log.debug(e.getMessage(),e);
+               request.setAttribute("error",e.getMessage());
+               return mapping.findForward("submission");
+           }
+           
+           
+           
+           //TODO: validate username - make sure url is not null
+           BillActivityDAO billActDAO = new BillActivityDAO();
+           List l = billActDAO.getBillactivityByID(id);
+           
+           Billactivity b = (Billactivity) l.get(0);
+           String filename = b.getOhipfilename();
+           
+           OscarProperties prop = OscarProperties.getInstance();
+           String datacenter = prop.getProperty("HOME_DIR","");
+           
+           File f = new File(datacenter,filename);
+           
+           
+           if ( f != null && log.isDebugEnabled()){
+               log.debug("File is Readable: "+f.canRead());
+               log.debug("File exists: "+f.exists());
+               log.debug("File Path " +f.getCanonicalPath());
+           }
+           log.info("sending file "+f.getAbsolutePath());
+           
+           TeleplanResponse tr = tAPI.putMSPFile(f);
+           log.debug("sendFile End"+tr.getResult());
+           if(!tr.isSuccess()){
+               request.setAttribute("error",tr.getMsgs());
+           }
+           return mapping.findForward("submission");
+    }        
+            
+    public ActionForward changePass(ActionMapping mapping, ActionForm  form,
+           HttpServletRequest request, HttpServletResponse response)
+           throws Exception {
+           String oldpass  = (String) request.getParameter("oldpass");
+           String newpass  = (String) request.getParameter("newpass");
+           String confpass = (String) request.getParameter("confpass");
+           
+           //TODO: validate username - make sure url is not null
+           
+           if (!newpass.equals(confpass)){  
+               return mapping.findForward("error");
+           }
+           //CHECK TELEPLAN
+           
+           TeleplanUserPassDAO dao = new TeleplanUserPassDAO();
+           dao.saveUpdatePasssword(newpass);
+           return mapping.findForward("success");
+    }
+    
+    
+}
