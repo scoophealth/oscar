3205ec1b8b97c11d6a85c2e3c7f931f38f035ba7
rjonasz <rjonasz>
2007-07-10 20:45:14 +0000

    update appointment status

---
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/hibernate/AppointmentDAOHibernate.java
@@ -0,0 +1,57 @@
+/*
+* 
+* Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+* This software is published under the GPL GNU General Public License. 
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation; either version 2 
+* of the License, or (at your option) any later version. * 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+* along with this program; if not, write to the Free Software 
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+* 
+* <OSCAR TEAM>
+* 
+* This software was written for 
+* Centre for Research on Inner City Health, St. Michael's Hospital, 
+* Toronto, Ontario, Canada 
+*/
+
+
+package org.oscarehr.casemgmt.dao.hibernate;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.caisi.model.Appointment;
+import org.oscarehr.casemgmt.dao.ApptDAO;
+import org.oscarehr.casemgmt.model.CaseManagementCPP;
+import org.oscarehr.casemgmt.model.CaseManagementNote;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class AppointmentDAOHibernate extends HibernateDaoSupport implements
+		ApptDAO
+{
+        public void updateAppointmentStatus(String apptId, String status, String type) {
+            
+            String sql = "from Appointment appt where appt.id = ?";
+            List list = getHibernateTemplate().find(sql, new Integer(apptId));
+            Appointment appt;
+            if (list.size() != 0) {
+                appt = (Appointment)list.get(list.size()-1);
+                oscar.appt.ApptStatusData as = new oscar.appt.ApptStatusData();
+                as.setApptStatus(status);
+                if( type.equalsIgnoreCase("sign"))
+                    appt.setStatus(as.signStatus());
+                if( type.equalsIgnoreCase("verify"))
+                    appt.setStatus(as.verifyStatus());
+                
+                getHibernateTemplate().update(appt);
+            }
+        }       
+}
