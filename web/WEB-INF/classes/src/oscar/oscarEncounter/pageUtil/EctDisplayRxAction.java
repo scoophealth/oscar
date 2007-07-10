b9753eb6f3a2d180b01ec6dd13c0a292b8fa9962
rjonasz <rjonasz>
2007-07-10 21:06:49 +0000

    display Rx in casemanagement screen

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/pageUtil/EctDisplayRxAction.java
@@ -0,0 +1,86 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License. 
+// * This program is free software; you can redistribute it and/or 
+// * modify it under the terms of the GNU General Public License 
+// * as published by the Free Software Foundation; either version 2 
+// * of the License, or (at your option) any later version. * 
+// * This program is distributed in the hope that it will be useful, 
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+// * along with this program; if not, write to the Free Software 
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+// * 
+// * <OSCAR TEAM>
+// * This software was written for the 
+// * Department of Family Medicine 
+// * McMaster Unviersity 
+// * Hamilton 
+// * Ontario, Canada 
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+
+package oscar.oscarEncounter.pageUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.sql.SQLException;
+import org.apache.struts.util.MessageResources;
+import oscar.util.StringUtils;
+import oscar.util.DateUtils;
+
+/**
+ *
+ * retrieves info to display Disease entries for demographic
+ */
+public class EctDisplayRxAction extends EctDisplayAction {    
+    private String cmd = "Rx";
+    
+    public boolean getInfo(EctSessionBean bean, HttpServletRequest request, NavBarDisplayDAO Dao, MessageResources messages) {
+
+        //set lefthand module heading and link
+        String winName = "Rx" + bean.demographicNo;
+        String url = "popupPage(580,900,'" + winName + "','" + request.getContextPath() + "/oscarRx/choosePatient.do?providerNo=" + bean.providerNo + "&demographicNo=" + bean.demographicNo + "')";        
+        Dao.setLeftHeading(messages.getMessage("oscarEncounter.NavBar.Prescriptions"));
+        Dao.setLeftURL(url);
+        
+        //set righthand link to same as left so we have visual consistency with other modules
+        url += "; return false;";
+        Dao.setRightURL(url);        
+        Dao.setRightHeadingID(cmd);  //no menu so set div id to unique id for this action 
+        
+        //grab all of the diseases associated with patient and add a list item for each
+        String dbFormat = "yyyy-MM-dd";
+        String serviceDateStr;
+        Date date; 
+        oscar.oscarRx.data.RxPrescriptionData prescriptData = new oscar.oscarRx.data.RxPrescriptionData();
+        oscar.oscarRx.data.RxPrescriptionData.Prescription [] arr = {};
+        arr = prescriptData.getUniquePrescriptionsByPatient(Integer.parseInt(bean.demographicNo));
+        
+        for(int idx = 0; idx < arr.length; ++idx ) {
+            NavBarDisplayDAO.Item item = Dao.Item();                                    
+                                     
+            date = arr[idx].getRxDate();
+            serviceDateStr = DateUtils.getDate(date, dateFormat);                                        
+      
+            item.setDate(date);
+            String tmp = arr[idx].getFullOutLine().replaceAll(";", " ");
+            String strTitle = StringUtils.maxLenString(tmp, MAX_LEN_TITLE, CROP_LEN_TITLE, ELLIPSES);
+            
+            item.setTitle(strTitle + " " + serviceDateStr);
+            item.setLinkTitle(tmp + " " + serviceDateStr + " - " + arr[idx].getEndDate());
+            item.setURL("return false;");
+            Dao.addItem(item);
+        }
+        Dao.sortItems(NavBarDisplayDAO.DATESORT_ASC);
+                
+        return true;        
+    }
+    
+    public String getCmd() {
+      return cmd;
+    }
+}
