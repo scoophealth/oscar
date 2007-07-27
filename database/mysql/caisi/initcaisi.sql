cea78b84439ae1382c1eedc4f30233f159a40019
annie_zhou91 <annie_zhou91>
2007-07-27 03:35:48 +0000

    add one table programSignature

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -1987,6 +1987,20 @@ CREATE TABLE `formDischargeSummary` (
   PRIMARY KEY  (`id`)  
 );
 
+drop table if exists `programSignature`;
+create table programSignature (
+	`id` int(10) NOT NULL auto_increment,
+  	`programId` int(10) NOT NULL default '0',   	 
+  	`programName` varchar(70) NOT NULL default '',
+  	`providerId` varchar(6) NOT NULL default '0',
+  	`providerName` varchar(60) NOT NULL default '',
+  	`caisiRoleName` varchar(255) NOT NULL default '',
+  	`updateDate` datetime default '0000-00-00 00:00:00',
+  	PRIMARY KEY (`id`)
+)ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+
 -- Caisi Alter Oscar
 alter table preference add `new_tickler_warning_window` varchar(10) NOT NULL default '' after color_template;
 
