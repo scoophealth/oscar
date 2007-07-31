a81fa1e82d83bde5919db43249e3a1e2a1327ba8
annie_zhou91 <annie_zhou91>
2007-07-31 03:46:52 +0000

    update role name from 'ER Clerk' to 'er_clerk' for table secUserRole.

--- database/mysql/updates/update-2007-07-30.sql
+++ database/mysql/updates/update-2007-07-30.sql
@@ -2,4 +2,7 @@
 
 
 update secRole set role_name='er_clerk' where role_name='ER Clerk';
-insert into `secRole` values(null, 'er_clerk');
+update secUserRole set role_name='er_clerk' where role_name='ER Clerk'; 
+
+#The following line should be put in oscar db init file.
+#insert into `secRole` values(null, 'er_clerk');
