7014b8a9aae5963ced2f498b34134f68bda3238b
tobybian <tobybian>
2007-08-02 15:45:00 +0000

    2007-08-02 Alter talbe "waitingList" & "waitingListName" instead of drop/create.

--- database/mysql/updates/update-2007-08-01.sql
+++ database/mysql/updates/update-2007-08-01.sql
@@ -1,25 +1,16 @@
 
+ALTER TABLE `waitingList` 
+    ADD COLUMN `id` bigint(20) NOT NULL auto_increment PRIMARY KEY FIRST,
+    ADD COLUMN `is_history` char(1) DEFAULT NULL AFTER `onListSince`,
+    MODIFY COLUMN `listID` int(11) DEFAULT NULL,
+    MODIFY COLUMN `position` bigint(20) NOT NULL default 0;
 
-DROP TABLE IF EXISTS `waitingList`;
-CREATE TABLE `waitingList` (
-  `id` bigint(20) NOT NULL auto_increment,
-  `listID` int(11) default NULL,
-  `demographic_no` int(10) NOT NULL default '0',
-  `note` varchar(255) default NULL,
-  `position` bigint(20) NOT NULL default '0',
-  `onListSince` datetime NOT NULL default '0000-00-00 00:00:00',
-  `is_history` char(1) default NULL,
-  PRIMARY KEY  (`id`)
-);
 
-DROP TABLE IF EXISTS `waitingListName`;
-CREATE TABLE `waitingListName` (
-  `ID` bigint(11) NOT NULL auto_increment,
-  `name` varchar(80) NOT NULL default '',
-  `group_no` varchar(10) default '',
-  `provider_no` varchar(6) default '',
-  `create_date` datetime NOT NULL default '0000-00-00 00:00:00',
-  `is_history` char(1) default 'N',
-  PRIMARY KEY  (`ID`)
-);
+ALTER TABLE `waitingListName` 
+  MODIFY COLUMN `ID` bigint(11) NOT NULL auto_increment,
+  MODIFY COLUMN `name` varchar(80) NOT NULL default '',
+  ADD COLUMN `group_no` varchar(10) default '' AFTER `name`,
+  ADD COLUMN `provider_no` varchar(6) default '' AFTER  `group_no`,
+  ADD COLUMN `create_date` datetime NOT NULL default '0000-00-00 00:00:00' AFTER `provider_no`,
+  ADD COLUMN `is_history` char(1) default 'N' AFTER `create_date`;
 
