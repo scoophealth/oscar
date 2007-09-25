8c9184914e63295d5f972b0ba645df5311ca3929
opiate <opiate>
2007-09-25 14:42:51 +0000

    Refactored away all program manager module Base* classes; can no longer use the Eclipse hibernate mapping tool here, but significant reduction in number of pointless classes.

--- web/WEB-INF/classes/src/org/caisi/model/ProviderDefaultProgram.java
+++ web/WEB-INF/classes/src/org/caisi/model/ProviderDefaultProgram.java
@@ -22,39 +22,140 @@
 
 package org.caisi.model;
 
-import org.caisi.model.base.BaseProviderDefaultProgram;
+import java.io.Serializable;
 
 /**
  * This is the object class that relates to the provider_default_program table.
  * Any customizations belong here.
  */
-public class ProviderDefaultProgram extends BaseProviderDefaultProgram {
+public class ProviderDefaultProgram implements Serializable {
+    public static String PROP_PROVIDER_NO = "providerNo";
+    public static String PROP_PROGRAM_ID = "programId";
+    public static String PROP_SIGNNOTE = "signnote";
+    public static String PROP_ID = "id";
+    private int hashCode = Integer.MIN_VALUE;// primary key
+    private Integer _id;// fields
+    private String _providerNo;
+    private boolean _signnote;
+    private Integer _programId;
 
-/*[CONSTRUCTOR MARKER BEGIN]*/
-	public ProviderDefaultProgram () {
-		super();
-	}
 
-	/**
-	 * Constructor for primary key
-	 */
-	public ProviderDefaultProgram (java.lang.Integer _id) {
-		super(_id);
-	}
+    // constructors
+    public ProviderDefaultProgram () {
+        initialize();
+    }
 
-	/**
-	 * Constructor for required fields
-	 */
-	public ProviderDefaultProgram (
-		java.lang.Integer _id,
-		java.lang.String _providerNo,
-		java.lang.Integer _programId) {
+    /**
+     * Constructor for primary key
+     */
+    public ProviderDefaultProgram (Integer _id) {
+        this.setId(_id);
+        initialize();
+    }
 
-		super (
-			_id,
-			_providerNo,
-			_programId);
-	}
+    /**
+     * Constructor for required fields
+     */
+    public ProviderDefaultProgram (
+            Integer _id,
+            String _providerNo,
+            Integer _programId) {
 
-/*[CONSTRUCTOR MARKER END]*/
+        this.setId(_id);
+        this.setProviderNo(_providerNo);
+        this.setProgramId(_programId);
+        initialize();
+    }
+
+    /*[CONSTRUCTOR MARKER END]*/
+    protected void initialize () {}
+
+    /**
+     * Return the unique identifier of this class
+     * @hibernate.id
+     *  generator-class="native"
+     *  column="id"
+     */
+    public Integer getId () {
+        return _id;
+    }
+
+    /**
+     * Set the unique identifier of this class
+     * @param _id the new ID
+     */
+    public void setId (Integer _id) {
+        this._id = _id;
+        this.hashCode = Integer.MIN_VALUE;
+    }
+
+    /**
+     * Return the value associated with the column: provider_no
+     */
+    public String getProviderNo () {
+        return _providerNo;
+    }
+
+    /**
+     * Set the value related to the column: provider_no
+     * @param _providerNo the provider_no value
+     */
+    public void setProviderNo (String _providerNo) {
+        this._providerNo = _providerNo;
+    }
+
+    /**
+     * Return the value associated with the column: signnote
+     */
+    public boolean isSignnote () {
+        return _signnote;
+    }
+
+    /**
+     * Set the value related to the column: signnote
+     * @param _signnote the signnote value
+     */
+    public void setSignnote (boolean _signnote) {
+        this._signnote = _signnote;
+    }
+
+    /**
+     * Return the value associated with the column: program_id
+     */
+    public Integer getProgramId () {
+        return _programId;
+    }
+
+    /**
+     * Set the value related to the column: program_id
+     * @param _programId the program_id value
+     */
+    public void setProgramId (Integer _programId) {
+        this._programId = _programId;
+    }
+
+    public boolean equals (Object obj) {
+        if (null == obj) return false;
+        if (!(obj instanceof ProviderDefaultProgram)) return false;
+        else {
+            ProviderDefaultProgram mObj = (ProviderDefaultProgram) obj;
+            if (null == this.getId() || null == mObj.getId()) return false;
+            else return (this.getId().equals(mObj.getId()));
+        }
+    }
+
+    public int hashCode () {
+        if (Integer.MIN_VALUE == this.hashCode) {
+            if (null == this.getId()) return super.hashCode();
+            else {
+                String hashStr = this.getClass().getName() + ":" + this.getId().hashCode();
+                this.hashCode = hashStr.hashCode();
+            }
+        }
+        return this.hashCode;
+    }
+
+    public String toString () {
+        return super.toString();
+    }
 }
