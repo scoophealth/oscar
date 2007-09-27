145707894a09becf421bbd17b87922a96dfc99d6
tedleung <tedleung>
2007-09-27 18:28:27 +0000

    unified applicationContext

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/utility/CopyHealthCardInfoToDemographics.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/utility/CopyHealthCardInfoToDemographics.java
@@ -22,11 +22,8 @@
 
 package org.oscarehr.PMmodule.utility;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
-import javax.sql.DataSource;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -34,31 +31,20 @@ import org.oscarehr.PMmodule.model.Demographic;
 import org.oscarehr.PMmodule.model.Formintakea;
 import org.oscarehr.PMmodule.service.ClientManager;
 import org.oscarehr.PMmodule.service.IntakeAManager;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.oscarehr.util.DbConnectionFilter;
+import org.oscarehr.util.SpringUtils;
 
 public class CopyHealthCardInfoToDemographics {
 	protected final Log log = LogFactory.getLog(getClass());
-	
-    protected ApplicationContext ctx = null;
-    protected DataSource ds;
-    protected Connection conn;
-    
-    
+	    
     public CopyHealthCardInfoToDemographics() throws Exception {
     	int totalUpdated = 0;
     	int totalWarnings = 0;
     	
-    	String[] paths = {"/WEB-INF/applicationContext.xml"};
-    	ctx = new ClassPathXmlApplicationContext(paths);
-    
-    	IntakeAManager intakeMgr = (IntakeAManager)ctx.getBean("intakeAManager");
-    	ClientManager clientMgr = (ClientManager)ctx.getBean("clientManager");
+    	IntakeAManager intakeMgr = (IntakeAManager)SpringUtils.beanFactory.getBean("intakeAManager");
+    	ClientManager clientMgr = (ClientManager)SpringUtils.beanFactory.getBean("clientManager");
     	
-    	ds = (DataSource)ctx.getBean("dataSource");
-    	conn = ds.getConnection();
- 
-    	Statement stmt = conn.createStatement();
+    	Statement stmt = DbConnectionFilter.getThreadLocalDbConnection().createStatement();
     	stmt.execute("SELECT demographic_no FROM demographic");
     	ResultSet rs = stmt.getResultSet();
     	while(rs.next()) {
