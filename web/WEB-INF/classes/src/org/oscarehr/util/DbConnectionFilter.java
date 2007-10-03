77b6995557a85766c340ed1df0f3499a455d740f
tedleung <tedleung>
2007-10-03 18:11:55 +0000

    fixed formatting

--- web/WEB-INF/classes/src/org/oscarehr/util/DbConnectionFilter.java
+++ web/WEB-INF/classes/src/org/oscarehr/util/DbConnectionFilter.java
@@ -37,57 +37,47 @@ import javax.servlet.ServletResponse;
 
 import oscar.util.SqlUtils;
 
-public class DbConnectionFilter implements javax.servlet.Filter
-{
-    private static ThreadLocal<Connection> dbConnection=new ThreadLocal<Connection>();
+public class DbConnectionFilter implements javax.servlet.Filter {
+    private static ThreadLocal<Connection> dbConnection = new ThreadLocal<Connection>();
 
     /**
      * This map was added because not all the code was using hibernate/JDBC properly, once we 
      * fix the data access we should remove this map as it's a waste of cpu time and memory.
      */
-    public static Map<Thread, StackTraceElement[]> debugMap=Collections.synchronizedMap(new WeakHashMap<Thread, StackTraceElement[]>());
-    
-    public static Connection getThreadLocalDbConnection() throws SQLException
-    {
-        Connection c=dbConnection.get();
-        if (c==null || c.isClosed())
-        {
-            c=SpringUtils.getDbConnection();
+    public static Map<Thread, StackTraceElement[]> debugMap = Collections.synchronizedMap(new WeakHashMap<Thread, StackTraceElement[]>());
+
+    public static Connection getThreadLocalDbConnection() throws SQLException {
+        Connection c = dbConnection.get();
+        if (c == null || c.isClosed()) {
+            c = SpringUtils.getDbConnection();
             dbConnection.set(c);
             debugMap.put(Thread.currentThread(), (new Exception()).getStackTrace());
         }
 
         return(c);
     }
-    
-    
-	public void init(FilterConfig filterConfig) throws ServletException
-	{
-        // nothing
-	}
 
-	public void doFilter(ServletRequest tmpRequest, ServletResponse tmpResponse, FilterChain chain) throws IOException, ServletException
-	{
-        try
-        {
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // nothing
+    }
+
+    public void doFilter(ServletRequest tmpRequest, ServletResponse tmpResponse, FilterChain chain) throws IOException, ServletException {
+        try {
             chain.doFilter(tmpRequest, tmpResponse);
         }
-        finally
-        {
+        finally {
             releaseThreadLocalDbConnection();
         }
-	}
-
+    }
 
     public static void releaseThreadLocalDbConnection() {
-        Connection c=dbConnection.get();
+        Connection c = dbConnection.get();
         SqlUtils.closeResources(c, null, null);
         dbConnection.remove();
         debugMap.remove(Thread.currentThread());
     }
 
-	public void destroy()
-	{
-		// can't think of anything to do right now.
-	}
+    public void destroy() {
+        // can't think of anything to do right now.
+    }
 }
