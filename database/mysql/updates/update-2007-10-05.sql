d506a9a7c2082107250856fe44b5dd1b10908dac
tedleung <tedleung>
2007-10-05 18:31:14 +0000

    *** empty log message ***

--- database/mysql/updates/update-2007-10-05.sql
+++ database/mysql/updates/update-2007-10-05.sql
@@ -1,3 +1,1 @@
-INSERT INTO icd10 (code, short_desc, long_desc) VALUES ('O829','Delivery by caesarean section','Delivery by caesarean section');
-
-insert into issue values (null, 'O829','Delivery by caesarean section', 'doctor', now(), null); 
+INSERT INTO icd10 (code, short_desc, long_desc) VALUES ("O829","Delivery by caesarean section","Delivery by caesarean section");
