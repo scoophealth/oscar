9129703b909376abe453e38255f65b8597a0e6f2
jaygallagher <jaygallagher>
2007-10-24 16:57:32 +0000

    added method public int getNumTicklers(CustomFilter filter) to retrieve the number of active ticklers for a custom filter.  Also added method to simplify hql string.

--- web/WEB-INF/classes/src/org/caisi/dao/TicklerDAO.java
+++ web/WEB-INF/classes/src/org/caisi/dao/TicklerDAO.java
@@ -85,91 +85,120 @@ public class TicklerDAO extends HibernateDaoSupport {
         return (List)getHibernateTemplate().find("from Tickler");
     }
 
+    
     public List getTicklers(CustomFilter filter) {
-
         String tickler_date_order = filter.getSort_order();
         String query = "from Tickler t where t.service_date >= ? and t.service_date <= ? ";
-        boolean includeProviderClause = true;
-        boolean includeAssigneeClause = true;
-        boolean includeStatusClause = true;
-        boolean includeClientClause = true;
-        boolean includeDemographicClause = true;
-
-        if (filter.getStartDate() == null || filter.getStartDate().length() == 0) {
-            filter.setStartDate("1900-01-01");
-        }
-        if (filter.getEndDate() == null || filter.getEndDate().length() == 0) {
-            filter.setEndDate("8888-12-31");
-        }
-
-        if (filter.getProvider() == null || filter.getProvider().equals("All Providers")) {
-            includeProviderClause = false;
-        }
-        if (filter.getAssignee() == null || filter.getAssignee().equals("All Providers")) {
-            includeAssigneeClause = false;
-        }
-        if (filter.getClient() == null || filter.getClient().equals("All Clients")) {
-            includeClientClause = false;
-        }
-        if (filter.getDemographic_no() == null || filter.getDemographic_no().equals("") || filter.getDemographic_no().equalsIgnoreCase("All Clients")) {
-            includeDemographicClause = false;
-        }
-
-        if (filter.getStatus().equals("") || filter.getStatus().equals("Z")) {
-            includeStatusClause = false;
-        }
-
-        List paramList = new ArrayList();
-        paramList.add(filter.getStart_date());
-        paramList.add(new Date(filter.getEnd_date().getTime()+DateUtils.MILLIS_PER_DAY));
-
-        //TODO: IN clause
-        if (includeProviderClause) {
-            query = query + " and t.creator IN (";
-            Set pset = filter.getProviders();
-            Provider[] providers = (Provider[])pset.toArray(new Provider[pset.size()]);
-            for (int x = 0; x < providers.length; x++) {
-                if (x > 0) {
-                    query += ",";
-                }
-                query += "?";
-                paramList.add(providers[x].getProvider_no());
-            }
-            query += ")";
-        }
-
-        //TODO: IN clause
-        if (includeAssigneeClause) {
-            query = query + " and t.task_assigned_to IN (";
-            Set pset = filter.getAssignees();
-            Provider[] providers = (Provider[])pset.toArray(new Provider[pset.size()]);
-            for (int x = 0; x < providers.length; x++) {
-                if (x > 0) {
-                    query += ",";
-                }
-                query += "?";
-                paramList.add(providers[x].getProvider_no());
-            }
-            query += ")";
-        }
-
-        if (includeStatusClause) {
-            query = query + " and t.status = ?";
-            paramList.add(String.valueOf(filter.getStatus()));
-        }
-        if (includeClientClause) {
-            query = query + "and t.demographic_no = ?";
-            paramList.add(filter.getClient());
-        }
-        if (includeDemographicClause) {
-            query = query + "and t.demographic_no = ?";
-            paramList.add(filter.getDemographic_no());
-        }
+        ArrayList paramList = new ArrayList();
+        query = getTicklerQueryString(query,  paramList,  filter);
         Object params[] = paramList.toArray(new Object[paramList.size()]);
-
         return (List)getHibernateTemplate().find(query + "order by t.service_date " + tickler_date_order, params);
     }
+    
+    public int getNumTicklers(CustomFilter filter){
+            ArrayList paramList = new ArrayList();
+            String query = "select count(*) from Tickler t where t.service_date >= ? and t.service_date <= ? ";   
+            query = getTicklerQueryString(query,  paramList,  filter);
+            Object params[] = paramList.toArray(new Object[paramList.size()]);
+            listParams(params);
+            Long count = (Long) getHibernateTemplate().find(query ,params).get(0);
+            return count.intValue();
+     }
+    
+    private String getTicklerQueryString(String query, List paramList, CustomFilter filter){
+            boolean includeProviderClause = true;
+            boolean includeAssigneeClause = true;
+            boolean includeStatusClause = true;
+            boolean includeClientClause = true;
+            boolean includeDemographicClause = true;
 
+            if (filter.getStartDate() == null || filter.getStartDate().length() == 0) {
+                filter.setStartDate("1900-01-01");
+            }
+            if (filter.getEndDate() == null || filter.getEndDate().length() == 0) {
+                filter.setEndDate("8888-12-31");
+            }
+             
+            if (filter.getProvider() == null || filter.getProvider().equals("All Providers")) {
+                    includeProviderClause=false;
+            }
+            if (filter.getAssignee() == null || filter.getAssignee().equals("All Providers")) {
+                    includeAssigneeClause=false;
+            }
+            if (filter.getClient() == null || filter.getClient().equals("All Clients")) {
+                    includeClientClause=false;
+            }
+            if (filter.getDemographic_no()==null||filter.getDemographic_no().equals("")||filter.getDemographic_no().equalsIgnoreCase("All Clients")) {
+                    includeDemographicClause=false;
+            }
+
+            if (filter.getStatus().equals("") || filter.getStatus().equals("Z")) {
+                    includeStatusClause = false;
+            }
+
+            
+            paramList.add(filter.getStart_date());
+            paramList.add(new Date(filter.getEnd_date().getTime()+DateUtils.MILLIS_PER_DAY));
+
+            //TODO: IN clause
+            if(includeProviderClause) {
+                    query = query + " and t.creator IN (";
+                    Set pset = filter.getProviders();
+                    Provider[] providers = (Provider[])pset.toArray(new Provider[pset.size()]);
+                    for(int x=0;x<providers.length;x++) {
+                            if(x>0) {
+                                    query += ",";
+                            }
+                            query += "?";
+                            paramList.add(providers[x].getProvider_no());
+                    }
+                    query += ")";
+            }
+
+            //TODO: IN clause
+            if(includeAssigneeClause) {
+                    query = query + " and t.task_assigned_to IN (";
+                    Set pset = filter.getAssignees();
+                    Provider[] providers = (Provider[])pset.toArray(new Provider[pset.size()]);
+                    for(int x=0;x<providers.length;x++) {
+                            if(x>0) {
+                                    query += ",";
+                            }
+                            query += "?";
+                            paramList.add(providers[x].getProvider_no());
+                    }
+                    query += ")";
+            }
+
+            if(includeStatusClause) {
+                    query = query + " and t.status = ?";
+                    paramList.add(String.valueOf(filter.getStatus()));
+            }
+            if(includeClientClause) {
+                    query = query + "and t.demographic_no = ?";
+                    paramList.add(filter.getClient());
+            }
+            if(includeDemographicClause) {
+                    query = query + "and t.demographic_no = ?";
+                    paramList.add(filter.getDemographic_no());
+            }
+            return query;
+    }
+   
+     
+    
+
+
+    private void listParams(Object params[]){
+        for(Object obj: params){
+            System.out.print("Object Type:"+obj.getClass().getName());
+            System.out.print(" "+obj.toString());
+            System.out.println("");
+        }
+        
+    }
+    
+    
     private void updateTickler(Long tickler_id, String provider, char status) {
         Tickler tickler = this.getTickler(tickler_id);
         if (tickler != null) {
