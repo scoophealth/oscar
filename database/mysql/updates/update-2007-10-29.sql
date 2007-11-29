712e7d67cf7108e7e3d856d697def71683751137
annie_zhou91 <annie_zhou91>
2007-11-29 19:34:03 +0000

    added 'default NULL'.

--- database/mysql/updates/update-2007-10-29.sql
+++ database/mysql/updates/update-2007-10-29.sql
@@ -1,6 +1,6 @@
-ALTER TABLE demographic ADD COLUMN alias varchar(70);
-ALTER TABLE demographic ADD COLUMN previousAddress varchar(255);
-ALTER TABLE demographic ADD COLUMN children varchar(255);
-ALTER TABLE demographic ADD COLUMN sourceOfIncome varchar(255);
-ALTER TABLE demographic ADD COLUMN citizenship varchar(40);
-ALTER TABLE demographic ADD COLUMN sin varchar(15);
+ALTER TABLE demographic ADD COLUMN alias varchar(70) default NULL;
+ALTER TABLE demographic ADD COLUMN previousAddress varchar(255) default NULL;
+ALTER TABLE demographic ADD COLUMN children varchar(255) default NULL;
+ALTER TABLE demographic ADD COLUMN sourceOfIncome varchar(255) default NULL;
+ALTER TABLE demographic ADD COLUMN citizenship varchar(40) default NULL;
+ALTER TABLE demographic ADD COLUMN sin varchar(15) default NULL;
