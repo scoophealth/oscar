93c2dc0e72bea7b628a4b61082b850f2b5db76b4
cronnie <cronnie>
2007-11-30 15:02:41 +0000

    added "addAppointment" class and an "ohip_no" field

--- web/WEB-INF/classes/src/oscar/appt/AppointmentDAO.java
+++ web/WEB-INF/classes/src/oscar/appt/AppointmentDAO.java
@@ -23,8 +23,8 @@
  */
 package oscar.appt;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.math.BigInteger;
+import java.sql.*;
 import java.text.FieldPosition;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -40,10 +40,38 @@ public class AppointmentDAO extends DAO {
     public AppointmentDAO(Properties pvar) throws SQLException {
         super(pvar);
     }
+    
+    public int addAppointment(String providerNo, Date appointmentDate, Date startTime, Date endTime, String name, String demographicNo, String notes, String reason, String status) throws SQLException {
+	String add_record_string = "insert into appointment (provider_no,appointment_date,start_time,end_time,name,demographic_no,notes,reason,status) values (?,?,?,?,?,?,?,?,?)";
+	int key = 0;
+	
+	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+	Connection conn = db.GetConnection();
+	PreparedStatement add_record = conn.prepareStatement(add_record_string);
+	
+	add_record.setString(1, providerNo);
+	add_record.setDate(2, new java.sql.Date(appointmentDate.getTime()));
+	add_record.setDate(3, new java.sql.Date(startTime.getTime()));
+	add_record.setDate(4, new java.sql.Date(endTime.getTime()));
+	add_record.setString(5, name);
+	add_record.setString(6, demographicNo);
+	add_record.setString(7, notes);
+	add_record.setString(8, reason);
+	add_record.setString(9, status);
 
+	add_record.executeUpdate();
+	ResultSet rs = add_record.getGeneratedKeys();
+	if(rs.next()) key = rs.getInt(1);
+	add_record.close();
+	rs.close();
+	db.CloseConn();
+	
+	return key;
+    }
+    
     public Vector retrieve(String demo_no) throws SQLException, ParseException {
 	Vector appointments = new Vector();
-        String sql = "select app.*, prov.first_name, prov.last_name from appointment app, provider prov " +
+        String sql = "select app.*, prov.first_name, prov.last_name, prov.ohip_no from appointment app, provider prov " +
 		     "where app.provider_no = prov.provider_no and app.demographic_no =" + demo_no;
 
         DBHandler db = getDb();
@@ -51,7 +79,7 @@ public class AppointmentDAO extends DAO {
             ResultSet rs = db.GetSQL(sql);
 
             while (rs.next()) {
-		Appointment ap = new Appointment(rs.getString("prov.first_name"), rs.getString("prov.last_name"));
+		Appointment ap = new Appointment(rs.getString("prov.first_name"), rs.getString("prov.last_name"), rs.getString("prov.ohip_no"));
 		
 		ap.setAppointment_no(rs.getString("appointment_no"));
 		ap.setAppointment_date(rs.getString("appointment_date"));
@@ -69,13 +97,15 @@ public class AppointmentDAO extends DAO {
         return appointments;
     }
     
-    public class Appointment extends ApptData {
+   public class Appointment extends ApptData {
 	String providerLastName = null;
 	String providerFirstName = null;
+	String ohipNo = null;
 	
-	public Appointment(String firstname, String lastname) {
+	public Appointment(String firstname, String lastname, String ohip_no) {
 	    this.providerFirstName = firstname;
 	    this.providerLastName  = lastname;
+	    this.ohipNo = ohip_no;
 	}
 	
 	public Date getDateAppointment() throws ParseException {
@@ -96,6 +126,10 @@ public class AppointmentDAO extends DAO {
 	
 	public String getProviderLastN() {
 	    return this.providerLastName;
+	}
+	
+	public String getOhipNo() {
+	    return this.ohipNo;
 	}
 	
 	private Date string_date(String s, boolean isDate) throws ParseException {
