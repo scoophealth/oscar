54c89a6aaab208756e3a79cc7927bc3fe475d3d8
opiate <opiate>
2007-12-01 04:57:18 +0000

    oops, I broked the build... Missing class

---
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/service/ClientRestrictionManager.java
@@ -0,0 +1,82 @@
+/*
+*
+* Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+* This software is published under the GPL GNU General Public License.
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version. *
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+*
+* <OSCAR TEAM>
+*
+* This software was written for
+* Centre for Research on Inner City Health, St. Michael's Hospital,
+* Toronto, Ontario, Canada
+*/
+
+package org.oscarehr.PMmodule.service;
+
+import org.oscarehr.PMmodule.dao.ProgramClientRestrictionDAO;
+import org.oscarehr.PMmodule.model.ProgramClientRestriction;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Manage client restrictions
+ */
+public class ClientRestrictionManager {
+
+    private ProgramClientRestrictionDAO programClientRestrictionDAO;
+
+    public List<ProgramClientRestriction> getRestrictionsForProgram(int programId, Date asOfDate) {
+        // check dao for restriction
+        Collection<ProgramClientRestriction> pcrs = programClientRestrictionDAO.findForProgram(programId);
+        List<ProgramClientRestriction> returnPcrs = new ArrayList<ProgramClientRestriction>();
+        if (pcrs != null && !pcrs.isEmpty()) {
+            for (ProgramClientRestriction pcr : pcrs) {
+                if (pcr.getStartDate().getTime() >= asOfDate.getTime() && pcr.getEndDate().getTime() <= pcr.getEndDate().getTime())
+                    returnPcrs.add(pcr);
+            }
+        }
+        return returnPcrs;        
+    }
+
+    public ProgramClientRestriction checkClientRestriction(int programId, int demographicNo, Date asOfDate) {
+        // check dao for restriction
+        Collection<ProgramClientRestriction> pcrs = programClientRestrictionDAO.find(programId, demographicNo);
+        if (pcrs != null && !pcrs.isEmpty()) {
+            for (ProgramClientRestriction pcr : pcrs) {
+                if (pcr.getStartDate().getTime() <= asOfDate.getTime() && asOfDate.getTime() <= pcr.getEndDate().getTime())
+                    return pcr;
+            }
+        }
+        return null;
+    }
+
+    public void saveClientRestriction(ProgramClientRestriction restriction) {
+        programClientRestrictionDAO.save(restriction);
+    }
+
+    public void disableClientRestriction(int restrictionId) {
+        ProgramClientRestriction pcr = programClientRestrictionDAO.find(restrictionId);
+        pcr.setEnabled(false);
+        saveClientRestriction(pcr);
+    }
+
+    public ProgramClientRestrictionDAO getProgramClientRestrictionDAO() {
+        return programClientRestrictionDAO;
+    }
+
+    public void setProgramClientRestrictionDAO(ProgramClientRestrictionDAO programClientRestrictionDAO) {
+        this.programClientRestrictionDAO = programClientRestrictionDAO;
+    }
+}
