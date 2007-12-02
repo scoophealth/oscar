23057b911bf1faa2a1422aaa5fd0233ad48911e9
annie_zhou91 <annie_zhou91>
2007-12-02 21:46:48 +0000

    added one new table 'caisi_editor'

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -2093,3 +2093,14 @@ CREATE TABLE `joint_admissions` (
   KEY `head_client_id` (`head_client_id`)
 );
 
+CREATE TABLE `caisi_editor` (
+  `id` bigint(11) NOT NULL auto_increment,
+  `category` varchar(50) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `labelValue` varchar(255) default NULL,
+  `labelCode` varchar(50) default NULL,
+  `horizontal` char(3) default NULL,
+  `isActive` char(3) default NULL,  
+  PRIMARY KEY  (`id`)
+);
