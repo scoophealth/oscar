8747e4eecce235d769149ec1d5bce66bbb7eed80
annie_zhou91 <annie_zhou91>
2007-12-04 01:13:17 +0000

    *** empty log message ***

---
+++ database/mysql/caisi/updates/patch-2007-12-03(3).sql
@@ -0,0 +1,25 @@
+insert into secObjectName values ('_merge');
+
+insert into access_type (name, type) values("Sex restriction override on referral","access");
+insert into access_type (name, type) values("Sex restriction override on admission","access");
+insert into access_type (name, type) values("Gender restriction override on referral","access");
+insert into access_type (name, type) values("Gender restriction override on referral","access");
+insert into access_type (name, type) values("Age restriction override on referral","access");
+insert into access_type (name, type) values("Age restriction override on admission","access");
+insert into access_type (name, type) values("Service restriction override on referral","access");
+insert into access_type (name, type) values("Service restriction override on admission","access");
+insert into access_type (name, type) values("Allow duplicate client merge","access");
+insert into access_type (name, type) values("Service restriction override on restriction creation","access");
+
+insert into access_type (name, type) values("Perform program registration intake","access");
+insert into access_type (name, type) values("can make program admissions","access");
+insert into access_type (name, type) values("can make program discharge","access");
+insert into access_type (name, type) values("can make bed assignments","access");
+
+insert into access_type (name, type) values("perform registration intake","access");
+insert into access_type (name, type) values("perform admissions","access");
+insert into access_type (name, type) values("perform discharges","access");
+insert into access_type (name, type) values("perform bed assignments","access");
+
+insert into access_type (name, type) values("print bed rosters and reports","access");
+
