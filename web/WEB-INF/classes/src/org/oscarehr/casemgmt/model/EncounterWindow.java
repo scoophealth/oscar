3edd888bff02b63c76bf768445be60f4492c8c51
rjonasz <rjonasz>
2007-12-21 01:24:39 +0000

    Holds sizes of cpp textareas

---
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/model/EncounterWindow.java
@@ -0,0 +1,70 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * <OSCAR TEAM>
+ *
+ * EncounterWindow.java
+ *
+ * Created on December 18, 2007, 3:42 PM
+ *
+ *
+ *
+ */
+
+package org.oscarehr.casemgmt.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author rjonasz
+ */
+public class EncounterWindow implements Serializable {
+    
+    public static final int NORMAL = 60;
+    
+    private String provider_no;
+    private int rowOneSize;
+    private int rowTwoSize;
+    
+    public int getRowTwoSize() {
+        return this.rowTwoSize;
+    }
+    
+    public void setRowTwoSize(int rowTwoSize) {
+        this.rowTwoSize = rowTwoSize;
+    }     
+    
+    public int getRowOneSize() {
+        return this.rowOneSize;
+    }
+    
+    public void setRowOneSize(int rowOneSize) {
+        this.rowOneSize = rowOneSize;
+    } 
+    
+    public String getProvider_no() {
+        return this.provider_no;
+    }
+    
+    public void setProvider_no(String provider_no) {
+        this.provider_no = provider_no;
+    }
+    
+    /** Creates a new instance of EncounterWindow */
+    public EncounterWindow() {
+    }
+    
+}
