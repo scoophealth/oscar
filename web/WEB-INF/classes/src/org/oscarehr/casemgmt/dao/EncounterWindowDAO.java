737d006be94bbf3fbdc9620d4de58b07c4170bfb
rjonasz <rjonasz>
2007-12-21 01:25:14 +0000

    Manages db access for encounterWindow

---
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/EncounterWindowDAO.java
@@ -0,0 +1,49 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * <OSCAR TEAM>
+ *
+ * EncounterWindowDAO.java
+ *
+ * Created on December 18, 2007, 3:39 PM
+ *
+ *
+ *
+ */
+
+package org.oscarehr.casemgmt.dao;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.oscarehr.casemgmt.model.EncounterWindow;
+/**
+ *
+ * @author rjonasz
+ */
+public class EncounterWindowDAO extends HibernateDaoSupport {
+    
+    /** Creates a new instance of EncounterWindowDAO */
+    public EncounterWindowDAO() {
+    }
+    
+    public void saveWindowDimensions( EncounterWindow ectWin ) {
+        this.getHibernateTemplate().saveOrUpdate(ectWin);
+    }
+    
+    public EncounterWindow getWindow(String provider) {
+        EncounterWindow ectWin = (EncounterWindow)this.getHibernateTemplate().get(EncounterWindow.class,provider);
+        return ectWin;
+    }
+    
+}
