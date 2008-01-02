5d2b6ebc65df09920d085f3c11f94b316d4300cd
tedleung <tedleung>
2008-01-02 23:44:39 +0000

    removed unused imports

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/admin/ProgramManagerViewAction.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/admin/ProgramManagerViewAction.java
@@ -26,7 +26,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Enumeration;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
