59b1c2f4a0b4c8dbc71b52d60f6c4366f0c69f04
cronnie <cronnie>
2008-01-22 07:55:23 +0000

    Added a method to import data into the labReportInformation table

--- web/WEB-INF/classes/src/oscar/oscarLab/ca/on/LabResultImport.java
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/on/LabResultImport.java
@@ -30,40 +30,52 @@ import java.sql.*;
 
 public class LabResultImport {
    
-    public boolean SaveLabTR(String testName, String abn, String minimum, String maximum, String result, String units, String description, String ppId) throws SQLException {
+    public void SaveLabTR(String testName, String abn, String minimum, String maximum, String result, String units, String description, String ppId) throws SQLException {
 	boolean b = false;            
 	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);            
 	String sql = "INSERT INTO labTestResults (test_name, abn, minimum, maximum, result, units, description, labPatientPhysicianInfo_id) VALUES ('"
 		+ testName + "','" + abn + "','" + minimum + "','" + maximum + "','" + result + "','" + units + "','" + description + "','" + ppId + "')";
-	b = db.RunSQL(sql);
+	db.RunSQL(sql);
 	db.CloseConn();
-	
-	return b;            
     }
     
-    public String saveLabPPInfo(String collDate) throws SQLException {
-	String id = "1";
+    public String saveLabPPInfo(String labReportInfo_id, String accession_num, String firstname, String lastname, String sex, String hin, String birthdate, String collDate) throws SQLException {
+	int id = 1;
 	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
 	
-	String sql = "insert into labPatientPhysicianInfo (collection_date) values ('" + collDate + "')";
-	if (db.RunSQL(sql)) {
-	    sql = "select max(id) from labPatientPhysicianInfo";
-	    ResultSet rs = db.GetSQL(sql);
-	    if (rs.next()) id = rs.getString(1);
-	    rs.close();
-	}
+	String sql = "insert into labPatientPhysicianInfo (labReportInfo_id, accession_num, patient_first_name, patient_last_name, patient_sex, patient_health_num, patient_dob, collection_date) values ('"
+		+labReportInfo_id+"','"+accession_num+"','"+firstname+"','"+lastname+"','"+sex+"','"+hin+"','"+birthdate+"','"+collDate+"')";
+	db.RunSQL(sql);
+	sql = "select max(id) from labPatientPhysicianInfo";
+	ResultSet rs = db.GetSQL(sql);
+	if (rs.next()) id = rs.getInt(1);
+	
+	rs.close();
 	db.CloseConn();
 	
-	return id;
+	return String.valueOf(id);
     }
     
-    public boolean savePatientLR(String demo_no, String lab_no) throws SQLException {
+    public void savePatientLR(String demo_no, String lab_no) throws SQLException {
 	boolean b = false;
 	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
 	String sql = "insert into patientLabRouting (demographic_no, lab_no, lab_type) values ('"+demo_no+"', '"+lab_no+"', 'CML')";
 	b = db.RunSQL(sql);
 	db.CloseConn();
+    }
+    
+    public String saveLabRI(String location_id, String print_date, String print_time) throws SQLException {
+	int id = 1;
+	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+	String sql = "INSERT INTO labReportInformation (location_id, print_date, print_time) VALUES ('"+location_id+"', '"+print_date+"', '"+print_time+"')";
+	db.RunSQL(sql);
+	sql = "SELECT MAX(id) FROM labReportInformation";
+	ResultSet rs = db.GetSQL(sql);
+	if (rs.next()) id = rs.getInt(1);
 	
-	return b;
+	rs.close();
+	db.CloseConn();
+	
+	return String.valueOf(id);
     }
 }
