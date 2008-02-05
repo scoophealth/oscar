97f42a4752f905bb096446c85ccee43115c3c56e
annie_zhou91 <annie_zhou91>
2008-02-05 00:35:25 +0000

    added the new table facility_message.

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -1962,6 +1962,19 @@ CREATE TABLE `system_message` (
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+
+DROP TABLE IF EXISTS `system_message`;
+CREATE TABLE `facility_message` (
+  `id` int(10) NOT NULL auto_increment,
+  `message` text NOT NULL,
+  `creation_date` datetime NOT NULL default '1900-01-01 00:00:00',
+  `expiry_date` datetime NOT NULL default '1900-01-01 00:00:00',
+  `facility_id` int(22),
+  `facility_name` varchar(32),
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
 DROP TABLE IF EXISTS `pmm_log`;
 create table pmm_log (
     id bigint not null auto_increment,
