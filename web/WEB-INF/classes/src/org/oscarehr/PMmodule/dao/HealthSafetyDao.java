2b37683d961f1e6ef33ed0d748c7decd6852de9d
dawsonli <dawsonli>
2008-03-02 00:06:07 +0000

    *** empty log message ***

---
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/dao/HealthSafetyDao.java
@@ -0,0 +1,65 @@
+/*
+* 
+* Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+* This software is published under the GPL GNU General Public License. 
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation; either version 2 
+* of the License, or (at your option) any later version. * 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+* along with this program; if not, write to the Free Software 
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+* 
+* <OSCAR TEAM>
+* 
+* This software was written for 
+* Centre for Research on Inner City Health, St. Michael's Hospital, 
+* Toronto, Ontario, Canada 
+*/
+
+package org.oscarehr.PMmodule.dao;
+
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.oscarehr.PMmodule.model.HealthSafety;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class HealthSafetyDao extends HibernateDaoSupport {
+
+    private Log log = LogFactory.getLog(HealthSafetyDao.class);
+
+    public HealthSafety getHealthSafetyByDemographic(Long demographicNo) {
+        if (demographicNo == null || demographicNo.intValue() <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        HealthSafety result = null;
+
+        List list = this.getHibernateTemplate().find("from HealthSafety c where c.demographicNo=? order by c.updateDate desc", demographicNo);
+        if (!list.isEmpty()) result = (HealthSafety)list.get(0);
+
+        if (log.isDebugEnabled()) {
+            log.debug("getHealthSafetyByDemographic:id=" + demographicNo + ",found=" + (result != null));
+        }
+
+        return result;
+    }
+
+    public void saveHealthSafetyByDemographic(HealthSafety healthsafety) {
+        if (healthsafety == null) {
+            throw new IllegalArgumentException();
+        }
+
+        this.getHibernateTemplate().save(healthsafety);
+
+        if (log.isDebugEnabled()) {
+            log.debug("saveHealthSafetyByDemographic:id=" + healthsafety.getId());
+        }
+    }
+
+}
---
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/model/HealthSafety.java
@@ -0,0 +1,103 @@
+/*
+* 
+* Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+* This software is published under the GPL GNU General Public License. 
+* This program is free software; you can redistribute it and/or 
+* modify it under the terms of the GNU General Public License 
+* as published by the Free Software Foundation; either version 2 
+* of the License, or (at your option) any later version. * 
+* This program is distributed in the hope that it will be useful, 
+* but WITHOUT ANY WARRANTY; without even the implied warranty of 
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+* GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+* along with this program; if not, write to the Free Software 
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+* 
+* <OSCAR TEAM>
+* 
+* This software was written for 
+* Centre for Research on Inner City Health, St. Michael's Hospital, 
+* Toronto, Ontario, Canada 
+*/
+
+package org.oscarehr.PMmodule.model;
+
+import java.io.Serializable;
+
+/**
+ * This is the object class that relates to the health_safety table.
+ * Any customizations belong here.
+ */
+public class HealthSafety  implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int hashCode = Integer.MIN_VALUE;// primary key
+	
+	private long id;
+    private Long demographicNo;
+	private String message;
+    private String userName;
+    private java.util.Date updateDate;
+
+    public Long getId () {
+        return id;
+    }
+
+    public void setId (Long _id) {
+        this.id = _id;
+        this.hashCode = Integer.MIN_VALUE;
+    }
+    
+    public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public java.util.Date getUpdateDate() {
+		return updateDate;
+	}
+	public void setUpdateDate(java.util.Date updateDate) {
+		this.updateDate = updateDate;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Long getDemographicNo() {
+		return demographicNo;
+	}
+
+	public void setDemographicNo(Long demographicNo) {
+		this.demographicNo = demographicNo;
+	}
+	
+    public boolean equals (Object obj) {
+        if (null == obj) return false;
+        if (!(obj instanceof HealthSafety)) return false;
+        else {
+            HealthSafety mObj = (HealthSafety) obj;
+            if (null == this.getId() || null == mObj.getId()) return false;
+            else return (this.getId().equals(mObj.getId()));
+        }
+    }
+
+    public int hashCode () {
+        if (Integer.MIN_VALUE == this.hashCode) {
+            if (null == this.getId()) return super.hashCode();
+            else {
+                String hashStr = this.getClass().getName() + ":" + this.getId().hashCode();
+                this.hashCode = hashStr.hashCode();
+            }
+        }
+        return this.hashCode;
+    }
+
+    public String toString () {
+        return super.toString();
+    }
+	
+}
