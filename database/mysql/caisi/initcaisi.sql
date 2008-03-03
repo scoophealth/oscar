9625a2a5bda89430516f8b40424b3e3352034c58
annie_zhou91 <annie_zhou91>
2008-03-03 19:11:17 +0000

    added a new table health_safety.

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -2170,3 +2170,13 @@ CREATE TABLE `caisi_editor` (
   `isActive` char(3) default NULL,  
   PRIMARY KEY  (`id`)
 );
+
+create table health_safety
+(
+  id             	bigint(20) not null auto_increment,
+  demographic_no 	bigint(20) default '0' not null,
+  message        	text,
+  username       	varchar(128),
+  updatedate    	datetime,
+  primary key (id)
+);
