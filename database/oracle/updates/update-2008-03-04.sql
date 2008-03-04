8ef29054b6e26aaa0c2402831165e0342ef128bc
tonywzt1 <tonywzt1>
2008-03-04 17:01:30 +0000

    *** empty log message ***

---
+++ database/oracle/updates/update-2008-03-04.sql
@@ -0,0 +1,12 @@
+create table APP_CONFIG
+(
+  PARAMID       NUMBER(10) not null,
+  PARAMETER     VARCHAR2(50) not null,
+  PARAMVALUE    VARCHAR2(128) not null,
+  EFFECTIVEDATE DATE,
+  EXPIRYDATE    DATE,
+  primary key (PARAMID)
+)
+;
+insert into app_config(PARAMID,PARAMETER,PARAMVALUE,EFFECTIVEDATE,EXPIRYDATE)
+values(1,'PIN_ENCRYPTED',0,to_date('2008-01-01','YYYY-MM-DD'),TO_DATE('2999-12-31','YYYY-MM-DD'));
