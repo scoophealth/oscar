af523a0c788d4beb377b89764a28262b84e4cb19
annie_zhou91 <annie_zhou91>
2008-03-10 22:48:16 +0000

    *** empty log message ***

---
+++ database/mysql/caisi/updates/patch-2008-03-10.sql
@@ -0,0 +1,23 @@
+insert into `secObjectName` values('_pmm_editProgram.general');
+insert into `secObjectName` values('_pmm_editProgram.staff');
+insert into `secObjectName` values('_pmm_editProgram.functionUser');
+insert into `secObjectName` values('_pmm_editProgram.teams');
+insert into `secObjectName` values('_pmm_editProgram.clients');
+insert into `secObjectName` values('_pmm_editProgram.queue');
+insert into `secObjectName` values('_pmm_editProgram.access');
+insert into `secObjectName` values('_pmm_editProgram.bedCheck');
+insert into `secObjectName` values('_pmm_editProgram.clientStatus');
+insert into `secObjectName` values('_pmm_editProgram.serviceRestrictions');
+
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.general','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.staff','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.functionUser','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.teams','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.clients','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.queue','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.access','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.bedCheck','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.clientStatus','|*|',0,999998);
+insert into `secObjPrivilege` values('doctor','_pmm_editProgram.serviceRestrictions','|*|',0,999998);
+
+INSERT INTO `access_type` (name,type) VALUES ('read doctor ticklers','access'),('read nurse ticklers','access'),('read counsellor ticklers','access'),('read csw ticklers','access');
