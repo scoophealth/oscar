13d964cdf114973101f4330aec3070ab567e1442
tonywzt1 <tonywzt1>
2008-03-13 17:56:00 +0000

    *** empty log message ***

--- database/oracle/updates/update-2008-03-13.sql
+++ database/oracle/updates/update-2008-03-13.sql
@@ -9,3 +9,31 @@ BEGIN
      SELECT HIBERNATE_SEQUENCE.NEXTVAL INTO IDX FROM DUAL;
      :new.id := IDX;
 END;
+
+create table REDIRECTLINKTRACKING
+(
+  DATE1          DATE not null,
+  PROVIDER_NO    VARCHAR2(6) not null,
+  REDIRECTLINKID INTEGER not null
+)
+;
+create index IDX_REDIRECTLINKRTRACKING on REDIRECTLINKTRACKING (DATE1, REDIRECTLINKID);
+CREATE OR REPLACE TRIGGER TRI_FACILITY_ID
+BEFORE INSERT
+ON FACILITY
+FOR EACH ROW
+DECLARE
+       IDX NUMBER;
+BEGIN
+     IF :new.ID IS NULL THEN
+          SELECT HIBERNATE_SEQUENCE.NEXTVAL INTO IDX FROM DUAL;
+          :new.ID := IDX;
+     END IF;
+END;
+-- update provider_facility's provider_no to varchar2(6) from int
+create table provider_facility_tmp as select * from provider_facility;
+truncate table provider_facility;
+alter table provider_facility modify provider_id varchar2(6);
+insert into provider_facility select * from provider_facility_tmp;
+drop table provider_facility_tmp;
+
