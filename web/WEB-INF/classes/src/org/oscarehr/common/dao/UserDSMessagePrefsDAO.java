06507ac8896eb7e87c7da11c6e382b1dec00ef22
jaygallagher <jaygallagher>
2008-03-31 22:26:08 +0000

    Init-Commit, classes used to control which ds messages are shown to users

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/dao/UserDSMessagePrefsDAO.java
@@ -0,0 +1,71 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * UserPropertyDAO.java
+ *
+ * Created on December 19, 2007, 4:29 PM
+ *
+ *
+ *
+ */
+
+package org.oscarehr.common.dao;
+
+import java.util.Hashtable;
+import java.util.List;
+
+import org.oscarehr.common.model.UserDSMessagePrefs;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ *
+ * @author Jason Gallagher
+ */
+public class UserDSMessagePrefsDAO extends HibernateDaoSupport {
+    
+    /** Creates a new instance of UserPropertyDAO */
+    public UserDSMessagePrefsDAO() {
+    }
+    
+    public void saveProp(UserDSMessagePrefs prop) {
+        this.getHibernateTemplate().saveOrUpdate(prop);
+    }
+    
+    public UserDSMessagePrefs getMessagePrefsOnType(String prov, String name) {
+        List list = this.getHibernateTemplate().find("from UserDSMessagePrefs p where p.providerNo = ? and p.resourceType = ? and p.archived = false ", new Object[] {prov,name});
+        if( list != null && list.size() > 0 ) {
+            UserDSMessagePrefs prop = (UserDSMessagePrefs)list.get(0);
+            return prop;
+        }
+        else
+            return null;            
+    }
+    
+    
+    
+    public Hashtable getHashofMessages(String providerNo,String name){
+        Hashtable retHash = new Hashtable();
+        List<UserDSMessagePrefs> list = this.getHibernateTemplate().find("from UserDSMessagePrefs p where p.providerNo = ? and p.resourceType = ? and p.archived = false", new Object[] {providerNo,name});
+        if( list != null && list.size() > 0 ) {
+            for(UserDSMessagePrefs pref: list){
+                retHash.put(pref.getResourceType()+pref.getResourceId(),pref.getResourceUpdatedDate().getTime());
+            }
+        }
+        return retHash;
+    }
+    
+}
---
+++ web/WEB-INF/classes/src/org/oscarehr/common/model/UserDSMessagePrefs.java
@@ -0,0 +1,110 @@
+/*
+ *
+ * Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * Jason Gallagher
+ *
+ * UserProperty.java
+ *
+ * Created on December 19, 2007, 4:30 PM
+ *
+ *
+ *
+ */
+
+package org.oscarehr.common.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author rjonasz
+ */
+public class UserDSMessagePrefs implements Serializable {
+    
+    public final static String MYDRUGREF = "mydrugref";
+   
+    private long id;
+    private String resourceType;
+    private String resourceId;
+    private Date resourceUpdatedDate;
+    private String providerNo;
+    private Date recordCreated;
+    private Boolean archived;
+        
+    
+    
+    public long getId() {
+        return this.id;
+    }
+    
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    
+    /** Creates a new instance of UserProperty */
+    public UserDSMessagePrefs() {
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public void setResourceType(String resourceType) {
+        this.resourceType = resourceType;
+    }
+
+    public String getResourceId() {
+        return resourceId;
+    }
+
+    public void setResourceId(String resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public Date getResourceUpdatedDate() {
+        return resourceUpdatedDate;
+    }
+
+    public void setResourceUpdatedDate(Date resourceUpdatedDate) {
+        this.resourceUpdatedDate = resourceUpdatedDate;
+    }
+
+    public String getProviderNo() {
+        return providerNo;
+    }
+
+    public void setProviderNo(String providerNo) {
+        this.providerNo = providerNo;
+    }
+
+    public Date getRecordCreated() {
+        return recordCreated;
+    }
+
+    public void setRecordCreated(Date recordCreated) {
+        this.recordCreated = recordCreated;
+    }
+
+    public Boolean getArchived() {
+        return archived;
+    }
+
+    public void setArchived(Boolean archived) {
+        this.archived = archived;
+    }
+    
+}
