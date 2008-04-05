34d963787efd3cb3c982f19003401054e5e29c91
jaygallagher <jaygallagher>
2008-04-05 00:52:57 +0000

    added rows needed for controling age specific codes

--- database/mysql/updates/update-2008-04-04.sql
+++ database/mysql/updates/update-2008-04-04.sql
@@ -8,3 +8,10 @@ insert into issue (`code`,`description`,`role`,`update_date`) Values('SocHistory
 insert into issue (`code`,`description`,`role`,`update_date`) Values('MedHistory','Medical History as part of cpp', 'nurse', now());
 insert into issue (`code`,`description`,`role`,`update_date`) Values('Concerns','Ongoing Concerns as part of cpp', 'nurse', now());
 insert into issue (`code`,`description`,`role`,`update_date`) Values('Reminders','Reminders as part of cpp', 'nurse', now());
+
+
+
+insert into ctl_billingservice_age_rules (minAge, maxAge, service_code ) values (70,79,'14047');
+insert into ctl_billingservice_age_rules (minAge, maxAge, service_code ) values (80,130,'14048');
+insert into ctl_billingservice_age_rules (minAge, maxAge, service_code ) values (50,59,'14045');
+insert into ctl_billingservice_age_rules (minAge, maxAge, service_code ) values (60,69,'14046');
