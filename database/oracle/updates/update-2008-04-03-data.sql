fc562f5ea99ee94999fa9604c1641023bb3d750b
tonywzt1 <tonywzt1>
2008-04-08 17:24:34 +0000

    *** empty log message ***

--- database/oracle/updates/update-2008-04-03-data.sql
+++ database/oracle/updates/update-2008-04-03-data.sql
@@ -3,7 +3,8 @@ insert into lst_orgcd (code, description, activeyn, orderbyindex,codetree)
 valuse('R0000001','Shelter Management Information System',1,10,'R000001');
 insert into lst_orgcd (code, description, activeyn, orderbyindex,codetree)
 values('O0000020','Salvation Army',1,30,'R0000001O0000020');
-
+-- init secuserrole
+update secuserrole set ORGCD='R0000001', activeyn=1
 -- update privilege table, 
 -- the privilge implies a hierachy o-r-u-w-x, in alphabetic order
 truncate table secprivilege;
