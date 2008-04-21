22160ef75ead8d7c02f078a6799ef035bc38e8b3
tobybian <tobybian>
2008-04-21 17:10:21 +0000

    customizable color& des for appointment status -service add "NABLE_EDIT_APPT_STATUS=yes" in property file to enable it.

---
+++ web/WEB-INF/classes/src/oscar/appt/status/service/AppointmentStatusMgr.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package oscar.appt.status.service;
+
+import java.util.List;
+import oscar.appt.status.model.AppointmentStatus;
+
+/**
+ *
+ * @author toby
+ */
+public interface AppointmentStatusMgr {
+	public List getAllStatus();
+        public AppointmentStatus getStatus(int ID);
+        public void changeStatus(int ID, int iActive);
+	public void modifyStatus(int ID, String strDesc, String strColor);
+        public int checkStatusUsuage(List allStatus);
+        public void reset();
+}
---
+++ web/WEB-INF/classes/src/oscar/appt/status/service/impl/AppointmentStatusMgrImpl.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oscar.appt.status.service.impl;
+
+import java.util.List;
+import oscar.appt.status.dao.AppointmentStatusDAO;
+import oscar.appt.status.model.AppointmentStatus;
+import oscar.appt.status.service.AppointmentStatusMgr;
+
+/**
+ *
+ * @author toby
+ */
+public class AppointmentStatusMgrImpl implements AppointmentStatusMgr {
+
+    private AppointmentStatusDAO appointStatusDao = null;
+
+    public AppointmentStatusDAO getAppointStatusDao(){
+        return appointStatusDao;
+    }
+    
+    public void setAppointStatusDao(AppointmentStatusDAO dao){
+        this.appointStatusDao = dao;
+    }
+    
+    public List getAllStatus(){
+        return appointStatusDao.getAllStatus();
+    };
+
+    public AppointmentStatus getStatus(int ID){
+        return appointStatusDao.getStatus(ID);
+    };
+
+    public void changeStatus(int ID, int iActive){
+        appointStatusDao.changeStatus(ID, iActive);
+    };
+
+    public void modifyStatus(int ID, String strDesc, String strColor){
+        appointStatusDao.modifyStatus(ID, strDesc, strColor);
+    };
+
+    public int checkStatusUsuage(List allStatus){
+        return appointStatusDao.checkStatusUsuage(allStatus);
+    };
+    
+    public void reset(){
+        appointStatusDao.modifyStatus(1, "To Do", "#FDFEC7");
+        appointStatusDao.modifyStatus(2, "Daysheet Printed", "#FDFEC7");
+        appointStatusDao.modifyStatus(3, "Here", "#00ee00");
+        appointStatusDao.modifyStatus(4, "Picked", "#FFBBFF");
+        appointStatusDao.modifyStatus(5, "Empty Room", "#FFFF33");
+        appointStatusDao.modifyStatus(6, "Costumized 1", "#897DF8");
+        appointStatusDao.modifyStatus(7, "Costumized 2", "#897DF8");
+        appointStatusDao.modifyStatus(8, "Costumized 3", "#897DF8");
+        appointStatusDao.modifyStatus(9, "Costumized 4", "#897DF8");
+        appointStatusDao.modifyStatus(10, "Costumized 5", "#897DF8");
+        appointStatusDao.modifyStatus(11, "No Show", "#cccccc");
+        appointStatusDao.modifyStatus(12, "Cancelled", "#999999");
+        appointStatusDao.modifyStatus(13, "Billed", "#3ea4e1");
+    }
+}
