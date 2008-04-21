8d3d18ab7c75f41c2a60e49fff17a4219dbcc394
tobybian <tobybian>
2008-04-21 17:09:55 +0000

    customizable color& des for appointment status -DAO add "NABLE_EDIT_APPT_STATUS=yes" in property file to enable it.

---
+++ web/WEB-INF/classes/src/oscar/appt/status/dao/AppointmentStatusDAO.java
@@ -0,0 +1,12 @@
+package oscar.appt.status.dao;
+
+import java.util.List;
+import oscar.appt.status.model.AppointmentStatus;
+
+public interface AppointmentStatusDAO {
+	public List getAllStatus();
+        public AppointmentStatus getStatus(int ID);
+        public void changeStatus(int ID, int iActive);
+	public void modifyStatus(int ID, String strDesc, String strColor);
+        public int checkStatusUsuage(List allStatus);	
+}
---
+++ web/WEB-INF/classes/src/oscar/appt/status/dao/hibernate/AppointmentStatusDAOHibernate.java
@@ -0,0 +1,63 @@
+package oscar.appt.status.dao.hibernate;
+
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
+import org.hibernate.SQLQuery;
+import org.hibernate.criterion.Order;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+import oscar.appt.status.dao.AppointmentStatusDAO;
+import oscar.appt.status.model.AppointmentStatus;
+
+public class AppointmentStatusDAOHibernate extends HibernateDaoSupport
+        implements AppointmentStatusDAO {
+
+    public List getAllStatus() {
+        Criteria c = getSession().createCriteria(AppointmentStatus.class);
+        c = c.addOrder(Order.asc("id"));
+        List list = c.list();
+        return list;
+    }
+
+    public AppointmentStatus getStatus(int ID) {
+        return (AppointmentStatus) getHibernateTemplate().get(AppointmentStatus.class, new Integer(ID));
+    }
+
+    public void modifyStatus(int ID, String strDesc, String strColor) {
+        AppointmentStatus appts = (AppointmentStatus) getHibernateTemplate().get(AppointmentStatus.class, ID);
+        appts.setDescription(strDesc);
+        appts.setColor(strColor);
+        getHibernateTemplate().saveOrUpdate(appts);
+    }
+
+    public void changeStatus(int ID, int iActive) {
+        AppointmentStatus appts = (AppointmentStatus) getHibernateTemplate().get(AppointmentStatus.class, ID);
+        appts.setActive(iActive);
+        getHibernateTemplate().saveOrUpdate(appts);
+    }
+    
+    public int checkStatusUsuage(List allStatus){
+        int iUsuage = 0;
+        SQLQuery query = null;
+        AppointmentStatus apptStatus = null;
+        String sql= null;
+        for(int i=0; i<allStatus.size();i++){
+            apptStatus = (AppointmentStatus) allStatus.get(i);
+            if (apptStatus.getActive()==1)
+                continue;
+            sql = "select count(*) as total from appointment where status like"+ "'"+apptStatus.getStatus()+"%' ";
+            sql = sql + "collate latin1_general_cs";
+            System.out.println(sql);
+            query = getSession().createSQLQuery(sql);
+            query.addScalar("total", Hibernate.INTEGER);
+            iUsuage = (Integer) query.uniqueResult();
+            if(iUsuage > 0){
+                iUsuage =i;
+                break;
+            }
+        }
+        return iUsuage;
+    }
+}
