618a911a572280b62a9fa08417b1f0980c0052c3
tobybian <tobybian>
2008-04-21 17:09:06 +0000

    customizable color& des for appointment status -ORM layer add "NABLE_EDIT_APPT_STATUS=yes" in property file to enable it.

---
+++ web/WEB-INF/classes/src/oscar/appt/status/model/AppointmentStatus.java
@@ -0,0 +1,45 @@
+package oscar.appt.status.model;
+
+import oscar.appt.status.model.base.BaseAppointmentStatus;
+
+
+
+public class AppointmentStatus extends BaseAppointmentStatus {
+	private static final long serialVersionUID = 1L;
+
+/*[CONSTRUCTOR MARKER BEGIN]*/
+	public AppointmentStatus () {
+		super();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public AppointmentStatus (java.lang.Integer id) {
+		super(id);
+	}
+
+	/**
+	 * Constructor for required fields
+	 */
+	public AppointmentStatus (
+		java.lang.Integer id,
+		java.lang.String status,
+		java.lang.String description,
+		java.lang.String color,
+		java.lang.String icon,
+		java.lang.Integer active) {
+
+		super (
+			id,
+			status,
+			description,
+			color,
+			icon,
+			active);
+	}
+
+/*[CONSTRUCTOR MARKER END]*/
+
+
+}
---
+++ web/WEB-INF/classes/src/oscar/appt/status/model/base/BaseAppointmentStatus.java
@@ -0,0 +1,231 @@
+package oscar.appt.status.model.base;
+
+import java.io.Serializable;
+
+
+/**
+ * This is an object that contains data related to the appointment_status table.
+ * Do not modify this class because it will be overwritten if the configuration file
+ * related to this class is modified.
+ *
+ * @hibernate.class
+ *  table="appointment_status"
+ */
+
+public abstract class BaseAppointmentStatus  implements Serializable {
+
+	public static String REF = "AppointmentStatus";
+	public static String PROP_EDITABLE = "Editable";
+	public static String PROP_ACTIVE = "Active";
+	public static String PROP_STATUS = "Status";
+	public static String PROP_DESCRIPTION = "Description";
+	public static String PROP_ID = "Id";
+	public static String PROP_ICON = "Icon";
+	public static String PROP_COLOR = "Color";
+
+
+	// constructors
+	public BaseAppointmentStatus () {
+		initialize();
+	}
+
+	/**
+	 * Constructor for primary key
+	 */
+	public BaseAppointmentStatus (java.lang.Integer id) {
+		this.setId(id);
+		initialize();
+	}
+
+	/**
+	 * Constructor for required fields
+	 */
+	public BaseAppointmentStatus (
+		java.lang.Integer id,
+		java.lang.String status,
+		java.lang.String description,
+		java.lang.String color,
+		java.lang.String icon,
+		java.lang.Integer active) {
+
+		this.setId(id);
+		this.setStatus(status);
+		this.setDescription(description);
+		this.setColor(color);
+		this.setIcon(icon);
+		this.setActive(active);
+		initialize();
+	}
+
+	protected void initialize () {}
+
+
+
+	private int hashCode = Integer.MIN_VALUE;
+
+	// primary key
+	private java.lang.Integer id;
+
+	// fields
+	private java.lang.String status;
+	private java.lang.String description;
+	private java.lang.String color;
+	private java.lang.String icon;
+	private java.lang.Integer active;
+	private java.lang.Integer editable;
+
+
+
+	/**
+	 * Return the unique identifier of this class
+     * @hibernate.id
+     *  generator-class="native"
+     *  column="id"
+     */
+	public java.lang.Integer getId () {
+		return id;
+	}
+
+	/**
+	 * Set the unique identifier of this class
+	 * @param id the new ID
+	 */
+	public void setId (java.lang.Integer id) {
+		this.id = id;
+		this.hashCode = Integer.MIN_VALUE;
+	}
+
+
+
+
+	/**
+	 * Return the value associated with the column: status
+	 */
+	public java.lang.String getStatus () {
+		return status;
+	}
+
+	/**
+	 * Set the value related to the column: status
+	 * @param status the status value
+	 */
+	public void setStatus (java.lang.String status) {
+		this.status = status;
+	}
+
+
+
+	/**
+	 * Return the value associated with the column: description
+	 */
+	public java.lang.String getDescription () {
+		return description;
+	}
+
+	/**
+	 * Set the value related to the column: description
+	 * @param description the description value
+	 */
+	public void setDescription (java.lang.String description) {
+		this.description = description;
+	}
+
+
+
+	/**
+	 * Return the value associated with the column: color
+	 */
+	public java.lang.String getColor () {
+		return color;
+	}
+
+	/**
+	 * Set the value related to the column: color
+	 * @param color the color value
+	 */
+	public void setColor (java.lang.String color) {
+		this.color = color;
+	}
+
+
+
+	/**
+	 * Return the value associated with the column: icon
+	 */
+	public java.lang.String getIcon () {
+		return icon;
+	}
+
+	/**
+	 * Set the value related to the column: icon
+	 * @param icon the icon value
+	 */
+	public void setIcon (java.lang.String icon) {
+		this.icon = icon;
+	}
+
+
+
+	/**
+	 * Return the value associated with the column: active
+	 */
+	public java.lang.Integer getActive () {
+		return active;
+	}
+
+	/**
+	 * Set the value related to the column: active
+	 * @param active the active value
+	 */
+	public void setActive (java.lang.Integer active) {
+		this.active = active;
+	}
+
+
+
+	/**
+	 * Return the value associated with the column: editable
+	 */
+	public java.lang.Integer getEditable () {
+		return editable;
+	}
+
+	/**
+	 * Set the value related to the column: editable
+	 * @param editable the editable value
+	 */
+	public void setEditable (java.lang.Integer editable) {
+		this.editable = editable;
+	}
+
+
+
+
+	public boolean equals (Object obj) {
+		if (null == obj) return false;
+		if (!(obj instanceof oscar.appt.status.model.AppointmentStatus)) return false;
+		else {
+			oscar.appt.status.model.AppointmentStatus appointmentStatus = (oscar.appt.status.model.AppointmentStatus) obj;
+			if (null == this.getId() || null == appointmentStatus.getId()) return false;
+			else return (this.getId().equals(appointmentStatus.getId()));
+		}
+	}
+
+	public int hashCode () {
+		if (Integer.MIN_VALUE == this.hashCode) {
+			if (null == this.getId()) return super.hashCode();
+			else {
+				String hashStr = this.getClass().getName() + ":" + this.getId().hashCode();
+				this.hashCode = hashStr.hashCode();
+			}
+		}
+		return this.hashCode;
+	}
+
+
+	public String toString () {
+		return super.toString();
+	}
+
+
+}
