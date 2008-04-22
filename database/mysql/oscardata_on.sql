5a9277b1449d88aa80fd5165ed7c040191898b4b
rjonasz <rjonasz>
2008-04-22 20:03:35 +0000

    add formReq07 insert to encounterform

--- database/mysql/oscardata_on.sql
+++ database/mysql/oscardata_on.sql
@@ -10414,6 +10414,7 @@ INSERT INTO diagnosticcode VALUES (533,'042','AIDS                              
 insert into encounterForm values('Growth Charts', '../form/formGrowthChart.jsp?demographic_no=', 'formGrowthChart', 0);
 insert into encounterForm values('AR2005', '../form/formonar.jsp?demographic_no=', 'formONAR', '0');
 insert into encounterForm values('Ovulation', '../form/formovulation.jsp?demographic_no=', 'formovulation', '0');
+INSERT INTO encounterForm (form_name, form_value, form_table, hidden) VALUES ('Lab Req 2007', '../form/formlabreq07.jsp?demographic_no=', 'formLabReq07', 0);
 
 INSERT INTO billing_payment_type (id, payment_type) VALUES (1,'CASH');
 INSERT INTO billing_payment_type (id, payment_type) VALUES (2,'CHEQUE');
