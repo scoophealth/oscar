ff59976c6161ab0fa8075d64efeda73c65b52560
jaygallagher <jaygallagher>
2008-04-29 15:49:35 +0000

    fixed spelling

--- database/mysql/updates/update-2008-04-21.sql
+++ database/mysql/updates/update-2008-04-21.sql
@@ -19,7 +19,7 @@
 -- Table structure for table `appointment_status`
 --
 
-DROP TABLE IF EXISTS `appointment_status`;
+
 CREATE TABLE `appointment_status` (
   `id` int(11) NOT NULL auto_increment,
   `status` char(2) NOT NULL,
@@ -37,7 +37,7 @@ CREATE TABLE `appointment_status` (
 
 LOCK TABLES `appointment_status` WRITE;
 /*!40000 ALTER TABLE `appointment_status` DISABLE KEYS */;
-INSERT INTO `appointment_status` VALUES (1,'t','To Do','#FDFEC7','starbill.gif',1,0),(2,'T','Daysheet Printed','#FDFEC7','todo.gif',1,0),(3,'H','Here','#00ee00','here.gif',1,1),(4,'P','Picked','#FFBBFF','picked.gif',1,1),(5,'E','Empty Room','#FFFF33','empty.gif',1,1),(11,'N','No Show','#cccccc','noshow.gif',1,0),(12,'C','Cancelled','#999999','cancel.gif',1,0),(13,'B','Billed','#3ea4e1','billed.gif',1,0),(6,'a','Costumized 1','#897DF8','1.gif',1,1),(7,'b','Costumized 2','#897DF8','2.gif',1,1),(8,'c','Costumized 3','#897DF8','3.gif',0,1),(9,'d','Costumized 4','#897DF8','4.gif',1,1),(10,'e','Costumized 5','#897DF8','5.gif',1,1);
+INSERT INTO `appointment_status` VALUES (1,'t','To Do','#FDFEC7','starbill.gif',1,0),(2,'T','Daysheet Printed','#FDFEC7','todo.gif',1,0),(3,'H','Here','#00ee00','here.gif',1,1),(4,'P','Picked','#FFBBFF','picked.gif',1,1),(5,'E','Empty Room','#FFFF33','empty.gif',1,1),(11,'N','No Show','#cccccc','noshow.gif',1,0),(12,'C','Cancelled','#999999','cancel.gif',1,0),(13,'B','Billed','#3ea4e1','billed.gif',1,0),(6,'a','Customized 1','#897DF8','1.gif',1,1),(7,'b','Customized 2','#897DF8','2.gif',1,1),(8,'c','Customized 3','#897DF8','3.gif',0,1),(9,'d','Customized 4','#897DF8','4.gif',1,1),(10,'e','Customized 5','#897DF8','5.gif',1,1);
 /*!40000 ALTER TABLE `appointment_status` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
