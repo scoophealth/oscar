863a1db54af63a02a97c6e750f85b831a851fccc
jaygallagher <jaygallagher>
2008-05-22 14:28:27 +0000

    changed to handle Timestamp

--- web/WEB-INF/classes/src/oscar/oscarDB/DBPreparedHandlerParam.java
+++ web/WEB-INF/classes/src/oscar/oscarDB/DBPreparedHandlerParam.java
@@ -1,6 +1,7 @@
 package oscar.oscarDB;
 
 import java.sql.Date;
+import java.sql.Timestamp;
 
 
 public class DBPreparedHandlerParam {
@@ -8,15 +9,18 @@ public class DBPreparedHandlerParam {
    private String stringValue;
    private int intValue;
    private String paramType;
+   private Timestamp timestampValue;
 
    public static String PARAM_STRING = "String";
    public static String PARAM_DATE = "Date";
    public static String PARAM_INT = "Int";
-
+   public static String PARAM_TIMESTAMP = "Timestamp";
+   
    public DBPreparedHandlerParam(String stringValue){
 	   this.intValue = 0;
 	   this.stringValue= stringValue;
 	   this.dateValue=null;
+           this.timestampValue = null;
 	   this.paramType=PARAM_STRING;
    }
    
@@ -24,20 +28,39 @@ public class DBPreparedHandlerParam {
 	   this.intValue = 0;
 	   this.stringValue=null;
 	   this.dateValue= dateValue;
-	   this.paramType=PARAM_DATE;
+           this.timestampValue = null;
+           this.paramType=PARAM_DATE;
    }
+   
+    public DBPreparedHandlerParam(Timestamp dateValue){
+	   this.intValue = 0;
+	   this.stringValue=null;
+	   this.dateValue= null;
+           this.timestampValue = dateValue;
+	   this.paramType=PARAM_TIMESTAMP;
+   }
+   
 
    public DBPreparedHandlerParam(int intValue){
 	   this.intValue= intValue;
 	   this.stringValue = "";
 	   this.dateValue=null;
+           this.timestampValue = null;
 	   this.paramType=PARAM_INT;
    }
 
    public Date getDateValue() {
+          System.out.println("DBPreparedHandlerParam.getDateValue "+dateValue);
 	  return dateValue;
    }
 
+   
+   public Timestamp getTimestampValue() {
+          System.out.println("DBPreparedHandlerParam.getTimestampValue "+timestampValue);
+	  return this.timestampValue;
+   }
+
+   
    public int getIntValue() {
 		  return intValue;
    }
