d96fe829c7b3ff8151cc44c239a440a5eebf9909
tedleung <tedleung>
2008-06-19 19:49:09 +0000

    new table

--- database/mysql/caisi/initcaisi.sql
+++ database/mysql/caisi/initcaisi.sql
@@ -2540,3 +2540,11 @@ create table IntegratorConsent
 	restrictConsentToHic tinyint(1) NOT NULL
 );
 
+DROP TABLE IF EXISTS IntegratorConsentComplexForm;
+create table IntegratorConsentComplexForm
+(
+	integratorConsentID int not null, foreign key (integratorConsentID) references IntegratorConsent(id) on delete cascade,
+	form varchar(32),
+	printedFormLocation varchar(255),
+	refusedToSign tinyint(1) not null
+);
