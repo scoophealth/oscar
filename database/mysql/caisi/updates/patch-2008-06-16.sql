aa1af4c5c37e875d0ee55488cae64ef830f48f38
tedleung <tedleung>
2008-06-19 15:55:49 +0000

    fixed overwriting of previous patch file

--- database/mysql/caisi/updates/patch-2008-06-16.sql
+++ database/mysql/caisi/updates/patch-2008-06-16.sql
@@ -1,1 +1,15 @@
-alter table IntegratorConsent add column restrictConsentToHic tinyint(1) NOT NULL;
+DROP TABLE IF EXISTS IntegratorConsent;
+create table IntegratorConsent
+(
+	id int primary key auto_increment,
+	facilityId int not null,
+	demographicId int not null,
+	unique(facilityId,demographicId),
+    provider_no varchar(6) not null, foreign key (provider_no) references provider(provider_no),
+	lastUpdate datetime not null,
+	consentToStatistics tinyint(1) NOT NULL,
+	consentToBasicPersonalId tinyint(1) NOT NULL,
+	consentToHealthCardId tinyint(1) NOT NULL,
+	consentToIssues tinyint(1) NOT NULL,
+	consentToNotes tinyint(1) NOT NULL
+);
---
+++ database/mysql/caisi/updates/patch-2008-06-18.sql
@@ -0,0 +1,1 @@
+alter table IntegratorConsent add column restrictConsentToHic tinyint(1) NOT NULL;
