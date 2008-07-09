fcec727d69a9c32b7b6ca74192f699e331bed864
rjonasz <rjonasz>
2008-07-09 15:08:39 +0000

    Create OSCAR DB for ONT

---
+++ database/mysql/createdatabase_on.sh
@@ -0,0 +1,14 @@
+##CREATE DATABASE
+
+PASSWORD=$1
+DATABASE_NAME=$2
+
+mysqladmin -uroot -p$PASSWORD create $DATABASE_NAME
+echo "grant all on $DATABASE_NAME.* to root@localhost identified by \"$PASSWORD\"" |  mysql -uroot -p$PASSWORD  $DATABASE_NAME
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscarinit.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscarinit_on.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscardata.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscardata_on.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < icd9.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < caisi/initcaisi.sql
+mysql -uroot -p$PASSWORD  $DATABASE_NAME  < caisi/initcaisidata.sql
