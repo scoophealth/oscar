bb76a5c466fea57fcc487a8cc835081a24e252e7
annie_zhou91 <annie_zhou91>
2008-08-01 15:54:59 +0000

    *** empty log message ***

--- database/mysql/caisi/updates/patch-2008-06-24.sql
+++ database/mysql/caisi/updates/patch-2008-06-24.sql
@@ -1,2 +1,7 @@
 alter table facility add column useQuickConsent tinyint(1) not null;
 update facility set useQuickConsent=1;
+
+update bed set facility_id=1 where facility_id=0;
+update room set facility_id=1 where facility_id=0;
+update intake set facility_id=1 where facility_id is NULL;
+update room set occupancy=20 where occupancy=0;
