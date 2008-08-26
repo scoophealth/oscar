3d125293a1c60fe910537f5cd41556a27aa2644e
annie_zhou91 <annie_zhou91>
2008-08-26 19:53:07 +0000

    removed it

--- database/mysql/updates/patch-2008-07-07.sql
+++
@@ -1,2 +0,0 @@
-insert into `secObjectName` (`objectName`,`description`,`orgapplicable`) values ('_pmm.eidtor','Caisi Intake Editor',0);
-insert into `secObjPrivilege` values('admin','_pmm.editor','x',0,999998);
