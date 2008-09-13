89436c4599f2d7d045d40f5e077f812f651443c9
jaygallagher <jaygallagher>
2008-09-13 01:28:22 +0000

    Struts Action for Searching Demographics

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/web/SearchDemographicAutoCompleteAction.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.web;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.oscarehr.common.dao.DemographicDao;
+import org.oscarehr.common.model.Demographic;
+import org.oscarehr.util.SpringUtils;
+
+/**
+ *
+ * @author jaygallagher
+ */
+public class SearchDemographicAutoCompleteAction extends Action {
+    
+    public ActionForward execute(ActionMapping mapping,ActionForm form,HttpServletRequest request,HttpServletResponse response) {
+        DemographicDao demographicDao = (DemographicDao) SpringUtils.getBean("demographicDao"); 
+        String searchStr = request.getParameter("demographicKeyword"); 
+        List<Demographic> list = demographicDao.searchDemographic(searchStr);
+        request.setAttribute("list", list);
+        return mapping.findForward("success");
+    }
+
+}
