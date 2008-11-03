27ba1f6f62f4e076508e0d2e04361e31314d83a8
singhj <singhj>
2008-11-03 21:21:35 +0000

    bugfix - 2192700

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/dao/ProviderDao.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/dao/ProviderDao.java
@@ -41,7 +41,9 @@ public class ProviderDao extends HibernateDaoSupport {
 	private static Log log = LogFactory.getLog(ProviderDao.class);
 
 	public boolean providerExists(String providerNo) {
-		boolean exists = (((Long) getHibernateTemplate().iterate("select count(*) from Provider p where p.ProviderNo = " + providerNo).next()) == 1);
+		boolean exists = (((Long) getHibernateTemplate().iterate(
+				"select count(*) from Provider p where p.ProviderNo = "
+						+ providerNo).next()) == 1);
 		log.debug("providerExists: " + exists);
 
 		return exists;
@@ -52,10 +54,12 @@ public class ProviderDao extends HibernateDaoSupport {
 			throw new IllegalArgumentException();
 		}
 
-		Provider provider = (Provider) getHibernateTemplate().get(Provider.class, providerNo);
+		Provider provider = (Provider) getHibernateTemplate().get(
+				Provider.class, providerNo);
 
 		if (log.isDebugEnabled()) {
-			log.debug("getProvider: providerNo=" + providerNo + ",found=" + (provider != null));
+			log.debug("getProvider: providerNo=" + providerNo + ",found="
+					+ (provider != null));
 		}
 
 		return provider;
@@ -78,7 +82,8 @@ public class ProviderDao extends HibernateDaoSupport {
 		}
 
 		if (log.isDebugEnabled()) {
-			log.debug("getProviderName: providerNo=" + providerNo + ",result=" + providerName);
+			log.debug("getProviderName: providerNo=" + providerNo + ",result="
+					+ providerName);
 		}
 
 		return providerName;
@@ -86,7 +91,8 @@ public class ProviderDao extends HibernateDaoSupport {
 
 	public List<Provider> getProviders() {
 		@SuppressWarnings("unchecked")
-		List<Provider> rs = getHibernateTemplate().find("FROM  Provider p ORDER BY p.LastName");
+		List<Provider> rs = getHibernateTemplate().find(
+				"FROM  Provider p ORDER BY p.LastName");
 
 		if (log.isDebugEnabled()) {
 			log.debug("getProviders: # of results=" + rs.size());
@@ -100,30 +106,35 @@ public class ProviderDao extends HibernateDaoSupport {
 		String sSQL;
 		List<Provider> rs;
 		if (programId != null && "0".equals(programId) == false) {
-			sSQL = "FROM  Provider p where p.Status='1' and p.ProviderNo in " + "(select c.ProviderNo from ProgramProvider c where c.ProgramId =?) ORDER BY p.LastName";
+			sSQL = "FROM  Provider p where p.Status='1' and p.ProviderNo in "
+					+ "(select c.ProviderNo from ProgramProvider c where c.ProgramId =?) ORDER BY p.LastName";
 			paramList.add(Long.valueOf(programId));
 			Object params[] = paramList.toArray(new Object[paramList.size()]);
 			rs = getHibernateTemplate().find(sSQL, params);
-		}
-		else if (facilityId != null && "0".equals(facilityId) == false) {
-			sSQL = "FROM  Provider p where p.Status='1' and p.ProviderNo in " + "(select c.ProviderNo from ProgramProvider c where c.ProgramId in "
+		} else if (facilityId != null && "0".equals(facilityId) == false) {
+			sSQL = "FROM  Provider p where p.Status='1' and p.ProviderNo in "
+					+ "(select c.ProviderNo from ProgramProvider c where c.ProgramId in "
 					+ "(select a.id from Program a where a.facilityId=?)) ORDER BY p.LastName";
-			paramList.add(facilityId);
+			// JS 2192700 - string facilityId seems to be throwing class cast
+			// exception
+			long intFacilityId = Long.valueOf(facilityId);
+			paramList.add(intFacilityId);
 			Object params[] = paramList.toArray(new Object[paramList.size()]);
 			rs = getHibernateTemplate().find(sSQL, params);
-		}
-		else {
+		} else {
 			sSQL = "FROM  Provider p where p.Status='1' ORDER BY p.LastName";
 			rs = getHibernateTemplate().find(sSQL);
 		}
-		// List<Provider> rs = getHibernateTemplate().find("FROM  Provider p ORDER BY p.LastName");
+		// List<Provider> rs =
+		// getHibernateTemplate().find("FROM  Provider p ORDER BY p.LastName");
 
 		return rs;
 	}
 
 	public List<Provider> getActiveProviders() {
 		@SuppressWarnings("unchecked")
-		List<Provider> rs = getHibernateTemplate().find("FROM  Provider p where p.Status='1' ORDER BY p.LastName");
+		List<Provider> rs = getHibernateTemplate().find(
+				"FROM  Provider p where p.Status='1' ORDER BY p.LastName");
 
 		if (log.isDebugEnabled()) {
 			log.debug("getProviders: # of results=" + rs.size());
@@ -132,13 +143,15 @@ public class ProviderDao extends HibernateDaoSupport {
 	}
 
 	public List<Provider> search(String name) {
-		boolean isOracle = OscarProperties.getInstance().getDbType().equals("oracle");
+		boolean isOracle = OscarProperties.getInstance().getDbType().equals(
+				"oracle");
 		Criteria c = this.getSession().createCriteria(Provider.class);
 		if (isOracle) {
-			c.add(Restrictions.or(Expression.ilike("FirstName", name + "%"), Expression.ilike("LastName", name + "%")));
-		}
-		else {
-			c.add(Restrictions.or(Expression.like("FirstName", name + "%"), Expression.like("LastName", name + "%")));
+			c.add(Restrictions.or(Expression.ilike("FirstName", name + "%"),
+					Expression.ilike("LastName", name + "%")));
+		} else {
+			c.add(Restrictions.or(Expression.like("FirstName", name + "%"),
+					Expression.like("LastName", name + "%")));
 		}
 		c.addOrder(Order.asc("ProviderNo"));
 
@@ -153,10 +166,12 @@ public class ProviderDao extends HibernateDaoSupport {
 
 	public List<Provider> getProvidersByType(String type) {
 		@SuppressWarnings("unchecked")
-		List<Provider> results = this.getHibernateTemplate().find("from Provider p where p.ProviderType = ?", type);
+		List<Provider> results = this.getHibernateTemplate().find(
+				"from Provider p where p.ProviderType = ?", type);
 
 		if (log.isDebugEnabled()) {
-			log.debug("getProvidersByType: type=" + type + ",# of results=" + results.size());
+			log.debug("getProvidersByType: type=" + type + ",# of results="
+					+ results.size());
 		}
 
 		return results;
@@ -164,31 +179,41 @@ public class ProviderDao extends HibernateDaoSupport {
 
 	public List<Provider> getActiveProvidersByType(String type) {
 		@SuppressWarnings("unchecked")
-		List<Provider> results = this.getHibernateTemplate().find("from Provider p where p.Status='1' and p.ProviderType = ?", type);
+		List<Provider> results = this.getHibernateTemplate().find(
+				"from Provider p where p.Status='1' and p.ProviderType = ?",
+				type);
 
 		return results;
 	}
 
 	public static void addProviderToFacility(String provider_no, int facilityId) {
 		try {
-			SqlUtils.update("insert into provider_facility values ('" + provider_no + "'," + facilityId + ')');
-		}
-		catch (RuntimeException e) {
-			// chances are it's a duplicate unique entry exception so it's safe to ignore.
-			// this is still unexpected because duplicate calls shouldn't be made
+			SqlUtils.update("insert into provider_facility values ('"
+					+ provider_no + "'," + facilityId + ')');
+		} catch (RuntimeException e) {
+			// chances are it's a duplicate unique entry exception so it's safe
+			// to ignore.
+			// this is still unexpected because duplicate calls shouldn't be
+			// made
 			log.warn("Unexpected exception occurred.", e);
 		}
 	}
 
-	public static void removeProviderFromFacility(String provider_no, int facilityId) {
-		SqlUtils.update("delete from provider_facility where provider_no='" + provider_no + "' and facility_id=" + facilityId);
+	public static void removeProviderFromFacility(String provider_no,
+			int facilityId) {
+		SqlUtils.update("delete from provider_facility where provider_no='"
+				+ provider_no + "' and facility_id=" + facilityId);
 	}
 
 	public static List<Integer> getFacilityIds(String provider_no) {
-		return (SqlUtils.selectIntList("select facility_id from provider_facility where provider_no='" + provider_no + '\''));
+		return (SqlUtils
+				.selectIntList("select facility_id from provider_facility where provider_no='"
+						+ provider_no + '\''));
 	}
 
 	public static List<String> getProviderIds(int facilityId) {
-		return (SqlUtils.selectStringList("select provider_no from provider_facility where facility_id=" + facilityId));
+		return (SqlUtils
+				.selectStringList("select provider_no from provider_facility where facility_id="
+						+ facilityId));
 	}
 }
