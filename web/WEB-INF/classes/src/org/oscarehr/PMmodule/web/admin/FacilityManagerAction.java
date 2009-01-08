0e3817880c05049832053cbc9427f4761641dae6
tedleung <tedleung>
2009-01-08 19:26:49 +0000

    refactored birth day access and removed unused code and removed warnings

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/admin/FacilityManagerAction.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/admin/FacilityManagerAction.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,13 +17,13 @@ import org.apache.struts.action.ActionMessages;
 import org.oscarehr.PMmodule.dao.AdmissionDao;
 import org.oscarehr.PMmodule.dao.ClientDao;
 import org.oscarehr.PMmodule.model.Admission;
-import org.oscarehr.common.model.Demographic;
 import org.oscarehr.PMmodule.model.Program;
 import org.oscarehr.PMmodule.service.LogManager;
 import org.oscarehr.PMmodule.service.ProgramManager;
 import org.oscarehr.PMmodule.web.BaseAction;
 import org.oscarehr.PMmodule.web.FacilityDischargedClients;
 import org.oscarehr.common.dao.FacilityDao;
+import org.oscarehr.common.model.Demographic;
 import org.oscarehr.common.model.Facility;
 import org.oscarehr.util.SessionConstants;
 import org.oscarehr.util.WebUtils;
@@ -36,215 +34,211 @@ import com.quatro.service.LookupManager;
  */
 public class FacilityManagerAction extends BaseAction {
 
-    private AdmissionDao admissionDao;
-    private ClientDao clientDao;
-    private ProgramManager programManager;
-    private LookupManager lookupManager;
-    private LogManager logManager;
+	private AdmissionDao admissionDao;
+	private ClientDao clientDao;
+	private ProgramManager programManager;
+	private LookupManager lookupManager;
+	private LogManager logManager;
 
-    private FacilityDao facilityDao;
+	private FacilityDao facilityDao;
 
-    public void setFacilityDao(FacilityDao facilityDao) {
+	public void setFacilityDao(FacilityDao facilityDao) {
 		this.facilityDao = facilityDao;
 	}
 
-    private static final String FORWARD_EDIT = "edit";
-    private static final String FORWARD_VIEW = "view";
-    private static final String FORWARD_LIST = "list";
+	private static final String FORWARD_EDIT = "edit";
+	private static final String FORWARD_VIEW = "view";
+	private static final String FORWARD_LIST = "list";
 
-    private static final String BEAN_FACILITIES = "facilities";
-    private static final String BEAN_ASSOCIATED_PROGRAMS = "associatedPrograms";
-    private static final String BEAN_ASSOCIATED_CLIENTS = "associatedClients";
+	private static final String BEAN_FACILITIES = "facilities";
+	private static final String BEAN_ASSOCIATED_PROGRAMS = "associatedPrograms";
+	private static final String BEAN_ASSOCIATED_CLIENTS = "associatedClients";
 
-    public ActionForward unspecified(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        return list(mapping, form, request, response);
-    }
+	public ActionForward unspecified(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		return list(mapping, form, request, response);
+	}
 
-    public ActionForward list(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        List<Facility> facilities = facilityDao.findAll(true);
+	public ActionForward list(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		List<Facility> facilities = facilityDao.findAll(true);
 
-        request.setAttribute(BEAN_FACILITIES, facilities);
+		request.setAttribute(BEAN_FACILITIES, facilities);
 
-        // get agency's organization list from caisi editor table
-        request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
+		// get agency's organization list from caisi editor table
+		request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
 
-        // get agency's sector list from caisi editor table
-        request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
+		// get agency's sector list from caisi editor table
+		request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
 
-        return mapping.findForward(FORWARD_LIST);
-    }
+		return mapping.findForward(FORWARD_LIST);
+	}
 
-    public ActionForward view(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        String idStr = request.getParameter("id");
-        Integer id = Integer.valueOf(idStr);
-        Facility facility = facilityDao.find(id);
+	public ActionForward view(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		String idStr = request.getParameter("id");
+		Integer id = Integer.valueOf(idStr);
+		Facility facility = facilityDao.find(id);
 
-        FacilityManagerForm facilityForm = (FacilityManagerForm) form;
-        facilityForm.setFacility(facility);
+		FacilityManagerForm facilityForm = (FacilityManagerForm) form;
+		facilityForm.setFacility(facility);
 
-        // Get facility associated client list -----------
-        Map facilityClientsMap = new LinkedHashMap();
-        List<FacilityDischargedClients> facilityClients = new ArrayList();
+		List<FacilityDischargedClients> facilityClients = new ArrayList<FacilityDischargedClients>();
 
-        // Get program list by facility id in table room.
-        for (Program program : programManager.getPrograms(id)) {
-            if (program != null) {
-                // Get admission list by program id and automatic_discharge=true
+		// Get program list by facility id in table room.
+		for (Program program : programManager.getPrograms(id)) {
+			if (program != null) {
+				// Get admission list by program id and automatic_discharge=true
 
-                List<Admission> admissions = admissionDao.getAdmissionsByProgramId(program.getId(), new Boolean(true), new Integer(-7));
-                if (admissions != null) {
-                    Iterator it = admissions.iterator();
-                    while (it.hasNext()) {
+				List<Admission> admissions = admissionDao.getAdmissionsByProgramId(program.getId(), new Boolean(true), new Integer(-7));
+				if (admissions != null) {
+					Iterator<Admission> it = admissions.iterator();
+					while (it.hasNext()) {
 
-                        Admission admission = (Admission) it.next();
+						Admission admission = (Admission) it.next();
 
-                        // Get demographic list by demographic_no
-                        Demographic client = clientDao.getClientByDemographicNo(admission.getClientId());
+						// Get demographic list by demographic_no
+						Demographic client = clientDao.getClientByDemographicNo(admission.getClientId());
 
-                        String name = client.getFirstName() + " " + client.getLastName();
-                        String dob = client.getYearOfBirth() + "/" + client.getMonthOfBirth() + "/" + client.getDateOfBirth();
-                        String pName = program.getName();
-                        Date dischargeDate = admission.getDischargeDate();
-                        String dDate = dischargeDate.toString();
+						String name = client.getFirstName() + " " + client.getLastName();
+						String dob = client.getFormattedDob();
+						String pName = program.getName();
+						Date dischargeDate = admission.getDischargeDate();
+						String dDate = dischargeDate.toString();
 
-                        // today's date
-                        Calendar calendar = Calendar.getInstance();
+						// today's date
+						Calendar calendar = Calendar.getInstance();
 
-                        // today's date - days
-                        calendar.add(Calendar.DAY_OF_YEAR, -1);
+						// today's date - days
+						calendar.add(Calendar.DAY_OF_YEAR, -1);
 
-                        Date oneDayAgo = calendar.getTime();
+						Date oneDayAgo = calendar.getTime();
 
-                        FacilityDischargedClients fdc = new FacilityDischargedClients();
-                        fdc.setName(name);
-                        fdc.setDob(dob);
-                        fdc.setProgramName(pName);
-                        fdc.setDischargeDate(dDate);
+						FacilityDischargedClients fdc = new FacilityDischargedClients();
+						fdc.setName(name);
+						fdc.setDob(dob);
+						fdc.setProgramName(pName);
+						fdc.setDischargeDate(dDate);
 
-                        if (dischargeDate.after(oneDayAgo)) {
-                            fdc.setInOneDay(true);
-                        }
-                        else {
-                            fdc.setInOneDay(false);
-                        }
-                        facilityClients.add(fdc);
+						if (dischargeDate.after(oneDayAgo)) {
+							fdc.setInOneDay(true);
+						} else {
+							fdc.setInOneDay(false);
+						}
+						facilityClients.add(fdc);
 
-                    }
-                }
-            }
-        }
-        request.setAttribute(BEAN_ASSOCIATED_CLIENTS, facilityClients);
+					}
+				}
+			}
+		}
+		request.setAttribute(BEAN_ASSOCIATED_CLIENTS, facilityClients);
 
-        request.setAttribute(BEAN_ASSOCIATED_PROGRAMS, programManager.getPrograms(id));
+		request.setAttribute(BEAN_ASSOCIATED_PROGRAMS, programManager.getPrograms(id));
 
-        request.setAttribute("id", facility.getId());
+		request.setAttribute("id", facility.getId());
 
-        return mapping.findForward(FORWARD_VIEW);
-    }
+		return mapping.findForward(FORWARD_VIEW);
+	}
 
-    public ActionForward edit(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        String id = request.getParameter("id");
-        Facility facility = facilityDao.find(Integer.valueOf(id));
+	public ActionForward edit(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		String id = request.getParameter("id");
+		Facility facility = facilityDao.find(Integer.valueOf(id));
 
-        FacilityManagerForm managerForm = (FacilityManagerForm) form;
-        managerForm.setFacility(facility);
+		FacilityManagerForm managerForm = (FacilityManagerForm) form;
+		managerForm.setFacility(facility);
 
-        request.setAttribute("id", facility.getId());
-        request.setAttribute("orgId", facility.getOrgId());
-        request.setAttribute("sectorId", facility.getSectorId());
+		request.setAttribute("id", facility.getId());
+		request.setAttribute("orgId", facility.getOrgId());
+		request.setAttribute("sectorId", facility.getSectorId());
 
-        // get agency's organization list from caisi editor table
-        request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
+		// get agency's organization list from caisi editor table
+		request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
 
-        // get agency's sector list from caisi editor table
-        request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
+		// get agency's sector list from caisi editor table
+		request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
 
-        return mapping.findForward(FORWARD_EDIT);
-    }
+		return mapping.findForward(FORWARD_EDIT);
+	}
 
-    public ActionForward delete(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        String id = request.getParameter("id");
-        Facility facility = facilityDao.find(Integer.valueOf(id));
-        facility.setDisabled(true);
-        facilityDao.merge(facility);
+	public ActionForward delete(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		String id = request.getParameter("id");
+		Facility facility = facilityDao.find(Integer.valueOf(id));
+		facility.setDisabled(true);
+		facilityDao.merge(facility);
 
-        return list(mapping, form, request, response);
-    }
+		return list(mapping, form, request, response);
+	}
 
-    public ActionForward add(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        Facility facility = new Facility("", "");
-        ((FacilityManagerForm) form).setFacility(facility);
+	public ActionForward add(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		Facility facility = new Facility("", "");
+		((FacilityManagerForm) form).setFacility(facility);
 
-        // get agency's organization list from caisi editor table
-        request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
+		// get agency's organization list from caisi editor table
+		request.setAttribute("orgList", lookupManager.LoadCodeList("OGN", true, null, null));
 
-        // get agency's sector list from caisi editor table
-        request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
+		// get agency's sector list from caisi editor table
+		request.setAttribute("sectorList", lookupManager.LoadCodeList("SEC", true, null, null));
 
-        return mapping.findForward(FORWARD_EDIT);
-    }
+		return mapping.findForward(FORWARD_EDIT);
+	}
 
-    public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        FacilityManagerForm mform = (FacilityManagerForm) form;
-        Facility facility = mform.getFacility();
+	public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
+		FacilityManagerForm mform = (FacilityManagerForm) form;
+		Facility facility = mform.getFacility();
 
-        if (request.getParameter("facility.hic") == null) facility.setHic(false);
+		if (request.getParameter("facility.hic") == null) facility.setHic(false);
 
-        if (isCancelled(request)) {
-            request.getSession().removeAttribute("facilityManagerForm");
+		if (isCancelled(request)) {
+			request.getSession().removeAttribute("facilityManagerForm");
 
-            return list(mapping, form, request, response);
-        }
+			return list(mapping, form, request, response);
+		}
 
-        try {
-        	facility.setIntegratorEnabled(WebUtils.isChecked(request, "facility.integratorEnabled"));
-        	facility.setAllowQuickConsent(WebUtils.isChecked(request, "facility.allowQuickConsent"));
-        	facility.setEnableIntegratedReferrals(WebUtils.isChecked(request, "facility.enableIntegratedReferrals"));
-        	facility.setEnableHealthNumberRegistry(WebUtils.isChecked(request, "facility.enableHealthNumberRegistry"));
-            
-        	if (facility.getId()==null || facility.getId()==0) facilityDao.persist(facility);
-        	else facilityDao.merge(facility);
+		try {
+			facility.setIntegratorEnabled(WebUtils.isChecked(request, "facility.integratorEnabled"));
+			facility.setAllowQuickConsent(WebUtils.isChecked(request, "facility.allowQuickConsent"));
+			facility.setEnableIntegratedReferrals(WebUtils.isChecked(request, "facility.enableIntegratedReferrals"));
+			facility.setEnableHealthNumberRegistry(WebUtils.isChecked(request, "facility.enableHealthNumberRegistry"));
 
-            Integer currentFacilityId=(Integer)request.getSession().getAttribute(SessionConstants.CURRENT_FACILITY_ID);
-            if (currentFacilityId.equals(facility.getId())) request.getSession().setAttribute(SessionConstants.CURRENT_FACILITY, facility);
-            
-            ActionMessages messages = new ActionMessages();
-            messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("facility.saved", facility.getName()));
-            saveMessages(request, messages);
+			if (facility.getId() == null || facility.getId() == 0) facilityDao.persist(facility);
+			else facilityDao.merge(facility);
 
-            request.setAttribute("id", facility.getId());
+			Integer currentFacilityId = (Integer) request.getSession().getAttribute(SessionConstants.CURRENT_FACILITY_ID);
+			if (currentFacilityId.equals(facility.getId())) request.getSession().setAttribute(SessionConstants.CURRENT_FACILITY, facility);
 
-            logManager.log("write", "facility", facility.getId().toString(), request);
+			ActionMessages messages = new ActionMessages();
+			messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("facility.saved", facility.getName()));
+			saveMessages(request, messages);
 
-            return list(mapping, form, request, response);
-        }
-        catch (Exception e) {
-            ActionMessages messages = new ActionMessages();
-            messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("duplicateKey", "The name " + facility.getName()));
-            saveMessages(request, messages);
+			request.setAttribute("id", facility.getId());
 
-            return mapping.findForward(FORWARD_EDIT);
-        }
-    }
+			logManager.log("write", "facility", facility.getId().toString(), request);
 
-    public void setAdmissionDao(AdmissionDao admissionDao) {
-        this.admissionDao = admissionDao;
-    }
+			return list(mapping, form, request, response);
+		} catch (Exception e) {
+			ActionMessages messages = new ActionMessages();
+			messages.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("duplicateKey", "The name " + facility.getName()));
+			saveMessages(request, messages);
 
-    public void setClientDao(ClientDao clientDao) {
-        this.clientDao = clientDao;
-    }
+			return mapping.findForward(FORWARD_EDIT);
+		}
+	}
 
-    public void setLookupManager(LookupManager lookupManager) {
-        this.lookupManager = lookupManager;
-    }
+	public void setAdmissionDao(AdmissionDao admissionDao) {
+		this.admissionDao = admissionDao;
+	}
 
-    public void setLogManager(LogManager mgr) {
-        this.logManager = mgr;
-    }
+	public void setClientDao(ClientDao clientDao) {
+		this.clientDao = clientDao;
+	}
 
-    public void setProgramManager(ProgramManager mgr) {
-        this.programManager = mgr;
-    }
+	public void setLookupManager(LookupManager lookupManager) {
+		this.lookupManager = lookupManager;
+	}
+
+	public void setLogManager(LogManager mgr) {
+		this.logManager = mgr;
+	}
+
+	public void setProgramManager(ProgramManager mgr) {
+		this.programManager = mgr;
+	}
 
 }
