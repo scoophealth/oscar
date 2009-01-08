1a4be1eb810206a739768706b6575a03a99d8952
tedleung <tedleung>
2009-01-08 21:46:12 +0000

    support for copying HNR remote client to local system

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/ManageHnrClientAction.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/web/ManageHnrClientAction.java
@@ -1,5 +1,7 @@
 package org.oscarehr.PMmodule.web;
 
+import java.net.MalformedURLException;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.LogManager;
@@ -22,13 +24,56 @@ public class ManageHnrClientAction {
 	private static ClientLinkDao clientLinkDao = (ClientLinkDao) SpringUtils.getBean("clientLinkDao");
 	private static ClientImageDAO clientImageDAO = (ClientImageDAO) SpringUtils.getBean("clientImageDAO");
 
-	public static void copyHnrToLocal()
+	public static void copyHnrToLocal(Facility currentFacility, Provider currentProvider, Integer clientId) throws MalformedURLException
 	{
-System.err.println("----- copyHnrToLocal");
+		logger.debug("copyHnrToLocal currentFacility="+currentFacility.getId()+", currentProvider="+currentProvider.getProviderNo()+", client="+clientId);
+
+		List<ClientLink> clientLinks=clientLinkDao.findByClientIdAndType(clientId, true, ClientLink.Type.HNR);
+		if (clientLinks.size()!=1) throw(new IllegalArgumentException("The client passed in should have had exactly 1 hnr link. links.size="+clientLinks.size())); 
+
+		ClientLink clientLink=clientLinks.get(0);		
+		org.oscarehr.hnr.ws.client.Client hnrClient=caisiIntegratorManager.getHnrClient(currentFacility, currentProvider, clientLink.getRemoteLinkId());
+		
+		Demographic demographic=demographicDao.getDemographicById(clientId);
+
+		if (hnrClient.getBirthDate()!=null)	demographic.setBirthDay(hnrClient.getBirthDate().toGregorianCalendar());
+		if (hnrClient.getCity()!=null) demographic.setCity(hnrClient.getCity());
+		if (hnrClient.getFirstName()!=null) demographic.setFirstName(hnrClient.getFirstName());
+		
+		// genders can't be synced until gender constants are resolved
+		// if (hnrClient.getGender()!=null) demographic.setSex(hnrClient.getGender().name());
+		
+		if (hnrClient.getHin()!=null) demographic.setHin(hnrClient.getHin());
+		if (hnrClient.getHinType()!=null) demographic.setHcType(hnrClient.getHinType());
+		if (hnrClient.getHinVersion()!=null) demographic.setVer(hnrClient.getHinVersion());
+		
+		if (hnrClient.getImage()!=null)
+		{
+			ClientImage clientImage=clientImageDAO.getClientImage(clientId);
+			if (clientImage==null)
+			{
+				clientImage=new ClientImage();
+				clientImage.setDemographic_no(clientId);
+			}
+			
+			clientImage.setImage_data(hnrClient.getImage());
+			clientImage.setImage_type("jpg");
+			clientImage.setUpdate_date(new Date());
+			
+			clientImageDAO.saveClientImage(clientImage);
+		}
+		
+		if (hnrClient.getLastName()!=null) demographic.setLastName(hnrClient.getLastName());
+		if (hnrClient.getProvince()!=null) demographic.setProvince(hnrClient.getProvince());
+		if (hnrClient.getSin()!=null) demographic.setSin(hnrClient.getSin());
+		if (hnrClient.getStreetAddress()!=null) demographic.setAddress(hnrClient.getStreetAddress());
+
+		demographicDao.getHibernateTemplate().saveOrUpdate(demographic);
 	}
 	
-	public static void copyLocalToHnr()
+	public static void copyLocalToHnr(Facility currentFacility, Provider currentProvider, Integer clientId)
 	{
-System.err.println("----- copyLocalToHnr");
+		logger.debug("copyLocalToHnr currentFacility="+currentFacility.getId()+", currentProvider="+currentProvider.getProviderNo()+", client="+clientId);
+
 	}
 }
