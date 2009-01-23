a031b5fdb04a9a41ce50360fb8b67b02c55dacf4
cronnie <cronnie>
2009-01-23 14:03:01 +0000

    Changed method to return latest allergyid

--- web/WEB-INF/classes/src/oscar/oscarRx/data/RxAllergyImport.java
+++ web/WEB-INF/classes/src/oscar/oscarRx/data/RxAllergyImport.java
@@ -24,19 +24,35 @@
 
 package oscar.oscarRx.data;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import oscar.oscarDB.DBHandler;
 
 public class RxAllergyImport {
    
-    public boolean Save(String demographicNo, String entryDate, String description, String typeCode, String reaction, String severity, String drugrefId) throws SQLException {
+    public Long Save(String demographicNo, String entryDate, String description, String typeCode, String reaction, String startDate, String severity, String drugrefId) throws SQLException {
 	boolean b = false;            
 	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);            
-	String sql = "INSERT INTO allergies (demographic_no, entry_date, DESCRIPTION, TYPECODE, reaction, severity_of_reaction, drugref_id) VALUES ('"
-		+ demographicNo + "','" + entryDate + "','" + description + "','" + typeCode + "','" + reaction + "','" + severity + "','" + drugrefId + "')";
-	b = db.RunSQL(sql);
-	db.CloseConn();            
-	return b;            
+	String sql = "INSERT INTO allergies (demographic_no, entry_date, DESCRIPTION, TYPECODE, reaction, start_date, severity_of_reaction, drugref_id) VALUES ('"
+		+ demographicNo + "','" + entryDate + "','" + description + "','" + typeCode + "','" + reaction + "','" + startDate + "','" + severity + "','" + drugrefId + "')";
+	if (db.RunSQL(sql)) {
+	    sql = "SELECT MAX(allergyid) FROM allergies WHERE " +
+		    "demographic_no='"	     + demographicNo + "' AND " +
+		    "entry_date='"	     + entryDate     + "' AND " +
+		    "DESCRIPTION='"	     + description   + "' AND " +
+		    "TYPECODE='"	     + typeCode	     + "' AND " +
+		    "reaction='"	     + reaction	     + "' AND " +
+		    "start_date='"	     + startDate     + "' AND " +
+		    "severity_of_reaction='" + severity	     + "' AND " +
+		    "drugref_id='"	     + drugrefId     + "'";
+	    ResultSet rs = db.GetSQL(sql);
+	    db.CloseConn();
+	    
+	    if (rs.next()) return rs.getLong(1);
+	    else return null;
+	} else {
+	    return null;
+	}
      }
 }
