a3488772074a55c55797919395ca02e17c88b265
tedleung <tedleung>
2009-02-11 21:40:55 +0000

    changed formatting to conform to oscar project starndard

--- web/WEB-INF/classes/src/org/oscarehr/util/VmStat.java
+++ web/WEB-INF/classes/src/org/oscarehr/util/VmStat.java
@@ -36,172 +36,147 @@ import org.apache.log4j.Logger;
 
 /**
  */
-public class VmStat
-{
-	private static final Logger logger=LogManager.getLogger(VmStat.class);
-	
-	private static Timer timer=new Timer(VmStat.class.getName(), true);
-	private static TimerTask timerTask=null;
-	private static int counter=0;	
-	
-	public static synchronized void startContinuousLogging(long logPeriodMilliseconds)
-	{
-		if (timerTask!=null) throw(new IllegalStateException("ContinuousLogging already started."));
-		
-		timerTask=new TimerTask()
-		{
-			public void run()
-			{
-				try
-				{
+public class VmStat {
+	private static final Logger logger = LogManager.getLogger(VmStat.class);
+
+	private static Timer timer = new Timer(VmStat.class.getName(), true);
+	private static TimerTask timerTask = null;
+	private static int counter = 0;
+
+	public static synchronized void startContinuousLogging(long logPeriodMilliseconds) {
+		if (timerTask != null) throw (new IllegalStateException("ContinuousLogging already started."));
+
+		timerTask = new TimerTask() {
+			public void run() {
+				try {
 					logAll();
-				}
-				catch (Exception e)
-				{
+				} catch (Exception e) {
 					logger.error("Unexpected error.", e);
 				}
 			}
 		};
-		
+
 		timer.schedule(timerTask, 0, logPeriodMilliseconds);
 	}
-	
-	public static synchronized void stopContinuousLogging()
-	{
-		if (timerTask!=null)
-		{
+
+	public static synchronized void stopContinuousLogging() {
+		if (timerTask != null) {
 			timerTask.cancel();
-			timerTask=null;
+			timerTask = null;
 		}
 	}
-	
-	public static String getMemoryFormat()
-	{
-		List<MemoryPoolMXBean> memoryPools=ManagementFactory.getMemoryPoolMXBeans();
-		StringBuilder sb=new StringBuilder();
-		
-		for (MemoryPoolMXBean memoryPool : memoryPools)
-		{
-			if (sb.length()>0) sb.append(',');
+
+	public static String getMemoryFormat() {
+		List<MemoryPoolMXBean> memoryPools = ManagementFactory.getMemoryPoolMXBeans();
+		StringBuilder sb = new StringBuilder();
+
+		for (MemoryPoolMXBean memoryPool : memoryPools) {
+			if (sb.length() > 0) sb.append(',');
 			sb.append(memoryPool.getName());
 			sb.append(".maxAllowed(bytes),");
 			sb.append(memoryPool.getName());
 			sb.append(".currentUsage(bytes)");
-		}		
-		
-		return(sb.toString());
+		}
+
+		return (sb.toString());
 	}
-	
-	public static String getMemoryInfo(MemoryPoolMXBean memoryPool)
-	{
-		StringBuilder sb=new StringBuilder();
-		
-		MemoryUsage memoryUsage=memoryPool.getUsage();
+
+	public static String getMemoryInfo(MemoryPoolMXBean memoryPool) {
+		StringBuilder sb = new StringBuilder();
+
+		MemoryUsage memoryUsage = memoryPool.getUsage();
 
 		sb.append(memoryUsage.getMax());
 		sb.append(',');
 
 		sb.append(memoryUsage.getUsed());
 
-		return(sb.toString());
+		return (sb.toString());
 	}
-	
-	public static String getMemoryInfo()
-	{
-		List<MemoryPoolMXBean> memoryPools=ManagementFactory.getMemoryPoolMXBeans();
-		StringBuilder sb=new StringBuilder();
-		
-		for (MemoryPoolMXBean memoryPool : memoryPools)
-		{
-			if (sb.length()>0) sb.append(',');
-			sb.append(getMemoryInfo(memoryPool));
-		}		
-		
-		return(sb.toString());
-	}
-	
-	public static String getThreadFormat()
-	{
-		return("PeakThreadCount,ThreadCount(includes deamons),DaemonThreadCount");
-	}
-	
-	public static String getThreadInfo()
-	{
-		StringBuilder sb=new StringBuilder();
 
-		ThreadMXBean threadMXBean=ManagementFactory.getThreadMXBean();
+	public static String getMemoryInfo() {
+		List<MemoryPoolMXBean> memoryPools = ManagementFactory.getMemoryPoolMXBeans();
+		StringBuilder sb = new StringBuilder();
+
+		for (MemoryPoolMXBean memoryPool : memoryPools) {
+			if (sb.length() > 0) sb.append(',');
+			sb.append(getMemoryInfo(memoryPool));
+		}
+
+		return (sb.toString());
+	}
+
+	public static String getThreadFormat() {
+		return ("PeakThreadCount,ThreadCount(includes deamons),DaemonThreadCount");
+	}
+
+	public static String getThreadInfo() {
+		StringBuilder sb = new StringBuilder();
+
+		ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
 		sb.append(threadMXBean.getPeakThreadCount());
 		sb.append(',');
 		sb.append(threadMXBean.getThreadCount());
 		sb.append(',');
 		sb.append(threadMXBean.getDaemonThreadCount());
-		
-		return(sb.toString());
+
+		return (sb.toString());
 	}
-	
-	public static String getGCFormat()
-	{
-		List<GarbageCollectorMXBean> garbageCollectorMXBeans=ManagementFactory.getGarbageCollectorMXBeans();
-		StringBuilder sb=new StringBuilder();
-		
-		for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans)
-		{
-			if (sb.length()>0) sb.append(',');
+
+	public static String getGCFormat() {
+		List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+		StringBuilder sb = new StringBuilder();
+
+		for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans) {
+			if (sb.length() > 0) sb.append(',');
 			sb.append(garbageCollectorMXBean.getName());
 			sb.append(".CollectionTime(ms),");
 			sb.append(garbageCollectorMXBean.getName());
 			sb.append(".CollectionCount");
 		}
 
-		return(sb.toString());
+		return (sb.toString());
 	}
-	
-	public static String getGCInfo(GarbageCollectorMXBean garbageCollectorMXBean)
-	{
-		StringBuilder sb=new StringBuilder();
+
+	public static String getGCInfo(GarbageCollectorMXBean garbageCollectorMXBean) {
+		StringBuilder sb = new StringBuilder();
 
 		sb.append(garbageCollectorMXBean.getCollectionTime());
 		sb.append(',');
 		sb.append(garbageCollectorMXBean.getCollectionCount());
-		
-		return(sb.toString());
+
+		return (sb.toString());
 	}
-	
-	public static String getGCInfo()
-	{
-		List<GarbageCollectorMXBean> garbageCollectorMXBeans=ManagementFactory.getGarbageCollectorMXBeans();
-		StringBuilder sb=new StringBuilder();
-		
-		for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans)
-		{
-			if (sb.length()>0) sb.append(',');
+
+	public static String getGCInfo() {
+		List<GarbageCollectorMXBean> garbageCollectorMXBeans = ManagementFactory.getGarbageCollectorMXBeans();
+		StringBuilder sb = new StringBuilder();
+
+		for (GarbageCollectorMXBean garbageCollectorMXBean : garbageCollectorMXBeans) {
+			if (sb.length() > 0) sb.append(',');
 			sb.append(getGCInfo(garbageCollectorMXBean));
 		}
-		
-		return(sb.toString());
+
+		return (sb.toString());
 	}
-	
-	public static void logAll()
-	{
-		if (counter%10==0) logAllFormat();
+
+	public static void logAll() {
+		if (counter % 10 == 0) logAllFormat();
 		logAllData();
 		counter++;
 	}
 
-	public static void logAllData()
-	{
-		logger.info("DATA,"+getMemoryInfo()+','+getGCInfo()+','+getThreadInfo());
+	public static void logAllData() {
+		logger.info("DATA," + getMemoryInfo() + ',' + getGCInfo() + ',' + getThreadInfo());
 	}
-	
-	public static void logAllFormat()
-	{
-		logger.info("HEADER,"+getMemoryFormat()+','+getGCFormat()+','+getThreadFormat());
+
+	public static void logAllFormat() {
+		logger.info("HEADER," + getMemoryFormat() + ',' + getGCFormat() + ',' + getThreadFormat());
 	}
-	
-	public static void main(String... argv) throws Exception
-	{
+
+	public static void main(String... argv) throws Exception {
 		startContinuousLogging(5000);
-		
+
 		Thread.sleep(60000);
 	}
 }
