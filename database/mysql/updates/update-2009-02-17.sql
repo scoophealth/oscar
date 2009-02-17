c6493611a3a2e474f54f2f199db653d8bb065089
cronnie <cronnie>
2009-02-17 18:35:07 +0000

    added to table "measurementType" 2 extra types: POSK & HYPE

---
+++ database/mysql/updates/update-2009-02-17.sql
@@ -0,0 +1,5 @@
+insert into measurementType (type, typeDisplayName, typeDescription, measuringInstruction, validation)
+values ('POSK', 'Packs of Cigarettes per day', 'Smoking', 'Packs per day', '12');
+insert into measurementType (type, typeDisplayName, typeDescription, measuringInstruction, validation)
+values ('HYPE', 'Hypoglycemic Episodes', 'Number of Hypoglycemic Episodes', '', '18');
+
