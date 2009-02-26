1d0a732da864730a0ae8a10f158a7c4ec0efb55c
danbrow <danbrow>
2009-02-26 05:04:38 +0000

    Adding 6th customization for appointment status.

---
+++ database/mysql/updates/update-2009-02-25b.sql
@@ -0,0 +1,8 @@
+
+
+
+update appointment_status set id="14" where status="B";
+update appointment_status set id="13" where status="C";
+update appointment_status set id="12" where status="N";
+
+insert into appointment_status value (11,'f','Custumized 6','#897DF8','6.gif',0,1);
