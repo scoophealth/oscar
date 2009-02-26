8ccad4c610a425a35a1f1b0166c9350027f832bb
cronnie <cronnie>
2009-02-26 17:33:34 +0000

    update for consistency

--- web/WEB-INF/classes/src/oscar/oscarRx/data/RxAllergyImport.java
+++ web/WEB-INF/classes/src/oscar/oscarRx/data/RxAllergyImport.java
@@ -31,28 +31,26 @@ import oscar.oscarDB.DBHandler;
 
 public class RxAllergyImport {
    
-    public Long Save(String demographicNo, String entryDate, String description, String typeCode, String reaction, String startDate, String severity, String drugrefId) throws SQLException {
+    public static Long save(String demographicNo, String entryDate, String description, String typeCode, String reaction, String startDate, String severity, String regionalId) throws SQLException {
 	boolean b = false;            
 	DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);            
-	String sql = "INSERT INTO allergies (demographic_no, entry_date, DESCRIPTION, TYPECODE, reaction, start_date, severity_of_reaction, drugref_id) VALUES ('"
-		+ demographicNo + "','" + entryDate + "','" + description + "','" + typeCode + "','" + reaction + "','" + startDate + "','" + severity + "','" + drugrefId + "')";
-	if (db.RunSQL(sql)) {
-	    sql = "SELECT MAX(allergyid) FROM allergies WHERE " +
-		    "demographic_no='"	     + demographicNo + "' AND " +
-		    "entry_date='"	     + entryDate     + "' AND " +
-		    "DESCRIPTION='"	     + description   + "' AND " +
-		    "TYPECODE='"	     + typeCode	     + "' AND " +
-		    "reaction='"	     + reaction	     + "' AND " +
-		    "start_date='"	     + startDate     + "' AND " +
-		    "severity_of_reaction='" + severity	     + "' AND " +
-		    "drugref_id='"	     + drugrefId     + "'";
-	    ResultSet rs = db.GetSQL(sql);
-	    db.CloseConn();
+	String sql = "INSERT INTO allergies (demographic_no, entry_date, DESCRIPTION, TYPECODE, reaction, start_date, severity_of_reaction, regional_identifier) VALUES ('"
+		+ demographicNo + "','" + entryDate + "','" + description + "','" + typeCode + "','" + reaction + "','" + startDate + "','" + severity + "','" + regionalId + "')";
+	db.RunSQL(sql);
+	
+	sql = "SELECT MAX(allergyid) FROM allergies WHERE " +
+		"demographic_no='"	+ demographicNo	+ "' AND " +
+		"entry_date='"		+ entryDate	+ "' AND " +
+		"DESCRIPTION='"		+ description	+ "' AND " +
+		"TYPECODE='"		+ typeCode	+ "' AND " +
+		"reaction='"		+ reaction	+ "' AND " +
+		"start_date='"		+ startDate	+ "' AND " +
+		"severity_of_reaction='"+ severity	+ "' AND " +
+		"regional_identifier='"	+ regionalId	+ "'";
+	ResultSet rs = db.GetSQL(sql);
+	db.CloseConn();
 	    
-	    if (rs.next()) return rs.getLong(1);
-	    else return null;
-	} else {
-	    return null;
-	}
+	if (rs.next()) return rs.getLong(1);
+	else return null;
      }
 }
