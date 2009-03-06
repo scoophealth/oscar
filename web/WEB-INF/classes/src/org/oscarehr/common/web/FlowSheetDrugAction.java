91adb75c3dee2033ccef04cca90dfee78a24f870
jaygallagher <jaygallagher>
2009-03-06 18:17:42 +0000

    Action to save references to Drug to Flowsheet

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/web/FlowSheetDrugAction.java
@@ -0,0 +1,61 @@
+package org.oscarehr.common.web;
+
+import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+
+import org.oscarehr.common.dao.FlowSheetDrugDAO;
+import org.oscarehr.common.model.FlowSheetDrug;
+
+public class FlowSheetDrugAction extends DispatchAction {
+
+    private static final Log log2 = LogFactory.getLog(FlowSheetDrugAction.class);
+    private FlowSheetDrugDAO flowSheetDrugDAO;
+
+    public void setFlowSheetDrugDAO(FlowSheetDrugDAO flowSheetDrugDAO) {
+        this.flowSheetDrugDAO = flowSheetDrugDAO;
+    }
+
+    @Override
+    protected ActionForward unspecified(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        log2.debug("AnnotationAction-unspec");
+        //return setup(mapping, form, request, response);
+        return null;
+    }
+
+    public ActionForward save(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String flowsheet = request.getParameter("flowsheet");
+        String demographicNo = request.getParameter("demographic");
+        String atcCode = request.getParameter("atcCode");
+
+        FlowSheetDrug cust = new FlowSheetDrug();
+        cust.setFlowsheet(flowsheet);
+        cust.setAtcCode(atcCode);
+        cust.setProviderNo((String) request.getSession().getAttribute("user"));
+        cust.setDemographicNo(Integer.parseInt(demographicNo));
+        cust.setCreateDate(new Date());
+
+        log2.debug("SAVE " + cust);
+
+        flowSheetDrugDAO.save(cust);
+        
+        ActionForward ff = mapping.findForward("success");
+        //ff.setRedirect(true);
+        ActionForward af = new ActionForward(ff.getPath()+"?demographic_no="+demographicNo+"&template="+flowsheet, true);
+        
+
+
+
+        //request.setAttribute("demographic", demographicNo);
+        //request.setAttribute("flowsheet", flowsheet);
+        return af;
+    }
+}
