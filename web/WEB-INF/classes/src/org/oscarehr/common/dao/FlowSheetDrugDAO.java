bff6b0f9f0620920319648d90b1c8dbce6341f38
jaygallagher <jaygallagher>
2009-03-06 18:13:45 +0000

    added flowSheetDrugDAO

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/dao/FlowSheetDrugDAO.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. 
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+ *
+ * Jason Gallagher
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.dao;
+
+import java.util.List;
+import org.oscarehr.common.model.FlowSheetCustomization;
+import org.oscarehr.common.model.FlowSheetDrug;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+/**
+ *
+ * @author jaygallagher
+ */
+public class FlowSheetDrugDAO extends HibernateDaoSupport {
+    
+    public void save(FlowSheetDrug fsc) {
+        this.getHibernateTemplate().saveOrUpdate(fsc);
+    }
+    
+    public FlowSheetCustomization getFlowSheetDrug(String id){
+        List<FlowSheetCustomization> list = this.getHibernateTemplate().find("from FlowSheetDrug p where  p.id = ?", new Object[] {Long.parseLong(id)});
+        if (list != null && list.size() > 0){
+            return list.get(0);
+        }
+        return null;
+    }
+
+    public List<FlowSheetDrug> getFlowSheetDrugs(String flowsheet,String demographic){
+        List<FlowSheetDrug> list = this.getHibernateTemplate().find("from FlowSheetDrug p where p.flowsheet = ? and p.archived = 0 and p.demographicNo = ?   ", new Object[] {flowsheet,Integer.parseInt(demographic)});
+        return list;
+    }
+
+    
+    
+    
+}
