457ca44754fe72be8b00f7f4dd9ebe46cd82f7d2
tedleung <tedleung>
2009-03-30 21:11:19 +0000

    fix for deregistering shutdown when it's shutting down

--- web/WEB-INF/classes/src/org/oscarehr/util/ContextStartupListener.java
+++ web/WEB-INF/classes/src/org/oscarehr/util/ContextStartupListener.java
@@ -6,37 +6,40 @@ import org.oscarehr.PMmodule.caisi_integrator.CaisiIntegratorUpdateTask;
 
 import oscar.OscarProperties;
 
-public class ContextStartupListener implements javax.servlet.ServletContextListener
-{
+public class ContextStartupListener implements javax.servlet.ServletContextListener {
 	private static final Logger logger = LogManager.getLogger(ContextStartupListener.class);
 
-	public void contextInitialized(javax.servlet.ServletContextEvent sce)
-	{
-// need tc6 for this?
-//		logger.info("Server processes starting. context=" + sce.getServletContext().getContextPath());
+	public void contextInitialized(javax.servlet.ServletContextEvent sce) {
+		// need tc6 for this?
+		// logger.info("Server processes starting. context=" + sce.getServletContext().getContextPath());
 
 		try {
-	        OscarProperties properties=OscarProperties.getInstance();
-	        String vmstatLoggingPeriod=properties.getProperty("VMSTAT_LOGGING_PERIOD");
-	        VmStat.startContinuousLogging(Long.parseLong(vmstatLoggingPeriod));
-        } catch (Exception e) {
-	        logger.error("Unexpected error.", e);
-        }
-		
-        MiscUtils.setShutdownSignaled(false);
-        MiscUtils.registerShutdownHook();
-        
-		CaisiIntegratorUpdateTask.startTask();		
+			OscarProperties properties = OscarProperties.getInstance();
+			String vmstatLoggingPeriod = properties.getProperty("VMSTAT_LOGGING_PERIOD");
+			VmStat.startContinuousLogging(Long.parseLong(vmstatLoggingPeriod));
+		} catch (Exception e) {
+			logger.error("Unexpected error.", e);
+		}
+
+		MiscUtils.setShutdownSignaled(false);
+		MiscUtils.registerShutdownHook();
+
+		CaisiIntegratorUpdateTask.startTask();
 	}
 
-	public void contextDestroyed(javax.servlet.ServletContextEvent sce)
-	{
-// need tc6 for this?
-//		logger.info("Server processes stopping. context=" + sce.getServletContext().getContextPath());
+	public void contextDestroyed(javax.servlet.ServletContextEvent sce) {
+		// need tc6 for this?
+		// logger.info("Server processes stopping. context=" + sce.getServletContext().getContextPath());
 
 		CaisiIntegratorUpdateTask.stopTask();
-        MiscUtils.setShutdownSignaled(true);
-        MiscUtils.deregisterShutdownHook();
 		VmStat.stopContinuousLogging();
+
+		try {
+			MiscUtils.checkShutdownSignaled();
+			MiscUtils.deregisterShutdownHook();
+			MiscUtils.setShutdownSignaled(true);
+		} catch (ShutdownException e) {
+			// do nothing it's okay.
+		}
 	}
 }
