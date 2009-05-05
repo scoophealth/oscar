d2efe6b5fd049d2c7bcba9ed0cfbe51b52d5552c
jaygallagher <jaygallagher>
2009-05-05 15:55:02 +0000

    no message

--- web/WEB-INF/classes/src/oscar/oscarLab/ca/all/upload/handlers/HHSEmrDownloadHandler.java
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/all/upload/handlers/HHSEmrDownloadHandler.java
@@ -1,10 +1,25 @@
 /*
- * DefaultHandler.java
+ *  Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ *  This software is published under the GPL GNU General Public License.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version. *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
  *
- * Created on May 23, 2007, 4:30 PM
+ *  Jason Gallagher
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *  This software was written for the
+ *  Department of Family Medicine
+ *  McMaster University
+ *  Hamilton
+ *  Ontario, Canada   
+ *
  */
 package oscar.oscarLab.ca.all.upload.handlers;
 
@@ -13,10 +28,6 @@ package oscar.oscarLab.ca.all.upload.handlers;
 import org.apache.log4j.Logger;
 
 
-/**
- *
- * @author wrighd
- */
 public class HHSEmrDownloadHandler extends DefaultHandler implements MessageHandler {
     Logger logger = Logger.getLogger(HHSEmrDownloadHandler.class);
     
