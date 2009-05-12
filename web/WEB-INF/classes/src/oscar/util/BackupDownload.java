bb24dd5cb9e9c953bf3b4d53aef47f29318c3e41
rjonasz <rjonasz>
2009-05-12 14:59:00 +0000

    Check permissions from secObjPrivilege table rather than just userrole

--- web/WEB-INF/classes/src/oscar/util/BackupDownload.java
+++ web/WEB-INF/classes/src/oscar/util/BackupDownload.java
@@ -1,10 +1,13 @@
 package oscar.util;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import oscar.oscarDB.DBHandler;
 
 public class BackupDownload extends GenericDownload {
 
@@ -17,17 +20,34 @@ public class BackupDownload extends GenericDownload {
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         HttpSession session = req.getSession(true);
 
-        // check the rights
-        session.getAttribute("role");
+        // check the rights        
         String filename = req.getParameter("filename") == null ? "null" : req.getParameter("filename");
         String dir = (String) session.getAttribute("backupfilepath") == null ? "/home/mysql/" : (String) session
                 .getAttribute("backupfilepath");
+        
+        boolean adminPrivs = false;
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+            String sql = "select objectName from secObjPrivilege where provider_no = '" + (String)session.getAttribute("user") + "'";
+            java.sql.ResultSet rs = db.GetSQL(sql);
+            
+            while(rs.next()) {
+                if( ROLE_BACKUP_ADMIN.equalsIgnoreCase(rs.getString("objectName")) ) {
+                    adminPrivs = true;
+                    break;
+                }
+            }
+            
+            rs.close();
+            db.CloseConn();
+        }catch(SQLException e) {
+            e.printStackTrace();
+        }
         boolean bDownload = false;
         if (filename != null
-                && (((String) session.getAttribute("userrole") != null) && ((String) session.getAttribute("userrole"))
-                        .indexOf(ROLE_BACKUP_ADMIN) >= 0)
+                && (adminPrivs
                 || ((String) session.getAttribute("userprofession") != null && ((String) session
-                        .getAttribute("userprofession")).equalsIgnoreCase(PROFESSINAL_ADMIN))) {
+                        .getAttribute("userprofession")).equalsIgnoreCase(PROFESSINAL_ADMIN)))) {
             bDownload = true;
         }
         download(bDownload, res, dir, filename, null);
