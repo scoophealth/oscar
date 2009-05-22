be5a714c696da1ca818c5171614bc4d57f64574a
tedleung <tedleung>
2009-05-22 20:04:54 +0000

    revert

--- database/mysql/updates/update-2009-05-22.sql
+++ database/mysql/updates/update-2009-05-22.sql
@@ -1,10 +1,2 @@
-alter table provider drop practitionerNo;
-alter table provider drop init;
-alter table provider drop job_title;
-alter table provider drop email;
-alter table provider drop title;
-alter table provider drop lastUpdateUser;
-alter table provider drop lastUpdateDate;
-
 alter table provider add lastUpdated datetime not null;
 update provider set lastUpdated=now();
