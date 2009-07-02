169d105f801f45a4fbb7e3b58730b9754c2b0071
rjonasz <rjonasz>
2009-07-02 17:48:17 +0000

    Log user action accept/refuse of confidentiality agreement

---
+++ web/WEB-INF/classes/src/oscar/login/LoginAgreementAction.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License. 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+ * 
+ * <OSCAR TEAM>
+ * This software was written for the 
+ * Department of Family Medicine 
+ * McMaster Unviersity 
+ * Hamilton 
+ * Ontario, Canada 
+ */
+
+package oscar.login;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+
+import oscar.log.LogAction;
+import oscar.log.LogConst;
+import org.apache.log4j.Logger;
+
+import org.oscarehr.PMmodule.dao.ProviderDao;
+import org.oscarehr.common.model.Provider;
+
+/**
+ *
+ * @author rjonasz
+ */
+public class LoginAgreementAction extends DispatchAction {
+    private static final Logger _logger = Logger.getLogger(LoginAgreementAction.class);
+
+    private ProviderDao providerDao;
+
+    public void setProviderDao(ProviderDao providerDao) {
+        this.providerDao = providerDao;
+    }
+
+    public ProviderDao getProviderDao() {
+        return this.providerDao;
+    }
+
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+        String userAgreement = request.getParameter("submit");
+        String user = (String)request.getSession().getAttribute("user");
+        if( userAgreement.equalsIgnoreCase("refuse") ) {
+            _logger.info(user + " refused agreement");
+        }
+        else if( userAgreement.equalsIgnoreCase("accept") ) {
+            _logger.info(user + " accepted agreement");
+            Provider provider = providerDao.getProvider(user);
+            Date now = new Date();
+            provider.setSignedConfidentiality(now);
+            providerDao.updateProvider(provider);
+        }
+
+        LogAction.addLog(user, LogConst.ACK, LogConst.CON_LOGIN_AGREEMENT, userAgreement, request.getRemoteAddr());
+
+        String proceedURL = (String)request.getSession().getAttribute("proceedURL");
+        request.getSession().setAttribute("proceedURL", null);
+        ActionForward fwd = new ActionForward();
+        fwd.setPath(proceedURL);
+        fwd.setRedirect(true);
+        return fwd;
+    }
+
+}
