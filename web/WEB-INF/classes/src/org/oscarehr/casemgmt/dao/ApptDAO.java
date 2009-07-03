dbf3fc6518d17f155ad4074472f9e18694aaf500
jaygallagher <jaygallagher>
2009-07-03 21:48:11 +0000

    added method getAppt

--- web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/ApptDAO.java
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/dao/ApptDAO.java
@@ -48,4 +48,6 @@ public class ApptDAO extends HibernateDaoSupport {
        Appointment appt = (Appointment) getHibernateTemplate().get(Appointment.class,new Long(apptId));
        return appt;
     }
+    
+   
 }
