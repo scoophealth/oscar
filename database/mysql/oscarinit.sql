31b71622496b2e6831948d12fb7db2dd3b89295b
rjonasz <rjonasz>
2009-07-29 14:56:11 +0000

    Add flowsheet_dx table     --------------------------------------------------------------------

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -7971,3 +7971,15 @@ CREATE TABLE `dsGuidelineProviderMap` (
   `guideline_uuid` varchar(60) NOT NULL,
   PRIMARY KEY (`mapid`)
 ) ENGINE=MyISAM;
+
+create table flowsheet_dx (
+      id int(10) NOT NULL auto_increment primary key,
+      flowsheet varchar(40),
+      dx_code varchar(40),
+      dx_code_type varchar(40),
+      provider_no varchar(6),
+      demographic_no int(10),
+      create_date datetime,
+      archived char(1) default '0',
+      archived_date datetime
+    ) ;
