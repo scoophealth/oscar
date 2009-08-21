850d362c6cd37cc459c5e657972612ceba2b9dd6
hexbinary <hexbinary>
2009-08-21 13:06:34 +0000

    ocan custom js example

---
+++ web/js/custom/ocan-staff.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+
+	$("input[name='intake.answerMapped(201146).value']").change(function(){
+		$("input[name='intake.answerMapped(201145).value']").attr('checked',true);
+	});
+
+});
