7aa7fa558db71836e3b3fc39493613fb73345b52
tedleung <tedleung>
2009-09-04 20:25:20 +0000

    oops inconsistency between db tables on devel and init script

--- web/WEB-INF/classes/src/org/oscarehr/common/model/SecRole.java
+++ web/WEB-INF/classes/src/org/oscarehr/common/model/SecRole.java
@@ -1,14 +1,12 @@
 package org.oscarehr.common.model;
 
 import java.io.Serializable;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
@@ -22,13 +20,6 @@ public class SecRole implements Serializable {
 	@Column(name = "role_name")
 	private String name;
 	private String description;
-	@Column(name = "isActive")
-	private boolean active;
-	private int displayOrder;
-	@Column(name = "lastUpdateUser")
-	private String lastUpdateProvider;
-	@Column(name = "lastUpdateDate")
-	private Date lastUpdated = new Date();
 
 	public Integer getId() {
 		return id;
@@ -48,39 +39,6 @@ public class SecRole implements Serializable {
 
 	public void setDescription(String description) {
 		this.description = description;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	public int getDisplayOrder() {
-		return displayOrder;
-	}
-
-	public void setDisplayOrder(int displayOrder) {
-		this.displayOrder = displayOrder;
-	}
-
-	public String getLastUpdateProvider() {
-		return lastUpdateProvider;
-	}
-
-	public void setLastUpdateProvider(String lastUpdateProvider) {
-		this.lastUpdateProvider = lastUpdateProvider;
-	}
-
-	public Date getLastUpdated() {
-		return lastUpdated;
-	}
-
-	@PreUpdate
-	protected void jpaUpdateLastUpdateTime() {
-		lastUpdated = new Date();
 	}
 
 	public boolean equals(Object o) {
