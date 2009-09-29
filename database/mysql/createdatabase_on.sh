bc1c57df040757a8e66664575b19b83f4ae17415
tedleung <tedleung>
2009-09-29 17:10:40 +0000

    updated scripts to use a common code base

--- database/mysql/createdatabase_on.sh
+++ database/mysql/createdatabase_on.sh
@@ -1,15 +1,3 @@
-##CREATE DATABASE
+#!/bin/sh
 
-PASSWORD=$1
-DATABASE_NAME=$2
-
-mysqladmin -uroot -p$PASSWORD create $DATABASE_NAME
-echo "grant all on $DATABASE_NAME.* to root@localhost identified by \"$PASSWORD\"" |  mysql -uroot -p$PASSWORD  $DATABASE_NAME
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscarinit.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscarinit_on.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscardata.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < oscardata_on.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < icd9.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < caisi/initcaisi.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < caisi/initcaisidata.sql
-mysql -uroot -p$PASSWORD  $DATABASE_NAME  < measurementMapData.sql
+./createdatabase_generic.sh $@ on
