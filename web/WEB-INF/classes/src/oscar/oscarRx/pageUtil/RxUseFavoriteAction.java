444db0980cad732a16ee0d2246fd848e13148c4a
ygmgbg <ygmgbg>
2009-10-22 21:28:26 +0000

    added useFav2 method

--- web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxUseFavoriteAction.java
+++ web/WEB-INF/classes/src/oscar/oscarRx/pageUtil/RxUseFavoriteAction.java
@@ -24,25 +24,28 @@
 package oscar.oscarRx.pageUtil;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
 import org.apache.struts.util.MessageResources;
 
 import oscar.oscarRx.data.RxPrescriptionData;
+import oscar.oscarRx.util.RxUtil;
 
 
-public final class RxUseFavoriteAction extends Action {
+public final class RxUseFavoriteAction extends DispatchAction {
     
     
-    public ActionForward execute(ActionMapping mapping,
+    public ActionForward unspecified(ActionMapping mapping,
     ActionForm form,
     HttpServletRequest request,
     HttpServletResponse response)
@@ -84,4 +87,66 @@ public final class RxUseFavoriteAction extends Action {
         
         return (mapping.findForward("success"));
     }
+
+    public ActionForward useFav2(ActionMapping mapping,
+    ActionForm form,
+    HttpServletRequest request,
+    HttpServletResponse response)
+    throws IOException, ServletException {
+       System.out.println("==========***###IN RxUseFavoriteAction.java=============");
+        // Extract attributes we will need
+        Locale locale = getLocale(request);
+        MessageResources messages = getResources(request);
+
+        // Setup variables
+        oscar.oscarRx.pageUtil.RxSessionBean bean =
+        (oscar.oscarRx.pageUtil.RxSessionBean)request.getSession().getAttribute("RxSessionBean");
+        if(bean==null){
+            response.sendRedirect("error.html");
+            return null;
+        }
+
+        try {
+            int favoriteId = Integer.parseInt(request.getParameter("favoriteId"));
+            String randomId=request.getParameter("randomId");
+            System.out.println("favoriteId ="+favoriteId);
+            System.out.println("randomId ="+randomId);
+            RxPrescriptionData rxData =
+            new RxPrescriptionData();
+
+            // get favorite
+            RxPrescriptionData.Favorite fav =
+            rxData.getFavorite(favoriteId);
+
+            // create Prescription
+            RxPrescriptionData.Prescription rx =
+            rxData.newPrescription(bean.getProviderNo(), bean.getDemographicNo(), fav);
+            rx.setRandomId(Long.parseLong(randomId));
+            if(rx!=null){
+                String spec=RxUtil.trimSpecial(rx);
+                rx.setSpecial(spec);
+                System.out.println(rx.getBrandName()+" rx.getspeiclia="+rx.getSpecial());
+            }
+            else{
+                System.out.println("rx is null!");
+            }
+            
+            bean.addAttributeName(rx.getAtcCode() + "-" + String.valueOf(bean.getStashIndex()));
+          //   System.out.println("***###addStathItem called22");
+            bean.setStashIndex(bean.addStashItem(rx));
+
+            List<RxPrescriptionData.Prescription> listRxDrugs=new ArrayList();
+            listRxDrugs.add(rx);
+            request.setAttribute("listRxDrugs",listRxDrugs);
+            request.setAttribute("BoxNoFillFirstLoad", "true");
+        }
+        catch (Exception e) {
+            e.printStackTrace(System.out);
+        }
+
+        RxUtil.printStashContent(bean);
+        System.out.println("==========***###END RxUseFavoriteAction.java=============");
+        return (mapping.findForward("useFav2"));
+    }
 }
+
