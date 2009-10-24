1150b782e8a81d22698efdacd25e820c758418cf
ygmgbg <ygmgbg>
2009-10-24 04:01:28 +0000

    colour class for document, can be used for extension to other type of notes

---
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/common/Colour.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.oscarehr.casemgmt.common;
+
+/**
+ *
+ * @author jackson
+ */
+public class Colour {
+    public static final String prevention = "009999";
+    public static final String tickler = "FF6600";
+    public static final String disease = "5A5A5A";
+    public static final String forms = "917611";
+    public static final String eForms = "11CC00";
+    public static final String documents = "476BB3";
+    public static final String labs = "A0509C";
+    public static final String messages = "DDDD00";
+    public static final String measurements = "344887";
+}
