9d588c1ec1fdabbcf677d2af9810e42f1294a6f5
ygmgbg <ygmgbg>
2009-12-14 20:45:55 +0000

    -add RX_DEFAULT_QUANTITY

--- web/WEB-INF/classes/src/org/oscarehr/common/model/UserProperty.java
+++ web/WEB-INF/classes/src/org/oscarehr/common/model/UserProperty.java
@@ -42,6 +42,7 @@ public class UserProperty implements Serializable {
     public final static String WORKLOAD_MANAGEMENT = "workload_management";
     public final static String CONSULTATION_REQ_PASTE_FMT = "consultation_req_paste_fmt";
     public final static String RX_PAGE_SIZE = "rx_page_size";
+    public final static String RX_DEFAULT_QUANTITY = "rx_default_quantity";
    
     private long id;
     private String name;
