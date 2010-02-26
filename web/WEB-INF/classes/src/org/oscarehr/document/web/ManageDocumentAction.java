08ad4048369224dc455f3e942c2b4875db1eccdd
ygmgbg <ygmgbg>
2010-02-26 19:22:45 +0000

    roll back to previous version

--- web/WEB-INF/classes/src/org/oscarehr/document/web/ManageDocumentAction.java
+++ web/WEB-INF/classes/src/org/oscarehr/document/web/ManageDocumentAction.java
@@ -4,16 +4,16 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version. 
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details. 
- * 
+ * GNU General Public License for more details.
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
  * Jason Gallagher
  *
@@ -44,16 +44,12 @@ import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
-import org.oscarehr.casemgmt.model.CaseManagementNote;
-import org.oscarehr.casemgmt.model.CaseManagementNoteLink;
-import org.oscarehr.casemgmt.service.CaseManagementManager;
 import org.oscarehr.document.dao.DocumentDAO;
 import org.oscarehr.document.model.CtlDocument;
 import org.oscarehr.document.model.Document;
@@ -61,52 +57,46 @@ import oscar.log.LogAction;
 import oscar.log.LogConst;
 import oscar.util.UtilDateUtilities;
 import org.oscarehr.common.dao.ProviderInboxRoutingDao;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-import oscar.dms.EDocUtil;
-import oscar.oscarEncounter.data.EctProgram;
 
 /**
  *
  * @author jaygallagher
  */
 public class ManageDocumentAction extends DispatchAction {
-    
+
     private static Log log = LogFactory.getLog(ManageDocumentAction.class);
 
     private DocumentDAO documentDAO = null;
     private ProviderInboxRoutingDao  providerInboxRoutingDAO = null;
-    
+
 
     public void setDocumentDAO(DocumentDAO documentDAO) {
         this.documentDAO = documentDAO;
     }
-    
+
     public void setProviderInboxRoutingDAO(ProviderInboxRoutingDao providerInboxRoutingDAO){
         this.providerInboxRoutingDAO = providerInboxRoutingDAO;
     }
 
     public ActionForward unspecified(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        System.out.println("AddEditDocumentAction - unspecified");
+        //System.out.println("AddEditDocumentAction - unspecified");
         return null;//execute2(mapping, form, request, response);
     }
 
     //public ActionForward multifast(
     //public ActionForward documentUpdate(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-    public ActionForward documentUpdate(org.apache.struts.action.ActionMapping mapping, org.apache.struts.action.ActionForm form,
-            javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
+    public ActionForward documentUpdate(org.apache.struts.action.ActionMapping mapping, org.apache.struts.action.ActionForm form, javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
         //System.out.println("In here DocumentUpdate");
-         String ret = "";
-       // providerInboxRoutingDAO=new ProviderInboxRoutingDao();
+        String ret = "";
 
 
         String observationDate = request.getParameter("observationDate");// :2008-08-22<
         String documentDescription = request.getParameter("documentDescription");//:test2<
         String documentId = request.getParameter("documentId");//:29<
         String docType = request.getParameter("docType");//:consult<
-        
+
         LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.ADD, LogConst.CON_DOCUMENT, documentId, request.getRemoteAddr());
-        
+
         String demog = request.getParameter("demog");// :22<
         String demographicKeyword = request.getParameter("demographicKeyword");// :ABLE, ALEX<
         String[] flagproviders = request.getParameterValues("flagproviders");//:999998<
@@ -114,24 +104,18 @@ public class ManageDocumentAction extends DispatchAction {
         System.out.println("link to prov "+request.getParameter("demoLink")+" demo "+demog);
         //DONT COPY THIS !!!
          if (flagproviders !=null && flagproviders.length > 0){ //TODO: THIS NEEDS TO RUN THRU THE  lab forwarding rules!
-             try{
-                for(String proNo:flagproviders){
-                    System.out.println("flagproviders="+flagproviders.length+"; flagproviders="+flagproviders);
-                    for(int i=0;i<flagproviders.length;i++){
-                        System.out.println("element="+flagproviders[i]);
-                    }
-                    System.out.println("proNo="+proNo+"; documentId="+documentId);
-                    providerInboxRoutingDAO.addToProviderInbox(proNo, documentId, "DOC");
-                   //try{
-                   //String docLab = "insert into providerLabRouting (provider_no,lab_no,status,timestamp,lab_type)  values ('"+proNo+"','"+documentId+"','N',now(),\"DOC\")";
-                   //DBHandler dbh = new DBHandler(DBHandler.OSCAR_DATA);
-                   //dbh.RunSQL(docLab);
-                   //}catch(Exception ee ){ee.printStackTrace();}
-                }
-             }catch(Exception e){e.printStackTrace();}
+
+            for(String proNo:flagproviders){
+               providerInboxRoutingDAO.addToProviderInbox(proNo, documentId, "DOC");
+               //try{
+               //String docLab = "insert into providerLabRouting (provider_no,lab_no,status,timestamp,lab_type)  values ('"+proNo+"','"+documentId+"','N',now(),\"DOC\")";
+               //DBHandler dbh = new DBHandler(DBHandler.OSCAR_DATA);
+               //dbh.RunSQL(docLab);
+               //}catch(Exception ee ){ee.printStackTrace();}
+            }
         }
-        //// END COPY  
-        
+        //// END COPY
+
 
         Document d = documentDAO.getDocument(documentId);
 
@@ -145,8 +129,8 @@ public class ManageDocumentAction extends DispatchAction {
             d.setObservationdate(obDate);
         }
 
-        
-        
+
+
 
 
         //System.out.println("bbb " + d);
@@ -165,47 +149,16 @@ public class ManageDocumentAction extends DispatchAction {
 
             //System.out.println("1" + ctlDocument.toString());
             documentDAO.saveCtlDocument(ctlDocument);
-            //save a document created note
-            if(ctlDocument.isDemographicDocument()){
-                    Date now=EDocUtil.getDmsDateTimeAsDate();
-                    String docDesc=d.getDocdesc();
-                    CaseManagementNote cmn=new CaseManagementNote();
-                    cmn.setUpdate_date(now);
-                    cmn.setObservation_date(now);
-                    cmn.setDemographic_no(demog);
-                    HttpSession se = request.getSession();
-                    String user_no = (String) se.getAttribute("user");
-                    String prog_no = new EctProgram(se).getProgram(user_no);
-                    WebApplicationContext  ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(se.getServletContext());
-                    CaseManagementManager cmm = (CaseManagementManager) ctx.getBean("caseManagementManager");
-                    cmn.setProviderNo("-1");// set the provider no to be -1 so the editor appear as 'System'.
-                    String provFirstName=EDocUtil.getProviderInfo("first_name", user_no);
-                    String provLastName=EDocUtil.getProviderInfo("last_name", user_no);
-                    String strNote="Document"+" "+docDesc+" "+  "created at "+now+" by "+provFirstName+" "+provLastName+".";
-                    System.out.println("here0 "+strNote);
-                   // String strNote="Document"+" "+docDesc+" "+  "created at "+now+".";
-                    cmn.setNote(strNote);
-                    cmn.setSigned(true);
-                    cmn.setSigning_provider_no("-1");
-                    cmn.setProgram_no(prog_no);
-                    cmn.setReporter_caisi_role("1");
-                    cmn.setReporter_program_team("0");
-                    cmn.setPassword("NULL");
-                    cmn.setLocked(false);
-                    cmn.setHistory(strNote);
-                    cmn.setPosition(0);
-                    cmm.saveNoteSimple(cmn);
-                    //Add a noteLink to casemgmt_note_link
-                    CaseManagementNoteLink cmnl=new CaseManagementNoteLink();
-                    cmnl.setTableName(cmnl.DOCUMENT);
-                    cmnl.setTableId(Long.parseLong(documentId));
-                    cmnl.setNoteId(Long.parseLong(EDocUtil.getLastNoteId()));
-                    EDocUtil.addCaseMgmtNoteLink(cmnl);
-            }
-
+            //System.out.println("2" + ctlDocument.toString());
+        //ret = "{\"success\": \"yes\", \"docId\" : \""+documentId+"\"}";
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
+
+
+
         if (flagproviders != null) {
             for (String str : flagproviders) {
                 //System.out.println("str " + str);
@@ -221,15 +174,15 @@ public class ManageDocumentAction extends DispatchAction {
         File docDir = new File(docdownload);
         String documentDirName = docDir.getName();
         File parentDir = docDir.getParentFile();
-        
+
         File cacheDir = new File(parentDir,documentDirName+"_cache");
-        
+
         if(!cacheDir.exists()){
             cacheDir.mkdir();
         }
         return cacheDir;
     }
-    
+
     private File hasCacheVersion(Document d){
         File documentCacheDir = getDocumentCacheDir(oscar.OscarProperties.getInstance().getProperty("DOCUMENT_DIR"));
         File outfile = new File(documentCacheDir,d.getDocfilename()+".png");
@@ -238,14 +191,14 @@ public class ManageDocumentAction extends DispatchAction {
         }
         return outfile;
     }
-    
+
     public File createCacheVersion(Document d) throws Exception{
-        
+
         String docdownload = oscar.OscarProperties.getInstance().getProperty("DOCUMENT_DIR");
         File documentDir = new File(docdownload);
         File documentCacheDir = getDocumentCacheDir(docdownload);
         log.debug("Document Dir is a dir"+documentDir.isDirectory());
-        System.out.println("getdocfilename: "+d.getDocfilename());
+
         File file = new File(documentDir,d.getDocfilename());
 
         RandomAccessFile raf = new RandomAccessFile(file, "r");
@@ -258,7 +211,7 @@ public class ManageDocumentAction extends DispatchAction {
 
         log.debug("WIDTH " + (int) ppage.getBBox().getWidth() + " height " + (int) ppage.getBBox().getHeight());
 
-        //get the width and height for the doc at the default zoom 
+        //get the width and height for the doc at the default zoom
         Rectangle rect = new Rectangle(0, 0,
                 (int) ppage.getBBox().getWidth(),
                 (int) ppage.getBBox().getHeight());
@@ -271,30 +224,28 @@ public class ManageDocumentAction extends DispatchAction {
                 true, // fill background with white
                 true // block until drawing is done
                 );
-       
+
         log.debug("about to Print to stream");
         File outfile = new File(documentCacheDir,d.getDocfilename()+".png");
         OutputStream outs = new FileOutputStream(outfile);
-  
+
         RenderedImage rendImage = (RenderedImage) img;
         ImageIO.write(rendImage, "png", outs);
         outs.flush();
-        outs.close(); 
+        outs.close();
         return outfile;
-        
+
     }
     //PNG version
     public ActionForward view(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        System.out.println("in managedocumentaction--view");
-       try{
-           String doc_no = request.getParameter("doc_no");
+        String doc_no = request.getParameter("doc_no");
            log.debug("Document No :"+doc_no);
-                   
+
         LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.READ, LogConst.CON_DOCUMENT, doc_no, request.getRemoteAddr());
-  
+
         Document d = documentDAO.getDocument(doc_no);
         log.debug("Document Name :"+d.getDocfilename());
-        
+
         File outfile = hasCacheVersion(d);
         if (outfile == null){
             System.out.println("No Cache Version");
@@ -303,16 +254,16 @@ public class ManageDocumentAction extends DispatchAction {
         }else{
             System.out.println("THERE WAS A CACHE Version "+outfile);
         }
-        
+
         response.setContentType("image/png");
         //response.setHeader("Content-Disposition", "attachment;filename=\"" + filename+ "\"");
         //read the file name.
-        
+
 
         log.debug("about to Print to stream");
         ServletOutputStream outs = response.getOutputStream();
- 
-        
+
+
         response.setHeader("Content-Disposition", "attachment;filename=" + d.getDocfilename());
         BufferedInputStream bfis = new BufferedInputStream(new FileInputStream(outfile));
         int data;
@@ -320,33 +271,29 @@ public class ManageDocumentAction extends DispatchAction {
             outs.write(data);
             outs.flush();
         }
-		
+
         bfis.close();
         outs.flush();
-        outs.close();     
-       }catch(Exception e){
-           e.printStackTrace();
-       }
+        outs.close();
         return null;
     }
 
     public ActionForward view2(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        System.out.println("in managedocumentaction--view2222");
         //TODO: NEED TO CHECK FOR ACCESS
         String doc_no = request.getParameter("doc_no");
            log.debug("Document No :"+doc_no);
-                   
+
         LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.READ, LogConst.CON_DOCUMENT, doc_no, request.getRemoteAddr());
-  
+
         String docdownload = oscar.OscarProperties.getInstance().getProperty("DOCUMENT_DIR");
         File documentDir = new File(docdownload);
         log.debug("Document Dir is a dir"+documentDir.isDirectory());
-        
+
         Document d = documentDAO.getDocument(doc_no);
         log.debug("Document Name :"+d.getDocfilename());
-        
+
         //TODO: Right now this assumes it's a pdf which it shouldn't
-        
+
         response.setContentType("image/png");
         //response.setHeader("Content-Disposition", "attachment;filename=\"" + filename+ "\"");
         //read the file name.
@@ -362,7 +309,7 @@ public class ManageDocumentAction extends DispatchAction {
 
         log.debug("WIDTH " + (int) ppage.getBBox().getWidth() + " height " + (int) ppage.getBBox().getHeight());
 
-        //get the width and height for the doc at the default zoom 
+        //get the width and height for the doc at the default zoom
         Rectangle rect = new Rectangle(0, 0,
                 (int) ppage.getBBox().getWidth(),
                 (int) ppage.getBBox().getHeight());
@@ -375,39 +322,39 @@ public class ManageDocumentAction extends DispatchAction {
                 true, // fill background with white
                 true // block until drawing is done
                 );
-       
+
         log.debug("about to Print to stream");
         ServletOutputStream outs = response.getOutputStream();
-  
+
         RenderedImage rendImage = (RenderedImage) img;
         ImageIO.write(rendImage, "png", outs);
         outs.flush();
-        outs.close();     
+        outs.close();
         return null;
     }
 
 
-    
+
         public ActionForward display(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) throws Exception{
-        System.out.println("in managedocumentaction--display");
+
         String doc_no = request.getParameter("doc_no");
            log.debug("Document No :"+doc_no);
-           
+
         CtlDocument ctld = documentDAO.getCtrlDocument(Integer.parseInt(doc_no));
         if(ctld.isDemographicDocument()){
             LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.READ, LogConst.CON_DOCUMENT, doc_no, request.getRemoteAddr(),""+ctld.getModuleId());
-        }else{           
+        }else{
             LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.READ, LogConst.CON_DOCUMENT, doc_no, request.getRemoteAddr());
         }
-        
+
         String docdownload = oscar.OscarProperties.getInstance().getProperty("DOCUMENT_DIR");
         File documentDir = new File(docdownload);
         log.debug("Document Dir is a dir"+documentDir.isDirectory());
-        
-        Document d = documentDAO.getDocument(doc_no); 
+
+        Document d = documentDAO.getDocument(doc_no);
         log.debug("Document Name :"+d.getDocfilename());
         String docxml = d.getDocxml();
-        
+
         String contentType = d.getContenttype();
         if (docxml != null && !docxml.trim().equals("")){
             ServletOutputStream outs = response.getOutputStream();
@@ -416,7 +363,7 @@ public class ManageDocumentAction extends DispatchAction {
             outs.close();
             return null;
         }
-        
+
         //TODO: Right now this assumes it's a pdf which it shouldn't
         if (contentType == null){
             contentType = "application/pdf";
@@ -432,8 +379,8 @@ public class ManageDocumentAction extends DispatchAction {
         response.setHeader("Content-Disposition", "inline; filename=" + d.getDocfilename());
         log.debug("about to Print to stream");
         ServletOutputStream outs = response.getOutputStream();
- 
-      
+
+
         BufferedInputStream bfis = new BufferedInputStream(new FileInputStream(file));
 
         byte[] buffer = new byte[1024];
@@ -443,9 +390,9 @@ public class ManageDocumentAction extends DispatchAction {
             outs.write(buffer, 0, bytesRead);
             //outs.flush();
         }
-	
+
         bfis.close();
-        
+
         outs.flush();
         outs.close();
         return null;
