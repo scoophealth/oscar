1660fe51d0050344be8adc5fdcef01eee9931397
annie_zhou91 <annie_zhou91>
2010-03-24 15:43:57 +0000

    *** empty log message ***

---
+++ database/mysql/caisi/updates/patch-2010-03-24.sql
@@ -0,0 +1,9 @@
+update OcanFormOption set ocanDataCategoryName="0 - No problem" where id=830;
+update OcanFormOption set ocanDataCategoryName="1 - No/Moderate problem due to help" where id=831;
+update OcanFormOption set ocanDataCategoryName="2 - Serious problem" where id=832;
+update OcanFormOption set ocanDataCategoryName="9 - Not known" where id=833;
+update OcanFormOption set ocanDataCategoryName="0 - None" where id=835;
+update OcanFormOption set ocanDataCategoryName="1 - Low help" where id=836;
+update OcanFormOption set ocanDataCategoryName="2 - Moderate help" where id=837;
+update OcanFormOption set ocanDataCategoryName="3 - High help" where id=838;
+update OcanFormOption set ocanDataCategoryName="9 - Unknown" where id=839;
