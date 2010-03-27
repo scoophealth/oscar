4e63c4e96049d70355d5946cda40d44ad6aab60f
cronnie <cronnie>
2010-03-27 14:19:43 +0000

    for measurementType table

---
+++ web/WEB-INF/classes/src/oscar/oscarEncounter/oscarMeasurements/dao/MeasurementTypeDao.java
@@ -0,0 +1,30 @@
+package oscar.oscarEncounter.oscarMeasurements.dao;
+
+import java.util.List;
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import oscar.oscarEncounter.oscarMeasurements.model.Measurementtype;
+
+public class MeasurementTypeDao extends HibernateDaoSupport {
+
+	public void addMeasurementType(Measurementtype measurementType) {
+		getHibernateTemplate().merge(measurementType);
+	}
+
+        public List<Measurementtype> getAllTypes() {
+            String queryStr = "FROM Measurementtype m";
+
+            @SuppressWarnings("unchecked")
+            List<Measurementtype> rs = getHibernateTemplate().find(queryStr);
+
+            return rs;
+        }
+
+        public List<Measurementtype> getByType(String type) {
+            String queryStr = "FROM Measurementtype m WHERE m.type = '"+type+"' ORDER BY m.id";
+
+            @SuppressWarnings("unchecked")
+            List<Measurementtype> rs = getHibernateTemplate().find(queryStr);
+
+            return rs;
+        }
+}
