b821b393ae6e127426e95ad8d3996305cb7500bf
cronnie <cronnie>
2010-03-27 14:33:07 +0000

    for the IntegratorControl table

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/model/IntegratorControl.java
@@ -0,0 +1,63 @@
+/*
+ * 
+ * Copyright (c) 2001-2002. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License. 
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License 
+ * as published by the Free Software Foundation; either version 2 
+ * of the License, or (at your option) any later version. * 
+ * This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
+ * along with this program; if not, write to the Free Software 
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
+ * 
+ * <OSCAR TEAM>
+ * 
+ * This software was written for 
+ * Centre for Research on Inner City Health, St. Michael's Hospital, 
+ * Toronto, Ontario, Canada 
+ */
+package org.oscarehr.common.model;
+
+import java.io.Serializable;
+
+/**
+ * This is the object class that relates to the program table. Any customizations belong here.
+ */
+public class IntegratorControl implements Serializable {
+
+    private Integer id;
+    private Integer facilityId;
+    private String control;
+    private Boolean execute;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getFacilityId() {
+        return facilityId;
+    }
+    public void setFacilityId(Integer facilityId) {
+        this.facilityId = facilityId;
+    }
+
+    public String getControl() {
+        return control;
+    }
+    public void setControl(String control) {
+        this.control = control;
+    }
+
+    public Boolean getExecute() {
+        return execute;
+    }
+    public void setExecute(Boolean execute) {
+        this.execute = execute;
+    }
+}
