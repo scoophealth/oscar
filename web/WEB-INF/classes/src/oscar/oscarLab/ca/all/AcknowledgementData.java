65f21b07528d9c2b677c22653322c646bff39d12
ygmgbg <ygmgbg>
2010-06-04 19:22:29 +0000

    -ID: 3009629 -get acknowledgements

--- web/WEB-INF/classes/src/oscar/oscarLab/ca/all/AcknowledgementData.java
+++ web/WEB-INF/classes/src/oscar/oscarLab/ca/all/AcknowledgementData.java
@@ -6,7 +6,6 @@
  * To change this template, choose Tools | Template Manager
  * and open the template in the editor.
  */
-
 package oscar.oscarLab.ca.all;
 
 import java.sql.ResultSet;
@@ -22,29 +21,47 @@ import oscar.oscarMDS.data.ReportStatus;
  * @author wrighd
  */
 public class AcknowledgementData {
-    
+
     Logger logger = Logger.getLogger(AcknowledgementData.class);
-    
+
     /** Creates a new instance of AcknowledgementData */
     public AcknowledgementData() {
     }
-    
-    public ArrayList getAcknowledgements(String segmentID){
+
+    public ArrayList getAcknowledgements(String segmentID) {
         ArrayList acknowledgements = null;
-        try{
+        try {
             DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
-            
+
             acknowledgements = new ArrayList();
-            String sql = "select provider.first_name, provider.last_name, provider.provider_no, providerLabRouting.status, providerLabRouting.comment, providerLabRouting.timestamp from provider, providerLabRouting where provider.provider_no = providerLabRouting.provider_no and providerLabRouting.lab_no='"+segmentID+"' and providerLabRouting.lab_type='HL7'";
+            String sql = "select provider.first_name, provider.last_name, provider.provider_no, providerLabRouting.status, providerLabRouting.comment, providerLabRouting.timestamp from provider, providerLabRouting where provider.provider_no = providerLabRouting.provider_no and providerLabRouting.lab_no='" + segmentID + "' and providerLabRouting.lab_type='HL7'";
             ResultSet rs = db.GetSQL(sql);
-            while(rs.next()){
-                acknowledgements.add( new ReportStatus(db.getString(rs,"first_name")+" "+db.getString(rs,"last_name"), db.getString(rs,"provider_no"), db.getString(rs,"status"), db.getString(rs,"comment"), db.getString(rs,"timestamp"), segmentID ) );
+            while (rs.next()) {
+                acknowledgements.add(new ReportStatus(db.getString(rs, "first_name") + " " + db.getString(rs, "last_name"), db.getString(rs, "provider_no"), db.getString(rs, "status"), db.getString(rs, "comment"), db.getString(rs, "timestamp"), segmentID));
             }
             rs.close();
-        }catch(Exception e){
+        } catch (Exception e) {
             logger.error("Could not retrieve acknowledgement data", e);
         }
         return acknowledgements;
     }
-    
+
+    public ArrayList getAcknowledgements(String docType, String segmentID) {
+        ArrayList acknowledgements = null;
+        try {
+            DBHandler db = new DBHandler(DBHandler.OSCAR_DATA);
+
+            acknowledgements = new ArrayList();
+            String sql = "select provider.first_name, provider.last_name, provider.provider_no, providerLabRouting.status, providerLabRouting.comment, providerLabRouting.timestamp from provider, providerLabRouting where provider.provider_no = providerLabRouting.provider_no and providerLabRouting.lab_no='" + segmentID + "' and providerLabRouting.lab_type='" + docType + "'";
+            ResultSet rs = db.GetSQL(sql);
+            while (rs.next()) {
+                acknowledgements.add(new ReportStatus(db.getString(rs, "first_name") + " " + db.getString(rs, "last_name"), db.getString(rs, "provider_no"), db.getString(rs, "status"), db.getString(rs, "comment"), db.getString(rs, "timestamp"), segmentID));
+            }
+            rs.close();
+        } catch (Exception e) {
+            logger.error("Could not retrieve acknowledgement data", e);
+        }
+        //System.out.println("acknowledgements="+acknowledgements);
+        return acknowledgements;
+    }
 }
