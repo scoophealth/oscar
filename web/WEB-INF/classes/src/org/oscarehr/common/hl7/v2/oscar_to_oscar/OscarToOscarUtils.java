77b205bad2be1210872812d99238c3186b832163
tedleung <tedleung>
2010-06-09 18:03:23 +0000

    fix the new line's according to hl7 spec

--- web/WEB-INF/classes/src/org/oscarehr/common/hl7/v2/oscar_to_oscar/OscarToOscarUtils.java
+++ web/WEB-INF/classes/src/org/oscarehr/common/hl7/v2/oscar_to_oscar/OscarToOscarUtils.java
@@ -4,6 +4,9 @@ import java.io.UnsupportedEncodingException;
 
 import org.apache.commons.codec.binary.Base64;
 
+import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.AbstractMessage;
+import ca.uhn.hl7v2.parser.EncodingNotSupportedException;
 import ca.uhn.hl7v2.parser.PipeParser;
 import ca.uhn.hl7v2.validation.impl.NoValidation;
 
@@ -27,5 +30,16 @@ public final class OscarToOscarUtils {
 	
 	public static String encodeBase64String(byte[] b) throws UnsupportedEncodingException {
 		return (new String(OscarToOscarUtils.base64.encode(b), OscarToOscarUtils.ENCODING));
+	}
+	
+	public static AbstractMessage pipeParserParse(String hl7Message) throws EncodingNotSupportedException, HL7Exception
+	{
+		// convert \n to \r as per hl7 spec section 2.8
+		hl7Message=hl7Message.replaceAll("\n", "\r");
+		// the above will have converted \r\n to \r\r so fix that too
+		hl7Message=hl7Message.replaceAll("\r\r", "\r");
+		
+		AbstractMessage message=(AbstractMessage) OscarToOscarUtils.pipeParser.parse(hl7Message);
+		return(message);
 	}
 }
