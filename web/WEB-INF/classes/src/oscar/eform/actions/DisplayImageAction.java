875bbd96d42887f46aeb877232296bbc6cb1b7b5
raphenya <raphenya>
2010-06-24 20:52:40 +0000

    ID: 3019253     -added a method for listing images for eform selection     -fix the file types Request(s) :             -add drop down list for the images in the eform generator(replace the text box with drop down list)             -add a link to UPLOAD E-FORM for eform generator                -load html code into Edit-E-FORM when the html is generated from the eform generator                -fix the submit button to save the eform to the E-Chart                -fix the file types in 'upload an image' for eform(needed to edit the mimetypes.default file which didn't include .png file type)

--- web/WEB-INF/classes/src/oscar/eform/actions/DisplayImageAction.java
+++ web/WEB-INF/classes/src/oscar/eform/actions/DisplayImageAction.java
@@ -30,6 +30,7 @@
 package oscar.eform.actions;
 
 import java.io.File;
+import java.io.IOException;
 
 import java.io.InputStream;
 import javax.activation.MimetypesFileTypeMap;
@@ -48,21 +49,21 @@ import oscar.OscarProperties;
  *and Paul
  */
 public class DisplayImageAction extends DownloadAction{
-    
+
     /** Creates a new instance of DisplayImageAction */
     public DisplayImageAction() {
-    }        
-    protected StreamInfo getStreamInfo(ActionMapping mapping, 
+    }
+    protected StreamInfo getStreamInfo(ActionMapping mapping,
                                        ActionForm form,
-                                       HttpServletRequest request, 
+                                       HttpServletRequest request,
                                        HttpServletResponse response)
             throws Exception {
         String fileName = request.getParameter("imagefile");
         //if (fileName.indexOf('/') != -1) return null;  //prevents navigating away from the page.
         String home_dir = OscarProperties.getInstance().getProperty("eform_image");
-        
+
         response.setHeader("Content-disposition","inline; filename=" + fileName);
-        
+
         File file = null;
         try{
            File directory = new File(home_dir);
@@ -74,7 +75,7 @@ public class DisplayImageAction extends DownloadAction{
            //System.out.println("absolutepath: " + canonicalPath);
            if (!directory.equals(file.getParentFile())) {
                System.out.println("SECURITY WARNING: Illegal file path detected, client attempted to navigate away from the file directory");
-               throw new Exception("Could not open file " + fileName + ".  Check the file path"); 
+               throw new Exception("Could not open file " + fileName + ".  Check the file path");
            }
         }catch(Exception e){
             e.printStackTrace();
@@ -82,8 +83,17 @@ public class DisplayImageAction extends DownloadAction{
         }
         //gets content type from image extension
         String contentType = new MimetypesFileTypeMap().getContentType(file);
-        //System.out.println("content type:" + contentType);
-        return new FileStreamInfo(contentType, file);   
+        /**
+         * for png files (this if-statement will force every file extension with "application/octet-stream" to png)
+         * This was a temporary fix.You need to look at mimetypes file to
+         *
+         */
+        //check if the file type you are using is included
+        if(contentType.equalsIgnoreCase("application/octet-stream")){
+            contentType = "image/png";
+            System.out.println("----hard-coded-file type:<error-check the mimetype file to see if the filetype of file: "+file.getName()+" is in there> ...");
+        }
+        return new FileStreamInfo(contentType, file);
     }
 
     public static File getImageFile(String imageFileName) throws Exception {
@@ -108,4 +118,20 @@ public class DisplayImageAction extends DownloadAction{
             throw new Exception("Could not open file "+home_dir+imageFileName +" does "+home_dir+ " exist ?",e);
         }
     }
+    /**
+     *
+     * Process only files under dir
+     * This method used to list images for eform generator
+     *
+     */
+ public String[] visitAllFiles(File dir) {
+    String[] children=null;
+    if (dir.isDirectory()) {
+         children = dir.list();
+        for (int i=0; i<children.length; i++) {
+            visitAllFiles(new File(dir, children[i]));
+        }
+    }
+     return children;
+}
 }
