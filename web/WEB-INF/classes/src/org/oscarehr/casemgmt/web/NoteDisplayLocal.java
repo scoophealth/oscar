8b1f468411a47b486a7170fbc165ec1755485e5e
cronnie <cronnie>
2010-07-07 18:56:16 +0000

    ID: 3017121 Added methods isEformData()

--- web/WEB-INF/classes/src/org/oscarehr/casemgmt/web/NoteDisplayLocal.java
+++ web/WEB-INF/classes/src/org/oscarehr/casemgmt/web/NoteDisplayLocal.java
@@ -2,12 +2,14 @@ package org.oscarehr.casemgmt.web;
 
 import java.util.ArrayList;
 import java.util.Date;
+import org.oscarehr.casemgmt.dao.CaseManagementNoteLinkDAO;
 
 import org.oscarehr.casemgmt.model.CaseManagementIssue;
 import org.oscarehr.casemgmt.model.CaseManagementNote;
 import org.oscarehr.casemgmt.model.CaseManagementNoteLink;
 import org.oscarehr.common.model.Provider;
 import org.oscarehr.util.LoggedInInfo;
+import org.oscarehr.util.SpringUtils;
 import oscar.oscarRx.data.RxPrescriptionData;
 
 public class NoteDisplayLocal implements NoteDisplay {
@@ -16,134 +18,141 @@ public class NoteDisplayLocal implements NoteDisplay {
 	private boolean readonly;
 	private boolean groupNote;
 	private String location;
-	
+
 	public NoteDisplayLocal(CaseManagementNote caseManagementNote)
 	{
 		this.caseManagementNote=caseManagementNote;
-		
+
 		LoggedInInfo loggedInInfo=LoggedInInfo.loggedInInfo.get();
 		if (loggedInInfo!=null) editable=!caseManagementNote.isSigned() || (loggedInInfo.loggedInProvider.getProviderNo().equals(caseManagementNote.getProviderNo()) && !caseManagementNote.isLocked());
 	}
-	
+
 	public void setReadOnly(boolean ro) {
 		readonly = ro;
 	}
 
 	public String getEncounterType() {
-	    return(caseManagementNote.getEncounter_type());
-    }
+		return(caseManagementNote.getEncounter_type());
+	}
 
 	public boolean getHasHistory() {
-	    return(caseManagementNote.getHasHistory());
-    }
+		return(caseManagementNote.getHasHistory());
+	}
 
 	public String getLocation() {
-		if(location != null)
-			return location;
-	    return("local");
-    }
+		if(location != null) return location;
+		return("local");
+	}
 
 	public String getNote() {
-	    return(caseManagementNote.getNote());
-    }
+		return(caseManagementNote.getNote());
+	}
 
 	public Integer getNoteId() {
-	    return(caseManagementNote.getId().intValue());
-    }
+		return(caseManagementNote.getId().intValue());
+	}
 
 	public Date getObservationDate() {
-	    return(caseManagementNote.getObservation_date());
-    }
+		return(caseManagementNote.getObservation_date());
+	}
 
 	public String getProgramName() {
-	    return(caseManagementNote.getProgramName());
-    }
+		return(caseManagementNote.getProgramName());
+	}
 
 	public String getProviderName() {
-	    return(caseManagementNote.getProviderName());
-    }
+		return(caseManagementNote.getProviderName());
+	}
 
 	public String getProviderNo() {
-	    return(caseManagementNote.getProviderNo());
-    }
+		return(caseManagementNote.getProviderNo());
+	}
 
 	public Integer getRemoteFacilityId() {
-	    return(null);
-    }
+		return(null);
+	}
 
 	public String getRevision() {
-	    return(caseManagementNote.getRevision());
-    }
+		return(caseManagementNote.getRevision());
+	}
 
 	public String getRoleName() {
-	    return(caseManagementNote.getRoleName());
-    }
+		return(caseManagementNote.getRoleName());
+	}
 
 	public String getStatus() {
-	    return(caseManagementNote.getStatus());
-    }
+		return(caseManagementNote.getStatus());
+	}
 
 	public Date getUpdateDate() {
-	    return(caseManagementNote.getUpdate_date());
-    }
+		return(caseManagementNote.getUpdate_date());
+	}
 
 	public String getUuid() {
-	    return(caseManagementNote.getUuid());
-    }
+		return(caseManagementNote.getUuid());
+	}
 
 	public boolean isDocument() {
-	    return(caseManagementNote.isDocumentNote());
-    }
+		return(caseManagementNote.isDocumentNote());
+	}
 
-        public boolean isRxAnnotation(){
-            System.out.println("in isRxAnnotation in notedisplaylocal.java, note id="+caseManagementNote.getId());
+	public boolean isEformData() {
+		return (caseManagementNote.isEformData());
+	}
 
-            return (caseManagementNote.isRxAnnotation());
-       }
+	public CaseManagementNoteLink getNoteLink() {
+		CaseManagementNoteLinkDAO cmDao = (CaseManagementNoteLinkDAO)SpringUtils.getBean("CaseManagementNoteLinkDAO");
+		return cmDao.getLastLinkByNote(caseManagementNote.getId());
+	}
 
-       public RxPrescriptionData.Prescription getRxFromAnnotation(CaseManagementNoteLink cmnl){
-            return (caseManagementNote.getRxFromAnnotation(cmnl));
-       }
+	public boolean isRxAnnotation(){
+		System.out.println("in isRxAnnotation in notedisplaylocal.java, note id="+caseManagementNote.getId());
+		return (caseManagementNote.isRxAnnotation());
+	}
+
+	public RxPrescriptionData.Prescription getRxFromAnnotation(CaseManagementNoteLink cmnl){
+	return (caseManagementNote.getRxFromAnnotation(cmnl));
+	}
 
 	public boolean isEditable() {
-		return(editable);
-    }
+	return(editable);
+	}
 
 	public boolean isLocked() {
-	    return(caseManagementNote.isLocked());
-    }
+	return(caseManagementNote.isLocked());
+	}
 
 	public boolean isSigned() {
-	    return(caseManagementNote.isSigned());
-    }
+	return(caseManagementNote.isSigned());
+	}
 
 	public ArrayList<String> getEditorNames() {
-		ArrayList<String> editorNames=new ArrayList<String>();
-		
-		for (Provider provider : caseManagementNote.getEditors()) editorNames.add(provider.getFormattedName());
-		
-		return(editorNames);
+	ArrayList<String> editorNames=new ArrayList<String>();
+
+	for (Provider provider : caseManagementNote.getEditors()) editorNames.add(provider.getFormattedName());
+
+	return(editorNames);
 	}
 
 	public ArrayList<String> getIssueDescriptions() {
-		ArrayList<String> issueDescriptions=new ArrayList<String>();
-		
-		for (CaseManagementIssue issue : caseManagementNote.getIssues()) issueDescriptions.add(issue.getIssue().getDescription());
-		
-		return(issueDescriptions);
-    }
-	
+	ArrayList<String> issueDescriptions=new ArrayList<String>();
+
+	for (CaseManagementIssue issue : caseManagementNote.getIssues()) issueDescriptions.add(issue.getIssue().getDescription());
+
+	return(issueDescriptions);
+	}
+
 	public boolean isReadOnly() {return readonly;}
 
 	public boolean isGroupNote() {
-		return groupNote;
+	return groupNote;
 	}
 
 	public void setGroupNote(boolean groupNote) {
-		this.groupNote = groupNote;
+	this.groupNote = groupNote;
 	}
-	
+
 	public void setLocation(String location) {
-		this.location= location;
+	this.location= location;
 	}
 }
