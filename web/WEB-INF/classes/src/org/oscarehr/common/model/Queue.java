822044fe20c76d4bee123c4a491f77e8c893453c
ygmgbg <ygmgbg>
2010-07-23 20:53:26 +0000

    -ID: 3009629 -database table map

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/model/Queue.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.oscarehr.common.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.apache.commons.lang.StringUtils;
+
+/**
+ *
+ * @author jackson
+ */
+@Entity
+@Table(name = "queue")
+
+public class Queue extends AbstractModel<Integer> implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = StringUtils.trimToNull(name);
+    }
+
+}
