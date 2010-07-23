dad891b6476d740792178f4aad1ebd6bd3c5f083
ygmgbg <ygmgbg>
2010-07-23 20:53:15 +0000

    -ID: 3009629 -dao for queue

---
+++ web/WEB-INF/classes/src/org/oscarehr/common/dao/QueueDao.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.oscarehr.common.dao;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import javax.persistence.Query;
+import org.springframework.stereotype.Repository;
+import org.oscarehr.common.model.Queue;
+import org.oscarehr.util.MiscUtils;
+
+/**
+ *
+ * @author jackson
+ */
+@Repository
+public class QueueDao extends AbstractDao<Queue>{
+    public QueueDao(){
+        super(Queue.class);
+    }
+
+    public List<Hashtable> getQueues(){
+        String q="select q from Queue q";
+        Query query=entityManager.createQuery(q);
+        List<Queue> result=new ArrayList();
+        result=query.getResultList();
+        List<Hashtable> r=new ArrayList();
+        for(Queue que:result){
+            Hashtable ht=new Hashtable();
+            ht.put("id", que.getId());
+            ht.put("queue", que.getName());
+            r.add(ht);
+        }
+        return r;
+    }
+
+    public String getLastId(){
+        String r="";
+        Query query=entityManager.createQuery("select MAX(q.id) from Queue q");
+        Integer ri=(Integer)query.getSingleResult();
+        if(ri!=null){
+            r=ri.toString();
+        }        
+        return r;
+    }
+    public String getQueueName(int id){
+
+        String q="select q from Queue q where q.id="+id;
+        Query query=entityManager.createQuery(q);
+        Queue result=(Queue)query.getSingleResult();
+        System.out.println(result);
+        if(result!=null){
+            return result.getName();
+        }else{
+            return "";
+        }
+            
+    }
+    public String getQueueid(String name){
+        String q="select q from Queue q where q.name="+name;
+        Query query=entityManager.createQuery(q);
+        Queue result=(Queue)query.getSingleResult();
+        if(result!=null){
+            return result.getId().toString();
+        }else{
+            return "";
+        }
+    }
+    public boolean addNewQueue(String qn){
+       try{
+            Queue q=new Queue();
+            q.setName(qn);
+            entityManager.persist(q);
+        }catch(Exception e){
+            MiscUtils.getLogger().error(e);
+            return false;
+        }
+       return true;
+    }
+}
