555e76295cce899c1b6ec4e71a69db6e898fac9f
tedleung <tedleung>
2010-07-27 10:36:26 +0000

    allow thread priorty in thread factory

--- web/WEB-INF/classes/src/org/oscarehr/util/DeamonThreadFactory.java
+++ web/WEB-INF/classes/src/org/oscarehr/util/DeamonThreadFactory.java
@@ -5,15 +5,18 @@ import java.util.concurrent.ThreadFactory;
 public final class DeamonThreadFactory implements ThreadFactory{
 
 	private String threadName;
+	private int threadPriority;
 	
-	public DeamonThreadFactory(String threadName)
+	public DeamonThreadFactory(String threadName, int threadPriority)
 	{
 		this.threadName=threadName;	
+		this.threadPriority=threadPriority;
 	}
 	
 	public Thread newThread(Runnable r) {
 		Thread thread=new Thread(r, threadName);
 		thread.setDaemon(true);
+		thread.setPriority(threadPriority);
 		return(thread);
     }
 
