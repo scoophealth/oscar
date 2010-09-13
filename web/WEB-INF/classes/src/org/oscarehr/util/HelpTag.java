3c7a823f17911ea54b53987db74bac00beac8969
jaygallagher <jaygallagher>
2010-09-13 20:54:56 +0000

    ID: 3065535 Now looks up the Help url from the OSCAR Property HELP_SEARCH_URL

--- web/WEB-INF/classes/src/org/oscarehr/util/HelpTag.java
+++ web/WEB-INF/classes/src/org/oscarehr/util/HelpTag.java
@@ -1,13 +1,35 @@
-//Error reading included file Templates/Classes/../Licenses/license-MyLicense.txt
+/*
+ *
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+ *
+ * <OSCAR TEAM>
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
 package org.oscarehr.util;
 
-import java.util.Locale;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.TagSupport;
 import org.apache.struts.Globals;
 import org.apache.struts.taglib.TagUtils;
 import org.apache.struts.util.MessageResources;
+import oscar.OscarProperties;
 
 /**
  * Creates a anchor tag that pops up a help window to
@@ -34,7 +56,7 @@ public class HelpTag extends TagSupport {
 
 
     private String keywords = null;
-    private String searchLink = "http://www.oscarmanual.org/system/app/pages/search?q=%s&scope=search-site";
+    private String searchLink = OscarProperties.getInstance().getProperty("HELP_SEARCH_URL");  //"http://www.oscarmanual.org/system/app/pages/search?q=%s&scope=search-site";
     private String classString= null;
     private String key= null;
     private String style=null;
