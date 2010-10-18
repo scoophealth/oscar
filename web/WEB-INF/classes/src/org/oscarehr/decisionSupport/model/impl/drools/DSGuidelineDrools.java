6679e3ccceb055147483c57b656134aac0618ab2
rjonasz <rjonasz>
2010-10-18 14:54:49 +0000

    -ID 3086177 return java condition classes

--- web/WEB-INF/classes/src/org/oscarehr/decisionSupport/model/impl/drools/DSGuidelineDrools.java
+++ web/WEB-INF/classes/src/org/oscarehr/decisionSupport/model/impl/drools/DSGuidelineDrools.java
@@ -5,6 +5,7 @@
 
 package org.oscarehr.decisionSupport.model.impl.drools;
 
+import java.lang.reflect.*;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
@@ -17,6 +18,7 @@ import org.jdom.Element;
 import org.jdom.Namespace;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
+import org.oscarehr.decisionSupport.model.DSParameter;
 import org.oscarehr.decisionSupport.model.DSCondition;
 import org.oscarehr.decisionSupport.model.DSConsequence;
 import org.oscarehr.decisionSupport.model.DSDemographicAccess;
@@ -64,14 +66,29 @@ public class DSGuidelineDrools extends DSGuideline {
                 }
             }
 
+            List<DSParameter> lDSP = this.getParameters();
+            for( DSParameter dsp: lDSP ) {
+                Class clas = Class.forName(dsp.getStrClass());
+                Constructor constructor = clas.getConstructor();
+                Object obj = constructor.newInstance();
+
+                workingMemory.assertObject(obj);
+            }
+
             workingMemory.fireAllRules();
             if (dsDemographicAccess.isPassedGuideline()) {
                 List<DSConsequence> returnDsConsequences = new ArrayList();
                 if (this.getConsequences() == null) return returnDsConsequences;
                 else {
                     for (DSConsequence dsConsequence: this.getConsequences()) {
-                        if (dsConsequence.getConsequenceType() != DSConsequence.ConsequenceType.java)
+                        if (dsConsequence.getConsequenceType() != DSConsequence.ConsequenceType.java) {
                             returnDsConsequences.add(dsConsequence);
+                    }
+                        else if( dsConsequence.getConsequenceType() == DSConsequence.ConsequenceType.java ) {
+                            List<Object> javaConsequences = workingMemory.getObjects();
+                            dsConsequence.setObjConsequence(javaConsequences);
+                            returnDsConsequences.add(dsConsequence);
+                        }
                     }
                     return returnDsConsequences;
                 }
@@ -80,13 +97,30 @@ public class DSGuidelineDrools extends DSGuideline {
             }
         } catch (FactException factException) {
             throw new DecisionSupportException("Unable to assert guideline", factException);
+        } catch( ClassNotFoundException e ) {
+            throw new DecisionSupportException("Unable to instantiate class", e);
+        } catch( NoSuchMethodException e ) {
+            throw new DecisionSupportException("Unable to instantiate class", e);
+        } catch( InstantiationException e ) {
+            throw new DecisionSupportException("Unable to instantiate class", e);
+        } catch( IllegalAccessException e ) {
+            throw new DecisionSupportException("Unable to instantiate class", e);
+        } catch( InvocationTargetException e ) {
+            throw new DecisionSupportException("Unable to instantiate class", e);
         }
     }
 
     public void generateRuleBase() throws DecisionSupportException {
         ArrayList<Element> rules = new ArrayList();
         ArrayList<Element> conditionElements = new ArrayList();
+        ArrayList<Element> lParameterElements = new ArrayList();
         
+
+        for( DSParameter dsParameter: this.getParameters()) {
+            Element parameterElement = this.getDroolsParameter(dsParameter);
+            lParameterElements.add(parameterElement);
+        }
+
         int paramCount=0;
        
         for (DSCondition condition: this.getConditions()) {
@@ -100,14 +134,45 @@ public class DSGuidelineDrools extends DSGuideline {
 
         Element consequencesElement = this.getDroolsConsequences(this.getConsequences());
 
-        rules.add(this.getRule(conditionElements, consequencesElement));
+        rules.add(this.getRule(conditionElements, lParameterElements, consequencesElement));
         
         RuleBaseCreator ruleBaseCreator = new RuleBaseCreator();
         try {
+            XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+            xmlOutputter.output(rules, System.out);
             _ruleBase = ruleBaseCreator.getRuleBase(this.getTitle(), rules);
         } catch (Exception e) {
             throw new DecisionSupportException("Could not create a rule base for guideline '" + this.getTitle() + "'", e);
         }
+    }
+
+    private Element getRule(List<Element> conditionElements, List<Element> parameterElements, Element consequenceElement) {
+        Element ruleElement = new Element("rule", namespace);
+        ruleElement.setAttribute("name", this.getTitle());
+
+        Element accessClassParameter = new Element("parameter", namespace);
+        accessClassParameter.setAttribute("identifier", "a");
+        Element accessClass = new Element("class", namespace);
+        accessClass.addContent(demographicAccessObjectClassPath);
+        accessClassParameter.addContent(accessClass);
+        ruleElement.addContent(accessClassParameter);
+
+        ruleElement.addContent(parameterElements);
+
+        for (DSCondition condition: this.getConditions()) {
+            if (condition.getParam() != null && !condition.getParam().isEmpty()){
+                Element paramsHashEle = new Element("parameter", namespace);
+                paramsHashEle.setAttribute("identifier", condition.getLabel());
+                Element paramClass = new Element("class", namespace);
+                paramClass.addContent("java.util.Hashtable");
+                paramsHashEle.addContent(paramClass);
+                ruleElement.addContent(paramsHashEle);
+            }
+        }
+
+        ruleElement.addContent(conditionElements);
+        ruleElement.addContent(consequenceElement);
+        return ruleElement;
     }
 
     private Element getRule(List<Element> conditionElements, Element consequenceElement) {
@@ -159,6 +224,16 @@ public class DSGuidelineDrools extends DSGuideline {
 
         javaCondition.addContent(functionStr);
         return javaCondition;
+    }
+
+    public Element getDroolsParameter(DSParameter dsParameter) throws DecisionSupportException {
+        Element accessClassParameter = new Element("parameter", namespace);
+        accessClassParameter.setAttribute("identifier", dsParameter.getStrAlias());
+        Element accessClass = new Element("class", namespace);
+        accessClass.addContent(dsParameter.getStrClass());
+        accessClassParameter.addContent(accessClass);
+
+        return accessClassParameter;
     }
 
     public Element getDroolsConsequences(List<DSConsequence> consequences) throws DecisionSupportException {
