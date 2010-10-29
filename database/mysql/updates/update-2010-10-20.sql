703fa397da3c281ceb1592d3ac0e4a85a3352ae4
tedleung <tedleung>
2010-10-29 23:32:10 +0000

    fixed clobbered commit

--- database/mysql/updates/update-2010-10-20.sql
+++ database/mysql/updates/update-2010-10-20.sql
@@ -1,3 +1,2 @@
-alter table ProviderPreference add column appointmentScreenFormNameDisplayLength int not null;
-
-update ProviderPreference set appointmentScreenFormNameDisplayLength=3;
+create table ProviderPreferenceAppointmentScreenForm(providerNo varchar(6) not null, appointmentScreenForm varchar(128) not null);
+create table ProviderPreferenceAppointmentScreenEForm(providerNo varchar(6) not null, appointmentScreenEForm int not null);
