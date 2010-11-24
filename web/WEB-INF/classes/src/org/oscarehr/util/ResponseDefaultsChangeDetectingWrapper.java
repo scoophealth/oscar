5ef0a9fa6f2aff0a666b96842447e044f106c9ac
tedleung <tedleung>
2010-11-24 03:21:03 +0000

    default entire system to utf-8

---
+++ web/WEB-INF/classes/src/org/oscarehr/util/ResponseDefaultsChangeDetectingWrapper.java
@@ -0,0 +1,42 @@
+package org.oscarehr.util;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import org.apache.log4j.Logger;
+
+public class ResponseDefaultsChangeDetectingWrapper extends HttpServletResponseWrapper
+{
+	private static final Logger logger = MiscUtils.getLogger();
+
+	public ResponseDefaultsChangeDetectingWrapper(HttpServletResponse response)
+	{
+		super(response);
+	}
+
+	private static void warnWithStackTrace(String message)
+	{
+		logger.warn(message, new Exception(message));
+	}
+	
+	@Override
+	public void setCharacterEncoding(String encoding)
+	{
+		getResponse().setCharacterEncoding(encoding);
+		warnWithStackTrace("Some one is switching the encoding on me! : " + encoding);		
+	}
+
+	@Override
+	public void setContentType(String contentType)
+	{
+		getResponse().setContentType(contentType);
+		if (contentType.contains("charset")) warnWithStackTrace("Some one is switching the encoding on me! : " + contentType);		
+	}
+	
+	@Override
+	public void setHeader(String key, String value)
+	{
+		if ("Content-Type".equals(key) && value.contains("charset")) warnWithStackTrace("Some one is switching the encoding : " + value);		
+		else if ("Cache-Control".equals(key)) warnWithStackTrace("Some one is setting the cache control. "+value);
+	}
+}
---
+++ web/WEB-INF/classes/src/org/oscarehr/util/ResponseDefaultsFilter.java
@@ -0,0 +1,62 @@
+package org.oscarehr.util;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+
+public final class ResponseDefaultsFilter implements Filter
+{
+	private static final Logger logger = MiscUtils.getLogger();
+	private static final String ENCODING="UTF-8";
+	
+	@Override
+	public void init(FilterConfig arg0) throws ServletException
+	{
+		logger.info("Initialising "+ResponseDefaultsFilter.class.getSimpleName());
+	}
+
+	@Override
+	public void destroy()
+	{
+		logger.info("shutdown "+ResponseDefaultsFilter.class.getSimpleName());
+	}
+
+	@Override
+	public void doFilter(ServletRequest originalRequest, ServletResponse originalResponse, FilterChain chain) throws IOException, ServletException
+	{
+		HttpServletRequest request=(HttpServletRequest)originalRequest;
+		HttpServletResponse response=(HttpServletResponse)originalResponse;
+		
+		setEncoding(request, response);
+		setCaching(request, response);
+		
+		chain.doFilter(request, new ResponseDefaultsChangeDetectingWrapper(response));
+	}
+
+	private void setCaching(HttpServletRequest request, HttpServletResponse response)
+	{
+		// so the caching scheme will be as follows :
+		// *.jsp should never be cached, presumably it's jsp because it's dynamic (and other jsp like pages)
+		// everything else is allowed to be cached, but we won't explicity set the expires, we'll just let the browser sort it out, I don't want to mess with date formatting right now
+		
+		String requestUri=request.getRequestURI();
+		if (requestUri.endsWith(".jsp") || requestUri.endsWith(".json") || requestUri.endsWith(".jsf")) response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+	}
+
+	private static void setEncoding(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException
+	{
+		// use the requested encoding, but if none is specified then we use UTF-8
+		if (request.getCharacterEncoding() == null)	request.setCharacterEncoding(ENCODING);
+		response.setCharacterEncoding(ENCODING);
+	}
+}
