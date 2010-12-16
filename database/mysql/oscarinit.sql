9aee45f4343aa364c0d24e7cc01cb3370ca9d8f2
jaygallagher <jaygallagher>
2010-12-16 04:52:42 +0000

    merged missing updates: update-2010-11-04.sql update-2010-11-05.sql update-2010-11-10.sql update-2010-11-10-2.sql update-2010-11-12.1.sql

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -279,7 +279,7 @@ CREATE TABLE consultationRequests (
   concurrentProblems text,
   urgency char(2) default NULL,
   patientWillBook tinyint(1),
-  site_name varchar(255) null,
+  followUpDate date default NULL,
   PRIMARY KEY  (requestId)
 ) ;
 
@@ -558,6 +558,7 @@ CREATE TABLE document (
   reviewer varchar(30) default '',
   reviewdatetime datetime default NULL,
   number_of_pages int(6) not null default 0,
+  appointment_no int(11) default NULL,
   PRIMARY KEY  (document_no)
 ) ;
 
@@ -731,7 +732,8 @@ CREATE TABLE `eform_values` (
   `demographic_no` int(10) default '0',
   `var_name` varchar(30) NOT NULL default '',
   `var_value` text,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY  (`id`),
+  KEY `eform_values_varname_varvalue` (`var_name`,`var_value`(30))
 );
 
 --
@@ -6391,7 +6393,8 @@ CREATE TABLE professionalSpecialists (
   eDataOscarKey varchar(1024),
   eDataServiceKey varchar(1024),
   eDataServiceName varchar(255),
-  lastUpdated datetime not null
+  lastUpdated datetime not null,
+  annotation text
 );
 
 --
@@ -8131,3 +8134,130 @@ CREATE TABLE appointmentArchive (
 
 create table ProviderPreferenceAppointmentScreenForm(providerNo varchar(6) not null, appointmentScreenForm varchar(128) not null);
 create table ProviderPreferenceAppointmentScreenEForm(providerNo varchar(6) not null, appointmentScreenEForm int not null);
+
+
+CREATE TABLE `eyeform` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appointment_no` int(11)  ,
+  `discharge` varchar(20),
+  `stat` varchar(20),
+  `opt` varchar(20),
+  `date` timestamp ,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `eyeform_followup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `appointment_no` int(11)  ,
+  `demographic_no` int(11) ,
+  `timespan` int(11) ,
+  `timeframe` varchar(25) ,
+  `followup_provider` varchar(100) ,
+  `date` timestamp ,
+  `type` varchar(25),
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `eyeform_macro` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `label` varchar(255) COLLATE utf8_bin NOT NULL,
+  `display_order` tinyint(4) NOT NULL,
+  `impression` text COLLATE utf8_bin,
+  `followup_no` smallint(5),
+  `followup_unit` varchar(50) COLLATE utf8_bin ,
+  `followup_doctor` varchar(6) COLLATE utf8_bin ,
+  `followup_reason` varchar(255) COLLATE utf8_bin ,
+  `tickler_staff` varchar(6) COLLATE utf8_bin ,
+  `billing_visit_type` varchar(50) COLLATE utf8_bin,
+  `billing_visit_location` varchar(50) COLLATE utf8_bin ,
+  `billing_codes` text COLLATE utf8_bin,
+  `billing_dxcode` varchar(50) COLLATE utf8_bin ,
+  `billing_total` varchar(50) COLLATE utf8_bin ,
+  `billing_comment` varchar(255) COLLATE utf8_bin ,
+  `billing_billtype` varchar(50) COLLATE utf8_bin ,
+  `billing_pay_method` varchar(50) COLLATE utf8_bin ,
+  `billing_billto` varchar(50) COLLATE utf8_bin ,
+  `billing_remitto` varchar(50) COLLATE utf8_bin ,
+  `billing_gstBilledTotal` varchar(50) COLLATE utf8_bin ,
+  `billing_payment` varchar(50) COLLATE utf8_bin ,
+  `billing_refund` varchar(50) COLLATE utf8_bin ,
+  `billing_gst` varchar(50) COLLATE utf8_bin ,
+  `test_records` text COLLATE utf8_bin,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `ocularprocedurehis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `demographic_no` int(11) NOT NULL ,
+  `provider` varchar(60) ,
+  `date` date NOT NULL ,
+  `eye` varchar(2) NOT NULL ,
+  `procedure_name` varchar(100) NOT NULL ,
+  `procedure_type` varchar(30) ,
+  `procedure_note` text,
+  `doctor` varchar(30) ,
+  `location` varchar(30) ,
+  `update_time` datetime ,
+  `status` varchar(2) ,
+  `appointment_no` int(11) ,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `procedurebook` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `eyeform_id` bigint(20) ,
+  `demographic_no` int(11) ,
+  `appointment_no` int(11) ,
+  `provider` varchar(60) ,
+  `procedure_name` varchar(30) ,
+  `eye` varchar(20) ,
+  `location` varchar(50) ,
+  `urgency` varchar(10) ,
+  `comment` text,
+  `date` datetime ,
+  `status` varchar(2) ,
+  PRIMARY KEY (`id`)
+);
+
+
+CREATE TABLE `specshis` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `demographic_no` int(11) NOT NULL ,
+  `provider` varchar(60) ,
+  `date` date ,
+  `doctor` varchar(30) ,
+  `type` varchar(30) ,
+  `od_sph` varchar(10),
+  `od_cyl` varchar(10) ,
+  `od_axis` varchar(10) ,
+  `od_add` varchar(10) ,
+  `od_prism` varchar(10) ,
+  `os_sph` varchar(10) ,
+  `os_cyl` varchar(10) ,
+  `os_axis` varchar(10) ,
+  `os_add` varchar(10) ,
+  `os_prism` varchar(10) ,
+  `update_time` datetime ,
+  `status` varchar(2) ,
+  `appointment_no` int(11) ,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `testbookrecord` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `testname` varchar(60) ,
+  `appointment_no` int(11) ,
+  `demographic_no` int(11) ,
+  `provider` varchar(60) ,
+  `eyeform_id` bigint(20) ,
+  `eye` varchar(20) ,
+  `urgency` varchar(30) ,
+  `comment` varchar(100) ,
+  `date` datetime ,
+  `status` varchar(2) ,
+  PRIMARY KEY (`id`)
+);
