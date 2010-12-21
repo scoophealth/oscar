7295d29943597dbb49886462e0b042ba6ffa665b
tedleung <tedleung>
2010-12-21 03:04:38 +0000

    first complete pass at automated deploy scripts

--- docs/git_gerrit/automated_deploy.sh
+++ docs/git_gerrit/automated_deploy.sh
@@ -3,27 +3,56 @@
 #####
 Paths
 #####
-#WORKING_ROOT=/opt/git/integrator
-WORKING_ROOT=/tmp/mog/integrator
+
+WORKING_ROOT=~/myoscar
+SERVER_NAME=myoscar_server
+
+SRC_DIR=${WORKING_ROOT}/${SERVER_NAME}
+
+JAVA_HOME=${WORKING_ROOT}/jdk1.6.0_23
+PATH=${PATH}:${JAVA_HOME}/bin
+
+MAVEN_HOME=${WORKING_ROOT}/apache-maven-2.2.1
+PATH=${PATH}:${MAVEN_HOME}/bin
+
+CATALINA_HOME=${WORKING_ROOT}/apache-tomcat-6.0.29
+PATH=${PATH}:${CATALINA_HOME}/bin
+
+CATALINA_BASE=${WORKING_ROOT}/catalina_base
 
 ######
 Script
 ######
 
-pushd ${WORKING_ROOT}
+pushd ${SRC_DIR}
 
 	# get latest code
 	git fetch origin
 	git merge origin
 	
 	# clear out local maven repo
-	mv ~/.m2/repository /tmp/`date --rfc-3339=seconds`.repo
+	# Nah we don't need to do this, we're not hudson checking the build, we're just a deployment
+	# mv ~/.m2/repository /tmp/`date --rfc-3339=seconds`.repo
 	
 	# build the code
 	mvn clean install
 	
 	# copy the built result to catalina base
+	cd ${CATALINA_BASE}
+	rm -rf ${SERVER_NAME}
+	mkdir ${SERVER_NAME}
+	cd ${SERVER_NAME}
+	unzip ${SRC_DIR}/${SERVER_NAME}-0.0-SNAPSHOT.war
 	
-	# restart tomcat
+	# stop tomcat
+	catalina.sh stop
+	
+	# reset database
+	mysql -e "drop database ${SERVER_NAME}"
+	mysql -e "create database ${SERVER_NAME}"
+	mysql -e "source ${SRC_DIR}/docs/freshIndivo.sql"
+	
+	# start tomcat
+	catalina.sh start
 	
 popd 
