3d1a4ed86c2e7eb313da63634480f8cca9425f10
jaygallagher <jaygallagher>
2010-12-22 21:58:55 +0000

    changed update table demographic to just update demographic

--- database/mysql/updates/update-2010-11-12.sql
+++ database/mysql/updates/update-2010-11-12.sql
@@ -1,5 +1,5 @@
 alter table demographic add roster_date date;
 
-update table demographic set roster_date = hc_renew_date;
+update demographic set roster_date = hc_renew_date;
 
 alter table demographicArchive add column roster_date date;
