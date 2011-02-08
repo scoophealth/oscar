80e8236a74961c3c016c66df3f9d42b97be8e92e
ygmgbg <ygmgbg>
2011-02-08 16:59:16 +0000

    -ID:3150681 -add exchange medication methods.

--- web/WEB-INF/classes/src/org/oscarehr/phr/web/PHRExchangeAction.java
+++ web/WEB-INF/classes/src/org/oscarehr/phr/web/PHRExchangeAction.java
@@ -29,12 +29,20 @@
 
 package org.oscarehr.phr.web;
 
+
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import net.sf.json.JSONObject;
 
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
@@ -42,11 +50,19 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionRedirect;
 import org.apache.struts.actions.DispatchAction;
+import org.oscarehr.common.dao.DemographicDao;
+import org.oscarehr.common.dao.DrugDao;
+import org.oscarehr.common.model.Drug;
 import org.oscarehr.phr.PHRAuthentication;
+import org.oscarehr.phr.dao.PHRDocumentDAO;
+import org.oscarehr.phr.model.PHRDocument;
+import org.oscarehr.phr.model.PHRMedication;
 import org.oscarehr.phr.service.PHRService;
 import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
 
 import oscar.OscarProperties;
+import oscar.oscarRx.util.RxUtil;
 
 /**
  *
@@ -66,7 +82,117 @@ public class PHRExchangeAction extends DispatchAction {
     public ActionForward unspecified(ActionMapping mapping, ActionForm  form,HttpServletRequest request, HttpServletResponse response) throws Exception {
         return doPhrExchange(mapping, form ,request, response);
     }
-    
+
+  
+    //save selected meds to drugs table.
+    public ActionForward saveSelectedPHRMeds(ActionMapping mapping, ActionForm  form,HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        oscar.oscarRx.pageUtil.RxSessionBean bean = (oscar.oscarRx.pageUtil.RxSessionBean) request.getSession().getAttribute("RxSessionBean");
+        if (bean == null) {
+                response.sendRedirect("error.html");
+                return null;
+        }
+        String selected=request.getParameter("selectedDrugs");
+        HashMap<Long,PHRMedication> meds=bean.getPairPHRMed();
+        Set<Long> ks=meds.keySet();
+        Iterator<Long> it= ks.iterator();
+        String [] chosen=selected.split(",");
+        List<String> chosenS=Arrays.asList(chosen);
+        PHRDocumentDAO  phrDocumentDAO=(PHRDocumentDAO)SpringUtils.getBean("phrDocumentDAO");
+        Long key;
+        String keyS;
+        String indexStr;
+        DrugDao drugDao = (DrugDao) SpringUtils.getBean("drugDao");
+        Drug saveDrug;
+        PHRDocument phrDoc;
+        while(it.hasNext()){
+            key=it.next();
+            keyS=key.toString();
+            PHRMedication m=meds.get(key);
+            indexStr=m.getPhrIndex();
+            phrDoc=phrDocumentDAO.getDocumentByIndex(indexStr);
+            if(chosenS.contains(keyS)){
+                //save it in drug table and update status to recieved and saved in other table in phr documents table
+                saveDrug=m.getDrug();
+                drugDao.addNewDrug(saveDrug);
+                phrDoc.setStatus(PHRDocument.STATUS_RECIEVED_SAVED_IN_OSCAR_TABLE);
+            }else{
+                //update status to recieved but not saved in other table.
+                phrDoc.setStatus(PHRDocument.STATUS_RECIEVED_NOT_SAVED_IN_OSCAR_TABLE);
+            }
+            phrDocumentDAO.update(phrDoc);
+        }
+        //TODO: update the status of the phr_document table to show it's been viewed but not save. Later in UI, show a category called viewed before to show them.
+        HashMap<String,Boolean> hm=new HashMap<String,Boolean>();
+        hm.put("success", Boolean.TRUE);
+        JSONObject jsonObject = JSONObject.fromObject(hm);
+        response.getOutputStream().write(jsonObject.toString().getBytes());
+        return null;
+    }
+
+    public ActionForward displayNewMedsFromPhr(ActionMapping mapping, ActionForm  form,HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        log.debug("-----------------displayNewMedsFromPhr has been called -------------");
+        String demoId=request.getParameter("demoId");
+        String demoPhrId="";
+        oscar.oscarRx.pageUtil.RxSessionBean bean = (oscar.oscarRx.pageUtil.RxSessionBean) request.getSession().getAttribute("RxSessionBean");
+        if (bean == null) {
+                response.sendRedirect("error.html");
+                return null;
+        }
+        if(demoId!=null){
+            DemographicDao demographicDao=(DemographicDao)SpringUtils.getBean("demographicDao");
+            demoPhrId=demographicDao.getDemographic(demoId).getPin();
+            if(demoPhrId!=null && demoPhrId.length()>0){
+                PHRAuthentication auth  = (PHRAuthentication) request.getSession().getAttribute(PHRAuthentication.SESSION_PHR_AUTH);
+                //PrintWriter out = response.getWriter();
+                log.error("auth object="+auth+"--"+request.getSession().getAttribute(phrService.SESSION_PHR_EXCHANGE_TIME));
+                if (auth != null && request.getSession().getAttribute(phrService.SESSION_PHR_EXCHANGE_TIME) != null){
+                //if (auth != null){
+                    String providerNo = (String) request.getSession().getAttribute("user");
+                    try{
+                        request.getSession().setAttribute(phrService.SESSION_PHR_EXCHANGE_TIME, null);
+                        long startTime = System.currentTimeMillis();
+                        List<PHRMedication> listDrugsToDisplay=phrService.retrieveSaveMedToDisplay(auth,providerNo,demoId,demoPhrId);
+                        HashMap<Long,PHRMedication> drugsToDisplay =RxUtil.createKeyValPair(listDrugsToDisplay);
+                        bean.setPairPHRMed(drugsToDisplay);
+                        //phrService.retrieveUploadDocs(auth,providerNo);
+                        request.getSession().setAttribute(phrService.SESSION_PHR_EXCHANGE_TIME, getNextExchangeTime());
+                        log.info("Time taken to perform doPhrExchangeMedication: " + (System.currentTimeMillis()-startTime) + "ms");
+                        //out.print("1");
+                    }catch(Exception e){
+                        MiscUtils.getLogger().error("Error", e);
+                        //out.print("exception thrown");
+                        request.getSession().removeAttribute(PHRAuthentication.SESSION_PHR_AUTH);
+                    }
+                }else{
+                    log.error("String Auth object was null or the previous action still executing");
+                }
+            }
+        }
+        return mapping.findForward("success");
+    }
+
+    public ActionForward displayPrevViewedMeds(ActionMapping mapping, ActionForm  form,HttpServletRequest request, HttpServletResponse response) throws Exception {
+        oscar.oscarRx.pageUtil.RxSessionBean bean = (oscar.oscarRx.pageUtil.RxSessionBean) request.getSession().getAttribute("RxSessionBean");
+        String provNo=bean.getProviderNo();
+        //need demographic number
+        String demoId=request.getParameter("demoId");
+        //get all drugs with status = 5 from phr_document table.
+        PHRDocumentDAO  phrDocumentDAO=(PHRDocumentDAO)SpringUtils.getBean("phrDocumentDAO");
+        List<PHRDocument> phrDocument=phrDocumentDAO.getDocumentsByReceiverSenderStatusClassification(PHRDocument.TYPE_DEMOGRAPHIC,
+                PHRDocument.TYPE_PROVIDER ,PHRDocument.CLASSIFICATION_MED,demoId, PHRDocument.STATUS_RECIEVED_NOT_SAVED_IN_OSCAR_TABLE);
+        List<PHRMedication> listDrugsToDisplay=new ArrayList<PHRMedication>();
+        for(PHRDocument d:phrDocument){
+            PHRMedication m=(PHRMedication)d;
+            m.initMedication();
+            m.createDrugFromPhrMed(provNo);
+            listDrugsToDisplay.add(m);
+        }
+        HashMap<Long,PHRMedication> drugsToDisplay =RxUtil.createKeyValPair(listDrugsToDisplay);
+        bean.setPairPrevViewedPHRMed(drugsToDisplay);
+        return mapping.findForward("displayPrevViewed");
+    }
     public ActionForward doPhrExchange(ActionMapping mapping, ActionForm  form,HttpServletRequest request, HttpServletResponse response) throws Exception {
         log.debug("-----------------Indivo Exchange has been called -------------");
         PHRAuthentication auth  = (PHRAuthentication) request.getSession().getAttribute(PHRAuthentication.SESSION_PHR_AUTH);
@@ -78,6 +204,7 @@ public class PHRExchangeAction extends DispatchAction {
                 long startTime = System.currentTimeMillis();
                 phrService.sendQueuedDocuments(auth,providerNo) ;
                 phrService.retrieveDocuments(auth,providerNo);
+                //phrService.retrieveUploadDocs(auth,providerNo);
                 request.getSession().setAttribute(phrService.SESSION_PHR_EXCHANGE_TIME, getNextExchangeTime());
                 log.info("Time taken to perform OSCAR-myOSCAR exchange: " + (System.currentTimeMillis()-startTime) + "ms");
                 out.print("1");
