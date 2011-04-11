52f5f977c516706f035fff88522c31ab6b4baffa
Jason Gallagher <jaygallagher@gmail.com>
2011-04-11 14:10:06 -0400

    Fixes bug ID: 3284726  The Decision Support module would not run because the Guideline didn't allow for null values in the parameters
    Change-Id: Id055eb8d9d485723dcd7acfc0e2fdf5165fce4cb

--- src/main/java/org/oscarehr/decisionSupport/model/impl/drools/DSGuidelineDrools.java
+++ src/main/java/org/oscarehr/decisionSupport/model/impl/drools/DSGuidelineDrools.java
@@ -68,12 +68,14 @@ public class DSGuidelineDrools extends DSGuideline {
             }
 
             List<DSParameter> lDSP = this.getParameters();
-            for( DSParameter dsp: lDSP ) {
-                Class clas = Class.forName(dsp.getStrClass());
-                Constructor constructor = clas.getConstructor();
-                Object obj = constructor.newInstance();
-
-                workingMemory.assertObject(obj);
+            if (lDSP != null){
+	            for( DSParameter dsp: lDSP ) {
+	                Class clas = Class.forName(dsp.getStrClass());
+	                Constructor constructor = clas.getConstructor();
+	                Object obj = constructor.newInstance();
+	
+	                workingMemory.assertObject(obj);
+	            }
             }
 
             workingMemory.fireAllRules();
@@ -116,12 +118,12 @@ public class DSGuidelineDrools extends DSGuideline {
         ArrayList<Element> conditionElements = new ArrayList();
         ArrayList<Element> lParameterElements = new ArrayList();
         
-
-        for( DSParameter dsParameter: this.getParameters()) {
-            Element parameterElement = this.getDroolsParameter(dsParameter);
-            lParameterElements.add(parameterElement);
+        if(this.getParameters() != null){
+	        for( DSParameter dsParameter: this.getParameters()) {
+	            Element parameterElement = this.getDroolsParameter(dsParameter);
+	            lParameterElements.add(parameterElement);
+	        }
         }
-
         int paramCount=0;
        
         for (DSCondition condition: this.getConditions()) {
