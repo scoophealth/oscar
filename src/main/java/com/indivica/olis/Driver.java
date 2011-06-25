ab5f2085bee5a20d2d22811a7d22b28908595678
Jen <jennifer@indivica.com>
2011-06-25 18:07:04 -0400

    Added more stuff for OLIS.
    Search UI for Z01 now shows an additional parameter.
    When overriding patient consent in Z01, Z02 and Z04 queries, you now
    specify who gives you permission to do so; this is recorded in the
    system audit log.
    Response UI now shows errors parsed from XML.
    Added a bunch of generated XML files from the OLIS response XSD.
    
    Modified the Log hibernate file so that it properly hooks into the
    data column in the table.  Also updated the model class so that you
    can set this column.

--- src/main/java/com/indivica/olis/Driver.java
+++ src/main/java/com/indivica/olis/Driver.java
@@ -1,9 +1,10 @@
 package com.indivica.olis;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.StringReader;
 import java.security.KeyStore;
 import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.Security;
 import java.security.cert.CertStore;
 import java.security.cert.Certificate;
@@ -13,8 +14,16 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.SchemaFactory;
 
 import org.bouncycastle.cms.CMSProcessableByteArray;
 import org.bouncycastle.cms.CMSSignedData;
@@ -23,9 +32,15 @@ import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.cms.SignerInformationStore;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64;
+import org.oscarehr.PMmodule.dao.LogDAO;
+import org.oscarehr.PMmodule.model.Log;
 import org.oscarehr.util.MiscUtils;
-
+import org.oscarehr.util.SpringUtils;
+import org.xml.sax.InputSource;
 import oscar.OscarProperties;
+import ca.ssha._2005.hial.ArrayOfError;
+import ca.ssha._2005.hial.ArrayOfString;
+import ca.ssha._2005.hial.Response;
 import ca.ssha.www._2005.hial.OLISStub;
 import ca.ssha.www._2005.hial.OLISStub.HIALRequest;
 import ca.ssha.www._2005.hial.OLISStub.HIALRequestSignedRequest;
@@ -36,89 +51,152 @@ import com.indivica.olis.queries.Query;
 
 public class Driver {
 
+	private static LogDAO logDao = (LogDAO) SpringUtils.getBean("logDAO");
+
 	public static String submitOLISQuery(HttpServletRequest request, Query query) {
-	    try {
-	    	OLISMessage message = new OLISMessage(query);
+		try {
+			OLISMessage message = new OLISMessage(query);
 
-	    	System.setProperty("javax.net.ssl.keyStore",
-	    			OscarProperties.getInstance().getProperty("olis_ssl_keystore").trim());
-	    	System.setProperty("javax.net.ssl.keyStorePassword", 
-	    			OscarProperties.getInstance().getProperty("olis_ssl_keystore_password").trim());
-	    	System.setProperty("javax.net.ssl.trustStore",
-	    			OscarProperties.getInstance().getProperty("olis_truststore").trim());
-	    	System.setProperty("javax.net.ssl.trustStorePassword", 
-	    			OscarProperties.getInstance().getProperty("olis_truststore_password").trim());
+			System.setProperty("javax.net.ssl.keyStore",
+					OscarProperties.getInstance().getProperty("olis_ssl_keystore").trim());
+			System.setProperty("javax.net.ssl.keyStorePassword", 
+					OscarProperties.getInstance().getProperty("olis_ssl_keystore_password").trim());
+			System.setProperty("javax.net.ssl.trustStore",
+					OscarProperties.getInstance().getProperty("olis_truststore").trim());
+			System.setProperty("javax.net.ssl.trustStorePassword", 
+					OscarProperties.getInstance().getProperty("olis_truststore_password").trim());
 
-	    	OLISRequest olisRequest = new OLISRequest();
-	    	olisRequest.setHIALRequest(new HIALRequest());
-	    	OLISStub olis = new OLISStub();
+			OLISRequest olisRequest = new OLISRequest();
+			olisRequest.setHIALRequest(new HIALRequest());
+			OLISStub olis = new OLISStub();
 
-	    	olisRequest.getHIALRequest().setClientTransactionID(message.getTransactionId());
-	    	olisRequest.getHIALRequest().setSignedRequest(new HIALRequestSignedRequest());
+			olisRequest.getHIALRequest().setClientTransactionID(message.getTransactionId());
+			olisRequest.getHIALRequest().setSignedRequest(new HIALRequestSignedRequest());
 
-	    	String msgInXML = String
-	    	.format("<Request xmlns=\"http://www.ssha.ca/2005/HIAL\"><Content><![CDATA[%s]]></Content></Request>",
-	    			message.getOlisHL7String());
+			String olisHL7String = message.getOlisHL7String();
+			String msgInXML = String
+			.format("<Request xmlns=\"http://www.ssha.ca/2005/HIAL\"><Content><![CDATA[%s]]></Content></Request>",
+					olisHL7String);
 
-	    	String signedRequest = Driver.signData(msgInXML);
-	    	olisRequest.getHIALRequest().getSignedRequest().setSignedData(signedRequest);
+			String signedRequest = Driver.signData(msgInXML);
+			olisRequest.getHIALRequest().getSignedRequest().setSignedData(signedRequest);
 
-	    	OLISRequestResponse olisResponse = olis.oLISRequest(olisRequest);
 
-	    	String signedData = olisResponse.getHIALResponse().getSignedResponse().getSignedData();
-	    	String unsignedData = Driver.unsignData(signedData);
-
-	    	if (request != null) { 
-		    	request.setAttribute("msgInXML", msgInXML);
-		    	request.setAttribute("signedRequest", signedRequest);
-		    	request.setAttribute("signedData", signedData);
-		    	request.setAttribute("unsignedResponse", unsignedData);
-	    	}
-	    	return unsignedData;
-
-	    } catch (Exception e) {
-	    	MiscUtils.getLogger().error("Can't perform OLIS query due to exception.", e);
-	    	return "";
-	    }
-    }
-    
-	public static String unsignData(String data) {
-		
-			byte[] dataBytes = Base64.decode(data);
-		
 			try {
-				
-		        CMSSignedData s = new CMSSignedData(dataBytes);
-		        CertStore certs = s.getCertificatesAndCRLs("Collection", "BC");
-		        SignerInformationStore signers = s.getSignerInfos();
-		        @SuppressWarnings("unchecked")
-		        Collection<SignerInformation> c = signers.getSigners();
-		        Iterator<SignerInformation> it = c.iterator();
-		        while (it.hasNext())
-		        {
-		                X509Certificate cert = null;
-		                SignerInformation signer = it.next();
-		                Collection certCollection = certs.getCertificates(signer.getSID());
-		                @SuppressWarnings("unchecked")
-		                Iterator<X509Certificate> certIt = certCollection.iterator();
-		                cert = certIt.next();
-		                if ( !signer.verify(cert.getPublicKey(), "BC")) throw new Exception("Doesn't verify");
-		        }
-		        
-		        CMSProcessableByteArray cpb = (CMSProcessableByteArray) s.getSignedContent();
-		        byte[] signedContent = (byte[]) cpb.getContent();
-		        String content = new String(signedContent);
-		        return content;
+				Log logItem = new Log();
+				logItem.setAction("OLIS");
+				logItem.setContent("query");
+				logItem.setData(olisHL7String);
+
+				logDao.saveLog(logItem);
+
 			} catch (Exception e) {
-				MiscUtils.getLogger().error("error",e);
+				MiscUtils.getLogger().error("Couldn't write log message for OLIS query", e);
 			}
-			return null;
-		
+
+
+
+			OLISRequestResponse olisResponse = olis.oLISRequest(olisRequest);
+
+			String signedData = olisResponse.getHIALResponse().getSignedResponse().getSignedData();
+			String unsignedData = Driver.unsignData(signedData);
+
+			if (request != null) { 
+				request.setAttribute("msgInXML", msgInXML);
+				request.setAttribute("signedRequest", signedRequest);
+				request.setAttribute("signedData", signedData);
+				request.setAttribute("unsignedResponse", unsignedData);
+			}
+
+			readResponseFromXML(request, unsignedData);
+
+			return unsignedData;
+
+		} catch (Exception e) {
+			MiscUtils.getLogger().error("Can't perform OLIS query due to exception.", e);
+			return "";
+		}
 	}
-	
+
+	private static void readResponseFromXML(HttpServletRequest request,
+			String olisResponse) {
+
+		try {
+			DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+
+			Source schemaFile = new StreamSource(new File("olis/response.xsd"));
+			factory.newSchema(schemaFile);
+
+			JAXBContext jc = JAXBContext.newInstance("ca.ssha._2005.hial");
+			Unmarshaller u = jc.createUnmarshaller();
+			Response root = (Response) u.unmarshal(new InputSource(new StringReader(olisResponse)));
+			
+			if (root.getErrors() != null) {
+				List<String> errorStringList = new LinkedList<String>();
+				
+				// Read all the errors
+				ArrayOfError errors = root.getErrors();
+				List<ca.ssha._2005.hial.Error> errorList = errors.getError();
+				
+				for (ca.ssha._2005.hial.Error error : errorList) {
+					String errorString = "";
+					errorString += "ERROR " + error.getNumber() + " (" + error.getSeverity() + ") : " + error.getMessage();
+					
+					ArrayOfString details = error.getDetails();
+					List<String> detailList = details.getString();
+					for (String detail : detailList) {
+						errorString += "\n" + detail;
+					}
+					
+					errorStringList.add(errorString);
+				}
+				
+				request.setAttribute("errors", errorStringList);
+			} else if (root.getContent() != null) {
+				request.setAttribute("olisResponseContent", root.getContent());
+			}
+		} catch (Exception e) {
+			MiscUtils.getLogger().error("Couldn't read XML from OLIS response.", e);
+		}
+	}
+
+	public static String unsignData(String data) {
+
+		byte[] dataBytes = Base64.decode(data);
+
+		try {
+
+			CMSSignedData s = new CMSSignedData(dataBytes);
+			CertStore certs = s.getCertificatesAndCRLs("Collection", "BC");
+			SignerInformationStore signers = s.getSignerInfos();
+			@SuppressWarnings("unchecked")
+			Collection<SignerInformation> c = signers.getSigners();
+			Iterator<SignerInformation> it = c.iterator();
+			while (it.hasNext())
+			{
+				X509Certificate cert = null;
+				SignerInformation signer = it.next();
+				Collection certCollection = certs.getCertificates(signer.getSID());
+				@SuppressWarnings("unchecked")
+				Iterator<X509Certificate> certIt = certCollection.iterator();
+				cert = certIt.next();
+				if ( !signer.verify(cert.getPublicKey(), "BC")) throw new Exception("Doesn't verify");
+			}
+
+			CMSProcessableByteArray cpb = (CMSProcessableByteArray) s.getSignedContent();
+			byte[] signedContent = (byte[]) cpb.getContent();
+			String content = new String(signedContent);
+			return content;
+		} catch (Exception e) {
+			MiscUtils.getLogger().error("error",e);
+		}
+		return null;
+
+	}
+
 	public static String signData(String data) {
 		X509Certificate cert = null;
-		PublicKey pub = null;
 		PrivateKey priv = null;
 		KeyStore keystore = null;
 		String pwd = "Olis2011";
@@ -169,10 +247,10 @@ public class Driver {
 			CMSSignedData csd = sgen.generate(new CMSProcessableByteArray(data.getBytes()), false, "BC");
 			byte[] signedData = csd.getEncoded();
 			byte[] signedDataB64 = Base64.encode(signedData);
-			
+
 			result = new String(signedDataB64);
-			
-			
+
+
 
 		} catch (Exception e) {
 			MiscUtils.getLogger().error("Can't sign HL7 message for OLIS", e);
