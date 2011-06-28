a2c0442f78aa25739782b3efb3f4683b15874a59
Jen <jennifer@indivica.com>
2011-06-28 16:21:56 -0400

    Added missing references to actions in struts-config.xml Added missing class Adjusted size of window that gets opened when you click on an HRM report in the inbox Fixed issue with not showing correct provider name from HRM XML document

---
+++ src/main/java/org/oscarehr/hospitalReportManager/HRMUploadLabAction.java
@@ -0,0 +1,42 @@
+package org.oscarehr.hospitalReportManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.upload.FormFile;
+import org.oscarehr.util.MiscUtils;
+
+import oscar.oscarLab.ca.all.pageUtil.LabUploadForm;
+import oscar.oscarLab.ca.all.util.Utilities;
+
+public class HRMUploadLabAction extends DispatchAction {
+
+	@Override
+    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)  {
+        LabUploadForm frm = (LabUploadForm) form;
+        FormFile importFile = frm.getImportFile();
+        
+        try {
+	        String filePath = Utilities.saveFile(importFile.getInputStream(), importFile.getFileName());
+	        
+	        HRMReport report = HRMReportParser.parseReport(filePath);
+	        if (report != null) HRMReportParser.addReportToInbox(report);
+	        
+	        request.setAttribute("success", true);
+	        
+        } catch (Exception e) {
+	        MiscUtils.getLogger().error("Couldn't handle uploaded HRM lab", e);
+	        
+	        request.setAttribute("success", false);
+        } 
+        
+        
+        
+        return mapping.findForward("success");
+	}
+	
+}
