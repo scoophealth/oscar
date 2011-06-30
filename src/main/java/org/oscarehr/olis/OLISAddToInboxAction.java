b968ebf3156d52ef2fff4943d9a30f183a3b0519
Adam Balanga <adam@indivica.com>
2011-06-29 22:31:17 -0400

    Modified OLISHL7Handler to allow for uploaded labs to be routed to the current provider and OLISAddToInboxAction to use this new functionality when labs are imported from OLIS search results.
    Change-Id: Ic8b815fa12788f57c9b22e55b41cba5fb93d4f51

--- src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
+++ src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
@@ -2,6 +2,7 @@ package org.oscarehr.olis;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,7 @@ import org.oscarehr.util.MiscUtils;
 
 import oscar.oscarLab.FileUploadCheck;
 import oscar.oscarLab.ca.all.upload.HandlerClassFactory;
-import oscar.oscarLab.ca.all.upload.handlers.MessageHandler;
+import oscar.oscarLab.ca.all.upload.handlers.OLISHL7Handler;
 
 
 public class OLISAddToInboxAction extends DispatchAction {
@@ -28,26 +29,33 @@ public class OLISAddToInboxAction extends DispatchAction {
 
 		String fileLocation = System.getProperty("java.io.tmpdir") + "/olis_" + uuidToAdd + ".response";
 		File file = new File(fileLocation);
-		MessageHandler msgHandler = HandlerClassFactory.getHandler("OLIS_HL7");
-
+		OLISHL7Handler msgHandler = (OLISHL7Handler) HandlerClassFactory.getHandler("OLIS_HL7");
+		
+		InputStream is = null;
 		try {
-			InputStream is = new FileInputStream(fileLocation);
-			int check = FileUploadCheck.addFile(file.getName(), is, LoggedInInfo.loggedInInfo.get().loggedInProvider.getProviderNo());
+			is = new FileInputStream(fileLocation);
+			String provNo =  LoggedInInfo.loggedInInfo.get().loggedInProvider.getProviderNo();
+			int check = FileUploadCheck.addFile(file.getName(), is, provNo);
+			
 			if (check != FileUploadCheck.UNSUCCESSFUL_SAVE) {
-				if (msgHandler.parse("OLIS_HL7",fileLocation, check) != null) {
-					request.setAttribute("result", "Success");
+				if (msgHandler.parse("OLIS_HL7",fileLocation, check, true) != null) {
+					request.setAttribute("result", "Success");					
 				} else {
 					request.setAttribute("result", "Error");
 				}
 			} else {
 				request.setAttribute("result", "Already Added");
-			}
-			is.close();
+			}			
 
 		} catch (Exception e) {
 			MiscUtils.getLogger().error("Couldn't add requested OLIS lab to Inbox.", e);
-
 			request.setAttribute("result", "Error");
+		} finally {
+			try {
+				is.close();
+			} catch(IOException e){
+				//ignore
+			}
 		}
 
 		return mapping.findForward("ajax");
