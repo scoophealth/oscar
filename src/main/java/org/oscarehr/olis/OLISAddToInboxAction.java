56468029f22293d17a683a789c87b2171fef376b
Marc Dumontier <marc@mdumontier.com>
2011-07-15 14:28:19 -0400

    Added filing/ack ability to search result screen

--- src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
+++ src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
@@ -15,17 +18,32 @@ import org.apache.struts.actions.DispatchAction;
 import org.oscarehr.util.LoggedInInfo;
 import org.oscarehr.util.MiscUtils;
 
+import oscar.log.LogAction;
+import oscar.log.LogConst;
+import oscar.oscarDB.DBHandler;
 import oscar.oscarLab.FileUploadCheck;
 import oscar.oscarLab.ca.all.upload.HandlerClassFactory;
 import oscar.oscarLab.ca.all.upload.handlers.OLISHL7Handler;
+import oscar.oscarLab.ca.on.CommonLabResultData;
 
 
 public class OLISAddToInboxAction extends DispatchAction {
+	
+	static Logger logger = MiscUtils.getLogger();
 
 	@Override
 	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
 
 		String uuidToAdd = request.getParameter("uuid");
+		String pFile = request.getParameter("file");
+		String pAck = request.getParameter("ack");
+		boolean doFile=false, doAck=false;
+		if(pFile != null && pFile.equals("true")) {
+			doFile=true;
+		}
+		if(pAck != null && pAck.equals("true")) {
+			doAck=true;
+		}
 
 		String fileLocation = System.getProperty("java.io.tmpdir") + "/olis_" + uuidToAdd + ".response";
 		File file = new File(fileLocation);
@@ -39,7 +57,19 @@ public class OLISAddToInboxAction extends DispatchAction {
 			
 			if (check != FileUploadCheck.UNSUCCESSFUL_SAVE) {
 				if (msgHandler.parse("OLIS_HL7",fileLocation, check, true) != null) {
-					request.setAttribute("result", "Success");					
+					request.setAttribute("result", "Success");
+					if(doFile) {
+						ArrayList<String[]> labsToFile = new ArrayList<String[]>();
+						String item[] = new String[]{String.valueOf(msgHandler.getLastSegmentId()),"HL7"};
+						labsToFile.add(item);
+						CommonLabResultData.fileLabs(labsToFile, provNo);
+					}
+					if(doAck) {
+						 String demographicID = getDemographicIdFromLab("HL7", msgHandler.getLastSegmentId());				           
+						 LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.ACK, LogConst.CON_HL7_LAB, ""+msgHandler.getLastSegmentId(), request.getRemoteAddr(),demographicID);
+						 CommonLabResultData.updateReportStatus(msgHandler.getLastSegmentId(), provNo, 'A', "comment","HL7");
+				            
+					}
 				} else {
 					request.setAttribute("result", "Error");
 				}
@@ -60,4 +90,23 @@ public class OLISAddToInboxAction extends DispatchAction {
 
 		return mapping.findForward("ajax");
 	}
+	
+    private static String getDemographicIdFromLab(String labType, int labNo)
+    {
+    	String demographicID="";
+        try{
+            String sql = "SELECT demographic_no FROM patientLabRouting WHERE lab_type = '"+labType+"' and lab_no='"+labNo+"'";
+            
+            ResultSet rs = DBHandler.GetSQL(sql);
+
+            while(rs.next()){
+                demographicID = oscar.Misc.getString(rs, "demographic_no");
+            }
+            rs.close();
+        }catch(Exception e){
+        	logger.error("Error", e);
+        }
+        
+        return(demographicID);
+    }
 }
