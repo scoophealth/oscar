f4b6da1242dc6042a8fc67a156e784f4e9110c92
Ted Leung <r6squeegee@gmail.com>
2011-08-18 23:48:19 +1000

    updated update script for people with bad data in their database
    Change-Id: I36bc461a8cca587ccbc0ba8a69e46f12b2ff6594

--- database/mysql/updates/update-2011-08-11.sql
+++ database/mysql/updates/update-2011-08-11.sql
@@ -1,4 +1,5 @@
 alter table demographic change pin myOscarUserName varchar(255);
-alter table demographic add unique(myOscarUserName);
+update demographic set myOscarUserName=null where  myOscarUserName='@myoscar.org';
 update demographic set myOscarUserName=null where  myOscarUserName='';
+alter table demographic add unique(myOscarUserName);
 
