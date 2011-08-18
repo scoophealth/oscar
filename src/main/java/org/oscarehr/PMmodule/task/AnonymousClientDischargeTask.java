c37a1f3de714718874da09763d30b6eb4400bb4e
Ted Leung <r6squeegee@gmail.com>
2011-08-18 13:03:33 +1000

    ensure threads release db connections
    Change-Id: I017054419a58f59ee9b27d8a8305cea733cf7801

--- src/main/java/org/oscarehr/PMmodule/task/AnonymousClientDischargeTask.java
+++ src/main/java/org/oscarehr/PMmodule/task/AnonymousClientDischargeTask.java
@@ -7,34 +7,38 @@ import java.util.TimerTask;
 import org.apache.log4j.Logger;
 import org.oscarehr.PMmodule.model.Admission;
 import org.oscarehr.PMmodule.service.AdmissionManager;
+import org.oscarehr.util.DbConnectionFilter;
 import org.oscarehr.util.LoggedInInfo;
 import org.oscarehr.util.MiscUtils;
 import org.oscarehr.util.SpringUtils;
 
 public class AnonymousClientDischargeTask extends TimerTask {
 
-    private static final Logger logger = MiscUtils.getLogger();
-    
-    public void run() {    	
-		LoggedInInfo.setLoggedInInfoToCurrentClassAndMethod();
-		AdmissionManager admissionManager = (AdmissionManager)SpringUtils.getBean("admissionManager");
-		
-		
-		List<Admission> admissions = admissionManager.getActiveAnonymousAdmissions();
-		
-		
-		for(Admission admission:admissions) {
-			if((new Date().getTime()-admission.getAdmissionDate().getTime()) > (1000*60*60*24)) {
-				logger.info("Admission ready for discharge.");
-				 admission.setDischargeDate(new Date());
-	             admission.setDischargeNotes("Auto-Discharge");
-	             admission.setAdmissionStatus(Admission.STATUS_DISCHARGED);
-	             admissionManager.saveAdmission(admission);
-			} else {
-				logger.info("Admission still under 24hrs - " + admission.getAdmissionDate());
-			}
-		}
-    }
-    
-}
+	private static final Logger logger = MiscUtils.getLogger();
 
+	public void run() {
+		try {
+			LoggedInInfo.setLoggedInInfoToCurrentClassAndMethod();
+			AdmissionManager admissionManager = (AdmissionManager) SpringUtils.getBean("admissionManager");
+
+			List<Admission> admissions = admissionManager.getActiveAnonymousAdmissions();
+
+			for (Admission admission : admissions) {
+				if ((new Date().getTime() - admission.getAdmissionDate().getTime()) > (1000 * 60 * 60 * 24)) {
+					logger.info("Admission ready for discharge.");
+					admission.setDischargeDate(new Date());
+					admission.setDischargeNotes("Auto-Discharge");
+					admission.setAdmissionStatus(Admission.STATUS_DISCHARGED);
+					admissionManager.saveAdmission(admission);
+				} else {
+					logger.info("Admission still under 24hrs - " + admission.getAdmissionDate());
+				}
+			}
+		} catch (Exception e) {
+			logger.error("Error", e);
+		} finally {
+			DbConnectionFilter.releaseAllThreadDbResources();
+		}
+	}
+
+}
