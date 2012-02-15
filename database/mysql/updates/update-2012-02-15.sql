126e541e86bf7d9d294a2a726c2581111cf36924
Marc Dumontier <marc@mdumontier.com>
2012-02-15 05:04:04 -0500

    eyeform: changed column definitions on report

---
+++ database/mysql/updates/update-2012-02-15.sql
@@ -0,0 +1,7 @@
+alter table EyeformConsultationReport change concurrentProblems concurrentProblems text;
+alter table EyeformConsultationReport change currentMeds currentMeds text;
+alter table EyeformConsultationReport change allergies allergies text;
+alter table EyeformConsultationReport change plan plan text;
+alter table EyeformConsultationReport change impression impression text;
+alter table EyeformConsultationReport change clinicalInfo clinicalInfo text;
+
