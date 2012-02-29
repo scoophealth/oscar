202c7068e295066302e44dd2971e16e099667fb5
Ted Leung <r6squeegee@gmail.com>
2012-02-29 15:14:05 +1100

    changes to clean up and make the appointment ws work
    Change-Id: I4f1673982c6c28290e67f7ca601faeb9ad43a847

--- src/main/java/org/oscarehr/ws/LoginWs.java
+++ src/main/java/org/oscarehr/ws/LoginWs.java
@@ -16,10 +16,9 @@ import com.quatro.model.security.Security;
 
 @WebService
 @Component
-public class LoginWs extends AbstractWs
-{
-	private static final Logger logger=MiscUtils.getLogger();
-	
+public class LoginWs extends AbstractWs {
+	private static final Logger logger = MiscUtils.getLogger();
+
 	@Autowired
 	private SecurityDao securityDao = null;
 
@@ -27,26 +26,25 @@ public class LoginWs extends AbstractWs
 	 * Returns LoginResultTransfer on valid login, will be provided with a security token too.
 	 * @throws NotAuthorisedException if password is incorrect
 	 */
-	public org.oscarehr.ws.transfer_objects.LoginResultTransfer login(String userName, String password) throws NotAuthorisedException
-	{
-		List<Security> securities=securityDao.findByUserName(userName);
-		Security security=null;
-		if (securities.size()>0) security=securities.get(0);
-		
-		if (WsUtils.checkAuthenticationAndSetLoggedInInfo(security, password))
-		{
-			LoginResultTransfer result=new LoginResultTransfer();
+	public org.oscarehr.ws.transfer_objects.LoginResultTransfer login(String userName, String password) throws NotAuthorisedException {
+		logger.info("Login attempt : user=" + userName);
+		logger.debug("Login attempt : p =" + password);
+
+		List<Security> securities = securityDao.findByUserName(userName);
+		Security security = null;
+
+		if (securities.size() > 0) security = securities.get(0);
+
+		if (WsUtils.checkAuthenticationAndSetLoggedInInfo(security, password)) {
+			LoginResultTransfer result = new LoginResultTransfer();
 			result.setSecurityId(security.getSecurityNo());
-			
+
 			// we haven't sorted out the token framework so we'll just return the pw for now
 			result.setSecurityTokenKey(password);
-			
-			return(result);
+
+			return (result);
 		}
 
-		logger.info("Login failed : user="+userName);		
-		logger.debug("Login failed : p entered ="+password);
-		
-		throw(new NotAuthorisedException("Invalid Username/Password"));
+		throw (new NotAuthorisedException("Invalid Username/Password"));
 	}
 }
