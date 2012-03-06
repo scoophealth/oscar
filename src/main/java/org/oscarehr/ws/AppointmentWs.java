438d1925e87697771cc57af48eba0e6f6ef378b2
Ted Leung <r6squeegee@gmail.com>
2012-03-06 15:24:58 +1100

    corrected the structure of the appointment ws
    Change-Id: Ib6c10e40969eab4338ebdeb4a41dca7e7430ba1b

--- src/main/java/org/oscarehr/ws/AppointmentWs.java
+++ src/main/java/org/oscarehr/ws/AppointmentWs.java
@@ -1,16 +1,12 @@
 package org.oscarehr.ws;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 import javax.jws.WebService;
 
-import org.oscarehr.common.model.Appointment;
 import org.oscarehr.managers.AppointmentManager;
 import org.oscarehr.managers.DaySchedule;
-import org.oscarehr.ws.transfer_objects.ProviderAppointmentAvailabilityTransfer;
-import org.oscarehr.ws.transfer_objects.TimeBlock;
+import org.oscarehr.ws.transfer_objects.DayScheduleTransfer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,59 +15,11 @@ import org.springframework.stereotype.Component;
 public class AppointmentWs extends AbstractWs {
 	@Autowired
 	private AppointmentManager appointmentManager;
-
-	/**
-	 * @deprecated I wrote this wrong... ws method should be generic and data filtering should be on client side, don't use, I will rewrite shortly. 
-	 */
-	public ProviderAppointmentAvailabilityTransfer getProviderAppointmentAvailability(String providerNo, Calendar date) {
+	
+	public DayScheduleTransfer getDaySchedule(String providerNo, Calendar date)
+	{
 		DaySchedule daySchedule = appointmentManager.getDaySchedule(providerNo, date);
-
-		ProviderAppointmentAvailabilityTransfer result = new ProviderAppointmentAvailabilityTransfer();
-		result.setEndTime(daySchedule.getEndTime());
-		result.setProviderNo(providerNo);
-		result.setStartTime(daySchedule.getStartTime());
-		result.setTimeSlotDurationMin(daySchedule.getTimeSlotDurationMin());
-
-		ArrayList<TimeBlock> timeBlocks = new ArrayList<TimeBlock>();
-		for (Appointment appointment : daySchedule.getAppointments()) {
-			TimeBlock timeBlock = new TimeBlock();
-
-			//--- sort out start time ---
-			Calendar cal = new GregorianCalendar();
-			cal.setTime(appointment.getAppointmentDate());
-
-			Calendar cal2 = new GregorianCalendar();
-			cal2.setTime(appointment.getStartTime());
-
-			cal.set(Calendar.HOUR_OF_DAY, cal2.get(Calendar.HOUR_OF_DAY));
-			cal.set(Calendar.MINUTE, cal2.get(Calendar.MINUTE));
-			cal.set(Calendar.SECOND, cal2.get(Calendar.SECOND));
-			cal.set(Calendar.MILLISECOND, cal2.get(Calendar.MILLISECOND));
-			cal.getTimeInMillis();
-
-			timeBlock.setStartTime(cal);
-
-			//--- sort out end time ---
-			cal = new GregorianCalendar();
-			cal.setTime(appointment.getAppointmentDate());
-
-			cal2 = new GregorianCalendar();
-			cal2.setTime(appointment.getEndTime());
-
-			cal.set(Calendar.HOUR_OF_DAY, cal2.get(Calendar.HOUR_OF_DAY));
-			cal.set(Calendar.MINUTE, cal2.get(Calendar.MINUTE));
-			cal.set(Calendar.SECOND, cal2.get(Calendar.SECOND));
-			cal.set(Calendar.MILLISECOND, cal2.get(Calendar.MILLISECOND));
-			cal.getTimeInMillis();
-
-			timeBlock.setEndTime(cal);
-
-			//--- add to list ---
-			timeBlocks.add(timeBlock);
-		}
-
-		result.setUnavailableTimes(timeBlocks.toArray(new TimeBlock[0]));
-
-		return (result);
+		DayScheduleTransfer dayScheduleTransfer=DayScheduleTransfer.toTransfer(daySchedule);
+		return(dayScheduleTransfer);
 	}
 }
