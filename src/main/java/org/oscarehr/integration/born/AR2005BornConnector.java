40db7438e4f16818e92f4f91f0ca0c34d3579b1f
Marc Dumontier <marc@mdumontier.com>
2012-03-18 13:32:14 -0400

    ID: 3508715 - Integrate with BORN (initial commit)
    -Generating XML from A1+A2 (AR2005) form
    -Scheduled FTP transfer of XML to BORN system
    -Page to see history/status of transfers

---
+++ src/main/java/org/oscarehr/integration/born/AR2005BornConnector.java
@@ -0,0 +1,71 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
+// * <OSCAR TEAM>
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster University
+// * Hamilton
+// * Ontario, Canada
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+
+package org.oscarehr.integration.born;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+/**
+ * This class will determine which AR2005 forms need to be sent to
+ * BORN, and perform the transaction.
+ *
+ * It will also check for any error files, and email any errors
+ * to specified administrator
+ *
+ *
+ * @author Marc Dumontier
+ *
+ */
+public class AR2005BornConnector {
+
+	public AR2005BornConnector() {
+
+	}
+
+	public void updateBorn() throws Exception {
+
+		Connection conn = org.oscarehr.util.DbConnectionFilter.getThreadLocalDbConnection();
+		Statement st = conn.createStatement();
+		ResultSet rs = st.executeQuery("select * from formONAR");
+
+		AR2005Form2XML ar2005Form2XML = new AR2005Form2XML();
+		File file = File.createTempFile("born", ".xml");
+		ar2005Form2XML.generateXML(rs, new FileOutputStream(file));
+
+		BornFtpManager ftpManager = new BornFtpManager();
+		String path = file.getPath();
+		path = path.substring(0,path.lastIndexOf(File.separator));
+		ftpManager.uploadDataToRepository(path, file.getName());
+
+		rs.close();
+		st.close();
+		conn.close();
+	}
+
+}
---
+++ src/test/resources/initFormONAR.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
+--
+-- Host: localhost    Database: oscar_eyeform
+-- ------------------------------------------------------
+-- Server version	5.1.41-3ubuntu12.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `formONAR`
+--
+
+LOCK TABLES `formONAR` WRITE;
+/*!40000 ALTER TABLE `formONAR` DISABLE KEYS */;
+INSERT INTO `formONAR` VALUES (9,3,999998,'2012-03-16','2012-03-17 02:27:29','TEST','MARC','29 Maple st.','Suite 2','Drayton','ON','N0G1P0','Test','John','604-555-5555','666-666-6666','English','Engineer','University Degree','43','1978-06-15','33','Software Developer','High School','Caucasian','2000010534','000054444',0,0,1,1,0,1,'other ba',1,0,1,'other newborn care','Christine Peterkin','penicillin','prozac','2012-03-16',0,0,'4',0,0,'none','2011-12-15','2012-08-16','2012-08-23',1,0,0,0,'1','2','3','4','5','1978','M','37','5lbs','37','woodstock',1,0,0,'no issues','1980','F','33','5','3','toronto',0,1,0,'issue 2','1982','M','34','6','4','drayton',0,0,1,'issue 3','1985','F','39','4','9','windsor',0,0,0,'xxx','1986','M','40','9','2','kitchener',1,0,0,'issue 5','1990','F','41','10','3','blah',0,1,0,'issue6',1,0,1,0,'10',1,0,0,1,0,0,0,1,1,0,1,0,0,0,1,0,0,1,0,0,1,0,0,1,0,1,1,0,0,1,0,0,1,0,0,1,'other21',1,0,1,0,1,0,1,0,0,1,0,1,1,0,0,1,1,0,'other30',1,0,1,0,0,0,1,0,0,1,1,0,1,0,1,0,0,1,'5\' 10\"','180','44.00','130/90',1,0,1,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,'4',1,0,'other50',1,0,'hb','hiv-','mcv',1,'abo','2012/03/16','pap','rh','anti','gc','rub','urine','hb','vdrl','sickle','true','false','true',1,'comments','marc dumontier','2012-03-16','nicole dumontier','2012-03-16','death','plan to manage death','risk factor2','plan2','risk factor3','plan3','risk factor4','plan4','risk factor5','plan5','risk factor6','plan6','risk factor7','plan7',1,'xxx',1,1,1,'2012-01-01','4','87','130/90','3','4','5','normal','fhr','comments','2012-02-01','5','8','89','bad','fm','6','7','120/40','comments2',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','',NULL,'','','','','','','','','','2012-03-16','4','result1','2012-03-23','5','result2',NULL,'','',NULL,'','','hb','abo','rh','abs','gc','gtt','gbs',1,1,1,0,0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0,0,'marc dumontier','2012-03-16','nicole dumontier','2012-03-16','',NULL,'',NULL,'',NULL,'',NULL);
+/*!40000 ALTER TABLE `formONAR` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-03-16 22:56:32
