d1553f911d008bb962f43c4ce342b47c36d532ed
Marc Dumontier <marc@mdumontier.com>
2012-03-18 13:32:14 -0400

    ID: 3508715 - Integrate with BORN (initial commit)
    -Generating XML from A1+A2 (AR2005) form
    -Scheduled FTP transfer of XML to BORN system
    -Page to see history/status of transfers

---
+++ src/main/java/org/oscarehr/integration/born/BornFtpManager.java
@@ -0,0 +1,48 @@
+// -----------------------------------------------------------------------------------------------------------------------
+// *
+// *
+// * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
+// * <OSCAR TEAM>
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster University
+// * Hamilton
+// * Ontario, Canada
+// *
+// -----------------------------------------------------------------------------------------------------------------------
+package org.oscarehr.integration.born;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.commons.net.ftp.FTPFile;
+import org.oscarehr.util.SpringUtils;
+import org.springframework.integration.file.remote.session.Session;
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory;
+
+public class BornFtpManager {
+
+	public BornFtpManager() {
+
+	}
+
+	public void uploadDataToRepository(String path, String filename) throws Exception {
+		DefaultFtpSessionFactory ftpFactory = (DefaultFtpSessionFactory)SpringUtils.getBean("ftpClientFactory");
+		Session<FTPFile> session = ftpFactory.getSession();
+		if(session.isOpen()) {
+			session.write(new FileInputStream(path + File.separator + filename), filename);
+		}
+	}
+}
