c9ed8fc5a266d931d1b2153769cf798b3f8a08c8
Marc Dumontier <marc@mdumontier.com>
2012-04-02 16:22:56 -0400

    DesAnnualReviewPlan - removed SQL
    anyone know what Des stands for?

--- src/main/java/org/oscarehr/common/model/DesAnnualReviewPlan.java
+++ src/main/java/org/oscarehr/common/model/DesAnnualReviewPlan.java
@@ -12,7 +12,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-@Table(name="demographicSets")
+@Table(name="desannualreviewplan")
 public class DesAnnualReviewPlan extends AbstractModel<Integer>{
 
 	@Id
