0bc07e05cc439e7b6b19f4000abee4b669ddfe4a
Marc Dumontier <marc@mdumontier.com>
2012-04-06 17:35:14 -0400

    removed all insert/update from dbReport.jspf

--- src/main/java/org/oscarehr/common/dao/ReportAgeSexDao.java
+++ src/main/java/org/oscarehr/common/dao/ReportAgeSexDao.java
@@ -1,5 +1,10 @@
 package org.oscarehr.common.dao;
 
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.oscarehr.common.model.ReportAgeSex;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +14,14 @@ public class ReportAgeSexDao extends AbstractDao<ReportAgeSex>{
 	public ReportAgeSexDao() {
 		super(ReportAgeSex.class);
 	}
+
+    public List<ReportAgeSex> findBeforeReportDate(Date reportDate) {
+    	String sql = "select x from ReportAgeSex x where x.reportDate=?";
+    	Query query = entityManager.createQuery(sql);
+    	query.setParameter(1,reportDate);
+
+        @SuppressWarnings("unchecked")
+        List<ReportAgeSex> results = query.getResultList();
+        return results;
+    }
 }
--- src/main/java/org/oscarehr/common/dao/ReportProviderDao.java
+++ src/main/java/org/oscarehr/common/dao/ReportProviderDao.java
@@ -1,5 +1,9 @@
 package org.oscarehr.common.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.oscarehr.common.model.ReportProvider;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +13,15 @@ public class ReportProviderDao extends AbstractDao<ReportProvider>{
 	public ReportProviderDao() {
 		super(ReportProvider.class);
 	}
+
+	public List<ReportProvider> findByAction(String action) {
+    	String sql = "select x from ReportProvider x where x.action=?";
+    	Query query = entityManager.createQuery(sql);
+    	query.setParameter(1,action);
+
+        @SuppressWarnings("unchecked")
+        List<ReportProvider> results = query.getResultList();
+        return results;
+    }
 
 }
