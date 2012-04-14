cd9ebc035b5a6bb637e23038be9ec16d1c5ce8b6
Marc Dumontier <marc@mdumontier.com>
2012-04-13 21:07:41 -0400

    Code cleanup: Quatro
    Removed unused, uncompleted parts of Quatro merge.

--- src/main/java/oscar/scratch/tld/ScratchTag.java
+++ src/main/java/oscar/scratch/tld/ScratchTag.java
@@ -2,24 +2,24 @@
 // *
 // *
 // * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved. *
-// * This software is published under the GPL GNU General Public License. 
-// * This program is free software; you can redistribute it and/or 
-// * modify it under the terms of the GNU General Public License 
-// * as published by the Free Software Foundation; either version 2 
-// * of the License, or (at your option) any later version. * 
-// * This program is distributed in the hope that it will be useful, 
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of 
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
-// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License 
-// * along with this program; if not, write to the Free Software 
-// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. * 
-// * 
+// * This software is published under the GPL GNU General Public License.
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the GNU General Public License
+// * as published by the Free Software Foundation; either version 2
+// * of the License, or (at your option) any later version. *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// * GNU General Public License for more details. * * You should have received a copy of the GNU General Public License
+// * along with this program; if not, write to the Free Software
+// * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+// *
 // * <OSCAR TEAM>
-// * This software was written for the 
-// * Department of Family Medicine 
-// * McMaster University 
-// * Hamilton 
-// * Ontario, Canada 
+// * This software was written for the
+// * Department of Family Medicine
+// * McMaster University
+// * Hamilton
+// * Ontario, Canada
 // *
 // -----------------------------------------------------------------------------------------------------------------------
 package oscar.scratch.tld;
@@ -48,11 +48,9 @@ public class ScratchTag extends TagSupport {
 
     	if(providerNo!=null){
     		//isScratchFilled is inefficient, removed until a more efficient method is available. by default show the filled graphic
-       	    //com.quatro.service.ScratchPadManager spm = (com.quatro.service.ScratchPadManager) WebApplicationContextUtils.getWebApplicationContext(
- 	       	//	pageContext.getServletContext()).getBean("scratchPadManager");
  		    scratchFilled= true;//spm.isScratchFilled(providerNo);
     	}
-        
+
         try        {
             JspWriter out = super.pageContext.getOut();
             if(scratchFilled)
--- src/test/java/org/oscarehr/common/dao/utils/EntityDataGenerator.java
+++ src/test/java/org/oscarehr/common/dao/utils/EntityDataGenerator.java
@@ -5,6 +5,7 @@ import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,8 +17,6 @@ import javax.persistence.Id;
 import org.oscarehr.common.model.DemographicExt;
 import org.oscarehr.common.model.Provider;
 import org.oscarehr.util.MiscUtils;
-
-import antlr.collections.List;
 
 public class EntityDataGenerator {
 
@@ -65,7 +64,7 @@ public class EntityDataGenerator {
 	        else if(f[i].getType() == char.class || f[i].getType() == Character.class) {
  	        	f[i].set(model,'A');
 	        }
-	 	    else if(f[i].getType() == Set.class || f[i].getType() == List.class || f[i].getType() == Map.class) {
+	        else if(f[i].getType() == Set.class || f[i].getType() == List.class || f[i].getType() == Map.class) {
 	 	    	//ignore
 	 	    }else if(f[i].getType() == Provider.class || f[i].getType() == DemographicExt[].class) {
 	 	    	//ignore
