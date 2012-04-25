a02a8a20e0843a8917f8a95382961547c1b9d3d9
Matthew Ma <matthew.ma20110628@gmail.com>
2012-04-24 23:23:32 -0400

    ID:3488352  CAISI: C.A.S.H. and ACCESS 1 Waiting List Management System
    Details: add DDL to create 5 new tables, and also add entity classes for
    the newly created tables.

---
+++ src/main/java/org/oscarehr/PMmodule/model/CriteriaType.java
@@ -0,0 +1,183 @@
+/**
+ *
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+package org.oscarehr.PMmodule.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.oscarehr.common.model.AbstractModel;
+
+/**
+ * CriteriaType entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "criteria_type")
+public class CriteriaType extends AbstractModel<Long> implements java.io.Serializable {
+
+	// Fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "CRITERIA_TYPE_ID", unique = true, nullable = false)
+	private Long criteriaTypeId;
+	@Column(name = "FIELD_NAME", nullable = false, length = 128)
+	private String fieldName;
+	@Column(name = "FIELD_TYPE", nullable = false, length = 128)
+	private String fieldType;
+	@Column(name = "DEFAULT_VALUE")
+	private String defaultValue;
+	@Column(name = "ACTIVE", nullable = false)
+	private Boolean active;
+	@Column(name = "WL_PROGRAM_ID")
+	private Integer wlProgramId;
+	@Column(name = "CAN_BE_ADHOC", nullable = false)
+	private Boolean canBeAdhoc;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return criteriaTypeId;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.criteriaTypeId = id;
+	}
+
+
+	/** default constructor */
+	public CriteriaType() {
+	}
+
+	/** minimal constructor */
+	public CriteriaType(String fieldName, String fieldType, Boolean active,
+			Boolean canBeAdhoc) {
+		this.fieldName = fieldName;
+		this.fieldType = fieldType;
+		this.active = active;
+		this.canBeAdhoc = canBeAdhoc;
+	}
+
+	/** full constructor */
+	public CriteriaType(String fieldName, String fieldType,
+			String defaultValue, Boolean active, Integer wlProgramId,
+			Boolean canBeAdhoc) {
+		this.fieldName = fieldName;
+		this.fieldType = fieldType;
+		this.defaultValue = defaultValue;
+		this.active = active;
+		this.wlProgramId = wlProgramId;
+		this.canBeAdhoc = canBeAdhoc;
+	}
+
+	/**
+     * @return the fieldName
+     */
+    public String getFieldName() {
+    	return fieldName;
+    }
+
+	/**
+     * @param fieldName the fieldName to set
+     */
+    public void setFieldName(String fieldName) {
+    	this.fieldName = fieldName;
+    }
+
+	/**
+     * @return the fieldType
+     */
+    public String getFieldType() {
+    	return fieldType;
+    }
+
+	/**
+     * @param fieldType the fieldType to set
+     */
+    public void setFieldType(String fieldType) {
+    	this.fieldType = fieldType;
+    }
+
+	/**
+     * @return the defaultValue
+     */
+    public String getDefaultValue() {
+    	return defaultValue;
+    }
+
+	/**
+     * @param defaultValue the defaultValue to set
+     */
+    public void setDefaultValue(String defaultValue) {
+    	this.defaultValue = defaultValue;
+    }
+
+	/**
+     * @return the active
+     */
+    public Boolean getActive() {
+    	return active;
+    }
+
+	/**
+     * @param active the active to set
+     */
+    public void setActive(Boolean active) {
+    	this.active = active;
+    }
+
+	/**
+     * @return the wlProgramId
+     */
+    public Integer getWlProgramId() {
+    	return wlProgramId;
+    }
+
+	/**
+     * @param wlProgramId the wlProgramId to set
+     */
+    public void setWlProgramId(Integer wlProgramId) {
+    	this.wlProgramId = wlProgramId;
+    }
+
+	/**
+     * @return the canBeAdhoc
+     */
+    public Boolean getCanBeAdhoc() {
+    	return canBeAdhoc;
+    }
+
+	/**
+     * @param canBeAdhoc the canBeAdhoc to set
+     */
+    public void setCanBeAdhoc(Boolean canBeAdhoc) {
+    	this.canBeAdhoc = canBeAdhoc;
+    }
+
+}
---
+++ src/main/java/org/oscarehr/PMmodule/model/CriteriaTypeOption.java
@@ -0,0 +1,181 @@
+/**
+ *
+ * Copyright (c) 2005-2012. Centre for Research on Inner City Health, St. Michael's Hospital, Toronto. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for
+ * Centre for Research on Inner City Health, St. Michael's Hospital,
+ * Toronto, Ontario, Canada
+ */
+package org.oscarehr.PMmodule.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.oscarehr.common.model.AbstractModel;
+
+/**
+ * CriteriaTypeOption entity. @author MyEclipse Persistence Tools
+ */
+@Entity
+@Table(name = "criteria_type_option")
+public class CriteriaTypeOption extends AbstractModel<Long> implements java.io.Serializable  {
+
+	// Fields
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "OPTION_ID", unique = true, nullable = false)
+	private Long optionId;
+	@Column(name = "CRITERIA_TYPE_ID", nullable = false)
+	private Integer criteriaTypeId;
+	@Column(name = "DISPLAY_ORDER_NUMBER", nullable = false)
+	private Integer displayOrderNumber;
+	@Column(name = "OPTION_LABEL", nullable = false, length = 128)
+	private String optionLabel;
+	@Column(name = "OPTION_VALUE")
+	private String optionValue;
+	@Column(name = "RANGE_START_VALUE")
+	private Integer rangeStartValue;
+	@Column(name = "RANGE_END_VALUE")
+	private Integer rangeEndValue;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return optionId;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.optionId = id;
+	}
+
+	/** default constructor */
+	public CriteriaTypeOption() {
+	}
+
+	/** minimal constructor */
+	public CriteriaTypeOption(Integer criteriaTypeId,
+			Integer displayOrderNumber, String optionLabel) {
+		this.criteriaTypeId = criteriaTypeId;
+		this.displayOrderNumber = displayOrderNumber;
+		this.optionLabel = optionLabel;
+	}
+
+	/** full constructor */
+	public CriteriaTypeOption(Integer criteriaTypeId,
+			Integer displayOrderNumber, String optionLabel, String optionValue,
+			Integer rangeStartValue, Integer rangeEndValue) {
+		this.criteriaTypeId = criteriaTypeId;
+		this.displayOrderNumber = displayOrderNumber;
+		this.optionLabel = optionLabel;
+		this.optionValue = optionValue;
+		this.rangeStartValue = rangeStartValue;
+		this.rangeEndValue = rangeEndValue;
+	}
+
+	/**
+     * @return the criteriaTypeId
+     */
+    public Integer getCriteriaTypeId() {
+    	return criteriaTypeId;
+    }
+
+	/**
+     * @param criteriaTypeId the criteriaTypeId to set
+     */
+    public void setCriteriaTypeId(Integer criteriaTypeId) {
+    	this.criteriaTypeId = criteriaTypeId;
+    }
+
+	/**
+     * @return the displayOrderNumber
+     */
+    public Integer getDisplayOrderNumber() {
+    	return displayOrderNumber;
+    }
+
+	/**
+     * @param displayOrderNumber the displayOrderNumber to set
+     */
+    public void setDisplayOrderNumber(Integer displayOrderNumber) {
+    	this.displayOrderNumber = displayOrderNumber;
+    }
+
+	/**
+     * @return the optionLabel
+     */
+    public String getOptionLabel() {
+    	return optionLabel;
+    }
+
+	/**
+     * @param optionLabel the optionLabel to set
+     */
+    public void setOptionLabel(String optionLabel) {
+    	this.optionLabel = optionLabel;
+    }
+
+	/**
+     * @return the optionValue
+     */
+    public String getOptionValue() {
+    	return optionValue;
+    }
+
+	/**
+     * @param optionValue the optionValue to set
+     */
+    public void setOptionValue(String optionValue) {
+    	this.optionValue = optionValue;
+    }
+
+	/**
+     * @return the rangeStartValue
+     */
+    public Integer getRangeStartValue() {
+    	return rangeStartValue;
+    }
+
+	/**
+     * @param rangeStartValue the rangeStartValue to set
+     */
+    public void setRangeStartValue(Integer rangeStartValue) {
+    	this.rangeStartValue = rangeStartValue;
+    }
+
+	/**
+     * @return the rangeEndValue
+     */
+    public Integer getRangeEndValue() {
+    	return rangeEndValue;
+    }
+
+	/**
+     * @param rangeEndValue the rangeEndValue to set
+     */
+    public void setRangeEndValue(Integer rangeEndValue) {
+    	this.rangeEndValue = rangeEndValue;
+    }
+
+}
