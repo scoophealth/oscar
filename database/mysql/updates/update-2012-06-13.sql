73d0caf6a2126f627a4815a03ba78921f9b42451
hexbinary <marc@mdumontier.com>
2012-06-14 00:55:19 -0400

    ID:3529194 - Creation of AR Enhanced form and XML generator
    -bug fixes
    -minor enhancements to form

---
+++ database/mysql/updates/update-2012-06-13.sql
@@ -0,0 +1,21 @@
+alter table formONAREnhanced add pg1_labCustom1Label varchar(40);
+alter table formONAREnhanced add pg1_labCustom1Result varchar(40);
+alter table formONAREnhanced add pg1_labCustom2Label varchar(40);
+alter table formONAREnhanced add pg1_labCustom2Result varchar(40);
+alter table formONAREnhanced add pg1_labCustom3Label varchar(40);
+alter table formONAREnhanced add pg1_labCustom3Result varchar(40);
+alter table formONAREnhanced add pg1_labCustom4Label varchar(40);
+alter table formONAREnhanced add pg1_labCustom4Result varchar(40);
+
+
+alter table formONAREnhanced add ar2_labCustom1Label varchar(40);
+alter table formONAREnhanced add ar2_labCustom1Result varchar(40);
+alter table formONAREnhanced add ar2_labCustom2Label varchar(40);
+alter table formONAREnhanced add ar2_labCustom2Result varchar(40);
+alter table formONAREnhanced add ar2_labCustom3Label varchar(40);
+alter table formONAREnhanced add ar2_labCustom3Result varchar(40);
+alter table formONAREnhanced add ar2_labCustom4Label varchar(40);
+alter table formONAREnhanced add ar2_labCustom4Result varchar(40);
+
+
+alter table formONAREnhanced change ar2_lab2GTT ar2_lab2GTT varchar(14) default NULL;
