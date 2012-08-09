5f80ff8a1a2cffa3544eec2f26d0037ebf6d8508
Jason Gallagher <jaygallagher@gmail.com>
2012-08-09 11:12:10 -0400

    ID: 3555732 Added integrated data to prevention reports
    Change-Id: Ice5e00bc42c4b58c672cd5cc3268aa0defae89d7

--- src/main/java/oscar/oscarPrevention/reports/FOBTReport.java
+++ src/main/java/oscar/oscarPrevention/reports/FOBTReport.java
@@ -70,7 +70,9 @@ public class FOBTReport implements PreventionReport{
 
              //search   prevention_date prevention_type  deleted   refused
              ArrayList<Map<String,Object>> prevs = PreventionData.getPreventionData("FOBT",demo);
+             PreventionData.addRemotePreventions(prevs, Integer.parseInt(demo),"FOBT",null);
              ArrayList<Map<String,Object>> colonoscopys = PreventionData.getPreventionData("COLONOSCOPY",demo);
+             PreventionData.addRemotePreventions(colonoscopys, Integer.parseInt(demo),"COLONOSCOPY",null);
              PreventionReportDisplay prd = new PreventionReportDisplay();
              prd.demographicNo = demo;
              prd.bonusStatus = "N";
