1d9cd76978b368a47c6784a77179e505c9f07624
hexbinary <marc@mdumontier.com>
2012-08-13 15:15:57 -0400

    added missing create table statements

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -8854,16 +8854,16 @@ CREATE TABLE `HRMSubClass` (
 
 CREATE TABLE `HRMDocumentComment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `providerNo` varchar(20),
-  `hrmDocumentId` int(11),
+  `providerNo` varchar(20) DEFAULT NULL,
+  `hrmDocumentId` int(11) DEFAULT NULL,
   `comment` text,
-  `commentTime` datetime,
-  `deleted` tinyint(1),
+  `commentTime` datetime DEFAULT NULL,
+  `deleted` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `HRMProviderConfidentialityStatement` (
-  `providerNo` varchar(20),
+  `providerNo` varchar(20) NOT NULL DEFAULT '',
   `statement` text,
   PRIMARY KEY (`providerNo`)
 );
