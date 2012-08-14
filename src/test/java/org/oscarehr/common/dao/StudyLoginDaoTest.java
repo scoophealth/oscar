ed12ba015ace61a009f076d8ec8ec1af665db434
ngoupinets <nick.goupinets@gmail.com>
2012-08-14 13:27:57 -0400

    Removed direct SQL code from the JSP page

--- src/test/java/org/oscarehr/common/dao/StudyLoginDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/StudyLoginDaoTest.java
@@ -4,7 +4,7 @@
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version. 
+ * of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -21,32 +21,30 @@
  * Hamilton
  * Ontario, Canada
  */
-
 package org.oscarehr.common.dao;
+
+import static junit.framework.Assert.assertNotNull;
 
 import java.util.List;
 
-import javax.persistence.Query;
-
+import org.junit.Before;
+import org.junit.Test;
+import org.oscarehr.common.dao.utils.SchemaUtils;
 import org.oscarehr.common.model.StudyLogin;
-import org.springframework.stereotype.Repository;
+import org.oscarehr.util.SpringUtils;
 
-@Repository
-public class StudyLoginDao extends AbstractDao<StudyLogin> {
+public class StudyLoginDaoTest extends DaoTestFixtures {
 
-	public StudyLoginDao() {
-		super(StudyLogin.class);
+	@Before
+	public void before() throws Exception {
+		SchemaUtils.restoreTable("studylogin");
 	}
 
-	public List<StudyLogin> find(String providerNo, String studyId) {
-		return find(providerNo, Integer.parseInt(studyId));
+	@Test
+	public void testFind() {
+		StudyLoginDao dao = SpringUtils.getBean(StudyLoginDao.class);
+		List<StudyLogin> studyLogins = dao.find("test", "1");
+		assertNotNull("Must return a result list", studyLogins);
 	}
-	
-	@SuppressWarnings("unchecked")
-	public List<StudyLogin> find(String providerNo, Integer studyId) {
-		Query query = entityManager.createQuery("FROM StudyLogin s WHERE s.providerNo = :providerNo and s.studyNo = :studyNo and s.current1 = 1");
-		query.setParameter("providerNo", providerNo);
-		query.setParameter("studyNo", studyId);
-		return query.getResultList();
-	}
+
 }
