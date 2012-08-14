a9566e36e8a4c55b78b35d136e747276089c7e67
ngoupinets <nick.goupinets@gmail.com>
2012-08-14 15:35:37 -0400

    Removed DBHandler access

--- src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
+++ src/main/java/org/oscarehr/olis/OLISAddToInboxAction.java
@@ -12,7 +12,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,20 +22,21 @@ import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
+import org.oscarehr.common.dao.PatientLabRoutingDao;
+import org.oscarehr.common.model.PatientLabRouting;
 import org.oscarehr.util.LoggedInInfo;
 import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
 
 import oscar.log.LogAction;
 import oscar.log.LogConst;
-import oscar.oscarDB.DBHandler;
 import oscar.oscarLab.FileUploadCheck;
 import oscar.oscarLab.ca.all.upload.HandlerClassFactory;
 import oscar.oscarLab.ca.all.upload.handlers.OLISHL7Handler;
 import oscar.oscarLab.ca.on.CommonLabResultData;
 
-
 public class OLISAddToInboxAction extends DispatchAction {
-	
+
 	static Logger logger = MiscUtils.getLogger();
 
 	@Override
@@ -45,45 +45,45 @@ public class OLISAddToInboxAction extends DispatchAction {
 		String uuidToAdd = request.getParameter("uuid");
 		String pFile = request.getParameter("file");
 		String pAck = request.getParameter("ack");
-		boolean doFile=false, doAck=false;
-		if(pFile != null && pFile.equals("true")) {
-			doFile=true;
+		boolean doFile = false, doAck = false;
+		if (pFile != null && pFile.equals("true")) {
+			doFile = true;
 		}
-		if(pAck != null && pAck.equals("true")) {
-			doAck=true;
+		if (pAck != null && pAck.equals("true")) {
+			doAck = true;
 		}
 
 		String fileLocation = System.getProperty("java.io.tmpdir") + "/olis_" + uuidToAdd + ".response";
 		File file = new File(fileLocation);
 		OLISHL7Handler msgHandler = (OLISHL7Handler) HandlerClassFactory.getHandler("OLIS_HL7");
-		
+
 		InputStream is = null;
 		try {
 			is = new FileInputStream(fileLocation);
-			String provNo =  LoggedInInfo.loggedInInfo.get().loggedInProvider.getProviderNo();
+			String provNo = LoggedInInfo.loggedInInfo.get().loggedInProvider.getProviderNo();
 			int check = FileUploadCheck.addFile(file.getName(), is, provNo);
-			
+
 			if (check != FileUploadCheck.UNSUCCESSFUL_SAVE) {
-				if (msgHandler.parse("OLIS_HL7",fileLocation, check, true) != null) {
+				if (msgHandler.parse("OLIS_HL7", fileLocation, check, true) != null) {
 					request.setAttribute("result", "Success");
-					if(doFile) {
+					if (doFile) {
 						ArrayList<String[]> labsToFile = new ArrayList<String[]>();
-						String item[] = new String[]{String.valueOf(msgHandler.getLastSegmentId()),"HL7"};
+						String item[] = new String[] { String.valueOf(msgHandler.getLastSegmentId()), "HL7" };
 						labsToFile.add(item);
 						CommonLabResultData.fileLabs(labsToFile, provNo);
 					}
-					if(doAck) {
-						 String demographicID = getDemographicIdFromLab("HL7", msgHandler.getLastSegmentId());				           
-						 LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.ACK, LogConst.CON_HL7_LAB, ""+msgHandler.getLastSegmentId(), request.getRemoteAddr(),demographicID);
-						 CommonLabResultData.updateReportStatus(msgHandler.getLastSegmentId(), provNo, 'A', "comment","HL7");
-				            
+					if (doAck) {
+						String demographicID = getDemographicIdFromLab("HL7", msgHandler.getLastSegmentId());
+						LogAction.addLog((String) request.getSession().getAttribute("user"), LogConst.ACK, LogConst.CON_HL7_LAB, "" + msgHandler.getLastSegmentId(), request.getRemoteAddr(), demographicID);
+						CommonLabResultData.updateReportStatus(msgHandler.getLastSegmentId(), provNo, 'A', "comment", "HL7");
+
 					}
 				} else {
 					request.setAttribute("result", "Error");
 				}
 			} else {
 				request.setAttribute("result", "Already Added");
-			}			
+			}
 
 		} catch (Exception e) {
 			MiscUtils.getLogger().error("Couldn't add requested OLIS lab to Inbox.", e);
@@ -91,30 +91,17 @@ public class OLISAddToInboxAction extends DispatchAction {
 		} finally {
 			try {
 				is.close();
-			} catch(IOException e){
+			} catch (IOException e) {
 				//ignore
 			}
 		}
 
 		return mapping.findForward("ajax");
 	}
-	
-    private static String getDemographicIdFromLab(String labType, int labNo)
-    {
-    	String demographicID="";
-        try{
-            String sql = "SELECT demographic_no FROM patientLabRouting WHERE lab_type = '"+labType+"' and lab_no='"+labNo+"'";
-            
-            ResultSet rs = DBHandler.GetSQL(sql);
 
-            while(rs.next()){
-                demographicID = oscar.Misc.getString(rs, "demographic_no");
-            }
-            rs.close();
-        }catch(Exception e){
-        	logger.error("Error", e);
-        }
-        
-        return(demographicID);
-    }
+	private static String getDemographicIdFromLab(String labType, int labNo) {
+		PatientLabRoutingDao dao = SpringUtils.getBean(PatientLabRoutingDao.class);
+		PatientLabRouting routing = dao.findDemographics(labType, labNo);
+		return routing == null ? "" : String.valueOf(routing.getDemographicNumber());
+	}
 }
