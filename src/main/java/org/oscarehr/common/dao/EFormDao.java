f24b69ca21ed1789bda1decaadb1361d778bcbd9
Nick Goupinets <nick.goupinets@gmail.com>
2012-08-23 12:25:15 -0400

    Removed some of the direct SQL code from EFormUtil and refactored it to use HashMap instead of Hashtable

--- src/main/java/org/oscarehr/common/dao/EFormDao.java
+++ src/main/java/org/oscarehr/common/dao/EFormDao.java
@@ -32,85 +32,166 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public class EFormDao extends AbstractDao<EForm> {
-	
+
+	public enum EFormSortOrder {
+		DATE, NAME, SUBJECT, FILE_NAME;
+	}
+
 	public EFormDao() {
 		super(EForm.class);
 	}
-	
-    public EForm findByName(String name){
-    	Query query = entityManager.createQuery("SELECT e from EForm e where e.formName = ? and e.current=?");
-    	query.setParameter(1, name);
-    	query.setParameter(2,true);
-    	
-        @SuppressWarnings("unchecked")
-        List<EForm> results = query.getResultList();
-        if(!results.isEmpty())
-        	return results.get(0);
-        return null;
-    }
+
+	public EForm findByName(String name) {
+		Query query = entityManager.createQuery("SELECT e from EForm e where e.formName = ? and e.current=?");
+		query.setParameter(1, name);
+		query.setParameter(2, true);
+
+		@SuppressWarnings("unchecked")
+		List<EForm> results = query.getResultList();
+		if (!results.isEmpty()) return results.get(0);
+		return null;
+	}
 
 	/**
 	 * @param current can be null for both
 	 * @return
 	 */
-    public List<EForm> findAll(Boolean current)
-	{
-    	StringBuilder sb=new StringBuilder();
-    	sb.append("select x from ");
-    	sb.append(modelClass.getSimpleName());
-    	sb.append(" x");
-    	
-    	if (current!=null)
-    	{
-    		sb.append(" where x.current=?1");
-    	}
+	public List<EForm> findAll(Boolean current) {
+		StringBuilder sb = new StringBuilder();
+		sb.append("select x from ");
+		sb.append(modelClass.getSimpleName());
+		sb.append(" x");
 
-    	Query query = entityManager.createQuery(sb.toString());
-    	if (current!=null)
-    	{
-    		query.setParameter(1, current);
-    	}
-		
-    	
+		if (current != null) {
+			sb.append(" where x.current=?1");
+		}
+
+		Query query = entityManager.createQuery(sb.toString());
+		if (current != null) {
+			query.setParameter(1, current);
+		}
+
 		@SuppressWarnings("unchecked")
 		List<EForm> results = query.getResultList();
 
-		return(results);
+		return (results);
 	}
-    
-    public boolean isIndivicaRTLEnabled() { 
 
-    	StringBuilder sb=new StringBuilder();
-    	sb.append("select x from ");
-    	sb.append(modelClass.getSimpleName());
-    	sb.append(" x where x.formName='Rich Text Letter' and x.subject='Rich Text Letter Generator'");
-       	Query query = entityManager.createQuery(sb.toString());
-    	
+	public boolean isIndivicaRTLEnabled() {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("select x from ");
+		sb.append(modelClass.getSimpleName());
+		sb.append(" x where x.formName='Rich Text Letter' and x.subject='Rich Text Letter Generator'");
+		Query query = entityManager.createQuery(sb.toString());
+
 		@SuppressWarnings("unchecked")
 		List<EForm> forms = query.getResultList();
-		
+
 		boolean enabled = false;
 		for (EForm form : forms) {
 			enabled |= form.isCurrent();
 		}
-		
-		return enabled;	
-    }
-    
-    public void setIndivicaRTLEnabled(boolean enabled) { 
 
-    	StringBuilder sb=new StringBuilder();
-    	sb.append("select x from ");
-    	sb.append(modelClass.getSimpleName());
-    	sb.append(" x where x.formName='Rich Text Letter' and x.subject='Rich Text Letter Generator'");
-       	Query query = entityManager.createQuery(sb.toString());
-    	
+		return enabled;
+	}
+
+	public void setIndivicaRTLEnabled(boolean enabled) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("select x from ");
+		sb.append(modelClass.getSimpleName());
+		sb.append(" x where x.formName='Rich Text Letter' and x.subject='Rich Text Letter Generator'");
+		Query query = entityManager.createQuery(sb.toString());
+
 		@SuppressWarnings("unchecked")
 		List<EForm> forms = query.getResultList();
 		for (EForm form : forms) {
-			form.setCurrent(enabled);			
+			form.setCurrent(enabled);
 			enabled |= form.isCurrent();
 			merge(form);
 		}
+	}
+
+	/**
+	 * Finds all eforms based on the status. 
+	 * 
+	 * @param status	
+	 * 		Status to be used when looking up forms. 
+	 * @return
+	 * 		Returns the list of all forms with the specified status.
+	 */
+	public List<EForm> findByStatus(boolean status) {
+		return findByStatus(status, null);
+	}
+	
+	/**
+	 * Finds all eforms based on the status. 
+	 * 
+	 * @param status	
+	 * 		Status to be used when looking up forms. 
+	 * @param sortOrder
+	 * 		Order how records should be sorted. Providing no sort order delegates to the default sorting order of the persistence provider 
+	 * @return
+	 * 		Returns the list of all forms with the specified status.
+	 */
+	@SuppressWarnings("unchecked")
+	public List<EForm> findByStatus(boolean status, EFormSortOrder sortOrder) {
+		StringBuilder buf = new StringBuilder("FROM " + modelClass.getSimpleName() + " ef WHERE ef.current = :current");
+		buf.append(getSortOrderClause(sortOrder));
+
+		Query query = entityManager.createQuery(buf.toString());
+		query.setParameter("current", status);
+
+		return query.getResultList();
+	}
+
+	private Object getSortOrderClause(EFormSortOrder sortOrder) {
+		if (sortOrder == null) return "";
+
+		switch (sortOrder) {
+		case DATE:
+			return " ORDER BY ef.formDate DESC, ef.formTime DESC";
+		case NAME:
+			return " ORDER BY ef.formName";
+		case FILE_NAME:
+			return " ORDER BY ef.fileName";
+		case SUBJECT:
+			return " ORDER BY ef.subject";
+		}
+
+		return "";
+	}
+
+	/**
+	 * Finds the largest identifier for the specified form name. 
+	 * 
+	 * @param formName
+	 * 		Form name to find the largest identifier for the form with the specified name and set as enabled (current)
+	 * @return
+	 * 		Returns the largest identifier found.
+	 */
+	public Integer findMaxIdForActiveForm(String formName) {
+		Query query = entityManager.createQuery("SELECT MAX(ef.id) FROM " + modelClass.getSimpleName() + " ef WHERE ef.formName = :formName AND ef.current = TRUE");
+		query.setParameter("formName", formName);
+		return (Integer) query.getSingleResult();
+	}
+
+	/**
+	 * Counts forms with the specified form name excluding the specified form ID.
+	 * 
+	 * @param formName
+	 * 		Form name to be counted
+	 * @param id
+	 * 		ID of the form to exclude from the count results
+	 * @return
+	 * 		Returns the number of all active forms with the forms with the specified ID 
+	 */
+	public Long countFormsOtherThanSpecified(String formName, Integer id) {
+	    // TODO test me
+		Query query = entityManager.createQuery("SELECT COUNT(ef) FROM " + modelClass.getSimpleName() + " ef WHERE ef.current = TRUE AND ef.formName = :formName AND ef.id != :id");
+	    query.setParameter("formName", formName);
+	    query.setParameter("id", id);
+		return (Long) query.getSingleResult();
     }
 }
---
+++ src/main/java/oscar/util/ConversionUtils.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package oscar.util;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Yet another conversion utility class for bridging JPA entity to legacy schema mismatch. 
+ *
+ */
+public class ConversionUtils {
+
+	public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
+	public static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
+
+	private ConversionUtils() {
+	}
+
+	public static Date fromTimeString(String timeString) {
+		return fromDateString(timeString, DEFAULT_TIME_PATTERN);
+	}
+
+	public static Date fromDateString(String dateString) {
+		return fromDateString(dateString, DEFAULT_TIME_PATTERN);
+	}
+
+	private static Date fromDateString(String dateString, String formatPattern) {
+		if (dateString == null || "".equals(dateString.trim())) return null;
+
+		SimpleDateFormat format = new SimpleDateFormat(formatPattern);
+		try {
+			return format.parse(dateString);
+		} catch (ParseException e) {
+			return null;
+		}
+	}
+
+	private static String toDateString(Date date, String formatPattern) {
+		if (date == null) return "";
+
+		SimpleDateFormat format = new SimpleDateFormat(formatPattern);
+		return format.format(date);
+	}
+
+	public static String toTimeString(Date time) {
+		return toDateString(time, DEFAULT_TIME_PATTERN);
+	}
+
+	public static String toDateString(Date date) {
+		return toDateString(date, DEFAULT_DATE_PATTERN);
+	}
+
+	public static Integer fromIntString(String intString) {
+		if (intString == null || "".equals(intString.trim())) return 0;
+		return Integer.parseInt(intString);
+	}
+
+	public static String toIntString(Integer integer) {
+		if (integer == null) return "0";
+		return integer.toString();
+	}
+	
+	public static String toBoolString(Boolean b) {
+		if (b == null || b == Boolean.FALSE)
+			return "0";
+		return "1";
+	}
+	
+	public static boolean fromBoolString(String str) {
+		if (str == null || "".equals(str.trim()) || "0".equals(str))
+			return false;
+		return true;
+	}
+
+}
