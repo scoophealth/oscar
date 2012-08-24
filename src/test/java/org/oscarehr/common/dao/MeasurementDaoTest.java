c0eff578911970a835f6a30ba870027d2b8aeaaa
Nick Goupinets <nick.goupinets@gmail.com>
2012-08-24 09:32:51 -0400

    Removed direct SQL from the FrmFormAction
    Change-Id: I85ff2c36c93e03556b4dec78d4c9d37b49a50d6e

---
+++ src/test/java/org/oscarehr/common/dao/MeasurementDaoTest.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.oscarehr.common.dao.MeasurementDao.SearchCriteria;
+import org.oscarehr.common.dao.utils.SchemaUtils;
+import org.oscarehr.common.model.Measurement;
+import org.oscarehr.util.SpringUtils;
+
+public class MeasurementDaoTest extends DaoTestFixtures {
+
+	private MeasurementDao dao = SpringUtils.getBean(MeasurementDao.class);
+
+	@Before
+	public void before() throws Exception {
+		SchemaUtils.restoreTable("measurements");
+	}
+
+	@Test
+	public void testFind() throws Exception {
+		Measurement m = new Measurement();
+		m.setDemographicId(999);
+		m.setAppointmentNo(100);
+		m.setComments("NUIOBLAHA");
+		m.setDataField("DTATAHEROVATA");
+		m.setDateObserved(new Date());
+		m.setMeasuringInstruction("MSRNIGINSRCTIONS");
+		m.setProviderNo("PRVDRE");
+		m.setType("TIPPITIP");
+		dao.persist(m);
+		
+		SearchCriteria c = new SearchCriteria();
+		c.setComments(m.getComments());
+		c.setDataField(m.getDataField());
+		c.setDateObserved(m.getDateObserved());
+		c.setDemographicNo(m.getDemographicId());
+		c.setMeasuringInstrc(m.getMeasuringInstruction());
+		c.setType(m.getType());
+		
+		List<Measurement> ms = dao.find(c);
+		assertEquals(1, ms.size());
+	}
+}
