62bced5aeadf34ae6f52b60c37cf4f0aa6e32df7
Nick Goupinets <nick.goupinets@gmail.com>
2012-08-24 12:05:06 -0400

    Removed DBHandler reference from ClinicData

--- src/main/java/oscar/oscarClinic/ClinicData.java
+++ src/main/java/oscar/oscarClinic/ClinicData.java
@@ -22,7 +22,6 @@
  * Ontario, Canada
  */
 
-
 /*
  * ClinicData.java
  *
@@ -30,165 +29,163 @@
  */
 
 package oscar.oscarClinic;
-import java.sql.SQLException;
 
-import org.oscarehr.util.MiscUtils;
+import org.oscarehr.common.dao.ClinicDAO;
+import org.oscarehr.common.model.Clinic;
+import org.oscarehr.util.SpringUtils;
 
-import oscar.oscarDB.DBHandler;
 /**
  *
  * @author  Oscar
  */
 public class ClinicData {
-    /**
-    +----------------------+-------------+------+-----+---------+----------------+
-    | Field                | Type        | Null | Key | Default | Extra          |
-    +----------------------+-------------+------+-----+---------+----------------+
-    | clinic_no            | int(10)     |      | PRI | NULL    | auto_increment |
-    | clinic_name          | varchar(50) | YES  |     | NULL    |                |
-    | clinic_address       | varchar(60) | YES  |     |         |                |
-    | clinic_city          | varchar(40) | YES  |     |         |                |
-    | clinic_postal        | varchar(15) | YES  |     |         |                |
-    | clinic_phone         | varchar(50) | YES  |     | NULL    |                |
-    | clinic_fax           | varchar(20) | YES  |     |         |                |
-    | clinic_location_code | varchar(10) | YES  |     | NULL    |                |
-    | status               | char(1)     | YES  |     | NULL    |                |
-    +----------------------+-------------+------+-----+---------+----------------+
+	/**
+	+----------------------+-------------+------+-----+---------+----------------+
+	| Field                | Type        | Null | Key | Default | Extra          |
+	+----------------------+-------------+------+-----+---------+----------------+
+	| clinic_no            | int(10)     |      | PRI | NULL    | auto_increment |
+	| clinic_name          | varchar(50) | YES  |     | NULL    |                |
+	| clinic_address       | varchar(60) | YES  |     |         |                |
+	| clinic_city          | varchar(40) | YES  |     |         |                |
+	| clinic_postal        | varchar(15) | YES  |     |         |                |
+	| clinic_phone         | varchar(50) | YES  |     | NULL    |                |
+	| clinic_fax           | varchar(20) | YES  |     |         |                |
+	| clinic_location_code | varchar(10) | YES  |     | NULL    |                |
+	| status               | char(1)     | YES  |     | NULL    |                |
+	+----------------------+-------------+------+-----+---------+----------------+
 
-     
-   
-    */
-    /** Creates a new instance of clinicData */
-    
-    
-    String clinic_no = null;
-    String clinic_name = null;          
-    String clinic_address = null;
-    String clinic_city = null;       
-    String clinic_postal = null;
-    String clinic_phone = null;         
-    String clinic_fax = null;           
-    String clinic_location_code = null;
-    
-    String clinic_province = null;
-    String clinic_delim_phone = null;
-    String clinic_delim_fax = null;
-    
-    boolean filled = false;
-    public ClinicData() {
-    }
-    
-    void fillClinicData(){
-        if (!filled){
-            try{
-               
-               java.sql.ResultSet rs;
-               String sql = "select * from clinic ";
-               rs = DBHandler.GetSQL(sql);
-                
-               if(rs.next()){
-                 clinic_no              = oscar.Misc.getString(rs, "clinic_no"); 
-                 clinic_name            = oscar.Misc.getString(rs, "clinic_name");  
-                 clinic_address         = oscar.Misc.getString(rs, "clinic_address"); 
-                 clinic_city            = oscar.Misc.getString(rs, "clinic_city"); 
-                 clinic_postal          = oscar.Misc.getString(rs, "clinic_postal"); 
-                 clinic_phone           = oscar.Misc.getString(rs, "clinic_phone"); 
-                 clinic_fax             = oscar.Misc.getString(rs, "clinic_fax"); 
-                 clinic_location_code   = oscar.Misc.getString(rs, "clinic_location_code"); 
-                 clinic_province        = oscar.Misc.getString(rs, "clinic_province"); 
-                 clinic_delim_phone     = oscar.Misc.getString(rs, "clinic_delim_phone"); 
-                 clinic_delim_fax       = oscar.Misc.getString(rs, "clinic_delim_fax"); 
-                  
-               }
-            }
-            catch(SQLException e)
-            {
-                MiscUtils.getLogger().error("Error", e);
-            } 
-            filled = true;
-        }
-    }
-    
-    public String getClinicNo() {
-		fillClinicData();
-		return clinic_no;    
+	 
+	
+	*/
+	/** Creates a new instance of clinicData */
+
+	String clinic_no = null;
+	String clinic_name = null;
+	String clinic_address = null;
+	String clinic_city = null;
+	String clinic_postal = null;
+	String clinic_phone = null;
+	String clinic_fax = null;
+	String clinic_location_code = null;
+
+	String clinic_province = null;
+	String clinic_delim_phone = null;
+	String clinic_delim_fax = null;
+
+	boolean filled = false;
+
+	public ClinicData() {
 	}
-    
-    public String getClinicName(){
-       fillClinicData();
-       return clinic_name;        
-    }    
-    public String getClinicAddress(){
-       fillClinicData();
-       return clinic_address;
-    }
-    public String getClinicCity(){
-       fillClinicData();
-       return clinic_city;
-    }
-    public String getClinicProvince(){
-       fillClinicData();
-       return clinic_province;
-    }
-    public String getClinicPostal(){
-      fillClinicData();
-       return  clinic_postal;
-    }
-    public String getClinicPhone(){
-       fillClinicData();
-       return clinic_phone;
-    }
-    public String getClinicFax(){
-       fillClinicData();
-       return clinic_fax;
-    }
-    public String getClinicLocationCode(){
-       fillClinicData();
-       return clinic_location_code;
-    }
-    
-    public String getClinicDelimPhone(){
-       fillClinicData();
-       return  clinic_delim_phone;       
-    }
-    
-    public String getClinicDelimFax(){
-       fillClinicData();
-       return  clinic_delim_fax;       
-    }
-    
-    public void refreshClinicData(){
-       filled = false;
-       fillClinicData();
-    }
 
-    public void setClinic_address(String clinic_address) {
-        this.clinic_address = clinic_address;
-    }
+	void fillClinicData() {
+		if (!filled) {
+			ClinicDAO dao = SpringUtils.getBean(ClinicDAO.class);
+			Clinic clinic = dao.getClinic();
 
-    public void setClinic_city(String clinic_city) {
-        this.clinic_city = clinic_city;
-    }
+			if (clinic != null) {
+				clinic_no = clinic.getId().toString();
+				clinic_name = clinic.getClinicName();
+				clinic_address = clinic.getClinicAddress();
+				clinic_city = clinic.getClinicCity();
+				clinic_postal = clinic.getClinicPostal();
+				clinic_phone = clinic.getClinicPhone();
+				clinic_fax = clinic.getClinicFax();
+				clinic_location_code = clinic.getClinicLocationCode();
+				clinic_province = clinic.getClinicProvince();
+				clinic_delim_phone = clinic.getClinicDelimPhone();
+				clinic_delim_fax = clinic.getClinicDelimFax();
+			}
 
-    public void setClinic_fax(String clinic_fax) {
-        this.clinic_fax = clinic_fax;
-    }
+			filled = true;
+		}
+	}
 
-    public void setClinic_name(String clinic_name) {
-        this.clinic_name = clinic_name;
-    }
+	public String getClinicNo() {
+		fillClinicData();
+		return clinic_no;
+	}
 
-    public void setClinic_phone(String clinic_phone) {
-        this.clinic_phone = clinic_phone;
-    }
+	public String getClinicName() {
+		fillClinicData();
+		return clinic_name;
+	}
 
-    public void setClinic_postal(String clinic_postal) {
-        this.clinic_postal = clinic_postal;
-    }
+	public String getClinicAddress() {
+		fillClinicData();
+		return clinic_address;
+	}
 
-    public void setClinic_province(String clinic_province) {
-        this.clinic_province = clinic_province;
-    }
-    
-    
+	public String getClinicCity() {
+		fillClinicData();
+		return clinic_city;
+	}
+
+	public String getClinicProvince() {
+		fillClinicData();
+		return clinic_province;
+	}
+
+	public String getClinicPostal() {
+		fillClinicData();
+		return clinic_postal;
+	}
+
+	public String getClinicPhone() {
+		fillClinicData();
+		return clinic_phone;
+	}
+
+	public String getClinicFax() {
+		fillClinicData();
+		return clinic_fax;
+	}
+
+	public String getClinicLocationCode() {
+		fillClinicData();
+		return clinic_location_code;
+	}
+
+	public String getClinicDelimPhone() {
+		fillClinicData();
+		return clinic_delim_phone;
+	}
+
+	public String getClinicDelimFax() {
+		fillClinicData();
+		return clinic_delim_fax;
+	}
+
+	public void refreshClinicData() {
+		filled = false;
+		fillClinicData();
+	}
+
+	public void setClinic_address(String clinic_address) {
+		this.clinic_address = clinic_address;
+	}
+
+	public void setClinic_city(String clinic_city) {
+		this.clinic_city = clinic_city;
+	}
+
+	public void setClinic_fax(String clinic_fax) {
+		this.clinic_fax = clinic_fax;
+	}
+
+	public void setClinic_name(String clinic_name) {
+		this.clinic_name = clinic_name;
+	}
+
+	public void setClinic_phone(String clinic_phone) {
+		this.clinic_phone = clinic_phone;
+	}
+
+	public void setClinic_postal(String clinic_postal) {
+		this.clinic_postal = clinic_postal;
+	}
+
+	public void setClinic_province(String clinic_province) {
+		this.clinic_province = clinic_province;
+	}
+
 }
