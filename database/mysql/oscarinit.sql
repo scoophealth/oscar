fbfb053e6e7c4de056c24cb5e07e48ad212a0382
Kim Hoffman <khoffman@uwo.ca>
2012-09-19 10:27:41 -0400

    ID 3555708: Existing Forms incorporated into OSCARTrunk for next release from kimleanhoffman
    Change-Id: I9fc9f32078fbcab55459f20c942b8de719b6c894

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -10976,3 +10976,36 @@ create table billing_on_payment (
     billing_no int(6) NOT NULL, 
     pay_date timestamp NOT NULL
 );
+
+--
+-- Table structure for table `formCounseling`
+--
+CREATE TABLE `formCounseling` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `provider_no` int(10),    
+  `doc_name` varchar(60),
+  `cl_name` varchar(60),
+  `cl_address1` varchar(170),
+  `cl_address2` varchar(170),
+  `cl_phone` varchar(16),
+  `cl_fax` varchar(16),
+  `billingreferral_no` int(10),
+  `t_name` varchar(60),
+  `t_name2` varchar(60),
+  `t_address1` varchar(170),
+  `t_address2` varchar(170),
+  `t_phone` varchar(16),
+  `t_fax` varchar(16),
+  `demographic_no` int(10) NOT NULL,  
+  `p_name` varchar(60),
+  `p_address1` varchar(170),
+  `p_address2` varchar(170),
+  `p_phone` varchar(16),
+  `p_birthdate` varchar(30),
+  `p_healthcard` varchar(20),  
+  `comments` text,
+  `formCreated` date,
+  `formEdited` timestamp,
+  `consultTime` date,  
+  PRIMARY KEY (`ID`)
+);
---
+++ src/main/java/oscar/form/FrmCounselingRecord.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package oscar.form;
+
+import java.sql.SQLException;
+import java.util.Properties;
+import org.oscarehr.common.dao.ClinicDAO;
+import org.oscarehr.common.dao.DemographicDao;
+import org.oscarehr.common.model.Clinic;
+import org.oscarehr.common.model.Demographic;
+import org.oscarehr.util.SpringUtils;
+import oscar.util.UtilDateUtilities;
+/**
+ *
+ * @author kimleanhoffman
+ */
+
+public class FrmCounselingRecord extends FrmRecord{
+    private DemographicDao demographicDao = (DemographicDao)SpringUtils.getBean("demographicDao");
+
+    @Override
+    public Properties getFormRecord(int demographicNo, int existingID) throws SQLException{
+        Properties props = new Properties();
+        Demographic demographic = demographicDao.getDemographicById(demographicNo);
+
+        if(existingID <= 0 && demographic != null) {
+            props.setProperty("demographic_no", String.valueOf(demographicNo));
+            props.setProperty("formCreated", UtilDateUtilities.DateToString(UtilDateUtilities.Today(),"dd/MM/yyyy"));
+        } else {
+            String sql = "SELECT * FROM formCounseling WHERE demographic_no = " +demographicNo +" AND ID = " +existingID;
+            FrmRecordHelp frmRec = new FrmRecordHelp();
+            frmRec.setDateFormat("dd/MM/yyyy");
+            props = frmRec.getFormRecord(sql);
+        }
+
+        //get local clinic information
+        ClinicDAO clinicDao = (ClinicDAO)SpringUtils.getBean("clinicDAO");
+        Clinic clinic = clinicDao.getClinic();
+
+            props.setProperty("clinicName",clinic.getClinicName()==null?"":clinic.getClinicName());
+            props.setProperty("clinicProvince",clinic.getClinicProvince()==null?"":clinic.getClinicProvince());
+            props.setProperty("clinicAddress", clinic.getClinicAddress()==null?"":clinic.getClinicAddress());
+            props.setProperty("clinicCity", clinic.getClinicCity()==null?"":clinic.getClinicCity());
+            props.setProperty("clinicPhone", clinic.getClinicPhone()==null?"":clinic.getClinicPhone());
+            props.setProperty("clinicFax", clinic.getClinicFax()==null?"":clinic.getClinicFax());  
+                
+        return props;
+    }
+
+    @Override
+    public int saveFormRecord(Properties props) throws SQLException {
+        String demographicNo = props.getProperty("demographic_no");
+        String sql = "SELECT * FROM formCounseling WHERE demographic_no=" +demographicNo +" AND ID=0";
+        FrmRecordHelp frmRec = new FrmRecordHelp();
+        frmRec.setDateFormat("dd/MM/yyyy");
+
+        return frmRec.saveFormRecord(props, sql);
+    }
+
+    @Override
+    public String findActionValue(String submit) throws SQLException {
+ 		return ((new FrmRecordHelp()).findActionValue(submit));
+    }
+
+    @Override
+    public String createActionURL(String where, String action, String demoId, String formId) throws SQLException {
+ 		return ((new FrmRecordHelp()).createActionURL(where, action, demoId, formId));
+    }
+
+}
