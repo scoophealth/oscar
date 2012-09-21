11e4fc5031d099960f205fa1a29ca10e9d163884
Ted Leung <r6squeegee@gmail.com>
2012-09-21 12:05:43 +1000

    added versions to snapshots
    Change-Id: I991f5f96bf65c8947ca8e6d510eeb288e1d54f5a

--- utils/ws/build_client_stubs.sh
+++ utils/ws/build_client_stubs.sh
@@ -11,11 +11,11 @@ done
 
 ant -f build_client_stubs.xml compile_client_stubs
 
-rm -f ~/.m2/repository/org/oscarehr/client_stubs/SNAPSHOT/*.jar
+rm -f ~/.m2/repository/org/oscarehr/client_stubs/0.0-SNAPSHOT/*.jar
 
 PROJECTS="oscar_patient_portal"
 
 for foo in $PROJECTS;
 do
-   mvn install:install-file -DgroupId=org.oscarehr.oscar -DartifactId=oscar_client_stubs -Dversion=SNAPSHOT -Dpackaging=jar -Dfile=../../target/oscar_client_stubs.jar -DcreateChecksum=true -DgeneratePom=true -DlocalRepositoryPath=../../../${foo}/local_repo -DlocalRepositoryId=local_repo
+   mvn install:install-file -DgroupId=org.oscarehr.oscar -DartifactId=oscar_client_stubs -Dversion=0.0-SNAPSHOT -Dpackaging=jar -Dfile=../../target/oscar_client_stubs.jar -DcreateChecksum=true -DgeneratePom=true -DlocalRepositoryPath=../../../${foo}/local_repo -DlocalRepositoryId=local_repo
 done;
