3a432d0cabfd6cae81019655f8d88c7dec547a36
Kim Hoffman <khoffman@uwo.ca>
2012-09-28 09:24:14 -0400

    Western Form No Show Policy
    Change-Id: I03fdd72c3bedfd9c089baa6073ef607f7e3e0cb6
    
    (ID: 3555708) kimleanhoffman Western Form No Show Policy
    
    Change-Id: I6b66632997c74adeb36744fd1a3b201c1b2df3d4

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -11010,3 +11010,16 @@ CREATE TABLE `formCounseling` (
   `consultTime` date,  
   PRIMARY KEY (`ID`)
 );
+
+--
+-- Table structure for table `formNoShowPolicy`
+--
+CREATE TABLE `formNoShowPolicy` (
+  `ID` int(10) NOT NULL AUTO_INCREMENT,
+  `provider_no` int(10),
+  `demographic_no` int(10) NOT NULL,
+  `formCreated` date,
+  `formEdited` timestamp,
+  `formVersion` varchar(10),
+  PRIMARY KEY (`ID`)
+);
