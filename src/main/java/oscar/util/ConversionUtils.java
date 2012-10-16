31e9be7e27f2628f9e46dab317ef81eb027af86d
Nick Goupinets <nick.goupinets@gmail.com>
2012-10-16 16:59:01 -0400

    Removed DBHandler refs from MsgDisplay* classes
    Change-Id: I8eb61e974bfa064e4a2ad21002bb8604b0aba8ac

--- src/main/java/oscar/util/ConversionUtils.java
+++ src/main/java/oscar/util/ConversionUtils.java
@@ -31,27 +31,46 @@ import java.util.List;
 
 /**
  * Yet another conversion utility class for bridging JPA entity to legacy schema mismatch. 
- *
  */
 public class ConversionUtils {
 
 	public static final String DEFAULT_DATE_PATTERN = "yyyy-MM-dd";
 	public static final String DEFAULT_TIME_PATTERN = "HH:mm:ss";
 
+	private static final Long ZERO_LONG = new Long(0);
+	private static final Integer ZERO_INT = new Integer(0);
+	private static final String ZERO_STRING = "0";
+
 	private ConversionUtils() {
 	}
 
 	public static List<Integer> toIntList(List<String> list) {
 		List<Integer> result = new ArrayList<Integer>();
-		for(String str : list)
+		for (String str : list)
 			result.add(fromIntString(str));
 		return result;
 	}
-	
+
+	/**
+	 * Converts the provided string representing time into a date object. Time must match the {@link #DEFAULT_TIME_PATTERN}.
+	 * 
+	 * @param timeString
+	 * 		Time string to be parsed
+	 * @return
+	 * 		Returns the parsed string
+	 */
 	public static Date fromTimeString(String timeString) {
 		return fromDateString(timeString, DEFAULT_TIME_PATTERN);
 	}
 
+	/**
+	 * Converts the provided date representing time into a date object. Time must match the {@link #DEFAULT_DATE_PATTERN}.
+	 * 
+	 * @param dateString
+	 * 		Date string to be parsed
+	 * @return
+	 * 		Returns the parsed string
+	 */
 	public static Date fromDateString(String dateString) {
 		return fromDateString(dateString, DEFAULT_DATE_PATTERN);
 	}
@@ -74,33 +93,92 @@ public class ConversionUtils {
 		return format.format(date);
 	}
 
+	/**
+	 * Formats the date instance into a string keeping only the time of the day and excluding the remaining info.   
+	 * 
+	 * @param time
+	 * 		Date to be formatted using {@link #DEFAULT_TIME_PATTERN}
+	 * @return
+	 * 		Returns the formatted string
+	 */
 	public static String toTimeString(Date time) {
 		return toDateString(time, DEFAULT_TIME_PATTERN);
 	}
 
+	/**
+	 * Formats the date instance into a string keeping only the date.   
+	 * 
+	 * @param time
+	 * 		Date to be formatted using {@link #DEFAULT_DATE_PATTERN}
+	 * @return
+	 * 		Returns the formatted string
+	 */
 	public static String toDateString(Date date) {
 		return toDateString(date, DEFAULT_DATE_PATTERN);
 	}
 
+	/**
+	 * Parses the specified string as a Long instance. 
+	 * 
+	 * @param longString
+	 * 		String to be parsed as long
+	 * @return
+	 * 		Returns the parsed long
+	 */
+	public static Long fromLongString(String longString) {
+		if (longString == null || longString.trim().isEmpty()) return ZERO_LONG;
+		return Long.parseLong(longString);
+	}
+
+	/**
+	 * Parses the specified string as an Integer instance. 
+	 * 
+	 * @param intString
+	 * 		String to be parsed as integer
+	 * @return
+	 * 		Returns the parsed integer
+	 */
 	public static Integer fromIntString(String intString) {
-		if (intString == null || "".equals(intString.trim())) return 0;
+		if (intString == null || intString.trim().isEmpty()) return ZERO_INT;
 		return Integer.parseInt(intString);
 	}
 
+	/**
+	 * Formats the specified integer as string 
+	 * 
+	 * @param integer
+	 * 		Integer to format as a string.
+	 * @return
+	 * 		Returns the formatted string, or {@value #ZERO_STRING} for null paramter value.
+	 */
 	public static String toIntString(Integer integer) {
-		if (integer == null) return "0";
+		if (integer == null) return ZERO_STRING;
 		return integer.toString();
 	}
-	
+
+	/**
+	 * Formats the specified boolean as string 
+	 * 
+	 * @param b
+	 * 		Boolean to format as a string.
+	 * @return
+	 * 		Returns {@value #ZERO_STRING} for false or null instance or "1" otherwise.
+	 */
 	public static String toBoolString(Boolean b) {
-		if (b == null || b == Boolean.FALSE)
-			return "0";
+		if (b == null || b == Boolean.FALSE) return ZERO_STRING;
 		return "1";
 	}
-	
+
+	/**
+	 * Parses the specified string as boolean.
+	 * 
+	 * @param str
+	 * 		String to be parsed
+	 * @return
+	 * 		Returns false for empty, null or {@value #ZERO_STRING} or true otherwise. 
+	 */
 	public static boolean fromBoolString(String str) {
-		if (str == null || "".equals(str.trim()) || "0".equals(str))
-			return false;
+		if (str == null || str.trim().isEmpty() || ZERO_STRING.equals(str)) return false;
 		return true;
 	}
 
