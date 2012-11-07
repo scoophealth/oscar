182f7374da5111182b7b1740776e254c26e82bd9
Nick Goupinets <nick.goupinets@gmail.com>
2012-11-07 14:03:41 -0500

    Removed DBhandler refs from BillingData
    Change-Id: Iba7901e044254af62a63bcb9e59274dd95947ef2

--- src/main/java/oscar/oscarBilling/ca/on/data/BillingData.java
+++ src/main/java/oscar/oscarBilling/ca/on/data/BillingData.java
@@ -22,17 +22,16 @@
  * Ontario, Canada
  */
 
-
 package oscar.oscarBilling.ca.on.data;
 
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Hashtable;
 
-import org.apache.commons.lang.StringEscapeUtils;
-import org.oscarehr.util.MiscUtils;
+import org.oscarehr.common.dao.BillingDao;
+import org.oscarehr.common.model.Billing;
+import org.oscarehr.util.SpringUtils;
 
-import oscar.oscarDB.DBHandler;
+import oscar.util.ConversionUtils;
 
 /**
  *
@@ -44,59 +43,20 @@ public class BillingData {
 	public BillingData() {
 	}
 
-	public ArrayList<Hashtable<String,Object>> getBills(String statusType, String providerNo, String startDate, String endDate, String demoNo) {
-		ArrayList<Hashtable<String,Object>> list = new ArrayList<Hashtable<String,Object>>();
-
-		String providerQuery = "";
-		String startDateQuery = "";
-		String endDateQuery = "";
-		String demoQuery = "";
-
-		if (providerNo != null && !providerNo.trim().equalsIgnoreCase("all")) {
-			providerQuery = " and apptProvider_no = '" + providerNo + "'";
-		}
-
-		if (startDate != null && !startDate.trim().equalsIgnoreCase("")) {
-			// startDateQuery = " and ( to_days(billing_date) >= to_days('" +
-			// startDate +"')) ";
-			startDateQuery = " and ( billing_date >= '" + startDate + "') ";
-		}
-
-		if (endDate != null && !endDate.trim().equalsIgnoreCase("")) {
-			// endDateQuery = " and ( to_days(billing_date) <= to_days('" +
-			// endDate +"')) ";
-			endDateQuery = " and ( billing_date <= '" + endDate + "') ";
-		}
-		if (demoNo != null && !demoNo.trim().equalsIgnoreCase("")) {
-			demoQuery = " and demographic_no = '" + demoNo + "' ";
-		}
-
-		String p = "select billing_no,demographic_no,status,provider_no,demographic_name,billing_date,billing_time,total from billing where  status like '"
-				+ StringEscapeUtils.escapeSql(statusType)
-				+ "' "
-				+ providerQuery
-				+ startDateQuery
-				+ endDateQuery
-				+ demoQuery;
-		MiscUtils.getLogger().debug("bill status query " + p);
-		try {
-
-			ResultSet rs = DBHandler.GetSQL(p);
-			while (rs.next()) {
-				Hashtable<String,Object> h = new Hashtable<String,Object>();
-				h.put("billing_no", "" + rs.getInt("billing_no"));
-				h.put("demographic_no", rs.getString("demographic_no"));
-				h.put("status", rs.getString("status"));
-				h.put("provider_no", rs.getString("provider_no"));
-				h.put("demographic_name", rs.getString("demographic_name"));
-				h.put("billing_date", rs.getString("billing_date"));
-				h.put("billing_time", rs.getString("billing_time"));
-				h.put("total", rs.getString("total"));
-				list.add(h);
-			}
-			rs.close();
-		} catch (Exception e) {
-			MiscUtils.getLogger().error("Error", e);
+	public ArrayList<Hashtable<String, Object>> getBills(String statusType, String providerNo, String startDate, String endDate, String demoNo) {
+		ArrayList<Hashtable<String, Object>> list = new ArrayList<Hashtable<String, Object>>();
+		BillingDao dao = SpringUtils.getBean(BillingDao.class);		
+		for (Billing b : dao.findBillings(ConversionUtils.fromIntString(demoNo), statusType, providerNo, ConversionUtils.fromDateString(startDate), ConversionUtils.fromDateString(endDate))) {
+			Hashtable<String, Object> h = new Hashtable<String, Object>();
+			h.put("billing_no", b.getId());
+			h.put("demographic_no", ConversionUtils.toIntString(b.getDemographicNo()));
+			h.put("status", b.getStatus());
+			h.put("provider_no", b.getProviderNo());
+			h.put("demographic_name", b.getDemographicName());
+			h.put("billing_date", ConversionUtils.toDateString(b.getBillingDate()));
+			h.put("billing_time", ConversionUtils.toTimeString(b.getBillingTime()));
+			h.put("total", b.getTotal());
+			list.add(h);
 		}
 		return list;
 	}
