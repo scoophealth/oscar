43f4a4cd936399b4a0871b3462e594f8dd12e5f1
Marc Dumontier <marc@mdumontier.com>
2012-11-11 18:39:59 -0500

    Refactored some hibernate to JPA

---
+++ src/main/java/org/oscarehr/common/model/FavoritesPrivilege.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="favoritesprivilege")
+public class FavoritesPrivilege extends AbstractModel<Integer> {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="provider_no")
+	private String providerNo;
+	
+	@Column(name="opentopublic")
+	private boolean openToPublic;
+	
+	private boolean writeable;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getProviderNo() {
+		return providerNo;
+	}
+
+	public void setProviderNo(String providerNo) {
+		this.providerNo = providerNo;
+	}
+
+	public boolean isOpenToPublic() {
+		return openToPublic;
+	}
+
+	public void setOpenToPublic(boolean openToPublic) {
+		this.openToPublic = openToPublic;
+	}
+
+	public boolean isWriteable() {
+		return writeable;
+	}
+
+	public void setWriteable(boolean writeable) {
+		this.writeable = writeable;
+	}
+	
+	
+}
--- src/main/java/oscar/oscarRx/web/CopyFavoritesAction.java
+++ src/main/java/oscar/oscarRx/web/CopyFavoritesAction.java
@@ -24,22 +24,18 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 import org.apache.struts.validator.LazyValidatorForm;
+import org.oscarehr.common.dao.FavoritesDao;
+import org.oscarehr.common.dao.FavoritesPrivilegeDao;
+import org.oscarehr.common.model.Favorites;
 import org.oscarehr.util.MiscUtils;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
-import oscar.oscarRx.service.RxPrescriptionMgr;
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+import org.oscarehr.util.SpringUtils;
 
 /**
  *
@@ -48,21 +44,23 @@ import oscar.oscarRx.service.RxPrescriptionMgr;
 public class CopyFavoritesAction extends DispatchAction{
 
     private static final Logger logger = MiscUtils.getLogger();
+    FavoritesPrivilegeDao favoritesPrivilegeDao = SpringUtils.getBean(FavoritesPrivilegeDao.class);
+    FavoritesDao favoritesDao = SpringUtils.getBean(FavoritesDao.class);
+    
 
     public ActionForward update(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        logger.warn("copyFavorites-update");
+        logger.debug("copyFavorites-update");
         
-        RxPrescriptionMgr rxMgr = getRxMgr();
         LazyValidatorForm lazyForm = (LazyValidatorForm) form;
         String providerNo = lazyForm.get("userProviderNo").toString();
         int share = Integer.parseInt(lazyForm.get("rb_share").toString());
-        rxMgr.setFavoritesPrivilege(providerNo, share==0?false:true, false);
+        favoritesPrivilegeDao.setFavoritesPrivilege(providerNo, share==0?false:true, false);
 
         return mapping.findForward("success");
     }
 
     public ActionForward refresh(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        logger.warn("copyFavorites-refresh");
+        logger.debug("copyFavorites-refresh");
 
         LazyValidatorForm lazyForm = (LazyValidatorForm) form;
         String providerNo = lazyForm.get("ddl_provider").toString();
@@ -72,7 +70,7 @@ public class CopyFavoritesAction extends DispatchAction{
     }
 
     public ActionForward copy(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response) {
-        logger.warn("copyFavorites-copy");
+        logger.debug("copyFavorites-copy");
 
         LazyValidatorForm lazyForm = (LazyValidatorForm) form;
         String providerNo = lazyForm.get("userProviderNo").toString();
@@ -80,7 +78,7 @@ public class CopyFavoritesAction extends DispatchAction{
             return mapping.findForward("success");
 
         int count = Integer.parseInt(lazyForm.get("countFavorites").toString());
-        List favIDs = new ArrayList<Integer>();
+        List<Integer> favIDs = new ArrayList<Integer>();
         for (int i=0;i<count;i++){
             String search = "selected"+i;
             if (lazyForm.get(search)!=null){
@@ -88,16 +86,21 @@ public class CopyFavoritesAction extends DispatchAction{
                 favIDs.add(id);
             }
         }
-        RxPrescriptionMgr rxMgr = getRxMgr();
-        rxMgr.setFavoritesForProvider(providerNo, favIDs);
+       
+        for(Integer id:favIDs) {
+        	Favorites f = favoritesDao.find(id);
+        	Favorites copy = new Favorites();
+        	try {
+	        	BeanUtils.copyProperties(copy, f);
+	        	copy.setProviderNo(providerNo);
+	        	copy.setId(null);
+	        	favoritesDao.persist(copy);
+        	}catch(Exception e) {
+        		logger.error("error",e);
+        	}
+        }
+         
         return mapping.findForward("success");
     }
 
-    public WebApplicationContext getApptContext() {
-        return WebApplicationContextUtils.getRequiredWebApplicationContext(getServlet().getServletContext());
-    }
-
-    public RxPrescriptionMgr getRxMgr() {
-        return (RxPrescriptionMgr) getApptContext().getBean("RxPrescriptionMgr");
-    }
 }
