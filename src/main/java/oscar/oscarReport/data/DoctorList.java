e0a8fc335ef20a1d32d9398213194b6c68194ed6
Nick Goupinets <nick.goupinets@gmail.com>
2012-11-15 16:27:16 -0500

    Removed DH from DL

--- src/main/java/oscar/oscarReport/data/DoctorList.java
+++ src/main/java/oscar/oscarReport/data/DoctorList.java
@@ -9,40 +9,30 @@
 
 package oscar.oscarReport.data;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
-import org.oscarehr.util.MiscUtils;
+import org.oscarehr.PMmodule.dao.ProviderDao;
+import org.oscarehr.common.model.Provider;
+import org.oscarehr.util.SpringUtils;
 
-import oscar.oscarDB.DBHandler;
 import oscar.oscarProvider.bean.ProviderNameBean;
 
 public class DoctorList {
-    public DoctorList() {
-    }
+	
+	public ArrayList<ProviderNameBean> getDoctorNameList() {
 
-    public ArrayList<ProviderNameBean> getDoctorNameList(){
+		ArrayList<ProviderNameBean> dnl = new ArrayList<ProviderNameBean>();
 
-        ArrayList<ProviderNameBean> dnl = new ArrayList<ProviderNameBean>();
-        try{
+		ProviderDao dao = SpringUtils.getBean(ProviderDao.class);
+		List<Provider> docs = dao.getProvidersByType("doctor");
 
-
-            java.sql.ResultSet rs;
-            String sql = "select last_name, first_name, provider_no from provider where provider_type='doctor'";
-            rs = DBHandler.GetSQL(sql);
-
-            while(rs.next()){
-                ProviderNameBean pb = new ProviderNameBean();
-                pb.setProviderID(oscar.Misc.getString(rs, 3));
-                pb.setProviderName(oscar.Misc.getString(rs, 2)+ " " +oscar.Misc.getString(rs, 1));
-                dnl.add(pb);
-
-            }
-        }
-        catch(SQLException e)
-        {
-            MiscUtils.getLogger().error("Error", e);
-        }
-        return dnl;
-    }
+		for (Provider doc : docs) {
+			ProviderNameBean pb = new ProviderNameBean();
+			pb.setProviderID(doc.getProviderNo());
+			pb.setProviderName(doc.getFullName());
+			dnl.add(pb);
+		}
+		return dnl;
+	}
 }
