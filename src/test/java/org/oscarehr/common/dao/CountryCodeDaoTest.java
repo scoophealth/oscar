eccd8ce22a1e34a8a8d0d215d455592454db72c0
Craig Clark <craig.clark2@mohawkcollege.ca>
2012-11-22 14:30:06 -0600

    Added DAO tests for ConsultationServices, CountryCode and DemographicExt

--- src/test/java/org/oscarehr/common/dao/CountryCodeDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/CountryCodeDaoTest.java
@@ -23,6 +23,7 @@
  */
 package org.oscarehr.common.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Before;
@@ -34,23 +35,39 @@ import org.oscarehr.util.SpringUtils;
 
 public class CountryCodeDaoTest extends DaoTestFixtures {
 
-	private CountryCodeDao dao = SpringUtils.getBean(CountryCodeDao.class);
+private CountryCodeDao dao = SpringUtils.getBean(CountryCodeDao.class);
 
-	public CountryCodeDaoTest() {
+public CountryCodeDaoTest() {
 
-	}
+}
 
-	@Before
-	public void before() throws Exception {
-		SchemaUtils.restoreTable("country_codes");
-	}
+@Before
+public void before() throws Exception {
+SchemaUtils.restoreTable("country_codes");
+}
 
-	@Test
-	public void testCreate() throws Exception {
-		CountryCode entity = new CountryCode();
-		EntityDataGenerator.generateTestDataForModelClass(entity);
-		dao.persist(entity);
-		assertNotNull(entity.getId());
-	}
+@Test
+public void testCreate() throws Exception {
+CountryCode entity = new CountryCode();
+EntityDataGenerator.generateTestDataForModelClass(entity);
+dao.persist(entity);
+assertNotNull(entity.getId());
+}
+@Test
+public void testGetAllCountryCodes() throws Exception{
+int initialSize = dao.findAll().size();
+String locale = "en";
+CountryCode entity = new CountryCode();
+EntityDataGenerator.generateTestDataForModelClass(entity);
+dao.persist(entity);
+
+assertEquals(dao.findAll().size(),initialSize + 1);
+}
+public void testGetCountryCode() throws Exception{
+CountryCode country = new CountryCode();
+EntityDataGenerator.generateTestDataForModelClass(country);
+country = dao.getCountryCode("CA");
+assertEquals("Canada", country.getCountryName());
+}
 
 }
