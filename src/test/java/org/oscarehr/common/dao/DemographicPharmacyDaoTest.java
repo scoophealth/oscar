ae9f72254b8dca17463a29a53d0a25e4a635fdf7
shazib <shazib.zaffar@gmail.com>
2012-12-07 12:29:52 -0500

    Updated a few Dao tests. See listing:
    	DesapriskDaoTest.java
    	ClinicDAOTest.java
    	DemographicCustDaoTest.java
    	DemographicMergedDaoTest.java
    	DemographicPharmacyDaoTest.java
    	OcanStaffFormDaoTest.java
    	TicklerLinkDaoTest.java
    
    Also fixed a typo in TicklerLinkDao.java

--- src/test/java/org/oscarehr/common/dao/DemographicPharmacyDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/DemographicPharmacyDaoTest.java
@@ -26,6 +26,10 @@ package org.oscarehr.common.dao;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.oscarehr.common.dao.utils.EntityDataGenerator;
@@ -35,11 +39,11 @@ import org.oscarehr.util.SpringUtils;
 
 public class DemographicPharmacyDaoTest extends DaoTestFixtures {
 
-	protected DemographicPharmacyDao dao = (DemographicPharmacyDao)SpringUtils.getBean(DemographicPharmacyDao.class);
+	private DemographicPharmacyDao dao = (DemographicPharmacyDao)SpringUtils.getBean(DemographicPharmacyDao.class);
+	DateFormat dfm = new SimpleDateFormat("yyyyMMdd");
 
 	public DemographicPharmacyDaoTest() {
 	}
-
 
 	@Before
 	public void before() throws Exception {
@@ -58,5 +62,52 @@ public class DemographicPharmacyDaoTest extends DaoTestFixtures {
 	public void testDataMethods()  {
 		dao.addPharmacyToDemographic(1,100);
 		assertEquals(1,dao.findByDemographicId(100).getPharmacyId());
+	}
+	
+	@Test
+	public void testFindByDemographicId() throws Exception {
+		
+		Date addDate1 = new Date(dfm.parse("20080101").getTime());
+		Date addDate2 = new Date(dfm.parse("20100101").getTime());
+		Date addDate3 = new Date(dfm.parse("20110101").getTime());
+		
+		int demographicNo1 = 101;
+		int demographicNo2 = 202;
+		
+		String status1 = "1";
+		String status2 = "2";
+		
+		DemographicPharmacy demoPhramacy1 = new DemographicPharmacy();
+		EntityDataGenerator.generateTestDataForModelClass(demoPhramacy1);
+		demoPhramacy1.setStatus(status1);
+		demoPhramacy1.setDemographicNo(demographicNo1);
+		demoPhramacy1.setAddDate(addDate1);
+		dao.persist(demoPhramacy1);
+		
+		DemographicPharmacy demoPhramacy2 = new DemographicPharmacy();
+		EntityDataGenerator.generateTestDataForModelClass(demoPhramacy2);
+		demoPhramacy2.setStatus(status2);
+		demoPhramacy2.setDemographicNo(demographicNo1);
+		demoPhramacy2.setAddDate(addDate2);
+		dao.persist(demoPhramacy2);
+		
+		DemographicPharmacy demoPhramacy3 = new DemographicPharmacy();
+		EntityDataGenerator.generateTestDataForModelClass(demoPhramacy3);
+		demoPhramacy3.setStatus(status1);
+		demoPhramacy3.setDemographicNo(demographicNo1);
+		demoPhramacy3.setAddDate(addDate3);
+		dao.persist(demoPhramacy3);
+		
+		DemographicPharmacy demoPhramacy4 = new DemographicPharmacy();
+		EntityDataGenerator.generateTestDataForModelClass(demoPhramacy4);
+		demoPhramacy4.setStatus(status1);
+		demoPhramacy4.setDemographicNo(demographicNo2);
+		demoPhramacy4.setAddDate(addDate3);
+		dao.persist(demoPhramacy4);
+		
+		DemographicPharmacy expectedResult = demoPhramacy1;
+		DemographicPharmacy result = dao.findByDemographicId(demographicNo1);
+		
+		assertEquals(expectedResult, result);
 	}
 }
--- src/test/java/org/oscarehr/common/dao/TicklerLinkDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/TicklerLinkDaoTest.java
@@ -23,22 +23,24 @@
  */
 package org.oscarehr.common.dao;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.oscarehr.common.dao.utils.EntityDataGenerator;
 import org.oscarehr.common.dao.utils.SchemaUtils;
 import org.oscarehr.common.model.TicklerLink;
+import org.oscarehr.util.MiscUtils;
 import org.oscarehr.util.SpringUtils;
 
-public class TicklerLinkDaoTest {
+public class TicklerLinkDaoTest extends DaoTestFixtures {
 
 	protected TicklerLinkDao dao = SpringUtils.getBean(TicklerLinkDao.class);
-
-	public TicklerLinkDaoTest() {
-	}
-
 
 	@Before
 	public void before() throws Exception {
@@ -50,7 +52,101 @@ public class TicklerLinkDaoTest {
 		TicklerLink entity = new TicklerLink();
 		EntityDataGenerator.generateTestDataForModelClass(entity);
 		dao.persist(entity);
-
 		assertNotNull(entity.getId());
+	}
+	
+	@Test 
+	public void testGetLinkByTableId() throws Exception {
+		
+		String tableName1 = "alp";
+		String tableName2 = "brv";
+		
+		long tableId1 = 101;
+		long tableId2 = 202;
+		
+		TicklerLink ticklerLink1 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink1);
+		ticklerLink1.setTableName(tableName1);
+		ticklerLink1.setTableId(tableId1);
+		dao.persist(ticklerLink1);
+		
+		TicklerLink ticklerLink2 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink2);
+		ticklerLink2.setTableName(tableName2);
+		ticklerLink2.setTableId(tableId1);
+		dao.persist(ticklerLink2);
+		
+		TicklerLink ticklerLink3 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink3);
+		ticklerLink3.setTableName(tableName1);
+		ticklerLink3.setTableId(tableId1);
+		dao.persist(ticklerLink3);
+		
+		TicklerLink ticklerLink4 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink4);
+		ticklerLink4.setTableName(tableName1);
+		ticklerLink4.setTableId(tableId2);
+		dao.persist(ticklerLink4);
+		
+		List<TicklerLink> expectedResult = new ArrayList<TicklerLink>(Arrays.asList(ticklerLink1, ticklerLink3));		
+		List<TicklerLink> result = dao.getLinkByTableId(tableName1, tableId1);
+		
+		Logger logger = MiscUtils.getLogger();
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match. Result: "+result.size());
+			fail("Array sizes do not match.");
+		}
+
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items do not match.");
+				fail("Items do not match.");
+			}
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetLinkByTickler() throws Exception {
+		
+		int ticklerNo1 = 101;
+		int ticklerNo2 = 202;
+		
+		TicklerLink ticklerLink1 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink1);
+		ticklerLink1.setTicklerNo(ticklerNo1);
+		dao.persist(ticklerLink1);
+		
+		TicklerLink ticklerLink2 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink2);
+		ticklerLink2.setTicklerNo(ticklerNo2);
+		dao.persist(ticklerLink2);
+		
+		TicklerLink ticklerLink3 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink3);
+		ticklerLink3.setTicklerNo(ticklerNo1);
+		dao.persist(ticklerLink3);
+		
+		TicklerLink ticklerLink4 = new TicklerLink();
+		EntityDataGenerator.generateTestDataForModelClass(ticklerLink4);
+		ticklerLink4.setTicklerNo(ticklerNo1);
+		dao.persist(ticklerLink4);
+		
+		List<TicklerLink> expectedResult = new ArrayList<TicklerLink>(Arrays.asList(ticklerLink1, ticklerLink3, ticklerLink4));		
+		List<TicklerLink> result = dao.getLinkByTickler(ticklerNo1);
+		
+		Logger logger = MiscUtils.getLogger();
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match. Result: "+result.size());
+			fail("Array sizes do not match.");
+		}
+
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items do not match.");
+				fail("Items do not match.");
+			}
+		}
+		assertTrue(true);
 	}
 }
