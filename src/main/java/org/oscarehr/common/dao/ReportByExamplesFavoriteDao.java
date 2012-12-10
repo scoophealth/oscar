f2d1eab38c1e969b7b3444c9c8595250922a4d4f
Nick Goupinets <nick.goupinets@gmail.com>
2012-12-10 12:06:49 -0500

    Removed DH from RBEFA

--- src/main/java/org/oscarehr/common/dao/ReportByExamplesFavoriteDao.java
+++ src/main/java/org/oscarehr/common/dao/ReportByExamplesFavoriteDao.java
@@ -25,13 +25,32 @@
 
 package org.oscarehr.common.dao;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
 import org.oscarehr.common.model.ReportByExamplesFavorite;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@SuppressWarnings("unchecked")
 public class ReportByExamplesFavoriteDao extends AbstractDao<ReportByExamplesFavorite>{
 
 	public ReportByExamplesFavoriteDao() {
 		super(ReportByExamplesFavorite.class);
 	}
+
+    public List<ReportByExamplesFavorite> findByQuery(String query) {
+		Query q = createQuery("ex", "ex.query LIKE :query");
+        q.setParameter("query", query);
+        return q.getResultList();
+    }
+
+	public List<ReportByExamplesFavorite> findByEverything(String providerNo, String favoriteName, String queryString) {
+		Query query = createQuery("ex", "ex.providerNo = :providerNo AND ex.name LIKE :name OR ex.query LIKE :query");
+        query.setParameter("providerNo", providerNo);
+        query.setParameter("name", favoriteName);
+        query.setParameter("query", queryString);
+        return query.getResultList();
+    }
 }
