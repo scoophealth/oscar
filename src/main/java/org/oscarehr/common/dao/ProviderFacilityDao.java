d3485b060792bd9cda4e4d98f40498087e07732a
lingmin.zhou <jackeyoption@gmail.com>
2012-12-27 18:46:33 +0800

    Change-Id: I26084453fc6997fe624e82641fadf765c841f042 WL: unable to unassign facility from a provider -- ID: 3598170

--- src/main/java/org/oscarehr/common/dao/ProviderFacilityDao.java
+++ src/main/java/org/oscarehr/common/dao/ProviderFacilityDao.java
@@ -40,7 +40,7 @@ public class ProviderFacilityDao extends AbstractDao<ProviderFacility>{
 	}
 	
 	public List<ProviderFacility> findByProviderNoAndFacilityId(String providerNo, int facilityId) {
-		Query q = entityManager.createQuery("SELECT x FROM ProviderFacility x WHERE x.providerNo=? AND x.facilityId=?");
+		Query q = entityManager.createQuery("SELECT x FROM ProviderFacility x WHERE x.id.providerNo=? AND x.id.facilityId=?");
 		q.setParameter(1, providerNo);
 		q.setParameter(2, facilityId);
 		
