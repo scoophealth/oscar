e7bc882f5c19dc9f6056afee7a1311d72df5e6be
Nick Goupinets <nick.goupinets@gmail.com>
2012-12-31 12:21:44 -0500

    Removed DH from LinkReq.jsp

---
+++ src/test/java/org/oscarehr/common/dao/forms/FormDaoTest.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.dao.forms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.oscarehr.common.dao.DaoTestFixtures;
+import org.oscarehr.common.dao.utils.SchemaUtils;
+import org.oscarehr.util.SpringUtils;
+
+public class FormDaoTest extends DaoTestFixtures {
+
+	protected FormsDao dao = SpringUtils.getBean(FormsDao.class);
+
+	@Before
+	public void before() throws Exception {
+		SchemaUtils.restoreTable("formLabReq07");
+	}
+
+	@Test
+	public void testFindIdFormCreatedAndPatientNameFromFormLabReq07() {
+		assertNotNull(dao.findIdFormCreatedAndPatientNameFromFormLabReq07());
+	}
+
+	@Test
+	public void testFindFormCreatedFromFormLabReq07ById() {
+		assertNotNull(dao.findFormCreatedFromFormLabReq07ById(100));
+	}
+}
