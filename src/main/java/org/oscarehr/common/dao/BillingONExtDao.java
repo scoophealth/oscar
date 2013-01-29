77b25232edbc61a033530cd0e9182443d7cf7033
mbeldman <mweston4@uwo.ca>
2013-01-29 10:07:07 -0500

    Bug Fix: ID 3602520 Fixes for ID 3556999 Admin print to PDF or email billing
    This just reinstates a few billing features that were merged into trunk
    after ID 3556999 was created, but before it was merged.
    
    Those being:
    
    -Editable due date on 3rd party invoice
    -Select between master file demographic contact info and info entered on
    Billing Correction Bill To text field to populate the Bill To field on the
    3rd party invoice
    -PDF print of invoice doesn't make use of
    js/custom/westernu/billingInvoice.js formatting like on the 3rd Party
    invoice reprint screen
    
    Change-Id: I43e6c418f5a77d4722500f2bb75f2fec0d66acb4

--- src/main/java/org/oscarehr/common/dao/BillingONExtDao.java
+++ src/main/java/org/oscarehr/common/dao/BillingONExtDao.java
@@ -214,7 +214,29 @@ public class BillingONExtDao extends AbstractDao<BillingONExt>{
 	}
 	       
         return bExt;
-    }    
+    }
+    
+    public BillingONExt getUseBillTo(BillingONCHeader1 bCh1) {
+        BillingONExt bExt = null;
+        
+        String sql = "select bExt from BillingONExt bExt where billingNo=? and status=? and keyVal=?";
+        Query query = entityManager.createQuery(sql);
+        query.setParameter(1, bCh1.getId());
+        query.setParameter(2, '1');
+        query.setParameter(3, "useBillTo");
+         
+        List<BillingONExt> results = query.getResultList();
+        
+        if (results.size() > 1) {
+            MiscUtils.getLogger().warn("More than one active useBillTo result for invoice number: " + bCh1.getId());
+        }
+        
+        if (!results.isEmpty()) {
+            bExt = results.get(0);
+        }
+        
+        return bExt;
+    }
  
     public List<BillingONExt> find(Integer billingNo, String key, Date start, Date end) {
 		Query q = createQuery("b", "b.billingNo = :bNo AND b.keyVal = :key AND b.dateTime >= :start AND b.dateTime <= :end");
