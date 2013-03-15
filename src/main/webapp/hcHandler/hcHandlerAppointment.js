75272058b63c3c3d2d654c660d6f7f4e913c9429
Claus Strommer <baron@d0005.prylynx.lan>
2013-03-15 11:31:13 -0400

    Issue 3606933:  Indivica HC swipe feature broken
    Port of commit 483e1a22f9441468c9b8e35c39001dd1edfac2e1 from
    RELEASE_12_1 to trunk:
    
    Updated the JS for all HC swipe features to reference the appropriate
    page elements in the master demographic record, new demographic
    screen, and schedule screen.  Broken fuctionality that is now fixed:
    
    * Swiping a card in the master record (without clicking the 'swipe
      card' button) switches the master record to the update version and
      highlights the changes.  This works in both the view and edit
      mode of this screen
    * Swiping a health card in the new demographic page will fill out
      the appropriate information from the card.  It is not necessary to
      click 'swipe card'
    * Swiping a health card in the schedule screen brings up a dialog
      displaying the health card information, and if a matching
      demographic is found, the information from that record.  Links are
      displayed to go to the master record

--- src/main/webapp/hcHandler/hcHandlerAppointment.js
+++ src/main/webapp/hcHandler/hcHandlerAppointment.js
@@ -11,29 +11,29 @@ var _hc_windowTimeout;
 var _hc_apptScreenHandler = function(args) {
 	clearTimeout(_hc_windowTimeout);
 		
-	var window = jQuery("#_hc_window");
-	jQuery(window).find("#_hc_message, #_hc_read, #_hc_actions, #_hc_match, #_hc_noMatch").hide();
+	var hcwindow = jQuery("#_hc_window");
+	jQuery(hcwindow).find("#_hc_message, #_hc_read, #_hc_actions, #_hc_match, #_hc_noMatch").hide();
 		
-	jQuery(window).find("._hc_mismatch").removeClass("_hc_mismatch");
-	jQuery(window).find("#_hc_errors").children().remove();
+	jQuery(hcwindow).find("._hc_mismatch").removeClass("_hc_mismatch");
+	jQuery(hcwindow).find("#_hc_errors").children().remove();
 		
 	if (!(typeof args["error"] == "undefined")) {
-		jQuery(window).find("#_hc_closeBtn").hide();
+		jQuery(hcwindow).find("#_hc_closeBtn").hide();
 		
-		jQuery(window).find("#_hc_status_text_success").hide();
-		jQuery(window).find("#_hc_status_text_error, #_hc_message_tryAgain, #_hc_message").show();
+		jQuery(hcwindow).find("#_hc_status_text_success").hide();
+		jQuery(hcwindow).find("#_hc_status_text_error, #_hc_message_tryAgain, #_hc_message").show();
 			
-		jQuery(window).find("#_hc_status_icon").attr("class", "_hc_inlineBlock _hc_status_error");
+		jQuery(hcwindow).find("#_hc_status_icon").attr("class", "_hc_inlineBlock _hc_status_error");
 			
 		if (args["error"] == "INVALID") {
-			jQuery(window).find("#_hc_message_readError").css("display", "inline-block");
-			jQuery(window).find("#_hc_message_issuerError").css("display", "none");
+			jQuery(hcwindow).find("#_hc_message_readError").css("display", "inline-block");
+			jQuery(hcwindow).find("#_hc_message_issuerError").css("display", "none");
 		} else if (args["error"] == "ISSUER") {
-			jQuery(window).find("#_hc_message_readError").css("display", "none");
-			jQuery(window).find("#_hc_message_issuerError").css("display", "inline-block");
+			jQuery(hcwindow).find("#_hc_message_readError").css("display", "none");
+			jQuery(hcwindow).find("#_hc_message_issuerError").css("display", "inline-block");
 		} else {
-			jQuery(window).find("#_hc_message_readError").css("display", "none");
-			jQuery(window).find("#_hc_message_issuerError").css("display", "none");
+			jQuery(hcwindow).find("#_hc_message_readError").css("display", "none");
+			jQuery(hcwindow).find("#_hc_message_issuerError").css("display", "none");
 		}
 			
 		_hc_windowTimeout = setTimeout(function() {
@@ -42,35 +42,36 @@ var _hc_apptScreenHandler = function(args) {
 		
 			
 	} else {
-		jQuery(window).find("#_hc_closeBtn").show();
+		jQuery(hcwindow).find("#_hc_closeBtn").show();
 		
 		
-		jQuery(window).find("#_hc_status_text_success, #_hc_read, #_hc_layout, #_hc_matchSearch").show();
-		jQuery(window).find("#_hc_message, #_hc_status_text_error").hide();
-		jQuery(window).find("#_hc_status_icon").attr("class", "_hc_inlineBlock _hc_status_success");
+		jQuery(hcwindow).find("#_hc_status_text_success, #_hc_read, #_hc_layout, #_hc_matchSearch").show();
+		jQuery(hcwindow).find("#_hc_message, #_hc_status_text_error").hide();
+		jQuery(hcwindow).find("#_hc_status_icon").attr("class", "_hc_inlineBlock _hc_status_success");
 			
-		jQuery(window).find("#_hc_layout_name").text(args["lastName"] + ", " + args["firstName"]);
-		jQuery(window).find("#_hc_layout_hin_num").html(args["hin"].substring(0,4) + "&#149; " + args["hin"].substring(4,7) + "&#149; " + args["hin"].substring(7,10) + "&#149;");
-		jQuery(window).find("#_hc_layout_hin_ver").text(args["hinVer"]);
+		jQuery(hcwindow).find("#_hc_layout_name").text(args["lastName"] + ", " + args["firstName"]);
+		jQuery(hcwindow).find("#_hc_layout_hin_num").html(args["hin"].substring(0,4) + "&#149; " + args["hin"].substring(4,7) + "&#149; " + args["hin"].substring(7,10) + "&#149;");
+		jQuery(hcwindow).find("#_hc_layout_hin_ver").text(args["hinVer"]);
 			
-		jQuery(window).find("#_hc_layout_info_dob").text(args["dob"].substring(0,4) + "/" + args["dob"].substring(4,6) + "/" + args["dob"].substring(6,8));
-		jQuery(window).find("#_hc_layout_info_sex").text((args["sex"] == "1" ? "M" : (args["sex"] == "2" ? "F" : "")));
+		jQuery(hcwindow).find("#_hc_layout_info_dob").text(args["dob"].substring(0,4) + "/" + args["dob"].substring(4,6) + "/" + args["dob"].substring(6,8));
+		jQuery(hcwindow).find("#_hc_layout_info_sex").text((args["sex"] == "1" ? "M" : (args["sex"] == "2" ? "F" : "")));
 		
 		var issueDate = (args["issueDate"].substring(0,2) <= 30 ? "20" : "19") + args["issueDate"];
-		jQuery(window).find("#_hc_layout_valid_from").text(issueDate.substring(0,4) + "/" + issueDate.substring(4,6) + "/" + issueDate.substring(6,8));
+		jQuery(hcwindow).find("#_hc_layout_valid_from").text(issueDate.substring(0,4) + "/" + issueDate.substring(4,6) + "/" + issueDate.substring(6,8));
 		
-		if (hinExp != "0000") {
+        var hinExpNil = args["hinExp"].substring(0,4);
+        if (hinExpNil != "0000") {
 			var hinExp = (args["hinExp"].substring(0,2) <= 30 ? "20" : "19") + args["hinExp"] + args["dob"].substring(6,8);
-			jQuery(window).find("#_hc_layout_valid_to").text(hinExp.substring(0,4) + "/" + hinExp.substring(4,6) + "/" + hinExp.substring(6,8));
+			jQuery(hcwindow).find("#_hc_layout_valid_to").text(hinExp.substring(0,4) + "/" + hinExp.substring(4,6) + "/" + hinExp.substring(6,8));
 		
 			var date = new Date();
 			var hinExpDate = new Date(hinExp.substring(0,4) + "/" + hinExp.substring(4,6) + "/" + hinExp.substring(6, 8));
 			if (hinExpDate <= new Date()) {
-				jQuery(window).find("#_hc_layout_valid_to").addClass("_hc_mismatch");
-				jQuery(window).find("#_hc_errors").append("<div class='_hc_error'>This health card has expired.</div>");
+				jQuery(hcwindow).find("#_hc_layout_valid_to").addClass("_hc_mismatch");
+				jQuery(hcwindow).find("#_hc_errors").append("<div class='_hc_error'>This health card has expired.</div>");
 			}
 		} else {
-			jQuery(window).find("#_hc_layout_valid_to").text("No Expiry");
+			jQuery(hcwindow).find("#_hc_layout_valid_to").text("No Expiry");
 		}
 		
 		
@@ -78,7 +79,7 @@ var _hc_apptScreenHandler = function(args) {
 			
 		(function(win, hcArgs) {
 			jQuery.ajax({
-				url: ctx + "/indivica/HCSearch.do",
+				url: "../indivica/HCSearch.do",
 				data: "hin=" + hcArgs["hin"] + "&ver=" + hcArgs["hinVer"] + "&issueDate=" + hcArgs["issueDate"] + "&hinExp=" + hcArgs["hinExp"],
 				type: "POST",
 				dataType: "json",
@@ -113,7 +114,7 @@ var _hc_apptScreenHandler = function(args) {
 						if (error)
 							jQuery(win).find("#_hc_action_present").hide();
 						else {
-							jQuery(win).find("#_hc_action_present").show();
+							jQuery(win).find("#_hc_action_present").hide();
 							jQuery("td[demo_no=" + data.demoNo + "]").addClass("_hc_appointmentMatch");
 						}
 							
@@ -128,10 +129,10 @@ var _hc_apptScreenHandler = function(args) {
 						
 						(function(appointmentNo) {
 							jQuery(win).find("#_hc_action_present").click(function() {
-								document.location.href = ctx + "/provider/providercontrol.jsp?appointment_no=" + appointmentNo + "&year=" + getURLParameter("year") + "&month=" + getURLParameter("month") + "&day=" + getURLParameter("day") + "&status=&statusch=H&displaymode=addstatus&dboperation=updateapptstatus";
+								document.location.href = "providercontrol.jsp?appointment_no=" + appointmentNo + "&year=" + getURLParameter("year") + "&month=" + getURLParameter("month") + "&day=" + getURLParameter("day") + "&status=&statusch=H&displaymode=addstatus&dboperation=updateapptstatus";
 								jQuery(win).hide();
 							});
-						})(jQuery("td[demo_no=" + data.demoNo + "]").first().attr("appointment_no"));
+						})(jQuery(".apptLink[demographic_no="+data.demoNo+"]" ).first().attr("appointment_no"));
 						
 						jQuery(win).find("#_hc_action_present").hide();
 					} else {
@@ -145,14 +146,14 @@ var _hc_apptScreenHandler = function(args) {
 					}
 				}
 			});
-		})(window, args);
+		})(hcwindow, args);
 	}
 		
-	jQuery(window).css("display", "block");
+	jQuery(hcwindow).css("display", "block");
 }
 
 jQuery(document).ready(function() {
-	jQuery("#_hc_window #_hc_matchSearch img").attr("src", ctx + "/images/DMSLoader.gif");
+	jQuery("#_hc_window #_hc_matchSearch img").attr("src", "../images/DMSLoader.gif");
 
 	jQuery("#_hc_window #_hc_closeBtn").click(function() {
 		jQuery("#_hc_window").hide();
