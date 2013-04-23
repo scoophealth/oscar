5b09decd3557a23c10ed753a6229510f304ca4b3
Marc Dumontier <marc@mdumontier.com>
2013-04-23 13:35:11 -0400

    ID:3611656 - REST Integration (Transfer Objects/Converters)
    Basic implementation of Domain <--> Transfer Object
    Transfer Objects to be reused across all REST services

---
+++ src/main/java/org/oscarehr/ws/rest/conversion/PharmacyInfoConverter.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.ws.rest.conversion;
+
+import org.oscarehr.common.model.PharmacyInfo;
+import org.oscarehr.ws.rest.to.model.PharmacyInfoTo1;
+
+public class PharmacyInfoConverter extends AbstractConverter<PharmacyInfo, PharmacyInfoTo1> {
+
+	@Override
+	public PharmacyInfo getAsDomainObject(PharmacyInfoTo1 t) throws ConversionException {
+		PharmacyInfo d = new PharmacyInfo();
+		
+		d.setId(t.getId());
+		d.setId2(t.getId2());
+		d.setName(t.getName());
+		d.setAddress(t.getAddress().getAddress());
+		d.setCity(t.getAddress().getCity());
+		d.setProvince(t.getAddress().getProvince());
+		d.setPostalCode(t.getAddress().getPostal());
+		d.setPhone1(t.getPhone1());
+		d.setPhone2(t.getPhone2());
+		d.setFax(t.getFax());
+		d.setEmail(t.getEmail());
+		d.setServiceLocationIdentifier(t.getServiceLocationIdentifier());
+		d.setNotes(t.getNotes());
+		d.setAddDate(t.getAddDate());
+		d.setStatus(t.getStatus());
+		return d;
+	}
+
+	@Override
+	public PharmacyInfoTo1 getAsTransferObject(PharmacyInfo d) throws ConversionException {
+		PharmacyInfoTo1 t = new PharmacyInfoTo1();
+		
+		t.setId(d.getId());
+		t.setId2(d.getId2());
+		t.setName(d.getName());
+		t.getAddress().setAddress(d.getAddress());
+		t.getAddress().setCity(d.getCity());
+		t.getAddress().setProvince(d.getProvince());
+		t.getAddress().setPostal(d.getPostalCode());
+		t.setPhone1(d.getPhone1());
+		t.setPhone2(d.getPhone2());
+		t.setFax(d.getFax());
+		t.setEmail(d.getEmail());
+		t.setServiceLocationIdentifier(d.getServiceLocationIdentifier());
+		t.setNotes(d.getNotes());
+		t.setAddDate(d.getAddDate());
+		t.setStatus(d.getStatus());
+		return t;
+	}
+
+}
---
+++ src/main/java/org/oscarehr/ws/rest/to/model/PharmacyInfoTo1.java
@@ -0,0 +1,144 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.ws.rest.to.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class PharmacyInfoTo1 implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private int id2;
+	private String name;
+	private AddressTo1 address = new AddressTo1();
+	private String phone1;
+	private String phone2;
+	private String fax;
+	private String email;
+	private String serviceLocationIdentifier;
+	private String notes;
+	private Date addDate;
+	private String status;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public int getId2() {
+		return id2;
+	}
+
+	public void setId2(int id2) {
+		this.id2 = id2;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public AddressTo1 getAddress() {
+		return address;
+	}
+
+	public void setAddress(AddressTo1 address) {
+		this.address = address;
+	}
+
+	public String getPhone1() {
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) {
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() {
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) {
+		this.phone2 = phone2;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getServiceLocationIdentifier() {
+		return serviceLocationIdentifier;
+	}
+
+	public void setServiceLocationIdentifier(String serviceLocationIdentifier) {
+		this.serviceLocationIdentifier = serviceLocationIdentifier;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getAddDate() {
+		return addDate;
+	}
+
+	public void setAddDate(Date addDate) {
+		this.addDate = addDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+}
