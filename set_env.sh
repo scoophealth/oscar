44738427fe02397e32ed38247790fa5c833719dd
Randy Jonasz <rjonasz@gmail.com>
2013-04-26 15:31:44 -0700

    From Master: ID: 3611559 Keep referrer of consults to one who created it
    Change-Id: Ia0db8720b642aa898df4bc72a3d467fdce802e06

--- set_env.sh
+++ set_env.sh
@@ -5,11 +5,12 @@
 #export JAVA_HOME=${APP_ROOT}/openjdk1.7.0.3/jvm/java-1.7.0-openjdk-1.7.0.3.x86_64
 #export PATH=${JAVA_HOME}/bin:${PATH}
 
+export JAVA_HOME=/usr/lib/jvm/java/
 export WORKING_ROOT=`pwd`
 
 export CATALINA_BASE=${WORKING_ROOT}/catalina_base
 
-MEM_SETTINGS="-Xms640m -Xmx640m -Xss128k -XX:MaxNewSize=64m -XX:MaxPermSize=256m "
+MEM_SETTINGS="-Xms640m -Xmx640m -Xss640k -XX:MaxNewSize=64m -XX:MaxPermSize=256m "
 export JAVA_OPTS="-Djava.awt.headless=true -server -Xincgc -Dorg.apache.el.parser.COERCE_TO_ZERO=false -Dorg.apache.commons.logging.Log=org.apache.cxf.common.logging.Log4jLogger -Dorg.apache.cxf.Logger=org.apache.cxf.common.logging.Log4jLogger -Dlog4j.override.configuration=${WORKING_ROOT}/override_log4j.xml -Doscar_override_properties=${WORKING_ROOT}/oscar.properties "${MEM_SETTINGS}
 
 export MAVEN_OPTS="-Doscar_override_properties=${WORKING_ROOT}/oscar.properties"
