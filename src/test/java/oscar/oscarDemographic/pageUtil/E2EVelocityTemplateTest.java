4f19e8af4001508b2e129e26bd0b7a519fec951c
Marc Dumontier <marc@mdumontier.com>
2013-05-16 19:16:53 -0400

    bug fix - added missing restoreTable call to fix build
    Change-Id: Iac67dcc67b18fab3fd18058315f82ba90e1b2834

--- src/test/java/oscar/oscarDemographic/pageUtil/E2EVelocityTemplateTest.java
+++ src/test/java/oscar/oscarDemographic/pageUtil/E2EVelocityTemplateTest.java
@@ -58,7 +58,7 @@ public class E2EVelocityTemplateTest extends DaoTestFixtures {
 		SchemaUtils.restoreTable("demographicSets", "lst_gender", "demographic_merged",
 				"admission", "program", "health_safety", "demographic", "provider",
 				"allergies", "drugs", "preventions", "dxresearch", "patientLabRouting",
-				"icd9", "casemgmt_issue", "clinic", "issue");
+				"icd9", "casemgmt_issue", "clinic", "issue","measurements");
 		Demographic entity = new Demographic();
 		EntityDataGenerator.generateTestDataForModelClass(entity);
 		entity.setDemographicNo(null);
