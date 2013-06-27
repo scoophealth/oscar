ce5de63b4955df2c3ddf9568d7d829401970f75e
Marc Dumontier <marc@mdumontier.com>
2013-06-27 15:37:46 -0400

    ID: 3614589 - Make Relationship Dropdown Table Driven
    Ended up handling the inverses through the table.

--- database/mysql/oscardata.sql
+++ database/mysql/oscardata.sql
@@ -2544,3 +2544,32 @@ INSERT INTO secObjPrivilege VALUES('doctor','_caseload.CashAdmissionDate','o',0,
 INSERT INTO secObjPrivilege VALUES('doctor','_caseload.Access1AdmissionDate','o',0,'999998');
 
 INSERT INTO `OscarCode` VALUES (1,'CKDSCREEN','Ckd Screening');
+
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Mother','Mother',true,'Son','Daughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Father','Father',true,'Son','Daughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Parent','Parent',true,'Son','Daughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Wife','Wife',true,'Husband','Partner');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Husband','Husband',true,'Partner','Wife');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Partner','Partner',true,'Partner','Partner');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Brother','Brother',true,'Brother','Sister');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Sister','Sister',true,'Brother','Sister');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Son','Son',true,'Father','Mother');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Daughter','Daughter',true,'Father','Mother');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Aunt','Aunt',true,'Nephew','Niece');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Uncle','Uncle',true,'Nephew','Niece');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Nephew','Nephew',true,'Uncle','Aunt');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Niece','Niece',true,'Uncle','Aunt');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('GrandFather','GrandFather',true,'GrandSon','GrandDaughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('GrandMother','GrandMother',true,'GrandSon','GrandDaughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Foster Parent','Foster Parent',true,'Foster Son','Foster Daughter');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Foster Son','Foster Son',true,'Foster Parent','Foster Parent');
+insert into CtlRelationships (value,label,active,maleInverse,femaleInverse) values ('Foster Daughter','Foster Daughter',true,'Foster Parent','Foster Parent');
+insert into CtlRelationships (value,label,active) values ('Guardian','Guardian',true);
+insert into CtlRelationships (value,label,active) values ('Next of Kin','Next of kin',true);
+insert into CtlRelationships (value,label,active) values ('Administrative Staff','Administrative Staff',true);
+insert into CtlRelationships (value,label,active) values ('Care Giver','Care Giver',true);
+insert into CtlRelationships (value,label,active) values ('Power of Attorney','Power of Attorney',true);
+insert into CtlRelationships (value,label,active) values ('Insurance','Insurance',true);
+insert into CtlRelationships (value,label,active) values ('Guarantor','Guarantor',true);
+insert into CtlRelationships (value,label,active) values ('Other','Other',true);
+
