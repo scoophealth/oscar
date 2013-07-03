3b2fde8d9a245e927644085cf5950a82cfcd4db9
Ted Leung <r6squeegee@gmail.com>
2013-07-03 14:28:22 +1000

    ID: 3614628 prevent useless session objects
    Change-Id: I09d65a9a5bab6a2bec2ebe45b7dfa9a4440b078e

---
+++ src/main/java/org/oscarehr/ws/WebServiceSessionInvalidatingFilter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+
+package org.oscarehr.ws;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Our web services are inherently stateless so we want to prevent excessive session object build up. This is caused because
+ * the oscar permissions system sets credentials into the session space upon authentication. 
+ */
+// @WebFilter(urlPatterns={"/ws/*"})
+public class WebServiceSessionInvalidatingFilter implements javax.servlet.Filter
+{
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException
+	{
+		// nothing
+	}
+
+	@Override
+	public void doFilter(ServletRequest tmpRequest, ServletResponse tmpResponse, FilterChain chain) throws IOException, ServletException
+	{
+		HttpServletRequest request = (HttpServletRequest) tmpRequest;
+
+		try
+		{
+			chain.doFilter(tmpRequest, tmpResponse);
+		}
+		finally
+		{
+			HttpSession session = request.getSession(false);
+			if (session != null) session.invalidate();
+		}
+	}
+
+	@Override
+	public void destroy()
+	{
+		// can't think of anything to do right now.
+	}
+}
