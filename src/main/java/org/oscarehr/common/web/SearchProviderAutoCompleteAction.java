01babd8e0741942e25ba6ab91b4c2ae70b688190
Randy Jonasz <rjonasz@gmail.com>
2013-07-04 16:30:22 -0400

    ID: 3614634 Implementation of auto-completion list for searching provider labs
    Make wkHTMLToPdf more informative about errors
    
    Change-Id: I97f0a5a382ffeccf8dd519a2fc76aa87bb1cbd1b

--- src/main/java/org/oscarehr/common/web/SearchProviderAutoCompleteAction.java
+++ src/main/java/org/oscarehr/common/web/SearchProviderAutoCompleteAction.java
@@ -24,6 +24,7 @@
 
 
 package org.oscarehr.common.web;
+
 import java.util.Hashtable;
 import java.util.List;
 
@@ -33,26 +34,29 @@ import javax.servlet.http.HttpServletResponse;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
 import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
 
 import oscar.oscarProvider.data.ProviderData;
+import org.oscarehr.common.dao.ProviderDataDao;
 /**
  *
  * @author jackson
  */
-public class SearchProviderAutoCompleteAction extends Action{
-    public ActionForward execute(ActionMapping mapping, ActionForm form,HttpServletRequest request,HttpServletResponse response) throws Exception{
+public class SearchProviderAutoCompleteAction extends DispatchAction{
+    public ActionForward unspecified(ActionMapping mapping, ActionForm form,HttpServletRequest request,HttpServletResponse response) throws Exception{
         String searchStr=request.getParameter("providerKeyword");
         if(searchStr==null){
             searchStr=request.getParameter("query");
         }
         if(searchStr==null){
             searchStr=request.getParameter("name");
-        }
+        }        
+       
         
         MiscUtils.getLogger().info("Search Provider " + searchStr);
         List provList=ProviderData.searchProvider(searchStr,true);
@@ -64,5 +68,54 @@ public class SearchProviderAutoCompleteAction extends Action{
         jsonArray.write(response.getWriter());
         return null;
 
+    }
+    
+    public ActionForward labSearch(ActionMapping mapping, ActionForm form,HttpServletRequest request,HttpServletResponse response) throws Exception{
+    	
+    	String searchStr = request.getParameter("term");
+    	String firstName, lastName;
+    	
+    	if( searchStr.indexOf(",") != -1 ) {
+    		String[] searchParams = searchStr.split(",");
+    		lastName = searchParams[0].trim();
+    		firstName = searchParams[1].trim();
+    	}
+    	else {
+    		lastName = searchStr;
+    		firstName = null;
+    	}
+    	
+    	ProviderDataDao providerDataDao = SpringUtils.getBean(ProviderDataDao.class);
+    	List<org.oscarehr.common.model.ProviderData> provList = providerDataDao.findByName(firstName, lastName, true);
+    	StringBuilder searchResults = new StringBuilder("[");
+    	int idx = 0;
+    	
+    	for( org.oscarehr.common.model.ProviderData provData : provList ) {
+    		searchResults.append("{\"label\":\"" + provData.getLastName() + ", " + provData.getFirstName() + "\",\"value\":\"" + provData.getId() + "\"}");
+    		if( idx < provList.size() - 1 ) {
+    			searchResults.append(",");
+    		}
+    		++idx;
+    	}
+    	
+    	searchResults.append("]");
+    	
+    	response.setContentType("text/x-json");
+    	MiscUtils.getLogger().info(searchResults.toString());
+    	response.getWriter().write(searchResults.toString());
+    	/*HashMap<String,String>searchResults = new HashMap<String,String>();
+    	
+    	for( org.oscarehr.common.model.ProviderData provData : provList ) {
+    		searchResults.put(provData.getId(), provData.getLastName() + ", " + provData.getFirstName());
+    	}
+    	
+    	JSONObject json = (JSONObject) JSONSerializer.toJSON(searchResults);
+    	
+    	response.setContentType("text/x-json");
+        json.write(response.getWriter());
+        
+    	MiscUtils.getLogger().info(json);
+    	*/
+    	return null;
     }
 }
