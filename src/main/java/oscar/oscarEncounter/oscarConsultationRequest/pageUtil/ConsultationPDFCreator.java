d947cf2f0cc61a8126976b04cb1b91e698102f35
vvanherk <vanherk@mac.com>
2013-07-15 12:52:12 -0400

    ID: 3614035 - Cannot print consults with fax enabled

--- src/main/java/oscar/oscarEncounter/oscarConsultationRequest/pageUtil/ConsultationPDFCreator.java
+++ src/main/java/oscar/oscarEncounter/oscarConsultationRequest/pageUtil/ConsultationPDFCreator.java
@@ -409,10 +409,15 @@ public class ConsultationPDFCreator extends PdfPageEventHelper {
 		ProviderDao proDAO = (ProviderDao) SpringUtils.getBean("providerDao");
 		org.oscarehr.common.model.Provider pro = proDAO.getProvider(reqFrm.providerNo);
 		String billingNo = pro.getBillingNo();
+		
 		DemographicDao demoDAO = (DemographicDao) SpringUtils.getBean("demographicDao");
 		Demographic demo = demoDAO.getDemographic(reqFrm.demoNo);
-		pro = proDAO.getProvider(demo.getProviderNo());
-		String famDocBillingNo = pro.getBillingNo();
+
+		String famDocBillingNo = "";
+		if(demo.getProviderNo()!=null && !demo.getProviderNo().equals("")) {
+			pro = proDAO.getProvider(demo.getProviderNo());
+			famDocBillingNo = pro.getBillingNo();
+		}
 		infoTable.addCell(setFooterCell(cell, getResource("msgAssociated2"), reqFrm.getProviderName(reqFrm.providerNo) + ((getlen(billingNo) > 0) ? " (" + billingNo + ")" : "")));
 		infoTable.addCell(setFooterCell(cell, getResource("msgFamilyDoc2"), reqFrm.getFamilyDoctor() + ((getlen(famDocBillingNo) > 0) ? " (" + famDocBillingNo + ")" : "")));
 		if (getlen(reqFrm.signatureImg) > 0) {
