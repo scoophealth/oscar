39976be12c88f34a839aca9ab4f7f3c7232d8289
oscar <oscar@oscar-VB.(none)>
2013-07-25 15:55:52 -0400

    Created Dao tests for the following daos:
       SecUserRoleDao
       AppointmentStatusDao
       BillingONItemDao
       CaseManagementTmpSaveDao
       CtlBillingServiceAgeRulesDao
       CtlBillingServiceSexRulesDao
       CtlBillingTypeDao

--- src/test/java/org/oscarehr/common/dao/BillingONItemDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/BillingONItemDaoTest.java
@@ -26,24 +26,324 @@
  */
 package org.oscarehr.common.dao;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
+import org.oscarehr.common.dao.utils.EntityDataGenerator;
 import org.oscarehr.common.dao.utils.SchemaUtils;
+import org.oscarehr.common.model.BillingONCHeader1;
+import org.oscarehr.common.model.BillingONItem;
+import org.oscarehr.util.MiscUtils;
 import org.oscarehr.util.SpringUtils;
 
 public class BillingONItemDaoTest extends DaoTestFixtures {
 
 	protected BillingONItemDao dao = SpringUtils.getBean(BillingONItemDao.class);
-
+	protected BillingONCHeader1Dao bONCH1Dao = SpringUtils.getBean(BillingONCHeader1Dao.class);
+	
 	@Before
 	public void before() throws Exception {
 		SchemaUtils.restoreTable("billing_on_item","billing_on_cheader1");
 	}
+	
+	@Test
+	public void testGetBillingItemByCh1Id() throws Exception {
+		
+		int ch1Id1 = 101, ch1Id2 = 202;
+		
+		BillingONItem bONI1 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI1);
+		bONI1.setCh1Id(ch1Id2);
+		dao.persist(bONI1);
+		
+		BillingONItem bONI2 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI2);
+		bONI2.setCh1Id(ch1Id1);
+		dao.persist(bONI2);
+		
+		BillingONItem bONI3 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI3);
+		bONI3.setCh1Id(ch1Id2);
+		dao.persist(bONI3);
+		
+		BillingONItem bONI4 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI4);
+		bONI4.setCh1Id(ch1Id1);
+		dao.persist(bONI4);
+		
+		BillingONItem bONI5 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI5);
+		bONI5.setCh1Id(ch1Id1);
+		dao.persist(bONI5);
+		
+		List<BillingONItem> expectedResult = new ArrayList<BillingONItem>(Arrays.asList(bONI2, bONI4, bONI5));
+		List<BillingONItem> result = dao.getBillingItemByCh1Id(ch1Id1);
+
+		Logger logger = MiscUtils.getLogger();
+		
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match.");
+			fail("Array sizes do not match.");
+		}
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items  do not match.");
+				fail("Items  do not match.");
+			}
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetActiveBillingItemByCh1Id() throws Exception {
+		
+		int ch1Id1 = 101, ch1Id2 = 202;
+		String status1 = "D", status2 = "Active";
+		
+		BillingONItem bONI1 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI1);
+		bONI1.setCh1Id(ch1Id2);
+		bONI1.setStatus(status1);
+		dao.persist(bONI1);
+		
+		BillingONItem bONI2 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI2);
+		bONI2.setCh1Id(ch1Id1);
+		bONI2.setStatus(status2);
+		dao.persist(bONI2);
+		
+		BillingONItem bONI3 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI3);
+		bONI3.setCh1Id(ch1Id2);
+		bONI3.setStatus(status2);
+		dao.persist(bONI3);
+		
+		BillingONItem bONI4 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI4);
+		bONI4.setCh1Id(ch1Id1);
+		bONI4.setStatus(status1);
+		dao.persist(bONI4);
+		
+		BillingONItem bONI5 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI5);
+		bONI5.setCh1Id(ch1Id1);
+		bONI5.setStatus(status2);
+		dao.persist(bONI5);
+		
+		BillingONItem bONI6 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI6);
+		bONI6.setCh1Id(ch1Id1);
+		bONI6.setStatus(status2);
+		dao.persist(bONI6);
+	
+		List<BillingONItem> expectedResult = new ArrayList<BillingONItem>(Arrays.asList(bONI2, bONI5, bONI6));
+		List<BillingONItem> result = dao.getActiveBillingItemByCh1Id(ch1Id1);
+
+		Logger logger = MiscUtils.getLogger();
+		
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match.");
+			fail("Array sizes do not match.");
+		}
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items  do not match.");
+				fail("Items  do not match.");
+			}
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testGetCh1ByDemographicNo() throws Exception {
+		
+		int demographicNo1 = 101, demographicNo2 = 202;
+		
+		BillingONCHeader1 bONCH11 = new BillingONCHeader1();
+		EntityDataGenerator.generateTestDataForModelClass(bONCH11);
+		bONCH11.setDemographicNo(demographicNo1);
+		bONCH1Dao.persist(bONCH11);
+		
+		BillingONCHeader1 bONCH12 = new BillingONCHeader1();
+		EntityDataGenerator.generateTestDataForModelClass(bONCH12);
+		bONCH12.setDemographicNo(demographicNo2);
+		bONCH1Dao.persist(bONCH12);
+		
+		BillingONCHeader1 bONCH13 = new BillingONCHeader1();
+		EntityDataGenerator.generateTestDataForModelClass(bONCH13);
+		bONCH13.setDemographicNo(demographicNo1);
+		bONCH1Dao.persist(bONCH13);
+		
+		BillingONCHeader1 bONCH14 = new BillingONCHeader1();
+		EntityDataGenerator.generateTestDataForModelClass(bONCH14);
+		bONCH14.setDemographicNo(demographicNo1);
+		bONCH1Dao.persist(bONCH14);
+		
+		BillingONCHeader1 bONCH15 = new BillingONCHeader1();
+		EntityDataGenerator.generateTestDataForModelClass(bONCH15);
+		bONCH15.setDemographicNo(demographicNo2);
+		bONCH1Dao.persist(bONCH15);
+		
+		List<BillingONCHeader1> expectedResult = new ArrayList<BillingONCHeader1>(Arrays.asList( bONCH11,  bONCH13,  bONCH14));
+		List<BillingONCHeader1> result = dao.getCh1ByDemographicNo(demographicNo1);
+
+		Logger logger = MiscUtils.getLogger();
+		
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match.");
+			fail("Array sizes do not match.");
+		}
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items  do not match.");
+				fail("Items  do not match.");
+			}
+		}
+		assertTrue(true);
+	}
 
 	@Test
-	public void testFindByCh1Id() {
-		assertNotNull(dao.findByCh1Id(100));
+	public void testFindByCh1Id() throws Exception {
+		
+		int ch1Id1 = 101, ch1Id2 = 202;
+		String status1 = "D", status2 = "N", status3 = "S";
+		
+		BillingONItem bONI1 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI1);
+		bONI1.setCh1Id(ch1Id2);
+		bONI1.setStatus(status1);
+		dao.persist(bONI1);
+		
+		BillingONItem bONI2 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI2);
+		bONI2.setCh1Id(ch1Id1);
+		bONI2.setStatus(status2);
+		dao.persist(bONI2);
+		
+		BillingONItem bONI3 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI3);
+		bONI3.setCh1Id(ch1Id2);
+		bONI3.setStatus(status2);
+		dao.persist(bONI3);
+		
+		BillingONItem bONI4 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI4);
+		bONI4.setCh1Id(ch1Id1);
+		bONI4.setStatus(status1);
+		dao.persist(bONI4);
+		
+		BillingONItem bONI5 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI5);
+		bONI5.setCh1Id(ch1Id1);
+		bONI5.setStatus(status2);
+		dao.persist(bONI5);
+		
+		BillingONItem bONI6 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI6);
+		bONI6.setCh1Id(ch1Id1);
+		bONI6.setStatus(status2);
+		dao.persist(bONI6);
+		
+		BillingONItem bONI7 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI7);
+		bONI7.setCh1Id(ch1Id1);
+		bONI7.setStatus(status3);
+		dao.persist(bONI7);
+		
+		BillingONItem bONI8 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI8);
+		bONI8.setCh1Id(ch1Id1);
+		bONI8.setStatus(status2);
+		dao.persist(bONI8);
+		
+		BillingONItem bONI9 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI9);
+		bONI9.setCh1Id(ch1Id2);
+		bONI9.setStatus(status2);
+		dao.persist(bONI9);
+	
+		List<BillingONItem> expectedResult = new ArrayList<BillingONItem>(Arrays.asList(bONI2, bONI5, bONI6, bONI8));
+		List<BillingONItem> result = dao.findByCh1Id(ch1Id1);
+
+		Logger logger = MiscUtils.getLogger();
+		
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match.");
+			fail("Array sizes do not match.");
+		}
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items  do not match.");
+				fail("Items  do not match.");
+			}
+		}
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testFindByCh1IdAndStatusNotEqual() throws Exception {
+		
+		int ch1Id1 = 101, ch1Id2 = 202;
+		String status1 = "D", status2 = "Active";
+		
+		BillingONItem bONI1 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI1);
+		bONI1.setCh1Id(ch1Id2);
+		bONI1.setStatus(status1);
+		dao.persist(bONI1);
+		
+		BillingONItem bONI2 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI2);
+		bONI2.setCh1Id(ch1Id1);
+		bONI2.setStatus(status2);
+		dao.persist(bONI2);
+		
+		BillingONItem bONI3 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI3);
+		bONI3.setCh1Id(ch1Id2);
+		bONI3.setStatus(status2);
+		dao.persist(bONI3);
+		
+		BillingONItem bONI4 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI4);
+		bONI4.setCh1Id(ch1Id1);
+		bONI4.setStatus(status1);
+		dao.persist(bONI4);
+		
+		BillingONItem bONI5 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI5);
+		bONI5.setCh1Id(ch1Id1);
+		bONI5.setStatus(status2);
+		dao.persist(bONI5);
+		
+		BillingONItem bONI6 = new BillingONItem();
+		EntityDataGenerator.generateTestDataForModelClass(bONI6);
+		bONI6.setCh1Id(ch1Id1);
+		bONI6.setStatus(status2);
+		dao.persist(bONI6);
+	
+		List<BillingONItem> expectedResult = new ArrayList<BillingONItem>(Arrays.asList(bONI2, bONI5, bONI6));
+		List<BillingONItem> result = dao.findByCh1IdAndStatusNotEqual(ch1Id1, status1);
+
+		Logger logger = MiscUtils.getLogger();
+		
+		if (result.size() != expectedResult.size()) {
+			logger.warn("Array sizes do not match.");
+			fail("Array sizes do not match.");
+		}
+		for (int i = 0; i < expectedResult.size(); i++) {
+			if (!expectedResult.get(i).equals(result.get(i))){
+				logger.warn("Items  do not match.");
+				fail("Items  do not match.");
+			}
+		}
+		assertTrue(true);
 	}
 }
--- src/test/java/org/oscarehr/common/dao/CaseManagementTmpSaveDaoTest.java
+++ src/test/java/org/oscarehr/common/dao/CaseManagementTmpSaveDaoTest.java
@@ -23,7 +23,12 @@
  */
 package org.oscarehr.common.dao;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -49,4 +54,81 @@ public class CaseManagementTmpSaveDaoTest extends DaoTestFixtures {
 		dao.persist(entity);
 		assertNotNull(entity.getId());
 	}
+	
+	@Test
+	public void testFindNoDate() throws Exception {
+		
+		String providerNo1 = "alpha";
+		int demographicNo1 = 101, demographicNo2 = 202;
+		int programId1 = 111, programId2 = 222;
+		
+		CaseManagementTmpSave cMTS1 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS1);
+		cMTS1.setProviderNo(providerNo1);
+		cMTS1.setDemographicNo(demographicNo1);
+		cMTS1.setProgramId(programId2);
+		dao.persist(cMTS1);
+		
+		CaseManagementTmpSave cMTS2 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS2);
+		cMTS2.setProviderNo(providerNo1);
+		cMTS2.setDemographicNo(demographicNo2);
+		cMTS2.setProgramId(programId1);
+		dao.persist(cMTS2);
+		
+		CaseManagementTmpSave cMTS3 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS3);
+		cMTS3.setProviderNo(providerNo1);
+		cMTS3.setDemographicNo(demographicNo1);
+		cMTS3.setProgramId(programId1);
+		dao.persist(cMTS3);
+		
+		CaseManagementTmpSave expectedResult = cMTS3;
+		CaseManagementTmpSave result = dao.find(providerNo1, demographicNo1, programId1);
+		
+		assertEquals(expectedResult, result);
+	}
+	
+	@Test
+	public void testFindDate() throws Exception {
+		
+		DateFormat dfm = new SimpleDateFormat("yyyyMMdd");
+		
+		Date date1 = new Date(dfm.parse("20110101").getTime());
+		Date date2 = new Date(dfm.parse("20010101").getTime());
+		Date date3 = new Date(dfm.parse("20100101").getTime());
+		
+		String providerNo1 = "alpha";
+		int demographicNo1 = 101, demographicNo2 = 202;
+		int programId1 = 111, programId2 = 222;
+		
+		CaseManagementTmpSave cMTS1 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS1);
+		cMTS1.setProviderNo(providerNo1);
+		cMTS1.setDemographicNo(demographicNo1);
+		cMTS1.setProgramId(programId2);
+		cMTS1.setUpdateDate(date1);
+		dao.persist(cMTS1);
+		
+		CaseManagementTmpSave cMTS2 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS2);
+		cMTS2.setProviderNo(providerNo1);
+		cMTS2.setDemographicNo(demographicNo2);
+		cMTS2.setProgramId(programId1);
+		cMTS2.setUpdateDate(date2);
+		dao.persist(cMTS2);
+		
+		CaseManagementTmpSave cMTS3 = new CaseManagementTmpSave();
+		EntityDataGenerator.generateTestDataForModelClass(cMTS3);
+		cMTS3.setProviderNo(providerNo1);
+		cMTS3.setDemographicNo(demographicNo1);
+		cMTS3.setProgramId(programId1);
+		cMTS3.setUpdateDate(date3);
+		dao.persist(cMTS3);
+		
+		CaseManagementTmpSave expectedResult = cMTS3;
+		CaseManagementTmpSave result = dao.find(providerNo1, demographicNo1, programId1, date3);
+				
+		assertEquals(expectedResult, result);
+	}	
 }
