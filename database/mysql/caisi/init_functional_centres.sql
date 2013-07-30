3ee4d1725de3b5c023786f2ebc3dbe5c9ad612f8
Ted Leung <r6squeegee@gmail.com>
2013-07-30 22:44:03 +1000

    ID: 3614838 add peer help functional centre
    Change-Id: I9206d11a3d9e7515f6caa17d701ec81c6cd160da

--- database/mysql/caisi/init_functional_centres.sql
+++ database/mysql/caisi/init_functional_centres.sql
@@ -8,3 +8,5 @@ insert into FunctionalCentre values ('725 40 76 30','COM Res. Mental Health Supp
 
 insert into FunctionalCentre values ('725 10 76 81','COM MH Home Care Psychiatric Rehab');
 insert into FunctionalCentre values ('725 10 78 30','COM Clinic/Program - Initial Assessment and Treatment Planning');
+
+insert into FunctionalCentre values ('725 51 76 11','Peer/Self Help Initiatives');
