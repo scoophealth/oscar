100a6ebae61f8367149a256e9751fd6b4a477c2c
Randy Jonasz <rjonasz@gmail.com>
2013-08-30 14:15:09 -0400

    ID:#2780 Fix integrated appointments crowding out local appts in appt history
    Change-Id: Ic4f0c9cd266f9206d3269f678c94c5a7a13dc030

---
+++ src/main/java/org/oscarehr/common/model/CachedAppointmentComparator.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.common.model;
+
+import java.util.Comparator;
+import org.oscarehr.caisi_integrator.ws.CachedAppointment;
+
+public class CachedAppointmentComparator implements Comparator<CachedAppointment> {
+
+	@Override
+    public int compare(CachedAppointment o1, CachedAppointment o2) {
+	    if( o1 == null && o2 != null ) return 1;
+	    if( o2 == null && o1 != null ) return  -1;
+	    if( o1 == null && o2 == null ) return 0;	    	   
+	    
+	    return o2.getAppointmentDate().compareTo(o1.getAppointmentDate());
+    }
+
+}
