54c96b05861125d01048846dfb22a53e24f2a944
Peter Hutten-Czapski <phuttenczapski@gmail.com>
2013-08-31 21:19:20 -0400

    ID #2760 PDF option disables browser print
    https://sourceforge.net/p/oscarmcmaster/bugs/2760/

--- src/main/webapp/share/javascript/eforms/printControl.js
+++ src/main/webapp/share/javascript/eforms/printControl.js
@@ -1,28 +1,37 @@
-/* printControl - Changes print behaviour to receive a server side generated PDF 
- *                instead of using the browsers print functionality.
+/* printControl - Changes eform to add a server side generated PDF 
+ *                with print functionality intact (if print button on the form).
  */
 
 if (typeof jQuery == "undefined") { alert("The printControl library requires jQuery. Please ensure that it is loaded first"); }
 var printControl = {
 	initialize: function () {
-		var print = jQuery("input[name='PrintButton']");
+
+		var submit = jQuery("input[name='SubmitButton']");
 		var printSave = jQuery("input[name='PrintSaveButton']");
-		if (printSave.size() == 0) { printSave = jQuery("input[name='PrintSubmitButton']"); }
-		
-		if (print.size() != 0) {
-			print.attr("onclick", "").unbind("click");
-			print.click(function(){submitPrintButton(false);})
+		submit.append("<input name='pdfSaveButton' type='button'>");
+		submit.append("<input name='pdfButton' type='button'>");
+		var pdf = jQuery("input[name='pdfButton']");
+		var pdfSave = jQuery("input[name='pdfSaveButton']");
+		if (pdf.size() == 0) { pdf = jQuery("input[name='pdfButton']"); }
+		if (pdfSave.size() == 0) { pdfSave = jQuery("input[name='pdfSaveButton']"); }
+	
+		pdf.insertAfter(submit);	
+		pdfSave.insertAfter(submit);
+
+		if (pdf.size() != 0) {
+			pdf.attr("onclick", "").unbind("click");
+			pdf.attr("value", "PDF");
+			pdf.click(function(){submitPrintButton(false);});
 		}
-		
+		if (pdfSave.size() != 0) {
+			pdfSave.attr("onclick", "").unbind("click");
+			pdfSave.attr("value", "Submit & PDF");
+			pdfSave.click(function(){submitPrintButton(true);});
+		}
 		if (printSave.size() != 0) {
-			//if (print) { print.remove(); }
-			printSave.attr("onclick", "").unbind("click");
 			printSave.attr("value", "Submit & Print");
-			printSave.click(function(){submitPrintButton(true);});
 		}
-		if (print.size() != 0 && printSave.size() != 0) {
-			print.insertAfter(printSave);
-		}
+
 	}
 };
 
@@ -48,14 +57,12 @@ function submitPrintButton(save) {
 		document.getElementById('Letter').value=editControlContents('edit');		
 	}	
 	
-	if (!save) { jQuery("form").attr("target", "_blank"); }
 	jQuery("form").submit();	
 	if (save) { setTimeout("window.close()", 3000); }
 	printHolder.val("false");
 	saveHolder.val("false");
 	
 }
-
 
 
 jQuery(document).ready(function(){printControl.initialize();});
