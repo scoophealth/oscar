72f118c102651fc26443ff8cc52ab208797538bd
jdwilson <jumptiger13@gmail.com>
2013-10-25 14:54:50 -0400

    [for review] ID#955	Show/Hide items in Health Tracker according to patient demographics
    -update phv flowsheet and created new measurements - there is still more
    to add but just sending this up so people can review whats
    done so far considering the timeline
    
    Change-Id: I266373f96c592434d3917f5f201f95497ac40ff4

---
+++ database/mysql/updates/update-2013-10-25.sql
@@ -0,0 +1,16 @@
+INSERT INTO `measurementType` (`type`, `typeDisplayName`, `typeDescription`, `measuringInstruction`, `validation`, `createDate`) VALUES
+( 'IBPL', 'Income below poverty line', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'FAS', 'Folic Acid supplementation', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'COGA', 'Cognitive Assessment', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'HPNP', 'Hearing protection/Noise control programs', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'SBLT', 'Seat belts', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'SDET', 'Smoke detector that works', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'PWC', 'Parents with children', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'SUNP', 'Sun protection', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'BTFP', 'Brush teeth with fluoride toothpaste', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'FLOS', 'Floss', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00'),
+( 'DILY', 'Dentist in the last year', 'NULL', 'Yes/No', '7', '2013-10-25 13:00:00');
+
+
+
+
