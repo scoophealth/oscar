405566d32fa6601a1767accb666d57347392fda5
Marc Dumontier <marc@mdumontier.com>
2013-11-22 09:15:45 -0500

    BPII and BPIS need to have the descriptions and display names swapped.
    Change-Id: Ic1e8c5b045f00259da041d2166a6a418d174c86c
    
    Conflicts:
    
    	database/mysql/updates/update-2013-11-05.sql

--- database/mysql/updates/update-2013-07-25.sql
+++ database/mysql/updates/update-2013-07-25.sql
@@ -1,6 +1,6 @@
 INSERT INTO `measurementType` (`type`, `typeDisplayName`, `typeDescription`, `measuringInstruction`, `validation`, `createDate`) VALUES
-( 'BPII', 'BPI Pain Severity', 'BPI Pain Severity', 'null', '2', '2013-07-25 13:00:00'),
-( 'BPIS', 'BPI Pain Interference', 'BPI Pain Interference', 'null', '2', '2013-07-25 00:00:00');
+( 'BPII', 'BPI Pain Interference', 'BPI Pain Interference', 'null', '2', '2013-07-25 13:00:00'),
+( 'BPIS', 'BPI Pain Severity', 'BPI Pain Severity', 'null', '2', '2013-07-25 00:00:00');
 
 
 
