26fb582ef915208e90f5ea4edc7943b237854923
ning.yin <frankyin720@gmail.com>
2013-11-22 15:15:01 -0500

    976 - Create New Label for Sexual Health Clinic Client Lab Specimens and Requisitions
    Add print client label button to the master record page and print it as
    pdf label

---
+++ src/main/java/oscar/oscarDemographic/PrintClientLabLabelAction.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+
+package oscar.oscarDemographic;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.oscarehr.util.DbConnectionFilter;
+import org.oscarehr.util.MiscUtils;
+
+import oscar.OscarAction;
+import oscar.OscarDocumentCreator;
+
+
+public class PrintClientLabLabelAction extends OscarAction {
+
+    private static Logger logger = MiscUtils.getLogger();
+
+    public PrintClientLabLabelAction() {
+    }
+
+    public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest request, HttpServletResponse response) {
+        //patient
+        String classpath = (String)request.getSession().getServletContext().getAttribute("org.apache.catalina.jsp_classpath");
+        if (classpath==null) classpath = (String)request.getSession().getServletContext().getAttribute("com.ibm.websphere.servlet.application.classpath");
+        System.setProperty("jasper.reports.compile.class.path", classpath);
+
+        HashMap<String, String> parameters = new HashMap<String, String>();
+        parameters.put("demo", request.getParameter("demographic_no"));
+
+        InputStream ins = null;
+        try {
+        	logger.debug("user home: " + System.getProperty("user.home"));
+        	File file = new File(System.getProperty("user.home") + "/ClientLabLabel.xml");
+        	if (file.exists()) {
+        		ins = new FileInputStream(file);
+        	} else {
+        		ins = getClass().getResourceAsStream("/oscar/oscarDemographic/ClientLabLabel.xml");
+                logger.debug("loading from : /oscar/oscarDemographic/ClientLabLabel.xml " + ins);
+        	}
+        	ServletOutputStream sos = response.getOutputStream();
+        	response.setHeader("Content-disposition", getHeader(response).toString());
+            OscarDocumentCreator osc = new OscarDocumentCreator();
+            osc.fillDocumentStream(parameters, sos, "pdf", ins, DbConnectionFilter.getThreadLocalDbConnection());
+        } catch (FileNotFoundException ex1) {
+        	logger.debug("Addresslabel.xml not found in user's home directory. Using default instead");
+        } catch (IOException ex) {
+        	MiscUtils.getLogger().error("Error", ex);
+        } catch (SQLException e) {
+        	MiscUtils.getLogger().error("Error", e);
+        } catch (Exception ex1) {
+        	MiscUtils.getLogger().error("Error", ex1);
+        } finally {
+        	IOUtils.closeQuietly(ins);
+        }
+        return actionMapping.findForward(this.target);
+    }
+
+    private StringBuilder getHeader(HttpServletResponse response) {
+        StringBuilder strHeader = new StringBuilder();
+        strHeader.append("label_");
+        strHeader.append(".pdf");
+        response.setHeader("Cache-Control", "max-age=0");
+        response.setDateHeader("Expires", 0);
+        response.setContentType("application/pdf");
+        StringBuilder sbContentDispValue = new StringBuilder();
+        sbContentDispValue.append("inline; filename="); //inline - display
+        sbContentDispValue.append(strHeader);
+        return sbContentDispValue;
+    }
+}
