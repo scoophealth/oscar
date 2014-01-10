09fcddfdf81d1fcaec0089285d37b5f8a28c0216
jdwilson <jumptiger13@gmail.com>
2014-01-10 14:01:44 -0500

    #981 Periodic Health Visit - Update PHV Flowsheet
    -allow measurement data, comment(text) flow to the progess note. This
    change removes the paste to note feature from Health Tracker and allows
    the user to select which measurements become a note. Then the note is
    compiled and saved. This feature will also stop the measurements being
    pasted into the incorrect chart (for health tracker but plans to update
    CDM Indicators with a similar update)

--- src/main/java/oscar/oscarEncounter/oscarMeasurements/pageUtil/FormUpdateAction.java
+++ src/main/java/oscar/oscarEncounter/oscarMeasurements/pageUtil/FormUpdateAction.java
@@ -13,6 +13,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -43,6 +45,12 @@ import oscar.oscarEncounter.oscarMeasurements.bean.EctMeasurementTypeBeanHandler
 import oscar.oscarEncounter.oscarMeasurements.bean.EctMeasurementTypesBean;
 import oscar.util.ConversionUtils;
 
+import org.oscarehr.common.dao.SecRoleDao;
+import org.oscarehr.common.model.SecRole;
+import oscar.oscarEncounter.data.EctProgram;
+import org.oscarehr.casemgmt.model.CaseManagementNote;
+import org.oscarehr.casemgmt.service.CaseManagementManager;
+
 import org.apache.log4j.Logger;
 
 public class FormUpdateAction extends Action {
@@ -57,15 +65,20 @@ public class FormUpdateAction extends Action {
 		boolean valid = true;
 		boolean errorPage = false;
 
-		
-		
 		HttpSession session = request.getSession();
 
 		String temp = request.getParameter("template");	//"diab3";
 		session.setAttribute("temp", temp);
 		String demographic_no = request.getParameter("demographic_no");
 		String providerNo = (String) session.getAttribute("user");
+		String note = "";
 		String apptNo = (String) session.getAttribute("cur_appointment_no");
+		int apptNoInt=0;
+		if(apptNo!=null){
+		apptNoInt = Integer.parseInt(apptNo);
+		}
+		String user_no = (String) session.getAttribute("user");
+		String prog_no = new EctProgram(session).getProgram(user_no);
 
 		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(session.getServletContext());
 
@@ -102,7 +115,8 @@ public class FormUpdateAction extends Action {
 					EctMeasurementTypesBean mtypeBean = mType.getMeasurementType(measure);
 
 					String name = h2.get("display_name").toString().replaceAll("\\W","");
-					
+					String displayName=h2.get("display_name").toString();
+							
 					if (request.getParameter(name) != null && !request.getParameter(name).equals("")) {
 
 						String comment = "";
@@ -113,6 +127,23 @@ public class FormUpdateAction extends Action {
 						if(request.getParameter(name + "_date") !=null && !request.getParameter(name + "_date").equals("")){
 							date=request.getParameter(name + "_date");
 						}
+						
+						
+							//create note text
+							if(request.getParameter(name + "_note") !=null && !request.getParameter(name + "_note").equals("")){
+								note = note + displayName + ": " + request.getParameter(name);
+								if(name.equals("BP")){
+									note = note + " " + mtypeBean.getMeasuringInstrc();
+								}
+										
+								note = note + "\n Date Observed: " + date;
+								if (request.getParameter(name + "_comments") != null && !request.getParameter(name + "_comments").equals("")) {
+									note = note + "\n comment: " + comment;
+								}
+								note=note +"\n\n ";
+							}
+							
+							
 						
 						valid = doInput(item, mtypeBean, mFlowsheet, mtypeBean.getType(), mtypeBean.getMeasuringInstrc(), request.getParameter(name), comment, date, apptNo, request);
 						
@@ -138,7 +169,7 @@ public class FormUpdateAction extends Action {
 
 				}
 		
-
+				
 		//if (request.getParameter("ycoord") != null) {
 		//	request.setAttribute("ycoord", request.getParameter("ycoord"));
 		//}
@@ -150,12 +181,54 @@ public class FormUpdateAction extends Action {
 		
 		session.setAttribute("textOnEncounter", textOnEncounter);
 		
-		if (request.getParameter("submit").equals("Add") || request.getParameter("submit").equals("Save")) {
+		addNote(demographic_no, providerNo, prog_no, note, apptNoInt, request);
+		
+		if (request.getParameter("submit").equals("Add") || request.getParameter("submit").equals("Save") || request.getParameter("submit").equals("Save All")) {
 			return mapping.findForward("reload");
 		} else {
 			return mapping.findForward("success");
 		}
 	}
+	
+	public void addNote(String demographic_no, String providerNo, String prog_no, String note, int apptNoInt, HttpServletRequest request){
+		HttpSession session = request.getSession();
+		WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(session.getServletContext());
+		CaseManagementManager cmm = (CaseManagementManager) ctx.getBean("caseManagementManager");
+
+		
+		SecRoleDao secRoleDao = (SecRoleDao) SpringUtils.getBean("secRoleDao");
+		SecRole doctorRole = secRoleDao.findByName("doctor");
+		String reporter_caisi_role=doctorRole.getId().toString();
+
+		SimpleDateFormat df = new SimpleDateFormat("dd-MMM-yyyy");  
+		Date date = new Date(); 
+		String formattedDate= "["+df.format(date)+" .: ]";
+		note = formattedDate+"\n"+ note;
+ 
+		    
+		        CaseManagementNote cmn = new CaseManagementNote();
+				cmn.setUpdate_date(date);
+				cmn.setObservation_date(date);
+				cmn.setDemographic_no(demographic_no);
+				cmn.setProviderNo(providerNo);
+				cmn.setNote(note);
+				cmn.setSigned(true);
+				cmn.setSigning_provider_no(providerNo);
+				cmn.setProgram_no(prog_no);
+				cmn.setReporter_caisi_role(reporter_caisi_role);
+								
+				cmn.setReporter_program_team("0");
+				cmn.setPassword("NULL");
+				cmn.setLocked(false);
+				cmn.setHistory(note+"-----hi story----");
+				cmn.setPosition(0);
+				cmn.setAppointmentNo(apptNoInt);
+				
+				
+				cmm.saveNoteSimple(cmn);
+		
+	}
+	
 
 	public void doCommentInput(FlowSheetItem item, EctMeasurementTypesBean mtypeBean, MeasurementFlowSheet mFlowsheet, String inputType, String mInstructions, String comment, String date, String apptNo, HttpServletRequest request) {
 		String demographicNo = request.getParameter("demographic_no");
