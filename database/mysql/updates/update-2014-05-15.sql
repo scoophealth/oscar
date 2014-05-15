fdd2e171acd4341081e24c0b12dfdff41691cfc7
jdwilson <jumptiger13@gmail.com>
2014-05-15 13:42:23 -0400

    #3287 New Explanatory Codes CNA, DF and AD1
    https://sourceforge.net/p/oscarmcmaster/bugs/3287/
    
    Change-Id: I032f29ea490283f2c6a06893bcd4d7c572f706d0

---
+++ database/mysql/updates/update-2014-05-15.sql
@@ -0,0 +1,4 @@
+insert into billing_on_errorCode values ('CNA', 'Counselling NOT Allowed');
+insert into billing_on_errorCode values ('DF', 'Corresponding fee code has not been claimed or was approved at zero');
+insert into billing_on_errorCode values ('AD1', 'Corresponding procedure not claimed by same or different physician');
+
