3e1523a8aef6de6a8ab4477cc88013432f39d644
Jason Gallagher <jaygallagher@gmail.com>
2014-09-10 04:03:03 -0400

    #1044 Initial Commit for summary lists of the summary page.
    Change-Id: Ic42b3d728c32ac3db777927cd8cb2bb720f78992

---
+++ src/main/java/org/oscarehr/ws/rest/to/model/SummaryItemTo1.java
@@ -0,0 +1,133 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.ws.rest.to.model;
+
+import java.util.Date;
+
+//import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+/**
+ title: "Ongoing Concerns",displaySize: 5, 
+ 			itemlist: [
+	              			{
+	              				id : 0,
+	              				desc : 'INR CLINIC, A FIB/AV (bovine) replacement, TARGET 2.5 Osteopenia'
+	              			},
+	              			{
+	              				id : 1,
+	              				desc : 'Rt Knee - Dr. Who ?OA; Lt Shoulder pain incisional hernia since laparoscopy April 03 Spinal stenosis 1999 - no gurantee for surgery; sciatic pain both legs esp since fall in Feb 06'
+	              			},
+	              			{
+	              				id : 2,
+	              				desc : 'Hearing loss R worse than L - tried hearing aid'
+	              			},
+	              			{
+	              				id : 3,
+	              				desc : 'Aortic stenosis 2nd to rheumatic fever; AV replaced Jan 08 Large irreducible hiatus Hernia 11/06 HTN on Accupril & HCTZ AF; Recurrent thromboembolism of lower legs with filter insertion - on coumadin'
+	              			} ]
+						};
+ */
+@XmlRootElement(name = "summaryItem")
+public class SummaryItemTo1 {
+	Integer id;
+	String type;
+	String displayName;
+	String action;
+	Date date;
+	String classification;
+	boolean abnormalFlag;
+
+	
+	public SummaryItemTo1(){}
+	
+	public SummaryItemTo1(int id, String displayName,String action,String type){
+		this.id = id;
+		this.displayName = displayName;
+		this.action = action;
+		this.type = type;
+	}
+	
+	
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer i){
+		id = i;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getClassification() {
+		return classification;
+	}
+
+	public void setClassification(String classification) {
+		this.classification = classification;
+	}
+
+	public boolean isAbnormalFlag() {
+		return abnormalFlag;
+	}
+
+	public void setAbnormalFlag(boolean abnormalFlag) {
+		this.abnormalFlag = abnormalFlag;
+	}
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
+
+
---
+++ src/main/webapp/web/common/uxServices.js
@@ -0,0 +1,58 @@
+/*
+
+    Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+    This software is published under the GPL GNU General Public License.
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+    This software was written for the
+    Department of Family Medicine
+    McMaster University
+    Hamilton
+    Ontario, Canada
+
+*/
+angular.module("uxServices", [])
+	.service("summaryService", function ($http,$q,$log) {
+		return {
+		apiPath:'../ws/rs/recordUX',
+        left: function (demographicNo) {
+        	//
+            console.log("Debug: calling left");
+            var deferred = $q.defer();
+            $http.get(this.apiPath+'/'+demographicNo+'/summary/left').success(function(data){
+              console.log(data);
+              deferred.resolve(data);
+          }).error(function(){
+        	  console.log("error fetching items")
+            deferred.reject("An error occured while fetching items");
+          });
+     
+          return deferred.promise;
+            
+        },
+        getFullSummary: function(demographicNo,summaryCode){
+        	var deferred = $q.defer();
+        	$http.get(this.apiPath+'/'+demographicNo+'/fullSummary/'+summaryCode).success(function(data){
+                deferred.resolve(data);
+            }).error(function(){
+          	  console.log("error fetching items")
+              deferred.reject("An error occured while fetching items");
+            });
+       
+            return deferred.promise;
+        	
+        }
+    };
+});
