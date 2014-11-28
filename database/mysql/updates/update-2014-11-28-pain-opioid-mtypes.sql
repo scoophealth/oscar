c7bf155f03e7e7390aab1e2de532238aa79228d2
jdwilson <jumptiger13@gmail.com>
2014-11-28 11:44:51 -0500

    #928 New Flowsheet for Pain
    I would rather not keep adding new measurements to the list here but
    since the pain flowsheet is part of the system flowsheets they are
    needed. 
    
    These measurements will finalize the set to close out this flowsheet.
    
    Change-Id: Ia69d5354bcede1abd09b642fd10c9311b92913a9

---
+++ database/mysql/updates/update-2014-11-28-pain-opioid-mtypes.sql
@@ -0,0 +1,6 @@
+INSERT INTO `measurementType` (`type`, `typeDisplayName`, `typeDescription`, `measuringInstruction`, `validation`, `createDate`) VALUES
+( 'OPAE', 'Opioid Adverse Effects', 'Opioid Adverse Effects', 'null', '17', '2014-11-27 13:00:00'),
+( 'OPAB', 'Opioid Aberrant Behaviour', 'Opioid Aberrant Behaviour', 'null', '17', '2014-11-27 13:00:00'),
+( 'OPUS', 'Opioid Urine Drug Screen', 'Opioid Urine Drug Screen', 'null', '17', '2014-11-27 13:00:00'),
+( 'DMOE', 'Daily Morphine Equivalent', 'Daily Morphine Equivalent', 'null', '11', '2014-11-27 13:00:00');
+
