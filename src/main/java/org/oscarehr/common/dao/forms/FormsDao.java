92303c346cf2fa9cddf37e2253ba57204df08025
Marc Dumontier <marc@mdumontier.com>
2014-11-28 11:57:14 -0500

    #1118 split ONAREnhanced table
    rebased

--- src/main/java/org/oscarehr/common/dao/forms/FormsDao.java
+++ src/main/java/org/oscarehr/common/dao/forms/FormsDao.java
@@ -114,7 +114,7 @@ public class FormsDao {
 	
 	@NativeSql("formONAREnhanced")
 	public List<Integer> select_maxformar_id2(String dateStart, String dateEnd) {
-		String sql = "select max(ID) from formONAREnhanced where c_finalEDB >= ? and c_finalEDB <= ? group by demographic_no";
+		String sql = "select max(ID) from formONAREnhancedRecord where c_finalEDB >= ? and c_finalEDB <= ? group by demographic_no";
 		Query query = entityManager.createNativeQuery(sql);
 		query.setParameter(1, dateStart);
 		query.setParameter(2, dateEnd);
--- src/main/java/oscar/form/FrmONAREnhancedRecord.java
+++ src/main/java/oscar/form/FrmONAREnhancedRecord.java
@@ -25,16 +25,30 @@
 
 package oscar.form;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.lang.StringUtils;
+import org.oscarehr.util.DbConnectionFilter;
+import org.oscarehr.util.MiscUtils;
 
 import oscar.OscarProperties;
+import oscar.oscarDB.DBHandler;
 import oscar.util.UtilDateUtilities;
 
 public class FrmONAREnhancedRecord extends FrmRecord {
+	
+	  
+	 protected String _newDateFormat = "yyyy-MM-dd"; //handles both date formats, but yyyy/MM/dd is displayed to avoid deprecation
+
 	
 	public FrmONAREnhancedRecord() {
 		this.dateFormat = "yyyy/MM/dd";
@@ -66,7 +80,8 @@ public class FrmONAREnhancedRecord extends FrmRecord {
 			props.setProperty("pg1_formDate", UtilDateUtilities.DateToString(new Date(), dateFormat));
 		} 
 		else {
-			String sql = "SELECT * FROM formONAREnhanced WHERE demographic_no = " + demographicNo + " AND ID = " + existingID;
+			//join it up so the resulting props have values from all 3 tables
+			String sql = "SELECT * FROM formONAREnhancedRecord rec, formONAREnhancedRecordExt1 ext1, formONAREnhancedRecordExt2 ext2 WHERE rec.ID = ext1.ID and rec.ID = ext2.ID and rec.demographic_no = " + demographicNo + " AND rec.ID = " + existingID;
 			props = (new FrmRecordHelp()).getFormRecord(sql);
 		}
 
@@ -74,15 +89,23 @@ public class FrmONAREnhancedRecord extends FrmRecord {
 	}
 
     public int saveFormRecord(Properties props) throws SQLException {
-        String demographic_no = props.getProperty("demographic_no");
-        String sql = "SELECT * FROM formONAREnhanced WHERE demographic_no=" + demographic_no + " AND ID=0";
-
-        return ((new FrmRecordHelp()).saveFormRecord(props, sql));
+    	//get the names/types of each column in the 3 tables
+        List<String> namesA = getColumnNames("formONAREnhancedRecord");
+        List<String> namesB = getColumnNames("formONAREnhancedRecordExt1");
+        List<String> namesC = getColumnNames("formONAREnhancedRecordExt2");
+        
+        //insert the initial record, and grab the ID to do the inserts on the other 2 tables
+        int id = addRecord(props, "formONAREnhancedRecord", namesA,null);
+        addRecord(props, "formONAREnhancedRecordExt1", namesB,id);
+        addRecord(props, "formONAREnhancedRecordExt2", namesC,id);
+         
+        return id;
     }
 
     public Properties getPrintRecord(int demographicNo, int existingID) throws SQLException {
-        String sql = "SELECT * FROM formONAREnhanced WHERE demographic_no = " + demographicNo + " AND ID = " + existingID;
-        return ((new FrmRecordHelp()).getPrintRecord(sql));
+    	//join the 3 tables
+    	String sql = "SELECT * FROM formONAREnhancedRecord rec, formONAREnhancedRecordExt1 ext1, formONAREnhancedRecordExt2 ext2 WHERE rec.ID = ext1.ID and rec.ID = ext2.ID and rec.demographic_no = " + demographicNo + " AND rec.ID = " + existingID;
+		return ((new FrmRecordHelp()).getPrintRecord(sql));
     }
 
     public String findActionValue(String submit) throws SQLException {
@@ -108,4 +131,157 @@ public class FrmONAREnhancedRecord extends FrmRecord {
 		return ret;
 	}
 
+	
+	private List<String> getColumnNames(String table) throws SQLException  {
+		List<String> result = new ArrayList<String>();
+		ResultSet rs2 = null; 
+
+		try {
+			rs2 = DBHandler.GetSQL("select * from " + table + " limit 1");
+			
+			ResultSetMetaData md = rs2.getMetaData();
+			
+			for (int i = 1; i <= md.getColumnCount(); i++) {
+		    	String name = md.getColumnName(i);
+		    	String type = md.getColumnTypeName(i);
+		    	result.add(name + "|" + type);
+			}
+		}finally {
+			if(rs2 != null)
+				rs2.close();
+		}
+		
+		return result;
+	}
+	
+	int addRecord(Properties props,String table, List<String> namesA, Integer id) throws SQLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO "+ table+ " (");
+		for(String name:namesA) {
+			sb.append(name.split("\\|")[0] + ",");
+		}
+		sb.deleteCharAt(sb.length()-1);
+		
+		sb.append(") VALUES (");
+		
+		for(String name:namesA) {
+			sb.append("?,");
+		}
+		sb.deleteCharAt(sb.length()-1);
+		sb.append(");");
+		
+		PreparedStatement preparedStmt = null;
+		try {
+			preparedStmt = DbConnectionFilter.getThreadLocalDbConnection().prepareStatement(sb.toString(), Statement.RETURN_GENERATED_KEYS);	
+			
+			for(int x=0;x<namesA.size();x++) {
+				String t = namesA.get(x);
+				String theName = t.split("\\|")[0];
+				String type = t.split("\\|")[1];
+				
+				if(theName.equals("ID")) {
+					if(id == null) {
+						preparedStmt.setNull(x+1, Types.INTEGER);
+					} else {
+						preparedStmt.setInt(x+1, id.intValue());
+					}
+					continue;
+				}
+				
+				
+				if(type.equals("VARCHAR") || type.equals("CHAR")) {
+					String value = props.getProperty(theName);
+					if(value == null) {
+						preparedStmt.setNull(x+1, getType(type));
+					} else {
+						preparedStmt.setString(x+1,value);
+					}
+				}
+				else if(type.equals("INT") || type.equals("TINYINT")) {
+					String value = props.getProperty(theName);
+					if(value != null && value.isEmpty()) {
+						MiscUtils.getLogger().info("empty value for "+ theName);
+					}
+					if(value == null || value.isEmpty()) {
+						value = "0";
+					}
+					else if (value.equalsIgnoreCase("on") || value.equalsIgnoreCase("checked='checked'")) {
+	                    value = "1";
+					}
+					preparedStmt.setInt(x+1, Integer.parseInt(value));
+				} else if(type.equals("DATE")) {
+					String  value = props.getProperty(theName);
+					Date d= null;
+					
+					if (theName.equalsIgnoreCase("formEdited")) {
+	                    d = new Date();
+	                } else {
+	                    if ((value == null) || (value.indexOf('/') != -1))
+	                        d = UtilDateUtilities.StringToDate(value, dateFormat);
+	                    else
+	                        d = UtilDateUtilities.StringToDate(value, _newDateFormat);
+	                }
+	                if (d == null)
+	                    preparedStmt.setNull(x+1, Types.DATE);
+	                else
+	                	preparedStmt.setDate(x+1,  new java.sql.Date(d.getTime()));
+	                				
+				} else if(type.equals("TIMESTAMP")) {
+					Date d;
+	                if (theName.equalsIgnoreCase("formEdited")) {
+	                    d = new Date();
+	                } else {
+	                    d = UtilDateUtilities.StringToDate(props.getProperty(theName), "yyyyMMddHHmmss");
+	                }
+	                if (d == null)
+	                	 preparedStmt.setNull(x+1, Types.TIMESTAMP);
+	                else
+	                	preparedStmt.setTimestamp(x+1, new java.sql.Timestamp(d.getTime()));
+	               
+					
+				} else {
+					MiscUtils.getLogger().error("missing type handler for this column " + theName, new Exception());
+				}
+				
+			}
+			
+			preparedStmt.executeUpdate();
+			
+			if(id == null) {
+				ResultSet rs = null;
+				try {
+					 rs = preparedStmt.getGeneratedKeys();
+					 
+			         if(rs.next()) {
+			             id = rs.getInt(1);
+			         }
+			     } finally {
+			    	 if(rs != null)
+			    		 rs.close();
+			     }
+		         
+			}
+		} finally {
+			if(preparedStmt != null) {
+				preparedStmt.close();
+			}
+		}
+         
+         return id;
+	}
+
+
+
+
+	private int getType(String type) {
+		if(type.equals("VARCHAR")) {
+			return Types.VARCHAR;
+		}
+		if(type.equals("CHAR")) {
+			return Types.CHAR;
+		}
+		return -1;
+	}
+	
 }
+
