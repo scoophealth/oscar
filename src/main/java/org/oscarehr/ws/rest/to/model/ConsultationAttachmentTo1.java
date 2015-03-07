0a5dff762aa0c59e5c8680f61fcc3ac2bc1da92a
Ronnie Cheng <cmc.ronnie.cheng@gmail.com>
2015-03-06 23:47:13 -0500

    Feature#1150 Consultation Response
    added to database with related model class and dao
    - consultationResponse table
    - consultResponseDoc table (for attachments to consult responses)
    - "Referring Doctor" to consultationServices table (for use with consult responses)
    
    Change-Id: I1dbe8d57026578e9e1d39100da9f2b07813f28e2

---
+++ src/main/java/org/oscarehr/ws/rest/to/model/ConsultationAttachmentTo1.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.ws.rest.to.model;
+
+import java.io.Serializable;
+
+public class ConsultationAttachmentTo1 implements Serializable {
+	public static final String TYPE_DOC = "D";
+	public static final String TYPE_LAB = "L";
+	public static final boolean ATTACHED = true;
+	public static final boolean DETACHED = false;
+
+	private static final long serialVersionUID = 1L;
+	
+	private int documentNo;
+	private String documentType;
+	private boolean attached;
+	private String displayName;
+	private String url;
+
+	public ConsultationAttachmentTo1() {}
+	
+	public ConsultationAttachmentTo1(int documentNo, String documentType, boolean attached, String displayName, String url) {
+		setDocumentNo(documentNo);
+		setDocumentType(documentType);
+		setAttached(attached);
+		setDisplayName(displayName);
+		setUrl(url);
+	}
+
+	public int getDocumentNo() {
+		return documentNo;
+	}
+
+	public void setDocumentNo(int documentNo) {
+		this.documentNo = documentNo;
+	}
+
+	public String getDocumentType() {
+		return documentType;
+	}
+
+	public void setDocumentType(String documentType) {
+		this.documentType = documentType;
+	}
+
+	public boolean isAttached() {
+		return attached;
+	}
+
+	public void setAttached(boolean attached) {
+		this.attached = attached;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
---
+++ src/main/java/org/oscarehr/ws/rest/to/model/ConsultationResponseSearchResult.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2001-2002. Department of Family Medicine, McMaster University. All Rights Reserved.
+ * This software is published under the GPL GNU General Public License.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This software was written for the
+ * Department of Family Medicine
+ * McMaster University
+ * Hamilton
+ * Ontario, Canada
+ */
+package org.oscarehr.ws.rest.to.model;
+
+import java.util.Date;
+
+import org.oscarehr.common.model.Demographic;
+import org.oscarehr.common.model.ProfessionalSpecialist;
+import org.oscarehr.common.model.Provider;
+
+public class ConsultationResponseSearchResult {
+
+	private Integer id;
+	private Demographic demographic;
+	private ProfessionalSpecialist referringDoctor;
+	private String teamName;
+	private String status;
+	private String statusDescription;
+	private Provider provider;
+	private Date appointmentDate;
+	private Date lastFollowUp;
+	private Date referralDate;
+	private Date responseDate;
+	private String urgency;
+	private String urgencyDescription;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Demographic getDemographic() {
+		return demographic;
+	}
+
+	public void setDemographic(Demographic demographic) {
+		this.demographic = demographic;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getAppointmentDate() {
+		return appointmentDate;
+	}
+
+	public void setAppointmentDate(Date appointmentDate) {
+		this.appointmentDate = appointmentDate;
+	}
+
+	public Date getLastFollowUp() {
+		return lastFollowUp;
+	}
+
+	public void setLastFollowUp(Date lastFollowUp) {
+		this.lastFollowUp = lastFollowUp;
+	}
+
+	public Date getReferralDate() {
+		return referralDate;
+	}
+
+	public void setReferralDate(Date referralDate) {
+		this.referralDate = referralDate;
+	}
+
+	public Date getResponseDate() {
+		return responseDate;
+	}
+
+	public void setResponseDate(Date responseDate) {
+		this.responseDate = responseDate;
+	}
+
+	public String getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(String urgency) {
+		this.urgency = urgency;
+	}
+
+	public ProfessionalSpecialist getReferringDoctor() {
+		return referringDoctor;
+	}
+
+	public void setReferringDoctor(ProfessionalSpecialist referringDoctor) {
+		this.referringDoctor = referringDoctor;
+	}
+
+	public Provider getProvider() {
+		return provider;
+	}
+
+	public void setProvider(Provider provider) {
+		this.provider = provider;
+	}
+
+	public String getStatusDescription() {
+		return statusDescription;
+	}
+
+	public void setStatusDescription(String statusDescription) {
+		this.statusDescription = statusDescription;
+	}
+
+	public String getUrgencyDescription() {
+		return urgencyDescription;
+	}
+
+	public void setUrgencyDescription(String urgencyDescription) {
+		this.urgencyDescription = urgencyDescription;
+	}
+}
