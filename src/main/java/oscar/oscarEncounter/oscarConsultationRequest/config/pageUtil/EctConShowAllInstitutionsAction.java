036e49e4f0ee17616ce41cb49ecbc9f539bc4b94
Ronnie Cheng <cmc.ronnie.cheng@gmail.com>
2015-05-01 22:54:48 -0400

    Feature#1102 Generic infrastructure change (security)
    Change-Id: I0e0e59e687ef4cc84d36615772103fef439a9096

--- src/main/java/oscar/oscarEncounter/oscarConsultationRequest/config/pageUtil/EctConShowAllInstitutionsAction.java
+++ src/main/java/oscar/oscarEncounter/oscarConsultationRequest/config/pageUtil/EctConShowAllInstitutionsAction.java
@@ -33,11 +33,20 @@ import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.oscarehr.managers.SecurityInfoManager;
+import org.oscarehr.util.LoggedInInfo;
+import org.oscarehr.util.SpringUtils;
 
 public class EctConShowAllInstitutionsAction extends Action {
+	private static SecurityInfoManager securityInfoManager = SpringUtils.getBean(SecurityInfoManager.class);
 
 	   public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request, HttpServletResponse response)
 			   throws ServletException, IOException {
+		   
+			if(!securityInfoManager.hasPrivilege(LoggedInInfo.getLoggedInInfoFromSession(request), "_con", "r", null)) {
+				throw new SecurityException("missing required security object (_con)");
+			}
+			
 			      EctConShowAllInstitutionsForm showAllInstitutionsForm = (EctConShowAllInstitutionsForm)form;
 			      String id = showAllInstitutionsForm.getId();
 			      String name = showAllInstitutionsForm.getName();
