979e5beb4c26eaea6b473978245779d074f5b574
Marc Dumontier <marc@mdumontier.com>
2015-05-21 16:00:33 -0400

    #1203 Add batch physician label printing
    made checked items persist across searches (session variable)
    
    Conflicts:
    	src/main/java/org/oscarehr/common/model/ProfessionalSpecialist.java

--- src/main/java/org/oscarehr/common/model/ProfessionalSpecialist.java
+++ src/main/java/org/oscarehr/common/model/ProfessionalSpecialist.java
@@ -326,5 +326,26 @@ public class ProfessionalSpecialist extends AbstractModel<Integer> implements Se
 		this.salutation = salutation;
 	}
 
+	@Override
+    public int hashCode() {
+	    final int prime = 31;
+	    int result = super.hashCode();
+	    result = prime * result + ((id == null) ? 0 : id.hashCode());
+	    return result;
+    }
+
+	@Override
+    public boolean equals(Object obj) {
+	    if (this == obj) return true;
+	    if (!super.equals(obj)) return false;
+	    if (getClass() != obj.getClass()) return false;
+	    ProfessionalSpecialist other = (ProfessionalSpecialist) obj;
+	    if (id == null) {
+		    if (other.id != null) return false;
+	    } else if (!id.equals(other.id)) return false;
+	    return true;
+    }
+
+	
 
 }
