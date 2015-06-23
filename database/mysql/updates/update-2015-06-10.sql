c97b963f8bf50f186809e90748baded33c835d00
Marc Dumontier <marc@mdumontier.com>
2015-06-23 10:51:50 -0400

    #3429 generic minor bug fix ticket

--- database/mysql/updates/update-2015-06-10.sql
+++ database/mysql/updates/update-2015-06-10.sql
@@ -1,2 +1,2 @@
-INSERT INTO secObjPrivilege ('doctor','_phr','x','0','999998');
-INSERT INTO secObjPrivilege ('admin','_phr','x','0','999998');
+INSERT INTO secObjPrivilege values ('doctor','_phr','x','0','999998');
+INSERT INTO secObjPrivilege values ('admin','_phr','x','0','999998');
