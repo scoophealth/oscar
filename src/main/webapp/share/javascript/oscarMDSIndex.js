9700166185000f0bcfc30911d8ea264fcbc58981
smerchantkia <sm@kaiinnovations.com>
2015-06-30 09:51:02 -0400

    #3500 Message Button not working when assigning the document in Inbox

--- src/main/webapp/share/javascript/oscarMDSIndex.js
+++ src/main/webapp/share/javascript/oscarMDSIndex.js
@@ -1552,7 +1552,7 @@ function updateDocumentAndNext(eleId){//save doc info
 						num=num.replace(/\s/g,'');
 						$("saveSucessMsg_"+num).show();
 						$('saved'+num).value='true';
-						$("msgBtn_"+num).onclick = function() { popup(700,960,'/oscar/oscarMessenger/SendDemoMessage.do?demographic_no='+patientId,'msg'); };
+						$("msgBtn_"+num).onclick = function() { popup(700,960, contextpath +'/oscarMessenger/SendDemoMessage.do?demographic_no='+patientId,'msg'); };
 						//Hide document						
 						Effect.BlindUp('labdoc_'+num);
 
@@ -1589,7 +1589,8 @@ function updateDocument(eleId){//save doc info
 			num=num.replace(/\s/g,'');
 			$("saveSucessMsg_"+num).show();
 			$('saved'+num).value='true';
-			$("msgBtn_"+num).onclick = function() { popup(700,960,'/oscar/oscarMessenger/SendDemoMessage.do?demographic_no='+patientId,'msg'); };
+			$("msgBtn_"+num).onclick = function() { popup(700,960,contextpath +'/oscarMessenger/SendDemoMessage.do?demographic_no='+patientId,'msg'); };
+			
 			var success= updateGlobalDataAndSideNav(num,patientId);
 			if(success){
 				success=updatePatientDocLabNav(num,patientId);
