cf15de72548d7a9457492fb12050ceca8b7e318e
Ronnie Cheng <cmc.ronnie.cheng@gmail.com>
2015-07-28 11:56:15 -0400

    Feature#1193 Add CAISI filtering to eforms
    - updated eform restrictToProgram field not null
    
    Change-Id: If14a6bb8a39f8ba223dc6fc67d8d85790d6ef395

--- database/mysql/updates/update-2015-04-27.sql
+++ database/mysql/updates/update-2015-04-27.sql
@@ -1,4 +1,3 @@
 alter table eform add programNo int(10);
-alter table eform add restrictToProgram tinyint(1);
-update eform set restrictToProgram = 0;
+alter table eform add restrictToProgram tinyint(1) not null;
 
