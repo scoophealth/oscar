08fbe929cb706bd85e50eadef3e4cd3a3c38a287
smerchantkia <sm@kaiinnovations.com>
2015-08-10 14:28:53 -0400

    #3932 Abnormal and Refused Preventions appear as different colour on Echart

--- src/main/java/oscar/oscarEncounter/pageUtil/EctDisplayPreventionAction.java
+++ src/main/java/oscar/oscarEncounter/pageUtil/EctDisplayPreventionAction.java
@@ -89,7 +89,7 @@ public class EctDisplayPreventionAction extends EctDisplayAction {
 
         String highliteColour 	= "#FF0000";
         //String inelligibleColour = "#FF6600";
-        String refusedColour 	= "#FFDDDD";	//light pink 
+        String refusedColour 	= "#EAACAC";	//light pink 
         String ineligibleColour	= "#FFCC24";	//Lightning Yellow
         String pendingColour 	= "#FF00FF";	//dark pink
         String abnormalColor 	= "#FF4D4D";	//dark salmon
