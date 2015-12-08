b84f10d3db0a912b9411ba44fa711c1d59bb8784
John Wilson <jumptiger13@gmail.com>
2015-12-08 09:50:35 -0500

    #4178 some content types are too large when uploading document - adding semicolons to sql

--- database/mysql/updates/update-2015-11-30.1.sql
+++ database/mysql/updates/update-2015-11-30.1.sql
@@ -1,3 +1,3 @@
 alter table document modify contenttype varchar(255);
-update document set contenttype='application/vnd.openxmlformats-officedocument.wordprocessingml.document' where contenttype='application/vnd.openxmlformats-officedocument.wordprocessing'
-update document set contenttype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' where contenttype='application/vnd.openxmlformats-officedocument.spreadsheetml.'
+update document set contenttype='application/vnd.openxmlformats-officedocument.wordprocessingml.document' where contenttype='application/vnd.openxmlformats-officedocument.wordprocessing';
+update document set contenttype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' where contenttype='application/vnd.openxmlformats-officedocument.spreadsheetml.';
