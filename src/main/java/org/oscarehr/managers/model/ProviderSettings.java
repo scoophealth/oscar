affae381d8b4174438add1303b79517d308a2780
John Wilson <jumptiger13@gmail.com>
2016-01-21 10:38:50 -0500

    #1103 Consultation Form - Letterhead Name drop down change - Part 1 of form changes
    This change allows user to set a preference that would determine the
    default option in the letterheadname field when creating a new consult.
    
    https://sourceforge.net/p/oscarmcmaster/feature-requests/1103/
    
    https://youtu.be/9wauhRcwt6I
    
    Change-Id: Ic79e7f376d44b1ea33feb1832d91a2dded00e9a5

--- src/main/java/org/oscarehr/managers/model/ProviderSettings.java
+++ src/main/java/org/oscarehr/managers/model/ProviderSettings.java
@@ -80,6 +80,7 @@ public class ProviderSettings {
 	private String consultationTimePeriodWarning;
 	private String consultationTeamWarning = "";
 	private String consultationPasteFormat;
+	private String consultationLetterHeadNameDefault;
 	
 	private boolean documentBrowserInDocumentReport;
 	private boolean documentBrowserInMasterFile;
@@ -383,6 +384,12 @@ public class ProviderSettings {
 	public void setConsultationPasteFormat(String consultationPasteFormat) {
 		this.consultationPasteFormat = consultationPasteFormat;
 	}
+	public String getConsultationLetterHeadNameDefault() {
+		return consultationLetterHeadNameDefault;
+	}
+	public void setConsultationLetterHeadNameDefault(String consultationLetterHeadNameDefault) {
+		this.consultationLetterHeadNameDefault = consultationLetterHeadNameDefault;
+	}	
 	public boolean isDocumentBrowserInDocumentReport() {
 		return documentBrowserInDocumentReport;
 	}
