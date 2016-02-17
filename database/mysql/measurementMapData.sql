6c8f733ba78f6725c2680bea505a228b95baa1ef
Peter Hutten-Czapski <phuttenczapski@gmail.com>
2016-02-17 13:33:14 -0500

    4228 dated defaults specified in measurementMapData.sql removed table defaults for schema (MyISAM) and encoding (latin1) to allow for global control of these

--- database/mysql/measurementMapData.sql
+++ database/mysql/measurementMapData.sql
@@ -1,27 +1,9 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: oscar_mcmaster
--- ------------------------------------------------------
--- Server version	5.0.51a-3ubuntu5.1-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `measurementMap`
 --
 
 DROP TABLE IF EXISTS `measurementMap`;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
 CREATE TABLE `measurementMap` (
   `id` int(10) NOT NULL auto_increment,
   `loinc_code` varchar(20) default NULL,
@@ -30,8 +12,7 @@ CREATE TABLE `measurementMap` (
   `lab_type` varchar(10) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `ident_code` (`ident_code`)
-) ENGINE=MyISAM AUTO_INCREMENT=1076 DEFAULT CHARSET=latin1;
-SET character_set_client = @saved_cs_client;
+) ;
 
 --
 -- Dumping data for table `measurementMap`
@@ -65,14 +46,4 @@ insert into measurementMap (loinc_code, ident_code, name, lab_type) VALUES ('931
 
 /*!40000 ALTER TABLE `measurementMap` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2009-04-28 19:05:56
