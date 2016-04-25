a7508aefc75325ecb0679bbfdbc5db0140c6649a
Marc Dumontier <marc@mdumontier.com>
2016-04-25 16:41:43 -0400

    #4312 BC billing: missing required table in init script - billingperclimit

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -12039,3 +12039,12 @@ CREATE TABLE `BORNPathwayMapping` (
 );
 
 
+CREATE TABLE billingperclimit (
+  service_code varchar(10) NOT NULL ,
+  min varchar(8),
+  max varchar(8),
+  effective_date date,
+  id int auto_increment,
+  PRIMARY KEY  (id)
+) ;
+
