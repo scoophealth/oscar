fc78b2d1998091fbee3bcd96bd78e93e9d786ecb
John Wilson <jumptiger13@gmail.com>
2016-06-08 11:13:19 -0400

    #4347 OSCAR 15 - Minimized Note editor issue on consults
    -added in ability to move the note editor left or right as well this way
    for smaller displays the editor could be moved out of the way of text or
    buttons behind it.
    
    Change-Id: Id6314cbf2a1400c5731061a73232ad99f269c06b

--- src/main/webapp/web/record/recordController.js
+++ src/main/webapp/web/record/recordController.js
@@ -229,6 +229,26 @@ $scope.$on('$destroy', function () { clearInterval(myVar); });
 		}
 	};
 
+	$scope.moveNote = function(p) {
+		noteEditor = $("[id^=noteInput]");
+		
+		if (p == "l") {
+			$(noteEditor).removeClass('col-md-offset-3');
+			$(noteEditor).removeClass('absolute-right');
+			
+			$(noteEditor).addClass('absolute-left');
+		} else if(p == "r") {
+			$(noteEditor).removeClass('col-md-offset-3');
+			$(noteEditor).removeClass('absolute-left');
+			
+			$(noteEditor).addClass('absolute-right');
+		}else{
+			$(noteEditor).removeClass('absolute-left');
+			$(noteEditor).removeClass('absolute-right');
+			
+			$(noteEditor).addClass('col-md-offset-3');
+		}
+	};
 		
 	$scope.saveNote = function(){
 		console.log("This is the note"+$scope.page.encounterNote);
