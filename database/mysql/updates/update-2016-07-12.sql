a34fbe5ac4591b070bd3f834b126e71fd6d03481
Marc Dumontier <marc@mdumontier.com>
2016-09-08 09:51:49 -0400

    #1399 Ontario MD Dashboard Proof of Concept

--- database/mysql/updates/update-2016-07-12.sql
+++ database/mysql/updates/update-2016-07-12.sql
@@ -1,8 +1,8 @@
 insert into `secObjectName` (`objectName`) values ('_dashboardManager');
-insert into `secObjPrivilege` values('admin', '_dashboardManager', 'x', 0, '999998');
+insert into `secObjPrivilege` values('admin', '_dashboardManager', 'o', 0, '999998');
 
 insert into `secObjectName` (`objectName`) values ('_dashboardDisplay');
-insert into `secObjPrivilege` values('admin', '_dashboardDisplay', 'x', 0, '999998');
+insert into `secObjPrivilege` values('admin', '_dashboardDisplay', 'o', 0, '999998');
 
 insert into `secObjectName` (`objectName`) values ('_dashboardDrilldown');
-insert into `secObjPrivilege` values('admin', '_dashboardDrilldown', 'x', 0, '999998');
+insert into `secObjPrivilege` values('admin', '_dashboardDrilldown', 'o', 0, '999998');
