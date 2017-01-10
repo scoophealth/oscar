cf491b0ab4b4572d8997027b0be6bf882c54a041
Jordan Nicholas <jordan.nicholas@oscarhost.ca>
2017-01-10 15:10:10 -0800

    OSCAREMR-6042: Add a property to set fax file location
    Change-Id: I4a589f19d37b1ac825650ad973f9d533c5329e1f

--- src/main/java/oscar/form/pdfservlet/FrmCustomedPDFServlet.java
+++ src/main/java/oscar/form/pdfservlet/FrmCustomedPDFServlet.java
@@ -114,13 +114,16 @@ public class FrmCustomedPDFServlet extends HttpServlet {
 		                	FileOutputStream fos = new FileOutputStream(path+pdfFile);
 		                	baosPDF.writeTo(fos);
 		                	fos.close();
+
+							String tempPath = OscarProperties.getInstance().getProperty(
+								"fax_file_location", System.getProperty("java.io.tmpdir"));
 		                	
 		                	// write to file
-		                	String tempPdf = System.getProperty("java.io.tmpdir")+"/prescription_"+req.getParameter("pdfId")+".pdf";
+		                	String tempPdf = tempPath + "/prescription_" + req.getParameter("pdfId") + ".pdf";
 		                	// Copying the fax pdf.
 							FileUtils.copyFile(new File(path+pdfFile), new File(tempPdf));
 
-			                String txtFile = System.getProperty("java.io.tmpdir")+"/prescription_"+req.getParameter("pdfId")+".txt";
+			                String txtFile = tempPath + "/prescription_" + req.getParameter("pdfId") + ".txt";
 		                	FileWriter fstream = new FileWriter(txtFile);
 		                	BufferedWriter out = new BufferedWriter(fstream);
 			                try {
