a3650875330eaff8a800861db500ed68f8670100
Ronnie Cheng <cmc.ronnie.cheng@gmail.com>
2017-01-17 14:12:16 -0500

    OSCAREMR-6028 Modify Dx Quick Pick Add list in billing confirmation page
    - added list of dxcode to be added and dxcode/icd9 matching table
    
    Conflicts:
    	src/main/java/org/oscarehr/common/model/UserProperty.java
    
    Change-Id: Iaad6328f68be4c17cdcbdf69613ea5467924f39e

--- src/main/java/org/oscarehr/common/model/UserProperty.java
+++ src/main/java/org/oscarehr/common/model/UserProperty.java
@@ -161,10 +161,11 @@ public class UserProperty extends AbstractModel<Integer> implements Serializable
 	public static final String CLINICALCONNECT_SERVICE_USERNAME = "clinicalconnect_service_username";
 	public static final String CLINICALCONNECT_SERVICE_PASSWORD = "clinicalconnect_service_password";
 	public static final String CLINICALCONNECT_SERVICE_LOCATION = "clinicalconnect_service_location";
-
 	public static final String DASHBOARD_SHARE = "dashboard_share";
-
-
+	
+	public static final String CODE_TO_ADD_PATIENTDX = "code_to_add_patientDx";
+	public static final String CODE_TO_MATCH_PATIENTDX = "code_to_match_patientDx";
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
