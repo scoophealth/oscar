1e019fbcae294868b8dde2c834ac9ca6c9050253
John Wilson <jumptiger13@gmail.com>
2017-09-21 13:44:36 -0400

    OSCAREMR-6254 Add Enhanced First Trimester Screen form option on Antenatal Record -final
    https://oscaremr.atlassian.net/browse/OSCAREMR-6254

--- database/mysql/oscardata_on.sql
+++ database/mysql/oscardata_on.sql
@@ -15025,6 +15025,9 @@ insert into frm_labreq_preset (lab_type,prop_name,prop_value,status) values ("Ed
 insert into frm_labreq_preset (lab_type,prop_name,prop_value,status) values ("Ed","o_otherTests4","testosterone",'1');
 insert into frm_labreq_preset (lab_type,prop_name,prop_value,status) values ("Ed","o_otherTests5","testosterone,free",'1');
 
+insert into encounterForm (form_name,form_value,form_table,hidden) values("LabReq07 eFTS","../form/formlabreq07.jsp?labType=eFTS&demographic_no=","",'0');
+insert into encounterForm (form_name,form_value,form_table,hidden) values("LabReq10 eFTS","../form/formlabreq10.jsp?labType=eFTS&demographic_no=","",'0');
+insert into frm_labreq_preset (lab_type, prop_name, prop_value, status) values("eFTS", "o_otherTests1", "Enhanced First Trimester Screen", "1");
 
 insert into encounterForm (form_name,form_value,form_table,hidden) values("LabReq07 Fatigue","../form/formlabreq07.jsp?labType=Fatigue&demographic_no=","",'0');
 insert into encounterForm (form_name,form_value,form_table,hidden) values("LabReq10 Fatigue","../form/formlabreq10.jsp?labType=Fatigue&demographic_no=","",'0');
