a1ba1d81922b2e88686fa6d6e969d97609dd1eea
Marc Dumontier <marc@mdumontier.com>
2018-01-22 10:27:37 -0500

    fixed DB name

--- database/mysql/createdatabase_generic.sh
+++ database/mysql/createdatabase_generic.sh
@@ -17,7 +17,7 @@ mysqladmin -u${USER} -p$PASSWORD create $DATABASE_NAME
 echo "grant all on ${DATABASE_NAME}.* to ${USER}@localhost identified by \"$PASSWORD\"" |  mysql -u${USER} -p$PASSWORD  $DATABASE_NAME
 
 echo updating character set to utf8
-echo "alter database testx DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci" | mysql -u${USER} -p$PASSWORD  $DATABASE_NAME
+echo "alter database $DATABASE_NAME DEFAULT CHARACTER SET utf8  DEFAULT COLLATE utf8_general_ci" | mysql -u${USER} -p$PASSWORD  $DATABASE_NAME
 
 echo loading oscarinit.sql...
 mysql -u${USER} -p$PASSWORD  $DATABASE_NAME  < oscarinit.sql
