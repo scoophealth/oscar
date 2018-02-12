480f0539e1d931ba3243396465c1539b4db8765e
Marc Dumontier <marc@mdumontier.com>
2018-02-12 13:50:05 -0500

    db file fixed

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -12219,7 +12219,7 @@ CREATE TABLE `CVCMapping` (
    `id` int(10) NOT NULL auto_increment,
    `oscarName` varchar(255),
    `cvcSnomedId` varchar(255),
-   `preferCVC` tinyint(1)
+   `preferCVC` tinyint(1),
   PRIMARY KEY (`id`)
 );
 
