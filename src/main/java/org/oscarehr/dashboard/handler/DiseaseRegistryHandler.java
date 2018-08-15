b85634a0652c083e5ebeb7ed7a0a7bee4d43ca8e
David Rusk <rusk.david@gmail.com>
2018-08-14 21:57:56 -0700

    Roughed in code for adding to disease registry based on code in DiseaseRegistryService. Still needs provider no to be set and should use the proper constant for ICD9 code type.

--- src/main/java/org/oscarehr/dashboard/handler/DiseaseRegistryHandler.java
+++ src/main/java/org/oscarehr/dashboard/handler/DiseaseRegistryHandler.java
@@ -1,15 +1,44 @@
 package org.oscarehr.dashboard.handler;
 
+import java.util.Date;
+
 import org.apache.log4j.Logger;
+import org.oscarehr.common.dao.DxresearchDAO;
+import org.oscarehr.common.model.Dxresearch;
 import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
 
 public class DiseaseRegistryHandler {
 
 	private static Logger logger = MiscUtils.getLogger();
-	
+
+	private DxresearchDAO dao = (DxresearchDAO) SpringUtils.getBean("DxresearchDAO");
+
 	public void addToDiseaseRegistry(int demographicNo, String icd9code) {
+		String codeType = "ICD9"; // TODO get the actual string const
+
+		boolean activeEntryExists = dao.activeEntryExists(demographicNo, codeType, icd9code);
+		if (activeEntryExists) {
+			logger.info(
+				"Patient (" + demographicNo +
+				") already has active entry for (" +
+				icd9code + ")"
+			);
+			return;
+		}
+
+		Dxresearch dx = new Dxresearch();
+		dx.setStartDate(new Date());
+		dx.setCodingSystem(codeType);
+		dx.setDemographicNo(demographicNo);
+		dx.setDxresearchCode(icd9code);
+		dx.setStatus('A');
+		// TODO get providerNo dx.setProviderNo(providerNo);
+
+		dao.persist(dx);
+
 		logger.info(
-			"Adding code (" + icd9code +
+			"Added code (" + icd9code +
 			") to disease registry for patient (" + demographicNo + ")"
 		);
 	}
