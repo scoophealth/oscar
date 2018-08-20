029ba49fccf3fb83480a1d50ad43245a8b1ea72c
Raymond Rusk <Raymond.Rusk@shaw.ca>
2018-08-20 12:24:17 -0700

    Updated ExcludeDemographicHanderTest to avoid using E2E database initialization script

--- src/test/java/org/oscarehr/dashboard/handler/ExcludeDemographicHandlerTest.java
+++ src/test/java/org/oscarehr/dashboard/handler/ExcludeDemographicHandlerTest.java
@@ -26,39 +26,49 @@ package org.oscarehr.dashboard.handler;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.oscarehr.common.dao.DemographicDao;
+import org.oscarehr.common.dao.utils.EntityDataGenerator;
 import org.oscarehr.common.dao.utils.SchemaUtils;
+import org.oscarehr.common.model.Demographic;
 import org.oscarehr.common.model.Provider;
 import org.oscarehr.util.LoggedInInfo;
-import org.oscarehr.e2e.constant.Constants;
+//import org.oscarehr.util.MiscUtils;
+import org.oscarehr.util.SpringUtils;
 
 import static org.junit.Assert.*;
 
 import java.util.Date;
 import java.util.ArrayList;
 import java.util.List;
+//import org.apache.log4j.Logger;
 
 public class ExcludeDemographicHandlerTest {
 	
+//	private static Logger logger = MiscUtils.getLogger();
+	private static DemographicDao demographicDao = (DemographicDao)SpringUtils.getBean(DemographicDao.class);
 	private static ExcludeDemographicHandler excludeDemographicHandler;
 	private static String indicatorName;
 	Date now = new java.util.Date();
+	static Demographic demographic;
+	static String providerNo = "100";
+	static List<Integer> demoNos = new ArrayList<Integer>();
 	
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         SchemaUtils.restoreTable("demographic", "lst_gender", "admission", "demographic_merged",
         		"program", "health_safety", "provider", "providersite", "site", "program_team",
         		"log", "Facility","demographicExt");
-    	// Can't just restore tables because the data generator isn't generating
-    	// data for demographicExt after some recent code changes.
-    	// Instead restoring 2 patient records that are used for E2E testing so that
-    	// demogrpahic_no 1 and 2 are populated.  Otherwise, there will be an error
-    	// that they are missing when trying to retrieve from the demographicExt table
-        // using those demographic_no.
-    	SchemaUtils.restoreTable(Constants.Runtime.TABLES);
-        assertEquals(0, SchemaUtils.loadFileIntoMySQL(Constants.Runtime.E2E_SETUP));
     	LoggedInInfo loggedInInfo = LoggedInInfo.getLoggedInInfoAsCurrentClassAndMethod();
         Provider provider = new Provider();
-        provider.setProviderNo("100");
+        provider.setProviderNo(providerNo);
+        for (int i = 0; i < 10; i++) {
+        	demographic = new Demographic();
+        	EntityDataGenerator.generateTestDataForModelClass(demographic);
+        	demographic.setDemographicNo(null);
+        	demographic.setProvider(provider);
+        	demographicDao.save(demographic);
+        	demoNos.add(demographic.getDemographicNo());
+        }
         loggedInInfo.setLoggedInProvider( provider );
         excludeDemographicHandler = new ExcludeDemographicHandler();
         excludeDemographicHandler.setLoggedinInfo(loggedInInfo);
@@ -79,19 +89,19 @@ public class ExcludeDemographicHandlerTest {
     @Test
     public void setDemoId() {
     	indicatorName = "myIndicatorName_setDemoId";
-    	excludeDemographicHandler.excludeDemoId(2, indicatorName);
+    	assertEquals(10, demoNos.size());
+    	excludeDemographicHandler.excludeDemoId(demoNos.get(5), indicatorName);
     	assertEquals(1, excludeDemographicHandler.getDemoIds(indicatorName).size());
-    	assertTrue(excludeDemographicHandler.getDemoIds(indicatorName).get(0).equals(2));
+    	assertTrue(excludeDemographicHandler.getDemoIds(indicatorName).get(0).equals(demoNos.get(5)));
     }
     
     @Test
     public void setDemoIDList() {
     	indicatorName = "myIndicatorName_setDemoIDList";
-    	List<Integer> list = getDemoNos();
-    	excludeDemographicHandler.excludeDemoIds(list, indicatorName);
+    	excludeDemographicHandler.excludeDemoIds(demoNos, indicatorName);
     	List<Integer> demoIds = excludeDemographicHandler.getDemoIds(indicatorName);
-    	assertEquals(list.size(), demoIds.size());
-     	for (Integer el: list) {
+    	assertEquals(demoNos.size(), demoIds.size());
+     	for (Integer el: demoNos) {
     		assertTrue(demoIds.contains(el));
     	}
     	//assertTrue(demoIds.containsAll(list));
@@ -100,49 +110,49 @@ public class ExcludeDemographicHandlerTest {
     @Test
     public void unsetDemoIDList() {
     	indicatorName = "myIndicatorName_unsetDemoIDList";
-    	List<Integer> list = getDemoNos();
-    	excludeDemographicHandler.excludeDemoIds(list, indicatorName);
+    	excludeDemographicHandler.excludeDemoIds(demoNos, indicatorName);
     	List<Integer> demoIds = excludeDemographicHandler.getDemoIds(indicatorName);
-    	assertEquals(list.size(), demoIds.size());
+    	assertEquals(demoNos.size(), demoIds.size());
     	// check whether we can add the same entries yet again
-    	excludeDemographicHandler.excludeDemoIds(list, indicatorName);
+    	excludeDemographicHandler.excludeDemoIds(demoNos, indicatorName);
     	demoIds = excludeDemographicHandler.getDemoIds(indicatorName);
-    	assertEquals(2*list.size(), demoIds.size());
+    	assertEquals(2*demoNos.size(), demoIds.size());
     	// also checks whether duplicated entries are removed
-    	excludeDemographicHandler.unExcludeDemoIds(list, indicatorName);
+    	excludeDemographicHandler.unExcludeDemoIds(demoNos, indicatorName);
     	assertEquals(0, excludeDemographicHandler.getDemoIds(indicatorName).size());
     }
     
     @Test
     public void setDemoIdJson() {
     	indicatorName = "myIndicatorName_setDemoIdJson";
-    	String jsonStr = "1,2";
+    	String jsonStr = getJsonDemoNoStr(demoNos);
     	excludeDemographicHandler.excludeDemoIds(jsonStr, indicatorName);
     	List<Integer> demoIds = excludeDemographicHandler.getDemoIds(indicatorName);
-    	for (int el = 1; el < 3; ++el) {
-    		assertTrue(demoIds.contains(el));
+    	for (int demoNo: demoNos) {
+    		assertTrue(demoIds.contains(demoNo));
     	}
     }
     
     @Test
     public void unsetDemoIdJson() {
     	indicatorName = "myIndicatorName_unsetDemoIdJson";
-    	String jsonStr = "1,2";
+    	String jsonStr = getJsonDemoNoStr(demoNos);
     	excludeDemographicHandler.excludeDemoIds(jsonStr, indicatorName);
     	List<Integer> demoIds = excludeDemographicHandler.getDemoIds(indicatorName);
-    	for (int el = 1; el < 3; ++el) {
-    		assertTrue(demoIds.contains(el));
+    	for (int demoNo: demoNos) {
+    		assertTrue(demoIds.contains(demoNo));
     	}
     	excludeDemographicHandler.unExcludeDemoIds(jsonStr, indicatorName);
     	assertEquals(0, excludeDemographicHandler.getDemoIds(indicatorName).size());
     }
     
-    private static List<Integer> getDemoNos() {
-    	int[] array = {1,2};
-    	List<Integer> list = new ArrayList<Integer>();
-    	for (int el: array) {
-    		list.add(el);
-    	}
-    	return list;
+    private static String getJsonDemoNoStr(List<Integer> demoNums) {
+        StringBuilder builder = new StringBuilder();
+        for(Integer i : demoNums) {
+            builder.append(i+",");
+        }
+        builder.deleteCharAt(builder.length()-1);  //remove trailing comma
+        String str = builder.toString();
+        return str;
     }
 }
