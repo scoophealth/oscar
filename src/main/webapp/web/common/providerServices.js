43264737264c21099c48cbb3a29bd3d4be52776c
Jason Gallagher <jaygallagher@gmail.com>
2018-10-03 00:12:54 -0400

    6237  Fix issues with FTPS calls

--- src/main/webapp/web/common/providerServices.js
+++ src/main/webapp/web/common/providerServices.js
@@ -120,7 +120,7 @@ angular.module("providerServices", [])
                 url: this.apiPath+'/providers_json',
                 method: "GET"
              }).then(function (data, status, headers, config) {
-            	 	deferred.resolve(data.content);
+            	 	deferred.resolve(data.data.content);
              },function (data, status, headers, config) {
             	 	deferred.reject("An error occured while fetching provider teams");
              });
