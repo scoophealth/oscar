84012e69ebf7e8b58fb0557c4d361181e7a6aa21
Marc Dumontier <marc@mdumontier>
2019-02-22 10:43:24 -0500

    updates to DB cleaned up

--- database/mysql/updates/update-2018-08-30.sql
+++
@@ -1,2 +0,0 @@
-alter table demographic add middleNames varchar(100);
-
--- database/mysql/updates/update-2018-09-01.sql
+++
@@ -1,4 +0,0 @@
-alter table demographic add roster_enrolled_to varchar(20);
-alter table demographicArchive add middleNames varchar(100);
-alter table demographicArchive add roster_enrolled_to varchar(20);
-
