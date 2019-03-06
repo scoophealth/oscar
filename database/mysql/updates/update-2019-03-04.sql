ddabd43d3c96b39b3af4a9ac409f4c691a2543b2
Marc Dumontier <marc@mdumontier>
2019-03-06 10:22:50 -0500

    small update to force build

--- database/mysql/updates/update-2019-03-04.sql
+++ database/mysql/updates/update-2019-03-04.sql
@@ -3,7 +3,6 @@ ALTER TABLE drugs modify dispense_interval varchar(100);
 ALTER TABLE drugs MODIFY past_med boolean;
 ALTER TABLE drugs MODIFY long_term boolean;
 ALTER TABLE drugs MODIFY patient_compliance boolean;
-
 ALTER TABLE drugs add protocol varchar(255);
 ALTER TABLE drugs add priorRxProtocol varchar(255);
 ALTER TABLE drugs ADD COLUMN pharmacyId int(11);
