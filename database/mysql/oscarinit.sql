c21a881acc6f9f52d287d602f39f436f3f0fa2e2
Raymond Rusk <Raymond.Rusk@shaw.ca>
2019-03-23 07:49:53 -0700

    Fixed schema so unit tests pass

--- database/mysql/oscarinit.sql
+++ database/mysql/oscarinit.sql
@@ -266,7 +266,7 @@ CREATE TABLE clinic (
   clinic_province varchar(40) default NULL,
   clinic_delim_phone text,
   clinic_delim_fax text,
-  cdx_oid varchar(30) UNIQUE ( cdx_oid ),
+  cdx_oid varchar(30),
   PRIMARY KEY  (clinic_no)
 ) ;
 
