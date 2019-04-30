3d366e94914e5e08899717530062a2ab2252b0c4
Raymond Rusk <Raymond.Rusk@shaw.ca>
2019-04-30 14:30:29 -0700

    Updated SubmitDoc unit tests to save provenance

--- database/mysql/updates/update-2019-03-26.sql
+++ database/mysql/updates/update-2019-03-26.sql
@@ -1,30 +1,28 @@
 CREATE TABLE cdx_attachment (
-															id                   int  NOT NULL  AUTO_INCREMENT,
-															document             int ,
-															attachment_type      varchar(30)  NOT NULL  ,
-															reference 					 varchar(60) NOT NULL ,
-															content              blob  NOT NULL  ,
-															CONSTRAINT pk_cdx_attachment PRIMARY KEY ( id )
+id                   int  NOT NULL  AUTO_INCREMENT,
+document             int,
+attachment_type      varchar(30)  NOT NULL,
+reference            varchar(60) NOT NULL,
+content              blob  NOT NULL,
+CONSTRAINT pk_cdx_attachment PRIMARY KEY ( id )
 );
-
 CREATE INDEX idx_cdx_attachment ON cdx_attachment ( document );
-
 CREATE TABLE oscar.cdx_provenance (
-																		id                   int  NOT NULL  AUTO_INCREMENT,
-																		doc_id               varchar(60)  NOT NULL  ,
-																		document_no						int,
-																		version              int   ,
-																		effective_time       datetime  NOT NULL  ,
-																		parent_doc           varchar(60)    ,
-																		set_id               varchar(60)    ,
-																		in_fulfillment_of_id varchar(60)    ,
-																		kind                 varchar(30)  NOT NULL  ,
-																		action               varchar(10)  NOT NULL  ,
-																		log                  bigint    ,
-																		payload              text    ,
-																		CONSTRAINT pk_cdx_provenance_id PRIMARY KEY ( id ),
-																		CONSTRAINT cdx_prov_unique UNIQUE ( doc_id, version ),
-																		CONSTRAINT cdx_prov_docno_unique UNIQUE ( document_no )
+id                   int  NOT NULL  AUTO_INCREMENT,
+doc_id               varchar(60)  NOT NULL,
+document_no          int,
+version              int,
+effective_time       datetime  NOT NULL,
+parent_doc           varchar(60),
+set_id               varchar(60),
+in_fulfillment_of_id varchar(60),
+kind                 varchar(30)  NOT NULL,
+action               varchar(10)  NOT NULL,
+log                  bigint,
+payload              text,
+CONSTRAINT pk_cdx_provenance_id PRIMARY KEY ( id ),
+CONSTRAINT cdx_prov_unique UNIQUE ( doc_id, version ),
+CONSTRAINT cdx_prov_docno_unique UNIQUE ( document_no )
 ) ;
 ALTER TABLE cdx_attachment ADD CONSTRAINT fk_cdx_attachment_prov FOREIGN KEY ( document ) REFERENCES cdx_provenance( id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
